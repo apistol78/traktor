193f90535df728354b4ea4ff18e908903cc6082b
Anders Pistol <anders.pistol@doctorentertainment.com>
2009-06-03 12:29:30 +0100

    Traktor: "Texture batch" wizard added.
    [git-p4: depot-paths = "//depot/traktor/": change = 192]

---
+++ code/Render/Editor/TextureBatchDialog.h
@@ -0,0 +1,53 @@
+#ifndef traktor_render_TextureBatchDialog_H
+#define traktor_render_TextureBatchDialog_H
+
+#include "Ui/ConfigDialog.h"
+
+namespace traktor
+{
+	namespace ui
+	{
+
+class ListBox;
+
+		namespace custom
+		{
+
+class AutoPropertyList;
+
+		}
+	}
+
+	namespace render
+	{
+
+class TextureAsset;
+
+class TextureBatchDialog : public ui::ConfigDialog
+{
+	T_RTTI_CLASS(TextureBatchDialog)
+
+public:
+	bool create(ui::Widget* parent);
+
+	void destroy();
+
+	bool showModal(RefArray< TextureAsset >& outAssets);
+
+private:
+	Ref< ui::ListBox > m_textureList;
+	Ref< ui::custom::AutoPropertyList > m_texturePropertyList;
+
+	void addTexture();
+
+	void removeTexture();
+
+	void eventTextureListToolClick(ui::Event* event);
+
+	void eventTextureListSelect(ui::Event* event);
+};
+
+	}
+}
+
+#endif	// traktor_render_TextureBatchDialog_H
---
+++ code/Render/Editor/TextureBatchWizardTool.cpp
@@ -0,0 +1,53 @@
+#include "Render/Editor/TextureBatchWizardTool.h"
+#include "Render/Editor/TextureBatchDialog.h"
+#include "Render/Editor/TextureAsset.h"
+#include "Editor/Editor.h"
+#include "Database/Database.h"
+#include "Database/Group.h"
+#include "Database/Instance.h"
+#include "Ui/FileDialog.h"
+#include "I18N/Text.h"
+#include "Core/Log/Log.h"
+
+namespace traktor
+{
+	namespace render
+	{
+
+T_IMPLEMENT_RTTI_SERIALIZABLE_CLASS(L"traktor.render.TextureBatchWizardTool", TextureBatchWizardTool, editor::WizardTool)
+
+std::wstring TextureBatchWizardTool::getDescription() const
+{
+	return i18n::Text(L"TEXTURE_BATCH_WIZARDTOOL_DESCRIPTION");
+}
+
+bool TextureBatchWizardTool::launch(ui::Widget* parent, editor::Editor* editor, db::Group* group)
+{
+	TextureBatchDialog textureDialog;
+
+	if (!textureDialog.create(parent))
+		return false;
+
+	RefArray< TextureAsset > textureAssets;
+	if (textureDialog.showModal(textureAssets))
+	{
+		for (RefArray< TextureAsset >::iterator i = textureAssets.begin(); i != textureAssets.end(); ++i)
+		{
+			std::wstring instanceName = Path((*i)->getFileName()).getFileName();
+			Ref< db::Instance > textureAssetInstance = group->createInstance(
+				instanceName,
+				*i
+			);
+			if (textureAssetInstance)
+				textureAssetInstance->commit();
+			else
+				log::error << L"Unable to create instance \"" << instanceName << L"\"; asset not imported" << Endl;
+		}
+	}
+	
+	textureDialog.destroy();
+	return true;
+}
+
+	}
+}
