9c7c9b264ecfb050be9300d6e3955f4705ef18ba
Anders Pistol <anders.pistol@doctorentertainment.com>
2009-07-19 11:42:55 +0100

    Traktor: Added accessor to script manager to query script class from concrete type.
    [git-p4: depot-paths = "//depot/traktor/": change = 420]

--- code/Script/Js/ScriptManagerJs.cpp
+++ code/Script/Js/ScriptManagerJs.cpp
@@ -2,6 +2,7 @@
 #define T_HAVE_TYPES
 #include "Script/Js/ScriptManagerJs.h"
 #include "Script/Js/ScriptContextJs.h"
+#include "Script/IScriptClass.h"
 #include "Core/Serialization/Serializable.h"
 
 namespace traktor
@@ -14,6 +15,24 @@ T_IMPLEMENT_RTTI_SERIALIZABLE_CLASS(L"traktor.script.ScriptManagerJs", ScriptMan
 void ScriptManagerJs::registerClass(IScriptClass* scriptClass)
 {
 	m_registeredClasses.push_back(scriptClass);
+}
+
+IScriptClass* ScriptManagerJs::findScriptClass(const Type& type) const
+{
+	Ref< IScriptClass > minScriptClass;
+	uint32_t minScriptClassDiff = ~0UL;
+
+	for (RefArray< IScriptClass >::const_iterator i = m_registeredClasses.begin(); i != m_registeredClasses.end(); ++i)
+	{
+		uint32_t scriptClassDiff = type_difference((*i)->getExportType(), type);
+		if (scriptClassDiff < minScriptClassDiff)
+		{
+			minScriptClass = *i;
+			minScriptClassDiff = scriptClassDiff;
+		}
+	}
+
+	return minScriptClass;
 }
 
 IScriptContext* ScriptManagerJs::createContext()
--- code/Script/Lua/ScriptManagerLua.cpp
+++ code/Script/Lua/ScriptManagerLua.cpp
@@ -1,5 +1,6 @@
 #include "Script/Lua/ScriptManagerLua.h"
 #include "Script/Lua/ScriptContextLua.h"
+#include "Script/IScriptClass.h"
 #include "Core/Serialization/Serializable.h"
 #include "Core/Heap/New.h"
 
@@ -13,6 +14,24 @@ T_IMPLEMENT_RTTI_SERIALIZABLE_CLASS(L"traktor.script.ScriptManagerLua", ScriptMa
 void ScriptManagerLua::registerClass(IScriptClass* scriptClass)
 {
 	m_registeredClasses.push_back(scriptClass);
+}
+
+IScriptClass* ScriptManagerLua::findScriptClass(const Type& type) const
+{
+	Ref< IScriptClass > minScriptClass;
+	uint32_t minScriptClassDiff = ~0UL;
+
+	for (RefArray< IScriptClass >::const_iterator i = m_registeredClasses.begin(); i != m_registeredClasses.end(); ++i)
+	{
+		uint32_t scriptClassDiff = type_difference((*i)->getExportType(), type);
+		if (scriptClassDiff < minScriptClassDiff)
+		{
+			minScriptClass = *i;
+			minScriptClassDiff = scriptClassDiff;
+		}
+	}
+
+	return minScriptClass;
 }
 
 IScriptContext* ScriptManagerLua::createContext()
