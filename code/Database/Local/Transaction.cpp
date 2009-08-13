11536c6c82682a85e96837d793f1f7447e637b0e
Anders Pistol <anders.pistol@doctorentertainment.com>
2009-08-13 09:16:50 +0100

    Traktor: Disabled transaction locks for now; need to figure out some globally unique way of pin-pointing instance.
    [git-p4: depot-paths = "//depot/traktor/": change = 530]

--- code/Database/Local/Transaction.cpp
+++ code/Database/Local/Transaction.cpp
@@ -4,6 +4,8 @@
 #include "Core/Thread/Mutex.h"
 #include "Core/Log/Log.h"
 
+#define T_USE_TRANSACTION_LOCK 0
+
 namespace traktor
 {
 	namespace db
@@ -17,26 +19,62 @@ const int c_transactionTimeout = 100;
 
 T_IMPLEMENT_RTTI_CLASS(L"traktor.db.Transaction", Transaction, Object)
 
+Transaction::Transaction()
+#if defined(_DEBUG)
+:	m_locked(false)
+#endif
+{
+}
+
+Transaction::~Transaction()
+{
+#if defined(_DEBUG)
+	T_ASSERT_M (!m_locked, L"Transaction still locked");
+#endif
+}
+
 bool Transaction::create(const Guid& transactionGuid)
 {
+	T_ASSERT_M (!m_locked, L"Transaction already created");
+
+#if T_USE_TRANSACTION_LOCK
 	m_lock = gc_new< Mutex >(cref(transactionGuid));
 	if (!m_lock->acquire(c_transactionTimeout))
 	{
-		log::debug << L"Unable to create transaction; already exclusively locked" << Endl;
+		log::debug << L"Unable to create transaction \"" << m_transactionGuid.format() << L"\"; already exclusively locked" << Endl;
 		m_lock->release();
 		m_lock = 0;
 		return false;
 	}
+#endif
+
+#if defined(_DEBUG)
+	m_locked = true;
+#endif
+
+	m_transactionGuid = transactionGuid;
+	log::debug << L"Transaction \"" << m_transactionGuid.format() << L"\" created successfully" << Endl;
+
 	return true;
 }
 
 void Transaction::destroy()
 {
+	T_ASSERT_M (m_locked, L"Transaction already destroyed");
+
+#if T_USE_TRANSACTION_LOCK
 	if (m_lock)
 	{
 		m_lock->release();
 		m_lock = 0;
 	}
+#endif
+
+#if defined(_DEBUG)
+	m_locked = false;
+#endif
+
+	log::debug << L"Transaction \"" << m_transactionGuid.format() << L"\" destroyed" << Endl;
 }
 
 void Transaction::add(Action* action)
@@ -46,6 +84,8 @@ void Transaction::add(Action* action)
 
 bool Transaction::commit(Context* context)
 {
+	T_ASSERT_M (m_locked, L"Transaction not created");
+
 	bool result = true;
 
 	// Execute actions.
@@ -66,6 +106,11 @@ bool Transaction::commit(Context* context)
 	// Cleanup action temporary data.
 	for (int32_t i = 0; i < int32_t(m_actions.size()); ++i)
 		m_actions[i]->clean(context);
+
+	if (result)
+		log::debug << L"Transaction \"" << m_transactionGuid.format() << L"\" commited successfully" << Endl;
+	else
+		log::debug << L"Transaction \"" << m_transactionGuid.format() << L"\" failed to commit" << Endl;
 
 	return result;
 }
