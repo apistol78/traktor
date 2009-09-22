03088c22f05f3840b71cd27355ca1f88543f6d23
Anders Pistol <anders.pistol@doctorentertainment.com>
2009-09-22 14:52:03 +0100

    Traktor: Lock ref array in serialization.
    [git-p4: depot-paths = "//depot/traktor/": change = 910]

--- code/Render/ShaderGraph.cpp
+++ code/Render/ShaderGraph.cpp
@@ -11,6 +11,34 @@ namespace traktor
 {
 	namespace render
 	{
+		namespace
+		{
+
+#if defined(_DEBUG)
+#	define SANITY_CHECK() sanityCheck(m_nodes, m_edges)
+
+void sanityCheck(const RefArray< Node >& nodes, const RefArray< Edge >& edges)
+{
+	for (RefArray< Edge >::const_iterator i = edges.begin(); i != edges.end(); ++i)
+	{
+		const OutputPin* sourcePin = (*i)->getSource();
+		T_ASSERT (sourcePin);
+
+		const Node* sourceNode = sourcePin->getNode();
+		T_ASSERT (std::find(nodes.begin(), nodes.end(), sourceNode) != nodes.end());
+
+		const InputPin* destinationPin = (*i)->getDestination();
+		T_ASSERT (destinationPin);
+
+		const Node* destinationNode = destinationPin->getNode();
+		T_ASSERT (std::find(nodes.begin(), nodes.end(), destinationNode) != nodes.end());
+	}
+}
+#else
+#	define SANITY_CHECK()
+#endif
+
+		}
 
 T_IMPLEMENT_RTTI_EDITABLE_CLASS(L"traktor.render.ShaderGraph", ShaderGraph, Serializable)
 
@@ -22,6 +50,7 @@ ShaderGraph::ShaderGraph(const RefArray< Node >& nodes, const RefArray< Edge >& 
 :	m_nodes(nodes)
 ,	m_edges(edges)
 {
+	SANITY_CHECK();
 }
 
 void ShaderGraph::addNode(Node* node)
@@ -37,11 +66,6 @@ void ShaderGraph::removeNode(Node* node)
 	m_nodes.erase(i);
 }
 
-void ShaderGraph::removeAllNodes()
-{
-	m_nodes.resize(0);
-}
-
 void ShaderGraph::addEdge(Edge* edge)
 {
 	T_ASSERT (std::find(m_edges.begin(), m_edges.end(), edge) == m_edges.end());
@@ -52,13 +76,13 @@ void ShaderGraph::removeEdge(Edge* edge)
 {
 	RefArray< Edge >::iterator i = std::find(m_edges.begin(), m_edges.end(), edge);
 	T_ASSERT (i != m_edges.end());
-
 	m_edges.erase(i);
 }
 
-void ShaderGraph::removeAllEdges()
+void ShaderGraph::removeAll()
 {
 	m_edges.resize(0);
+	m_nodes.resize(0);
 }
 
 size_t ShaderGraph::findNodesOf(const Type& nodeType, RefArray< Node >& outNodes) const
@@ -73,8 +97,10 @@ size_t ShaderGraph::findNodesOf(const Type& nodeType, RefArray< Node >& outNodes
 
 bool ShaderGraph::serialize(Serializer& s)
 {
+	SANITY_CHECK();
 	s >> MemberRefArray< Node >(L"nodes", m_nodes);
 	s >> MemberRefArray< Edge >(L"edges", m_edges);
+	SANITY_CHECK();
 	return true;
 }
 
