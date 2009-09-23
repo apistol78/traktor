99d2d02849b205a186a6db05af8c711be8efc919
Anders Pistol <anders.pistol@doctorentertainment.com>
2009-09-23 15:01:36 +0100

    Traktor: Various debug helper improvements.
    [git-p4: depot-paths = "//depot/traktor/": change = 920]

--- code/Core/Assert.h
+++ code/Core/Assert.h
@@ -25,6 +25,12 @@
 
 #endif
 
+#define T_FATAL_ASSERT(expression) \
+	(void)((!!(expression)) || (traktor::Debugger::getInstance().assertionFailed(#expression, __FILE__, __LINE__), 0));
+
+#define T_FATAL_ASSERT_M(expression, message) \
+	(void)((!!(expression)) || (traktor::Debugger::getInstance().assertionFailed(#expression, __FILE__, __LINE__, message), 0));
+
 #define T_FATAL_ERROR \
 	traktor::Debugger::getInstance().breakDebugger();
 
