ccea7dff81d8b025847998c5681a204ba2527ad6
Anders Pistol <anders.pistol@doctorentertainment.com>
2009-09-24 06:26:44 +0100

    Traktor: Heap test case vastly more aggressive with 6 allocator threads and 2 collector threads.
    [git-p4: depot-paths = "//depot/traktor/": change = 925]

--- code/UnitTest/CaseHeap.cpp
+++ code/UnitTest/CaseHeap.cpp
@@ -26,13 +26,15 @@ class HeapObject : public Object
 {
 public:
 	static int32_t ms_instanceCount;
+	int32_t m_index;
 
-	HeapObject() { Atomic::increment(ms_instanceCount); }
-	virtual ~HeapObject() { Atomic::decrement(ms_instanceCount); }
+	HeapObject(int32_t index) : m_index(index) { Atomic::increment(ms_instanceCount); }
+	virtual ~HeapObject() { m_index = -1; Atomic::decrement(ms_instanceCount); }
 };
 
 int32_t HeapObject::ms_instanceCount = 0;
 int32_t g_nullPointerCount = 0;
+int32_t g_indexErrorCount = 0;
 int32_t g_allocCount = 0;
 int32_t g_collectCount = 0;
 
@@ -41,12 +43,37 @@ void threadStressAlloc()
 	Ref< HeapObject > object;
 	while (!ThreadManager::getInstance().getCurrentThread()->stopped())
 	{
-		object = gc_new< HeapObject >();
+		object = gc_new< HeapObject >(0);
 		if (!object)
-			g_nullPointerCount++;
-		g_allocCount++;
+			Atomic::increment(g_nullPointerCount);
+		Atomic::increment(g_allocCount);
 		ThreadManager::getInstance().getCurrentThread()->sleep(0);
 	}
+}
+
+void threadStressAllocArray()
+{
+	while (!ThreadManager::getInstance().getCurrentThread()->stopped())
+	{
+		RefArray< HeapObject > objects;
+		for (int i = 0; i < 100; ++i)
+			objects.push_back(gc_new< HeapObject >(i));
+
+		Atomic::increment(g_allocCount);
+
+		ThreadManager::getInstance().getCurrentThread()->sleep(0);
+
+		for (int i = 0; i < 100; ++i)
+		{
+			if (objects[i])
+			{
+				if (objects[i]->m_index != i)
+					Atomic::increment(g_indexErrorCount);
+			}
+			else
+				Atomic::increment(g_nullPointerCount);
+		}
+	}	
 }
 
 void threadStressCollect()
@@ -54,7 +81,7 @@ void threadStressCollect()
 	while (!ThreadManager::getInstance().getCurrentThread()->stopped())
 	{
 		Heap::getInstance().collect();
-		g_collectCount++;
+		Atomic::increment(g_collectCount);
 		ThreadManager::getInstance().getCurrentThread()->sleep(10);
 	}
 }
@@ -69,11 +96,11 @@ void CaseHeap::run()
 	}
 
 	{
-		Ref< HeapObject > foo1 = gc_new< HeapObject >();
+		Ref< HeapObject > foo1 = gc_new< HeapObject >(0);
 
 		{
-			Ref< HeapObject > foo2 = gc_new< HeapObject >();
-			Ref< HeapObject > foo3 = gc_new< HeapObject >();
+			Ref< HeapObject > foo2 = gc_new< HeapObject >(1);
+			Ref< HeapObject > foo3 = gc_new< HeapObject >(2);
 
 			CASE_ASSERT(foo1);
 			CASE_ASSERT(foo2);
@@ -109,35 +136,79 @@ void CaseHeap::run()
 	}
 
 	{
-		Thread* threadAlloc = ThreadManager::getInstance().create(makeStaticFunctor(&threadStressAlloc));
-		Thread* threadCollect = ThreadManager::getInstance().create(makeStaticFunctor(&threadStressCollect));
+		Thread* threads[8];
 
-		threadAlloc->start();
-		threadCollect->start();
+		for (int i = 0; i < 6; ++i)
+			threads[i] = ThreadManager::getInstance().create(makeStaticFunctor(&threadStressAlloc));
+		for (int i = 6; i < 8; ++i)
+			threads[i]= ThreadManager::getInstance().create(makeStaticFunctor(&threadStressCollect));
 
-		ThreadManager::getInstance().getCurrentThread()->sleep(5000);
+		for (int i = 0; i < 8; ++i)
+			threads[i]->start();
 
-		threadAlloc->stop();
-		threadCollect->stop();
+		ThreadManager::getInstance().getCurrentThread()->sleep(10000);
 
-		ThreadManager::getInstance().destroy(threadAlloc);
-		ThreadManager::getInstance().destroy(threadCollect);
+		for (int i = 0; i < 8; ++i)
+			threads[i]->stop();
+
+		for (int i = 0; i < 8; ++i)
+			ThreadManager::getInstance().destroy(threads[i]);
 
 		log::info << g_allocCount << L" allocated objects" << Endl;
 		log::info << g_collectCount << L" forced garbage collects" << Endl;
 
 		CASE_ASSERT_EQUAL(g_nullPointerCount, 0);
 
-		ThreadManager::getInstance().getCurrentThread()->sleep(2000);
+		// Wait until orphan objects get promoted to collect-able.
+		ThreadManager::getInstance().getCurrentThread()->sleep(11000);
 
-		for (int i = 0; i < 100; ++i)
+		for (int i = 0; i < 10; ++i)
 			Heap::getInstance().collect();
 
-		log::info << HeapObject::ms_instanceCount << Endl;
+		ThreadManager::getInstance().getCurrentThread()->sleep(1000);
 
+		log::info << HeapObject::ms_instanceCount << Endl;
+		CASE_ASSERT_EQUAL(HeapObject::ms_instanceCount, 0);
+	}
+
+	g_allocCount = 0;
+	g_collectCount = 0;
+
+	{
+		Thread* threads[8];
+
+		for (int i = 0; i < 6; ++i)
+			threads[i] = ThreadManager::getInstance().create(makeStaticFunctor(&threadStressAllocArray));
+		for (int i = 6; i < 8; ++i)
+			threads[i]= ThreadManager::getInstance().create(makeStaticFunctor(&threadStressCollect));
+
+		for (int i = 0; i < 8; ++i)
+			threads[i]->start();
+
+		ThreadManager::getInstance().getCurrentThread()->sleep(10000);
+
+		for (int i = 0; i < 8; ++i)
+			threads[i]->stop();
+
+		for (int i = 0; i < 8; ++i)
+			ThreadManager::getInstance().destroy(threads[i]);
+
+		log::info << g_allocCount << L" allocated arrays" << Endl;
+		log::info << g_collectCount << L" forced garbage collects" << Endl;
+
+		// Wait until orphan objects get promoted to collect-able.
+		ThreadManager::getInstance().getCurrentThread()->sleep(11000);
+
+		for (int i = 0; i < 10; ++i)
+			Heap::getInstance().collect();
+
+		ThreadManager::getInstance().getCurrentThread()->sleep(1000);
+
+		log::info << HeapObject::ms_instanceCount << Endl;
 		CASE_ASSERT_EQUAL(HeapObject::ms_instanceCount, 0);
 
-		Heap::getInstance().collect();
+		CASE_ASSERT_EQUAL(g_nullPointerCount, 0);
+		CASE_ASSERT_EQUAL(g_indexErrorCount, 0);
 	}
 }
 
