de655e6e92d1188073aee1fcd355399bde692e80
Anders Pistol <anders.pistol@doctorentertainment.com>
2009-10-09 14:39:17 +0100

    Traktor: Reversed XML.
    [git-p4: depot-paths = "//depot/traktor/": change = 1016]

--- code/Xml/XmlPullParser.cpp
+++ code/Xml/XmlPullParser.cpp
@@ -10,16 +10,17 @@
 #include "Core/Log/Log.h"
 
 #if T_XML_PARSER_THREAD
-#	include "Core/Thread/JobManager.h"
-#	include "Core/Thread/Signal.h"
-#	include "Core/Thread/Semaphore.h"
-#	include "Core/Thread/Acquire.h"
+#include "Core/Thread/JobManager.h"
+#include "Core/Thread/Signal.h"
+#include "Core/Thread/Semaphore.h"
+#include "Core/Thread/Acquire.h"
 #endif
 
+// On Linux and Apple we externally depend on Expat being part of the system.
+#if !defined(linux) && !defined(__APPLE__)
 #define XML_STATIC
 #define XML_UNICODE
-#if defined(_WIN32)
-#	define XML_UNICODE_WCHAR_T
+#define XML_UNICODE_WCHAR_T
 #endif
 
 #include "expat.h"
@@ -34,15 +35,15 @@ namespace traktor
 inline std::wstring xmltows(const XML_Char* xmlstr, const XML_Char* term)
 {
 #if defined(XML_UNICODE)
-#	if defined(XML_UNICODE_WCHAR_T)
+#if defined(XML_UNICODE_WCHAR_T)
 	T_ASSERT (sizeof(XML_Char) == sizeof(wchar_t));
 	return std::wstring(xmlstr, term);
-#	else
+#else
 	std::wstringstream ss;
 	for (const XML_Char* s = xmlstr; s != term; ++s)
 		ss << wchar_t(*s);
 	return ss.str();
-#	endif
+#endif
 #else
 	return mbstows(std::string(xmlstr, term));
 #endif
@@ -51,15 +52,15 @@ inline std::wstring xmltows(const XML_Char* xmlstr, const XML_Char* term)
 inline std::wstring xmltows(const XML_Char* xmlstr)
 {
 #if defined(XML_UNICODE)
-#	if defined(XML_UNICODE_WCHAR_T)
+#if defined(XML_UNICODE_WCHAR_T)
 	T_ASSERT (sizeof(XML_Char) == sizeof(wchar_t));
 	return xmlstr;
-#	else
+#else
 	std::wstringstream ss;
 	for (const XML_Char* s = xmlstr; *s; ++s)
 		ss << wchar_t(*s);
 	return ss.str();
-#	endif
+#endif
 #else
 	return mbstows(xmlstr);
 #endif
@@ -72,7 +73,7 @@ class XmlPullParserImpl
 public:
 	XmlPullParserImpl(Stream* stream);
 
-	virtual ~XmlPullParserImpl();
+	~XmlPullParserImpl();
 	
 	bool get(XmlPullParser::Event& outEvent);
 
@@ -87,10 +88,10 @@ private:
 	Job m_parseJob;
 	Signal m_eventQueueSignal;
 	Semaphore m_eventQueueLock;
-#	if T_XML_PARSER_THREAD_LOG
+#if T_XML_PARSER_THREAD_LOG
 	uint32_t m_getCalled;
 	uint32_t m_getStalled;
-#	endif
+#endif
 #endif
 
 	bool parse();
@@ -143,9 +144,9 @@ XmlPullParserImpl::~XmlPullParserImpl()
 {
 #if T_XML_PARSER_THREAD
 	m_parseJob.wait();
-#	if T_XML_PARSER_THREAD_LOG
+#if T_XML_PARSER_THREAD_LOG
 	log::info << L"XmlPullParserImpl, " << m_getCalled << " get called, " << m_getStalled << " stalled" << Endl;
-#	endif
+#endif
 #endif
 	if (m_parser)
 		XML_ParserFree(m_parser);
@@ -154,15 +155,15 @@ XmlPullParserImpl::~XmlPullParserImpl()
 bool XmlPullParserImpl::get(XmlPullParser::Event& outEvent)
 {
 #if T_XML_PARSER_THREAD
-#	if T_XML_PARSER_THREAD_LOG
+#if T_XML_PARSER_THREAD_LOG
 	++m_getCalled;
-#	endif
+#endif
 	m_eventQueueSignal.reset();
 	while (m_eventQueue.empty())
 	{
-#	if T_XML_PARSER_THREAD_LOG
+#if T_XML_PARSER_THREAD_LOG
 		++m_getStalled;
-#	endif
+#endif
 		if (!m_eventQueueSignal.wait(1000))
 			return false;
 		m_eventQueueSignal.reset();
