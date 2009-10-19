be6f3ef59bf03fb596a477b2810fa0d9214f0617
Anders Pistol <anders.pistol@doctorentertainment.com>
2009-10-19 17:58:01 +0100

    Traktor: Implemented popup menu in Cocoa.
    [git-p4: depot-paths = "//depot/traktor/": change = 1124]

--- code/Ui/Cocoa/PopupMenuCocoa.h
+++ code/Ui/Cocoa/PopupMenuCocoa.h
@@ -1,6 +1,9 @@
 #ifndef traktor_ui_PopupMenuCocoa_H
 #define traktor_ui_PopupMenuCocoa_H
 
+#import <Cocoa/Cocoa.h>
+#import "Ui/Cocoa/NSTargetProxy.h"
+
 #include "Core/Heap/Ref.h"
 #include "Ui/Itf/IPopupMenu.h"
 
@@ -10,8 +13,11 @@ namespace traktor
 	{
 
 class EventSubject;
+class MenuItem;
 
-class PopupMenuCocoa : public IPopupMenu
+class PopupMenuCocoa
+:	public IPopupMenu
+,	public ITargetProxyCallback
 {
 public:
 	PopupMenuCocoa(EventSubject* owner);
@@ -25,6 +31,16 @@ public:
 	virtual void add(MenuItem* item);
 
 	virtual MenuItem* show(IWidget* parent, const Point& at);
+	
+	// ITargetProxyCallback
+	
+	virtual void targetProxy_Action(void* controlId);
+	
+	virtual void targetProxy_doubleAction(void* controlId);
+
+private:
+	NSMenu* m_menu;
+	Ref< MenuItem > m_selectedItem;
 };
 
 	}
--- code/Ui/Cocoa/PopupMenuCocoa.mm
+++ code/Ui/Cocoa/PopupMenuCocoa.mm
@@ -1,4 +1,9 @@
+#import "Ui/Cocoa/ObjCRef.h"
+
 #include "Ui/Cocoa/PopupMenuCocoa.h"
+#include "Ui/Cocoa/UtilitiesCocoa.h"
+#include "Ui/Itf/IWidget.h"
+#include "Ui/MenuItem.h"
 
 namespace traktor
 {
@@ -6,26 +11,80 @@ namespace traktor
 	{
 	
 PopupMenuCocoa::PopupMenuCocoa(EventSubject* owner)
+:	m_menu(0)
 {
 }
 
 bool PopupMenuCocoa::create()
 {
+	m_menu = [[NSMenu alloc] initWithTitle: @""];
+	[m_menu setAutoenablesItems: NO];
+
 	return true;
 }
 
 void PopupMenuCocoa::destroy()
 {
+	[m_menu release];
 }
 
 void PopupMenuCocoa::add(MenuItem* item)
 {
+	if (item->getText() != L"-")
+	{
+		NSTargetProxy* targetProxy = [[NSTargetProxy alloc] init];
+		[targetProxy setCallback: this];
+	
+		NSMenuItem* menuItem = [[NSMenuItem alloc]
+			initWithTitle: makeNSString(item->getText())
+			action: nil
+			keyEquivalent: @""
+		];
+		[menuItem setTarget: targetProxy];
+		[menuItem setAction: @selector(dispatchActionCallback:)];
+		[menuItem setRepresentedObject: [[ObjCRef alloc] initWithRef: item]];
+		
+		[m_menu addItem: menuItem];
+	}
+	else
+		[m_menu addItem: [NSMenuItem separatorItem]];
 }
 
 MenuItem* PopupMenuCocoa::show(IWidget* parent, const Point& at)
 {
-	return 0;
-}
+	NSView* view = (NSView*)parent->getInternalHandle();
+	T_ASSERT (view);
+
+	NSPoint nat = [view convertPoint: makeNSPoint(at) toView: nil];
 	
+	NSEvent* event = [NSEvent
+		mouseEventWithType: NSLeftMouseDown
+		location: nat
+		modifierFlags: NSLeftMouseDownMask
+		timestamp: 0
+		windowNumber: [[view window] windowNumber]
+		context: [[view window] graphicsContext]
+		eventNumber: 0
+		clickCount: 1
+		pressure: 1
+	];
+	
+	[NSMenu popUpContextMenu: m_menu withEvent: event forView: view];
+	
+	return m_selectedItem;
+}
+
+void PopupMenuCocoa::targetProxy_Action(void* controlId)
+{
+	NSMenuItem* menuItem = (NSMenuItem*)controlId;
+	ObjCRef* repsObj = (ObjCRef*)[menuItem representedObject];
+	if (repsObj)
+		m_selectedItem = dynamic_type_cast< MenuItem* >([repsObj get]);
+}
+
+void PopupMenuCocoa::targetProxy_doubleAction(void* controlId)
+{
+}
+
 	}
 }
