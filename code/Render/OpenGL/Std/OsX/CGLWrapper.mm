80103c2e12a2825f427b09f8ad0c11cacdaf70d4
Anders Pistol <anders.pistol@doctorentertainment.com>
2009-10-26 21:06:58 +0100

    Traktor: Ensure viewport is set when activating OpenGL context on OSX.
    [git-p4: depot-paths = "//depot/traktor/": change = 1215]

--- code/Render/OpenGL/Std/OsX/CGLWrapper.mm
+++ code/Render/OpenGL/Std/OsX/CGLWrapper.mm
@@ -66,7 +66,14 @@ bool cglwMakeCurrent(void* context)
 {
 	WrContext* wrc = (WrContext*)context;
 	if (wrc)
+	{
 		[wrc->context makeCurrentContext];
+		if (wrc->view)
+		{
+			NSRect frame = [wrc->view frame];
+			glViewport(0, 0, frame.size.width, frame.size.height);
+		}
+	}
 	return true;
 }
 
