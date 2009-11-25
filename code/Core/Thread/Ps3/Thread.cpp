5b010c2100a792b599e890cfd2f44fe9f83178db
Anders Pistol <anders.pistol@doctorentertainment.com>
2009-11-25 14:56:24 +0100

    Traktor: Waitable interface implemented on PS3 threads.
    [git-p4: depot-paths = "//depot/traktor/": change = 1592]

--- code/Core/Thread/Ps3/Thread.cpp
+++ code/Core/Thread/Ps3/Thread.cpp
@@ -1,4 +1,5 @@
 #include <sys/ppu_thread.h>
+#include <sys/synchronization.h>
 #include <sys/process.h>
 #include <sys/timer.h>
 #include "Core/Thread/Thread.h"
@@ -9,14 +10,27 @@ namespace traktor
 {
 	namespace
 	{
-	
+
+struct Internal
+{
+	sys_ppu_thread_t thread;
+	sys_mutex_t mutex;
+	sys_cond_t cond;
+	Functor* functor;
+	bool finished;
+};
+
 void threadEntry(uint64_t data)
 {
-	Functor* functor = reinterpret_cast< Functor* >(data);
-	T_ASSERT (functor);
-	
-	(functor->operator())();
-	
+	Internal* in = reinterpret_cast< Internal* >(data);
+
+	(in->functor->operator())();
+
+	sys_mutex_lock(in->mutex, SYS_NO_TIMEOUT);
+	in->finished = true;
+	sys_cond_signal_all(in->cond);
+	sys_mutex_unlock(in->mutex);
+
 	sys_ppu_thread_exit(0);
 }
 	
@@ -24,34 +38,69 @@ void threadEntry(uint64_t data)
 
 bool Thread::start(Priority priority)
 {
-	const uint32_t c_priorities[] = { 0, 2000, 4000 };
 	const uint32_t c_stackSize = 4 * 1024 * 1024;
 
-	sys_ppu_thread_t* th = new sys_ppu_thread_t();
+	Internal* in = new Internal();
+
+	m_handle = in;
+
+	in->functor = m_functor;
+	in->finished = false;
+
+	sys_mutex_create(&in->mutex, 0);
+	sys_cond_create(&in->cond, in->mutex, 0);
 
 	int rc = sys_ppu_thread_create(
-		th,
+		&in->thread,
 		threadEntry,
-		(uint64_t)m_functor,
-		c_priorities[priority],
+		(uint64_t)in,
+		1535 + int(priority) * 767,
 		c_stackSize,
 		SYS_PPU_THREAD_CREATE_JOINABLE,
 		m_name.c_str()
 	);
 
-	m_handle = th;
+	if (rc != CELL_OK)
+	{
+		delete in;
+		m_handle = 0;
+		return false;
+	}
 
-	return bool(rc == CELL_OK);
+	return true;
 }
 
 bool Thread::wait(int timeout)
 {
-	sys_ppu_thread_t* th = reinterpret_cast< sys_ppu_thread_t* >(m_handle);
-	T_ASSERT (th);
-	T_ASSERT (timeout == 0);
+	Internal* in = reinterpret_cast< Internal* >(m_handle);
+	T_ASSERT (in);
+
+	int rc;
+
+	if (timeout >= 0)
+	{
+		if (timeout > 0)
+		{
+			rc = sys_mutex_lock(in->mutex, SYS_NO_TIMEOUT);
+			if (rc != CELL_OK)
+				return false;
+
+			for (rc = 0; rc == 0 && !in->finished; )
+			{
+				rc = sys_cond_wait(in->cond, timeout);
+				if (rc == ETIMEDOUT)
+					break;
+			}
+
+			sys_mutex_unlock(in->mutex);
+		}
+
+		if (!in->finished)
+			return false;
+	}
 
 	uint64_t status;
-	int rc = sys_ppu_thread_join(*th, &status);
+	rc = sys_ppu_thread_join(in->thread, &status);
 
 	return bool(rc == CELL_OK);
 }
@@ -91,14 +140,14 @@ bool Thread::stopped() const
 
 bool Thread::current() const
 {
-	sys_ppu_thread_t* th = reinterpret_cast< sys_ppu_thread_t* >(m_handle);
-	if (!th)
+	Internal* in = reinterpret_cast< Internal* >(m_handle);
+	if (!in)
 		return false;
 
 	sys_ppu_thread_t current;
 	sys_ppu_thread_get_id(&current);
 
-	return bool(current == *th);
+	return bool(current == in->thread);
 }
 
 bool Thread::finished() const
@@ -118,8 +167,8 @@ Thread::Thread(Functor* functor, const std::wstring& name, int hardwareCore)
 
 Thread::~Thread()
 {
-	sys_ppu_thread_t* th = reinterpret_cast< sys_ppu_thread_t* >(m_handle);
-	delete th;
+	Internal* in = reinterpret_cast< Internal* >(m_handle);
+	delete in;
 }
 
 }
