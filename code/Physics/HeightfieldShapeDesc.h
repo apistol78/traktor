9461dea70c08c3758c375c2e46cdca35dca16637
Anders Pistol <anders.pistol@doctorentertainment.com>
2009-12-15 18:11:30 +0100

    Amalgam: Added terrain factories to Amalgam.
    [git-p4: depot-paths = "//depot/traktor/": change = 1799]

--- code/Physics/HeightfieldShapeDesc.h
+++ code/Physics/HeightfieldShapeDesc.h
@@ -31,6 +31,8 @@ public:
 
 	const resource::Proxy< Heightfield >& getHeightfield() const;
 
+	virtual bool bind(resource::IResourceManager* resourceManager);
+
 	virtual bool serialize(ISerializer& s);
 
 private:
