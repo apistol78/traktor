df6083153f962f9232a38e518c154fcfc48999c8
Anders Pistol <anders.pistol@doctorentertainment.com>
2009-12-29 12:00:45 +0100

    Traktor: Added a PoolAllocator class to Core/Memory.
    [git-p4: depot-paths = "//depot/traktor/": change = 1932]

---
+++ code/Core/Memory/PoolAllocator.h
@@ -0,0 +1,108 @@
+#ifndef traktor_PoolAllocator_H
+#define traktor_PoolAllocator_H
+
+#include <stack>
+#include "Core/Object.h"
+
+// import/export mechanism.
+#undef T_DLLCLASS
+#if defined(T_CORE_EXPORT)
+#	define T_DLLCLASS T_DLLEXPORT
+#else
+#	define T_DLLCLASS T_DLLIMPORT
+#endif
+
+namespace traktor
+{
+
+class IAllocator;
+
+/*! \brief Pool allocator.
+ * \ingroup Core
+ *
+ * Pool allocator is designed to allocate continuous chunks of
+ * memory quickly. All memory are released as soon as the pool
+ * is released thus there are no way of freeing individual
+ * allocations.
+ *
+ * In order to make this pool a bit more efficient it
+ * supports allocations scopes; it can nest several allocations
+ * in scopes and as soon as a scope is leaved objects allocated
+ * in that scope are destroyed.
+ */
+class T_DLLCLASS PoolAllocator : public Object
+{
+	T_RTTI_CLASS;
+
+public:
+	PoolAllocator(IAllocator* allocator, uint32_t totalSize);
+
+	PoolAllocator(uint32_t totalSize);
+
+	virtual ~PoolAllocator();
+
+	/*! \brief Enter allocation scope. */
+	void enter();
+
+	/*! \brief Leave allocation scope. */
+	void leave();
+
+	/*! \brief Allocate chunk.
+	 *
+	 * \param size Size of chunk.
+	 * \return Pointer to chunk.
+	 */
+	void* alloc(uint32_t size);
+
+	/*! \brief Allocate object.
+	 *
+	 * \return Pointer to object.
+	 */
+	template < typename Type >
+	Type* alloc()
+	{
+		void* ptr = alloc(sizeof(Type));
+		return new (ptr) Type();
+	}
+
+	/*! \brief Allocate array of objects.
+	 *
+	 * \param count Number of objects.
+	 * \return Pointer to first object.
+	 */
+	template < typename Type >
+	Type* alloc(uint32_t count)
+	{
+		if (!count)
+			return 0;
+
+		void* ptr = alloc(sizeof(Type) * count);
+		return new (ptr) Type [count];
+	}
+
+	/*! \brief Allocate array of object pointers.
+	 *
+	 * \param count Number of object pointers.
+	 * \return Pointer to first object pointer.
+	 */
+	template < typename Type >
+	Type** allocArray(uint32_t count)
+	{
+		if (!count)
+			return 0;
+
+		void* ptr = alloc(sizeof(Type*) * count);
+		return static_cast< Type** >(ptr);
+	}
+
+private:
+	Ref< IAllocator > m_allocator;
+	uint32_t m_totalSize;
+	uint8_t* m_head;
+	uint8_t* m_tail;
+	std::stack< uint8_t* > m_scope;
+};
+
+}
+
+#endif	// traktor_PoolAllocator_H
