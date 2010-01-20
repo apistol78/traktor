fc520a827706ddd3ae268c151cee8cc3dac57e73
Anders Pistol <anders.pistol@doctorentertainment.com>
2010-01-20 12:55:14 +0100

    Traktor: Fixed PS3 compile issues. Stubbed PSN online support.
    [git-p4: depot-paths = "//depot/traktor/": change = 2169]

---
+++ code/Online/Psn/SessionManagerPsn.h
@@ -0,0 +1,37 @@
+#ifndef traktor_online_SessionManagerPsn_H
+#define traktor_online_SessionManagerPsn_H
+
+#include "Online/ISessionManager.h"
+
+// import/export mechanism.
+#undef T_DLLCLASS
+#if defined(T_ONLINE_PSN_EXPORT)
+#	define T_DLLCLASS T_DLLEXPORT
+#else
+#	define T_DLLCLASS T_DLLIMPORT
+#endif 
+
+namespace traktor
+{
+	namespace online
+	{
+
+/*! \brief PlayStation Network online session manager.
+ * \ingroup Online
+ */
+class T_DLLCLASS SessionManagerPsn : public ISessionManager
+{
+	T_RTTI_CLASS;
+
+public:
+	virtual bool getAvailableUsers(RefArray< IUser >& outUsers);
+
+	virtual Ref< IUser > getCurrentUser();
+
+	virtual Ref< ISession > createSession(IUser* user);
+};
+
+	}
+}
+
+#endif	// traktor_online_SessionManagerPsn_H
