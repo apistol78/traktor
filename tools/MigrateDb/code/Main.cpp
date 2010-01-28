1f9c663fca736700cde1c9bcd4ee604b0e2b5b9c
Anders Pistol <anders.pistol@doctorentertainment.com>
2010-01-28 13:22:55 +0100

    Traktor: Database migration tool added.
    [git-p4: depot-paths = "//depot/traktor/": change = 2358]

---
+++ tools/MigrateDb/code/Main.cpp
@@ -0,0 +1,147 @@
+#include <Core/Io/IStream.h>
+#include <Core/Io/StreamCopy.h>
+#include <Core/Library/Library.h>
+#include <Core/Log/Log.h>
+#include <Core/Misc/CommandLine.h>
+#include <Database/Database.h>
+#include <Database/Group.h>
+#include <Database/Instance.h>
+
+using namespace traktor;
+
+void recursiveConvertInstances(db::Group* targetGroup, db::Group* sourceGroup)
+{
+	T_ANONYMOUS_VAR(ScopeIndent)(log::info);
+	log::info << IncreaseIndent;
+
+	for (RefArray< db::Instance >::iterator i = sourceGroup->getBeginChildInstance(); i != sourceGroup->getEndChildInstance(); ++i)
+	{
+		Ref< db::Instance > sourceInstance = *i;
+		T_ASSERT (sourceInstance);
+
+		log::info << L"Converting \"" << sourceInstance->getName() << L"\"..." << Endl;
+
+		Ref< ISerializable > sourceObject = sourceInstance->getObject();
+		if (!sourceObject)
+		{
+			log::error << L"Failed, unable to get source object" << Endl;
+			continue;
+		}
+
+		Guid sourceGuid = sourceInstance->getGuid();
+		Ref< db::Instance > targetInstance = targetGroup->createInstance(sourceInstance->getName(), db::CifReplaceExisting, &sourceGuid);
+		if (!targetInstance)
+		{
+			log::error << L"Failed, unable to create target instance" << Endl;
+			continue;
+		}
+
+		targetInstance->setObject(sourceObject);
+
+		std::vector< std::wstring > dataNames;
+		sourceInstance->getDataNames(dataNames);
+
+		for (std::vector< std::wstring >::iterator j = dataNames.begin(); j != dataNames.end(); ++j)
+		{
+			log::info << L"\t\"" << *j << L"\"..." << Endl;
+
+			Ref< IStream > sourceStream = sourceInstance->readData(*j);
+			if (!sourceStream)
+			{
+				log::error << L"Failed, unable to open source stream" << Endl;
+				continue;
+			}
+
+			Ref< IStream > targetStream = targetInstance->writeData(*j);
+			if (!targetStream)
+			{
+				log::error << L"Failed, unable to open target stream" << Endl;
+				continue;
+			}
+
+			if (!StreamCopy(targetStream, sourceStream).execute())
+				log::error << L"Failed, unable to copy data" << Endl;
+
+			targetStream->close();
+			sourceStream->close();
+		}
+
+		if (!targetInstance->commit())
+		{
+			log::error << L"Failed, unable to commit target instance" << Endl;
+			continue;
+		}
+	}
+
+	for (RefArray< db::Group >::iterator i = sourceGroup->getBeginChildGroup(); i != sourceGroup->getEndChildGroup(); ++i)
+	{
+		Ref< db::Group > sourceChildGroup = *i;
+		T_ASSERT (sourceChildGroup);
+
+		log::info << L"Creating group \"" << sourceChildGroup->getName() << L"\"..." << Endl;
+
+		Ref< db::Group > targetChildGroup = targetGroup->getGroup(sourceChildGroup->getName());
+		if (!targetChildGroup)
+		{
+			targetChildGroup = targetGroup->createGroup(sourceChildGroup->getName());
+			if (!targetChildGroup)
+			{
+				log::error << L"Failed, unable to create target group" << Endl;
+				continue;
+			}
+		}
+
+		recursiveConvertInstances(targetChildGroup, sourceChildGroup);
+	}
+}
+
+int main(int argc, const char** argv)
+{
+	CommandLine cmdLine(argc, argv);
+	if (cmdLine.getCount() < 2)
+	{
+		log::info << L"Usage: MigrateDb [source database] [destination database] (module)*" << Endl;
+		return 0;
+	}
+
+	for (int32_t i = 2; i < cmdLine.getCount(); ++i)
+	{
+		Library library;
+		if (!library.open(cmdLine.getString(i)))
+		{
+			log::error << L"Unable to load module \"" << cmdLine.getString(i) << L"\"" << Endl;
+			return 0;
+		}
+	}
+
+	std::wstring sourceCs = cmdLine.getString(0);
+	std::wstring destinationCs = cmdLine.getString(1);
+
+	Ref< db::Database > sourceDb = new db::Database();
+	if (!sourceDb->open(sourceCs))
+	{
+		log::error << L"Unable to open source database \"" << sourceCs << L"\"" << Endl;
+		return false;
+	}
+
+	Ref< db::Database > destinationDb = new db::Database();
+	if (!destinationDb->create(destinationCs))
+	{
+		log::error << L"Unable to create destination database \"" << destinationCs << L"\"" << Endl;
+		return false;
+	}
+
+	log::info << L"Migration begin" << Endl;
+
+	Ref< db::Group > sourceGroup = sourceDb->getRootGroup();
+	Ref< db::Group > targetGroup = destinationDb->getRootGroup();
+	if (sourceGroup && targetGroup)
+		recursiveConvertInstances(targetGroup, sourceGroup);
+
+	log::info << L"Migration complete" << Endl;
+
+	destinationDb->close();
+	sourceDb->close();
+
+	return 0;
+}
