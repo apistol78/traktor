3b036da0d734efa6dec2dd9597c91480ba878eac
Anders Pistol <anders.pistol@doctorentertainment.com>
2010-02-06 16:46:04 +0100

    Traktor: Missing files.
    [git-p4: depot-paths = "//depot/traktor/": change = 2595]

---
+++ code/Database/Local/Perforce/PerforceClient.h
@@ -0,0 +1,64 @@
+#ifndef traktor_db_PerforceClient_H
+#define traktor_db_PerforceClient_H
+
+#include <vector>
+#include <clientapi.h>
+#include "Core/Object.h"
+#include "Core/RefArray.h"
+#include "Core/Io/Path.h"
+#include "Database/Local/Perforce/PerforceTypes.h"
+#include "Database/Local/Perforce/PerforceClientDesc.h"
+
+namespace traktor
+{
+	namespace db
+	{
+
+class PerforceChangeList;
+class PerforceChangeListFile;
+
+/*! \brief P4 client.
+ * \ingroup Database
+ */
+class PerforceClient : public Object
+{
+	T_RTTI_CLASS;
+
+public:
+	PerforceClient(const PerforceClientDesc& clientDesc);
+
+	virtual ~PerforceClient();
+
+	bool getChangeLists(RefArray< PerforceChangeList >& outChangeLists);
+
+	Ref< PerforceChangeList > createChangeList(const std::wstring& description);
+
+	bool whereIsLocalFile(const std::wstring& depotFile, std::wstring& outLocalPath);
+
+	bool isOpened(const std::wstring& localFile, PerforceAction& outAction);
+
+	bool addFile(const PerforceChangeList* changeList, const std::wstring& localFile);
+
+	bool openForEdit(const PerforceChangeList* changeList, const std::wstring& localFile);
+
+	bool openForDelete(const PerforceChangeList* changeList, const std::wstring& localFile);
+
+	bool revertFile(const PerforceChangeList* changeList, const std::wstring& localFile);
+
+	bool synchronize();
+
+	const std::wstring& getLastError() const;
+
+private:
+	PerforceClientDesc m_clientDesc;
+	ClientApi m_p4client;
+	bool m_connected;
+	std::wstring m_lastError;
+
+	bool establishConnection();
+};
+
+	}
+}
+
+#endif	// traktor_db_PerforceClient_H
