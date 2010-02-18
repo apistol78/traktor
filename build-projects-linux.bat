7c9f85cc3998ed2539f62ae57effaa17f94182ef
Anders Pistol <anders.pistol@doctorentertainment.com>
2010-02-18 13:44:00 +0100

    Traktor: Removed unused bat files.
    [git-p4: depot-paths = "//depot/traktor/": change = 2970]

--- build-projects-linux.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-call %~dp0config.bat
-
-mkdir %TRAKTOR_HOME%\build
-
-%TRAKTOR_HOME%\bin\solutionbuilder -f=make TraktorLinux.xms -d=gnu -p=linux
--- build-projects-macosx.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call %~dp0config.bat
-%TRAKTOR_HOME%\bin\solutionbuilder -f=xcode TraktorMacOSX.xms -d=DebugStatic -r=ReleaseStatic
