3db186af5fef8cf5bf2945af9d7c59c02edf56c8
Anders Pistol <anders.pistol@doctorentertainment.com>
2010-02-19 08:06:05 +0100

    Traktor: Renamed some scripts.
    [git-p4: depot-paths = "//depot/traktor/": change = 2985]

--- build-projects-iphone.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call %~dp0config.bat
-%TRAKTOR_HOME%\bin\solutionbuilder -f=xcode TraktorIPhone.xms -d=DebugStatic -r=ReleaseStatic -i
--- build-projects-linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir $TRAKTOR_HOME/build
-
-$TRAKTOR_HOME/bin/Linux/SolutionBuilder -f=make $TRAKTOR_HOME/TraktorLinux.xms -d=gnu -p=linux
-
