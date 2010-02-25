d7b3583987caf94c2d2fd736a408afe69b837f9f
Anders Pistol <anders.pistol@doctorentertainment.com>
2010-02-25 22:34:03 +0100

    Traktor: Added "Meshes" report.
    [git-p4: depot-paths = "//depot/traktor/": change = 3207]

--- generate-reports.bat
+++ generate-reports.bat
@@ -8,3 +8,4 @@ set REPORT_DB=data/temp/Pipeline.db
 
 %TRAKTOR_HOME%/bin/ReportGenerator %TRAKTOR_HOME%/res/reports/Shaders.html > reports/Shaders.html
 %TRAKTOR_HOME%/bin/ReportGenerator %TRAKTOR_HOME%/res/reports/Textures.html > reports/Textures.html
+%TRAKTOR_HOME%/bin/ReportGenerator %TRAKTOR_HOME%/res/reports/Meshes.html > reports/Meshes.html
