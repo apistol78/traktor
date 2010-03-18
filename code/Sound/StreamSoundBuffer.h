8495f9cc4fbd1036a2a121a97a178ea0f95ef4bd
Anders Pistol <anders.pistol@doctorentertainment.com>
2010-03-18 11:28:12 +0100

    Traktor: Incorrect dll-export macros fixed in sound buffers.
    [git-p4: depot-paths = "//depot/traktor/": change = 3534]

--- code/Sound/StreamSoundBuffer.h
+++ code/Sound/StreamSoundBuffer.h
@@ -3,6 +3,14 @@
 
 #include "Sound/ISoundBuffer.h"
 
+// import/export mechanism.
+#undef T_DLLCLASS
+#if defined(T_SOUND_EXPORT)
+#	define T_DLLCLASS T_DLLEXPORT
+#else
+#	define T_DLLCLASS T_DLLIMPORT
+#endif
+
 namespace traktor
 {
 	namespace sound
