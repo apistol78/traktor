a3aacb41d9efc39cab1d2be59b9d64b50db3276f
Anders Pistol <anders.pistol@doctorentertainment.com>
2010-03-28 19:28:06 +0100

    Traktor: Point renderer SPUified. Disabled sorting of particles for now; no visual difference in 50L.
    [git-p4: depot-paths = "//depot/traktor/": change = 3699]

---
+++ code/Spray/Ps3/Spu/JobPointRenderer.cpp
@@ -0,0 +1,19 @@
+#include <cell/dma.h>
+#include <cell/spurs/job_queue.h>
+
+//struct JobPointRenderer
+//{
+//	CellSpursJobHeader header;
+//
+//	struct Data
+//	{
+//		uintptr_t pointsEA;
+//		uint32_t pointsCount;
+//		uintptr_t vertexOutEA;
+//	};
+//
+//};
+
+void cellSpursJobQueueMain(CellSpursJobContext2* context, CellSpursJob256* job256)
+{
+}
