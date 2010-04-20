e00ffe33a0a099a56c8c5d58fb0b7988587a69c9
Anders Pistol <anders.pistol@doctorentertainment.com>
2010-04-20 08:53:21 +0100

    Traktor: Some null-pointer checks in sound system.
    [git-p4: depot-paths = "//depot/traktor/": change = 4104]

--- code/Sound/Sound.h
+++ code/Sound/Sound.h
@@ -7,9 +7,9 @@
 // import/export mechanism.
 #undef T_DLLCLASS
 #if defined(T_SOUND_EXPORT)
-#define T_DLLCLASS T_DLLEXPORT
+#	define T_DLLCLASS T_DLLEXPORT
 #else
-#define T_DLLCLASS T_DLLIMPORT
+#	define T_DLLCLASS T_DLLIMPORT
 #endif
 
 namespace traktor
@@ -29,7 +29,7 @@ class T_DLLCLASS Sound : public Object
 public:
 	Sound(ISoundBuffer* soundBuffer);
 
-	Ref< ISoundBuffer > getSoundBuffer();
+	ISoundBuffer* getSoundBuffer() const;
 
 private:
 	Ref< ISoundBuffer > m_soundBuffer;
