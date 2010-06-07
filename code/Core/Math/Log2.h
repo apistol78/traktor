4e3830c0bf21f12298fc7b82e7231e10bfbc4935
Anders Pistol <anders.pistol@doctorentertainment.com>
2010-06-07 09:36:57 +0100

    Traktor: Moved log2 functions into Core.
    [git-p4: depot-paths = "//depot/traktor/": change = 4968]

---
+++ code/Core/Math/Log2.h
@@ -0,0 +1,58 @@
+#ifndef traktor_Log2_H
+#define traktor_Log2_H
+
+#include "Core/Config.h"
+
+namespace traktor
+{
+
+/*! \brief Get log-2 value.
+ * \ingroup Core
+ */
+int32_t T_FORCE_INLINE log2(int32_t v)
+{
+	for (int i = 32; i >= 0; --i)
+	{
+		if (v & (1 << i))
+			return i;
+	}
+	return 0;
+}
+
+/*! \brief Check if value is a log-2.
+ * \ingroup Core
+ */
+bool T_FORCE_INLINE isLog2(int32_t v)
+{
+	return (1 << log2(v)) == v;
+}
+
+/*! \brief Get nearest log-2.
+ * \ingroup Core
+ */
+uint32_t T_FORCE_INLINE nearestLog2(uint32_t num)
+{
+	uint32_t n = num > 0 ? num - 1 : 0;
+
+	n |= n >> 1;
+	n |= n >> 2;
+	n |= n >> 4;
+	n |= n >> 8;
+	n |= n >> 16;
+	n++;
+
+	return n;
+}
+
+/*! \brief Get previous log-2.
+ * \ingroup Core
+ */
+uint32_t T_FORCE_INLINE previousLog2(uint32_t num)
+{
+	uint32_t lg2 = nearestLog2(num);
+	return lg2 > 1 ? (lg2 >> 1) : 1;
+}
+
+}
+
+#endif	// traktor_Log2_H
