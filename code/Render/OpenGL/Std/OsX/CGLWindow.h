7f9b1fff7e590f91f817061a1cf1644d2033cc8e
Anders Pistol <anders.pistol@doctorentertainment.com>
2010-07-01 21:07:07 +0100

    Traktor: Able to set resolution in OSX OpenGL.
    [git-p4: depot-paths = "//depot/traktor/": change = 5366]

--- code/Render/OpenGL/Std/OsX/CGLWindow.h
+++ code/Render/OpenGL/Std/OsX/CGLWindow.h
@@ -2,11 +2,18 @@
 #define traktor_render_CGLWindow_H
 
 #include "Core/Config.h"
+#include "Render/Types.h"
 
 namespace traktor
 {
 	namespace render
 	{
+	
+uint32_t cglwGetDisplayModeCount();
+
+bool cglwGetDisplayMode(uint32_t index, DisplayMode& outDisplayMode);
+
+bool cglwSetDisplayMode(const DisplayMode& displayMode);
 	
 void* cglwCreateWindow(const std::wstring& title, uint32_t width, uint32_t height, bool fullscreen);
 
