1916e7ecad340421ed80824ee5ae43c21dac0092
Anders Pistol <anders.pistol@doctorentertainment.com>
2010-07-20 16:14:16 +0100

    Traktor: Added trigger input filter.
    [git-p4: depot-paths = "//depot/traktor/": change = 5406]

---
+++ code/Input/Binding/InTrigger.h
@@ -0,0 +1,47 @@
+#ifndef traktor_input_InTrigger_H
+#define traktor_input_InTrigger_H
+
+#include "Input/Binding/IInputNode.h"
+
+// import/export mechanism.
+#undef T_DLLCLASS
+#if defined(T_INPUT_EXPORT)
+#	define T_DLLCLASS T_DLLEXPORT
+#else
+#	define T_DLLCLASS T_DLLIMPORT
+#endif
+
+namespace traktor
+{
+	namespace input
+	{
+
+class T_DLLCLASS InTrigger : public IInputNode
+{
+	T_RTTI_CLASS;
+	
+public:
+	enum Flank
+	{
+		FlPositive,
+		FlNegative
+	};
+	
+	InTrigger();
+
+	InTrigger(IInputNode* source, Flank flank, float duration);
+	
+	virtual InputValue evaluate(const InputValueSet& valueSet, float T, float dT, float currentStateValue) const;
+	
+	virtual bool serialize(ISerializer& s);
+	
+private:
+	Ref< IInputNode > m_source;
+	Flank m_flank;
+	float m_duration;
+};
+
+	}
+}
+
+#endif	// traktor_input_InTrigger_H
