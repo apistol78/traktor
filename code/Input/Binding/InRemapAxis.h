057d4d9d31c01b7a295a720d2ff5c459df39b859
Anders Pistol <anders.pistol@doctorentertainment.com>
2010-07-25 13:25:17 +0100

    Traktor: Added more input signal filters. Added some joystick support to PD input binding mockup.
    [git-p4: depot-paths = "//depot/traktor/": change = 5420]

---
+++ code/Input/Binding/InRemapAxis.h
@@ -0,0 +1,48 @@
+#ifndef traktor_input_InRemapAxis_H
+#define traktor_input_InRemapAxis_H
+
+#include "Input/Binding/IInputNode.h"
+
+// import/export mechanism.
+#undef T_DLLCLASS
+#if defined(T_INPUT_EXPORT)
+#	define T_DLLCLASS T_DLLEXPORT
+#else
+#	define T_DLLCLASS T_DLLIMPORT
+#endif
+
+namespace traktor
+{
+	namespace input
+	{
+
+class T_DLLCLASS InRemapAxis : public IInputNode
+{
+	T_RTTI_CLASS;
+	
+public:
+	InRemapAxis();
+
+	InRemapAxis(IInputNode* source, float limitMin, float limitMax, float outputMin, float outputMid, float outputMax);
+	
+	virtual Ref< Instance > createInstance() const;
+
+	virtual float evaluate(
+		Instance* instance,
+		const InputValueSet& valueSet,
+		float T,
+		float dT
+	) const;
+
+	virtual bool serialize(ISerializer& s);
+	
+private:
+	Ref< IInputNode > m_source;
+	float m_limit[2];
+	float m_output[3];
+};
+
+	}
+}
+
+#endif	// traktor_input_InRemapAxis_H
