78d2caffe5615daa1d5c1785c5baf5a6d5d4df94
Anders Pistol <anders.pistol@doctorentertainment.com>
2010-07-25 13:25:17 +0100

    Traktor: Added more input signal filters. Added some joystick support to PD input binding mockup.
    [git-p4: depot-paths = "//depot/traktor/": change = 5420]

---
+++ code/Input/Binding/InHysteresis.cpp
@@ -0,0 +1,79 @@
+#include "Core/Serialization/ISerializer.h"
+#include "Core/Serialization/Member.h"
+#include "Core/Serialization/MemberRef.h"
+#include "Core/Serialization/MemberStaticArray.h"
+#include "Input/Binding/InHysteresis.h"
+#include "Input/Binding/InputValueSet.h"
+#include "Input/Binding/ValueDigital.h"
+
+namespace traktor
+{
+	namespace input
+	{
+		namespace
+		{
+
+struct InHysteresisInstance : public RefCountImpl< IInputNode::Instance >
+{
+	Ref< IInputNode::Instance > sourceInstance;
+	float currentValue;
+};
+
+		}
+
+T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.input.InHysteresis", 0, InHysteresis, IInputNode)
+
+InHysteresis::InHysteresis()
+{
+	m_limit[0] = -1.0f;
+	m_limit[1] = 1.0f;
+	m_output[0] = -1.0f;
+	m_output[1] = 1.0f;
+}
+
+InHysteresis::InHysteresis(IInputNode* source, float limitMin, float limitMax, float outputMin, float outputMax)
+:	m_source(source)
+{
+	m_limit[0] = limitMin;
+	m_limit[1] = limitMax;
+	m_output[0] = outputMin;
+	m_output[1] = outputMax;
+}
+
+Ref< IInputNode::Instance > InHysteresis::createInstance() const
+{
+	Ref< InHysteresisInstance > instance = new InHysteresisInstance();
+	instance->sourceInstance = m_source->createInstance();
+	instance->currentValue = 0.0f;
+	return instance;
+}
+
+float InHysteresis::evaluate(
+	Instance* instance,
+	const InputValueSet& valueSet,
+	float T,
+	float dT
+) const
+{
+	InHysteresisInstance* ihi = static_cast< InHysteresisInstance* >(instance);
+
+	float V = m_source->evaluate(ihi->sourceInstance, valueSet, T, dT);
+
+	if (V > m_limit[1])
+		ihi->currentValue = m_output[1];
+	if (V < m_limit[0])
+		ihi->currentValue = m_output[0];
+
+	return ihi->currentValue;
+}
+
+bool InHysteresis::serialize(ISerializer& s)
+{
+	s >> MemberRef< IInputNode >(L"source", m_source);
+	s >> MemberStaticArray< float, 2 >(L"limit", m_limit);
+	s >> MemberStaticArray< float, 2 >(L"output", m_output);
+	return true;
+}
+	
+	}
+}
---
+++ code/Input/Binding/InRemapAxis.cpp
@@ -0,0 +1,79 @@
+#include "Core/Serialization/ISerializer.h"
+#include "Core/Serialization/Member.h"
+#include "Core/Serialization/MemberRef.h"
+#include "Core/Serialization/MemberStaticArray.h"
+#include "Input/Binding/InRemapAxis.h"
+#include "Input/Binding/InputValueSet.h"
+#include "Input/Binding/ValueDigital.h"
+
+namespace traktor
+{
+	namespace input
+	{
+		namespace
+		{
+
+struct InRemapAxisInstance : public RefCountImpl< IInputNode::Instance >
+{
+	Ref< IInputNode::Instance > sourceInstance;
+};
+
+		}
+
+T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.input.InRemapAxis", 0, InRemapAxis, IInputNode)
+
+InRemapAxis::InRemapAxis()
+{
+	m_limit[0] = -0.5f;
+	m_limit[1] = 0.5f;
+	m_output[0] = -1.0f;
+	m_output[1] = 0.0f;
+	m_output[2] = 1.0f;
+}
+
+InRemapAxis::InRemapAxis(IInputNode* source, float limitMin, float limitMax, float outputMin, float outputMid, float outputMax)
+:	m_source(source)
+{
+	m_limit[0] = limitMin;
+	m_limit[1] = limitMax;
+	m_output[0] = outputMin;
+	m_output[1] = outputMid;
+	m_output[2] = outputMax;
+}
+
+Ref< IInputNode::Instance > InRemapAxis::createInstance() const
+{
+	Ref< InRemapAxisInstance > instance = new InRemapAxisInstance();
+	instance->sourceInstance = m_source->createInstance();
+	return instance;
+}
+
+float InRemapAxis::evaluate(
+	Instance* instance,
+	const InputValueSet& valueSet,
+	float T,
+	float dT
+) const
+{
+	InRemapAxisInstance* ihi = static_cast< InRemapAxisInstance* >(instance);
+
+	float V = m_source->evaluate(ihi->sourceInstance, valueSet, T, dT);
+
+	if (V < m_limit[0])
+		return m_output[0];
+	if (V > m_limit[1])
+		return m_output[2];
+
+	return m_output[1];
+}
+
+bool InRemapAxis::serialize(ISerializer& s)
+{
+	s >> MemberRef< IInputNode >(L"source", m_source);
+	s >> MemberStaticArray< float, 2 >(L"limit", m_limit);
+	s >> MemberStaticArray< float, 3 >(L"output", m_output);
+	return true;
+}
+	
+	}
+}
