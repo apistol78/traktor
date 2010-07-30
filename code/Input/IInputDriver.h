f15da6f6d4360a37dbfc264da675c6f89802ffb5
Anders Pistol <anders.pistol@doctorentertainment.com>
2010-07-30 15:27:29 +0100

    Traktor: Some minor documentation updates in input classes.
    [git-p4: depot-paths = "//depot/traktor/": change = 5428]

--- code/Input/IInputDriver.h
+++ code/Input/IInputDriver.h
@@ -33,10 +33,33 @@ public:
 		UrFailure = -1
 	};
 	
+	/*! \brief Get number of devices implemented by the driver.
+	 *
+	 * \note Users should not call this method.
+	 *
+	 * \return Number of devices.
+	 */
 	virtual int getDeviceCount() = 0;
 
+	/*! \brief Get device from ordinal.
+	 *
+	 * \note Users should not call this method.
+	 *
+	 * \param index Device ordinal; must be in 0 to getDeviceCount()-1 range.
+	 * \return Device implementation.
+	 */
 	virtual Ref< IInputDevice > getDevice(int index) = 0;
 	
+	/*! \brief Update driver.
+	 *
+	 * During driver update devices might be connected or disconnected
+	 * thus the driver implementation must return UrDevicesChanged in
+	 * such case to let the input system update it's mapping.
+	 *
+	 * \note Users should not call this method.
+	 *
+	 * \return Update result.
+	 */
 	virtual UpdateResult update() = 0;
 };
 
