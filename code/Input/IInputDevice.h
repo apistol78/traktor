9b5fddd356d13f64c4a90a903a5b484112a7991a
Anders Pistol <anders.pistol@doctorentertainment.com>
2010-07-30 15:27:29 +0100

    Traktor: Some minor documentation updates in input classes.
    [git-p4: depot-paths = "//depot/traktor/": change = 5428]

--- code/Input/IInputDevice.h
+++ code/Input/IInputDevice.h
@@ -20,34 +20,105 @@ namespace traktor
 
 /*! \brief Input device base.
  * \ingroup Input
+ *
+ * Implementations of this class is created
+ * through IInputDriver class.
  */
 class T_DLLCLASS IInputDevice : public Object
 {
 	T_RTTI_CLASS;
 
 public:
+	/*! \brief Human readable name of device provided by system.
+	 *
+	 * \return Human readable name of device.
+	 */
 	virtual std::wstring getName() const = 0;
 
+	/*! \brief Get device category.
+	 *
+	 * Devices are identified by category
+	 * such as keyboard, joystick etc.
+	 *
+	 * \return Device category.
+	 */
 	virtual InputCategory getCategory() const = 0;
 
+	/*! \brief Query device if it's currently connected.
+	 *
+	 * Devices might become disconnected during
+	 * input system update and thus should
+	 * be queried first in order to ensure
+	 * the device is still connected.
+	 *
+	 * \return True if device is connected.
+	 */
 	virtual bool isConnected() const = 0;
 
+	/*! \brief Number of controls the device support.
+	 *
+	 * \return Number of controls.
+	 */
 	virtual int getControlCount() = 0;
 
+	/*! \brief Get human readable name of control.
+	 *
+	 * \param control Control identifier.
+	 * \return Human readable name of control.
+	 */
 	virtual std::wstring getControlName(int control) = 0;
 
+	/*! \brief Check if control is analogue.
+	 *
+	 * Digital controls always return either 0 or 1
+	 * even if the return type of getControlValue is float.
+	 *
+	 * \param control Control identifier.
+	 * \return True if control is analogue.
+	 */
 	virtual bool isControlAnalogue(int control) const = 0;
 
+	/*! \brief Get current value of a control.
+	 *
+	 * \param control Control identifier.
+	 * \return Control value.
+	 */
 	virtual float getControlValue(int control) = 0;
 
+	/*! \brief Get control identifier of a default control type.
+	 *
+	 * \param controlType Default control type.
+	 * \param control Output control identifier.
+	 * \return True if device support control type and have successfully mapped identifier.
+	 */
 	virtual bool getDefaultControl(InputDefaultControlType controlType, int& control) const = 0;
 
+	/*! \brief Reset device state.
+	 *
+	 * Device "state" are implementation specific but
+	 * should be reset into some sort of idle state which
+	 * represent "no buttons held" etc.
+	 */
 	virtual void resetState() = 0;
 
+	/*! \brief Read device state.
+	 *
+	 * Update device internal state; state should be preserved
+	 * and thus getControlValue should return same value
+	 * until next readState is called; ie no async state update allowed.
+	 */
 	virtual void readState() = 0;
 
+	/*! \brief Check to see if device support rumble.
+	 *
+	 * \return True if device support rumble.
+	 */
 	virtual bool supportRumble() const = 0;
 
+	/*! \brief Set device rumble parameters.
+	 *
+	 * \param rumble Rumble parameter configuration.
+	 */
 	virtual void setRumble(const InputRumble& rumble) = 0;
 };
 
