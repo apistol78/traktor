6407be980fa1486efd5cdbcbc641165431e50140
Anders Pistol <anders.pistol@doctorentertainment.com>
2010-10-05 13:12:28 +0100

    Traktor: Exposed key and main notification in Cocoa UI through NSWindowDelegateProxy.
    [git-p4: depot-paths = "//depot/traktor/": change = 6018]

--- code/Ui/Cocoa/NSWindowDelegateProxy.h
+++ code/Ui/Cocoa/NSWindowDelegateProxy.h
@@ -13,12 +13,20 @@ struct INSWindowEventsCallback
 	virtual void event_windowDidResize() = 0;
 	
 	virtual bool event_windowShouldClose() = 0;
+	
+	virtual void event_windowDidBecomeKey() = 0;
+	
+	virtual void event_windowDidResignKey() = 0;
+	
+	virtual void event_windowDidBecomeMain() = 0;
+	
+	virtual void event_windowDidResignMain() = 0;
 };
 
 	}
 }
 
-@interface NSWindowDelegateProxy : NSObject
+@interface NSWindowDelegateProxy : NSObject /*< NSWindowDelegate >*/
 {
 	traktor::ui::INSWindowEventsCallback* m_eventsCallback;
 }
@@ -32,5 +40,13 @@ struct INSWindowEventsCallback
 - (void) windowDidResize: (NSNotification*)notification;
 
 - (BOOL) windowShouldClose: (id)sender;
+
+- (void)windowDidBecomeKey: (NSNotification*)notification;
+
+- (void)windowDidResignKey: (NSNotification*)notification;
+
+- (void)windowDidBecomeMain: (NSNotification*)notification;
+
+- (void)windowDidResignMain: (NSNotification*)notification;
 
 @end
