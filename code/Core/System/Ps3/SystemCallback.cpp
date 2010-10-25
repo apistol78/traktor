8d8c833e57d58f71a5d019c09dc4675d3578d6e2
Anders Pistol <anders.pistol@doctorentertainment.com>
2010-10-25 15:29:36 +0100

    Traktor: Added a SystemCallback class to Core in order to have all system callbacks on PS3 in one place.
    [git-p4: depot-paths = "//depot/traktor/": change = 6169]

---
+++ code/Core/System/Ps3/SystemCallback.cpp
@@ -0,0 +1,53 @@
+#include <sysutil/sysutil_common.h>
+#include "Core/Singleton/SingletonManager.h"
+#include "Core/System/Ps3/SystemCallback.h"
+
+namespace traktor
+{
+
+SystemCallback& SystemCallback::getInstance()
+{
+	static SystemCallback* s_instance = 0;
+	if (!s_instance)
+	{
+		s_instance = new SystemCallback();
+		SingletonManager::getInstance().add(s_instance);
+	}
+	return *s_instance;
+}
+
+void SystemCallback::add(callback_t callback)
+{
+	m_callbacks.push_back(callback);
+}
+
+void SystemCallback::update()
+{
+	cellSysutilCheckCallback();
+}
+
+SystemCallback::SystemCallback()
+{
+	cellSysutilRegisterCallback(0, systemCallback, this);
+}
+
+SystemCallback::~SystemCallback()
+{
+	cellSysutilUnregisterCallback(0);
+}
+
+void SystemCallback::destroy()
+{
+	delete this;
+}
+
+void SystemCallback::systemCallback(uint64_t status, uint64_t param, void* userData)
+{
+	SystemCallback* this_ = static_cast< SystemCallback* >(userData);
+	T_ASSERT (this_);
+
+	for (std::vector< callback_t >::iterator i = this_->m_callbacks.begin(); i != this_->m_callbacks.end(); ++i)
+		(*i)(status, param);
+}
+
+}
---
+++ code/Core/System/Ps3/SystemCallback.h
@@ -0,0 +1,50 @@
+#ifndef traktor_SystemCallback_H
+#define traktor_SystemCallback_H
+
+#include "Core/Singleton/ISingleton.h"
+
+// import/export mechanism.
+#undef T_DLLCLASS
+#if defined(T_CORE_EXPORT)
+#	define T_DLLCLASS T_DLLEXPORT
+#else
+#	define T_DLLCLASS T_DLLIMPORT
+#endif
+
+namespace traktor
+{
+
+/*! \brief System callback manager.
+ * \ingroup Core
+ *
+ * \note
+ * This is only available for PS3 and it's
+ * peculiar system callbacks.
+ */
+class T_DLLCLASS SystemCallback : public ISingleton
+{
+public:
+	typedef void (*callback_t)(uint64_t status, uint64_t param);
+
+	static SystemCallback& getInstance();
+
+	void add(callback_t callback);
+
+	void update();
+
+protected:
+	SystemCallback();
+
+	virtual ~SystemCallback();
+
+	virtual void destroy();
+
+private:
+	std::vector< callback_t > m_callbacks;
+
+	static void systemCallback(uint64_t status, uint64_t param, void* userData);
+};
+
+}
+
+#endif	// traktor_SystemCallback_H
