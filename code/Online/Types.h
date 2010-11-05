9f236a40ab077d0a768d9dd54095abc99ddd586b
Anders Pistol <anders.pistol@doctorentertainment.com>
2010-11-05 11:47:17 +0100

    Traktor: Missing file.
    [git-p4: depot-paths = "//depot/traktor/": change = 6261]

---
+++ code/Online/Types.h
@@ -0,0 +1,20 @@
+#ifndef traktor_online_Types_H
+#define traktor_online_Types_H
+
+#include <string>
+
+namespace traktor
+{
+	namespace online
+	{
+
+struct SaveDataDesc
+{
+	std::wstring title;
+	std::wstring description;
+};
+
+	}
+}
+
+#endif	// traktor_online_Types_H
