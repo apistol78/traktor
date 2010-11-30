8e0910922835a891260d38187e89b047ccdff110
Anders Pistol <anders.pistol@doctorentertainment.com>
2010-11-30 20:55:07 +0100

    Traktor: Implemented a resource factory to create single entity data objects as resources.
    [git-p4: depot-paths = "//depot/traktor/": change = 6405]

---
+++ code/World/Entity/EntityResourceFactory.cpp
@@ -0,0 +1,35 @@
+#include "Database/Database.h"
+#include "World/Entity/EntityData.h"
+#include "World/Entity/EntityResourceFactory.h"
+
+namespace traktor
+{
+	namespace world
+	{
+
+T_IMPLEMENT_RTTI_CLASS(L"traktor.world.EntityResourceFactory", EntityResourceFactory, resource::IResourceFactory)
+
+EntityResourceFactory::EntityResourceFactory(db::Database* db)
+:	m_db(db)
+{
+}
+
+const TypeInfoSet EntityResourceFactory::getResourceTypes() const
+{
+	TypeInfoSet typeSet;
+	typeSet.insert(&type_of< EntityData >());
+	return typeSet;
+}
+
+bool EntityResourceFactory::isCacheable() const
+{
+	return true;
+}
+
+Ref< Object > EntityResourceFactory::create(resource::IResourceManager* resourceManager, const TypeInfo& resourceType, const Guid& guid)
+{
+	return m_db->getObjectReadOnly< EntityData >(guid);
+}
+
+	}
+}
---
+++ code/World/Entity/EntityResourceFactory.h
@@ -0,0 +1,49 @@
+#ifndef traktor_world_EntityResourceFactory_H
+#define traktor_world_EntityResourceFactory_H
+
+#include "Resource/IResourceFactory.h"
+
+// import/export mechanism.
+#undef T_DLLCLASS
+#if defined(T_WORLD_EXPORT)
+#	define T_DLLCLASS T_DLLEXPORT
+#else
+#	define T_DLLCLASS T_DLLIMPORT
+#endif
+
+namespace traktor
+{
+	namespace db
+	{
+
+class Database;
+
+	}
+
+	namespace world
+	{
+
+/*! \brief Single entity resource factory.
+ * \ingroup World
+ */
+class T_DLLCLASS EntityResourceFactory : public resource::IResourceFactory
+{
+	T_RTTI_CLASS;
+
+public:
+	EntityResourceFactory(db::Database* db);
+
+	virtual const TypeInfoSet getResourceTypes() const;
+
+	virtual bool isCacheable() const;
+
+	virtual Ref< Object > create(resource::IResourceManager* resourceManager, const TypeInfo& resourceType, const Guid& guid);
+
+private:
+	Ref< db::Database > m_db;
+};
+
+	}
+}
+
+#endif	// traktor_world_EntityResourceFactory_H
