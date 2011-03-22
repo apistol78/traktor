02470b9320abd818011fa152ed8fa143d4aac6a2
Anders Pistol <anders.pistol@doctorentertainment.com>
2011-03-22 13:24:13 +0100

    Traktor: Enable/disable scene controller update.
    [git-p4: depot-paths = "//depot/traktor/": change = 6723]

--- code/Scene/Scene.h
+++ code/Scene/Scene.h
@@ -53,7 +53,7 @@ public:
 
 	void destroy();
 
-	void update(float time, float deltaTime);
+	void update(float time, float deltaTime, bool updateController = true);
 
 	void build(world::IWorldRenderer* worldRenderer, world::WorldRenderView& worldRenderView, int frame);
 
