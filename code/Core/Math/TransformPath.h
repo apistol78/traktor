2c24ff5798379693473a4c52428aab3a72e94e59
Anders Pistol <anders.pistol@doctorentertainment.com>
2011-03-27 21:36:47 +0100

    Traktor: Parameterized velocity in transform path.
    [git-p4: depot-paths = "//depot/traktor/": change = 6754]

--- code/Core/Math/TransformPath.h
+++ code/Core/Math/TransformPath.h
@@ -90,6 +90,7 @@ private:
 	AlignedVector< Key > m_keys;
 	mutable AutoPtr< ISpline< Key, Scalar, Frame > > m_spline;
 	mutable std::vector< float > m_arcLengths;
+	mutable std::vector< std::pair< float, float > > m_timeCurve;
 	mutable bool m_loop;
 };
 
