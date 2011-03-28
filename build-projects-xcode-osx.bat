f6a488e9f7f6ed20210a9c270efb9df32bbe3a60
Anders Pistol <anders.pistol@doctorentertainment.com>
2011-03-28 12:32:25 +0100

    Traktor: New OSX remote tools binaries.
    [git-p4: depot-paths = "//depot/traktor/": change = 6757]

--- build-projects-xcode-osx.bat
+++ build-projects-xcode-osx.bat
@@ -1,3 +1,10 @@
 @echo off
 call %~dp0config.bat
-%TRAKTOR_HOME%\bin\solutionbuilder -f=xcode TraktorMacOSX.xms -d=DebugShared -r=ReleaseShared -a
+if "%1"=="static" (
+	echo Static solution
+	%TRAKTOR_HOME%\bin\solutionbuilder -f=xcode TraktorMacOSX.xms -d=DebugStatic -r=ReleaseStatic -a
+) else (
+	echo Shared solution
+	%TRAKTOR_HOME%\bin\solutionbuilder -f=xcode TraktorMacOSX.xms -d=DebugShared -r=ReleaseShared -a
+)
+
