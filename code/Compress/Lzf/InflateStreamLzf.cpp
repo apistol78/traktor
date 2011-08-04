07e25effdf11f74637108e75d45292ec3d2d96cf
Anders Pistol <anders.pistol@doctorentertainment.com>
2011-08-04 10:30:43 +0100

    Traktor: Implemented seeking in LZF streams.
    [git-p4: depot-paths = "//depot/traktor/": change = 7010]

--- code/Compress/Lzf/InflateStreamLzf.cpp
+++ code/Compress/Lzf/InflateStreamLzf.cpp
@@ -20,6 +20,8 @@ public:
 	,	m_compressedBlock(blockSize + blockSize / 16 + 64 + 3)
 	,	m_decompressedBuffer(blockSize)
 	,	m_decompressedBufferSize(0)
+	,	m_startPosition(stream->tell())
+	,	m_position(m_startPosition)
 	{
 	}
 
@@ -49,7 +51,10 @@ public:
 		}
 
 		if (nbytes <= 0)
+		{
+			m_position += int32_t(ptr - top);
 			return int32_t(ptr - top);
+		}
 
 		T_ASSERT (m_decompressedBufferSize == 0);
 
@@ -160,7 +165,34 @@ public:
 			nbytes -= ncopy;
 		}
 
+		m_position += int32_t(ptr - top);
 		return int32_t(ptr - top);
+	}
+
+	int setLogicalPosition(int position)
+	{
+		// Seeking backwards, restart from beginning.
+		if (position < m_position)
+		{
+			m_stream->seek(IStream::SeekSet, m_startPosition);
+			m_decompressedBufferSize = 0;
+		}
+
+		// Read dummy blocks until we're at the desired position.
+		uint8_t dummy[1024];
+		while (m_position < position)
+		{
+			int nread = read(dummy, std::min< int >(sizeof_array(dummy), position - m_position));
+			if (nread <=  0)
+				return -1;
+		}
+
+		return m_position;
+	}
+
+	int getLogicalPosition() const
+	{
+		return m_position;
 	}
 
 private:
@@ -168,6 +200,8 @@ private:
 	AlignedVector< uint8_t > m_compressedBlock;
 	AlignedVector< uint8_t > m_decompressedBuffer;
 	int32_t m_decompressedBufferSize;
+	int32_t m_startPosition;
+	int32_t m_position;
 };
 
 T_IMPLEMENT_RTTI_CLASS(L"traktor.compress.InflateStreamLzf", InflateStreamLzf, IStream)
@@ -203,13 +237,12 @@ bool InflateStreamLzf::canWrite() const
 
 bool InflateStreamLzf::canSeek() const
 {
-	return false;
+	return true;
 }
 
 int InflateStreamLzf::tell() const
 {
-	T_FATAL_ERROR;
-	return 0;
+	return m_impl->getLogicalPosition();
 }
 
 int InflateStreamLzf::available() const
@@ -220,8 +253,10 @@ int InflateStreamLzf::available() const
 
 int InflateStreamLzf::seek(SeekOriginType origin, int offset)
 {
-	T_FATAL_ERROR;
-	return 0;
+	T_ASSERT_M (origin != SeekEnd, L"SeekEnd is not allowed");
+	if (origin == SeekCurrent)
+		offset += m_impl->getLogicalPosition();
+	return m_impl->setLogicalPosition(offset);
 }
 
 int InflateStreamLzf::read(void* block, int nbytes)
