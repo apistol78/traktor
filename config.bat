86a7408e926bb1357b7334898324cb01a1dac6b1
Anders Pistol <anders.pistol@doctorentertainment.com>
2011-08-09 10:20:48 +0100

    Traktor: Changed to SteamWorks SDK 1.16
    [git-p4: depot-paths = "//depot/traktor/": change = 7043]

--- config.bat
+++ config.bat
@@ -1,10 +1,12 @@
 @echo off
 
 set TRAKTOR_HOME=%~dp0
+set STEAMWORKS_SDK=%TRAKTOR_HOME%3rdp\steamworks-sdk-116
 
 set PATH=%PATH%;%TRAKTOR_HOME%build\win32\releaseshared
 set PATH=%PATH%;%TRAKTOR_HOME%build\win32\debugshared
 
+set PATH=%PATH%;%STEAMWORKS_SDK%\redistributable_bin
 set PATH=%PATH%;%TRAKTOR_HOME%3rdp\POWERVR SDK\OGLES2_WINDOWS_PCEMULATION_2.07.27.0484\Builds\OGLES2\WindowsPC\Lib
 set PATH=%PATH%;%TRAKTOR_HOME%3rdp\POWERVR SDK\OGLES2_WINDOWS_PCEMULATION_2.07.27.0484\Utilities\PVRTC\dll
 set PATH=%PATH%;%SCE_PS3_ROOT%\host-win32\Cg\bin
