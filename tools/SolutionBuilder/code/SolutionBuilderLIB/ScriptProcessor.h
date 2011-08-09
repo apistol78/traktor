04a226a4284fab37f7e87cc879d7c5a6930a5e06
Anders Pistol <anders.pistol@doctorentertainment.com>
2011-08-09 12:07:40 +0100

    Traktor: Added preliminary Eclipse support to SolutionBuilder. Xcode generator now copies local dependencies for application bundles.
    [git-p4: depot-paths = "//depot/traktor/": change = 7046]

---
+++ tools/SolutionBuilder/code/SolutionBuilderLIB/ScriptProcessor.h
@@ -0,0 +1,27 @@
+#ifndef ScriptProcessor_H
+#define ScriptProcessor_H
+
+#include <Core/Ref.h>
+#include <Script/IScriptManager.h>
+
+class Project;
+class Solution;
+
+class ScriptProcessor : public traktor::Object
+{
+	T_RTTI_CLASS;
+
+public:
+	bool create();
+
+	void destroy();
+
+	bool generateFromFile(const Solution* solution, const Project* project, const std::wstring& projectPath, const std::wstring& fileName, std::wstring& output) const;
+
+	bool generateFromSource(const Solution* solution, const Project* project, const std::wstring& projectPath, const std::wstring& source, std::wstring& output) const;
+
+private:
+	traktor::Ref< traktor::script::IScriptManager > m_scriptManager;
+};
+
+#endif	// ScriptProcessor_H
