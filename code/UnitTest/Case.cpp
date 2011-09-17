4a38bc4982cd9155487545b2ea4f12c521820253
Anders Pistol <anders.pistol@doctorentertainment.com>
2011-09-17 10:20:15 +0100

    Traktor: UnitTest fixes and improvements, showing stats at the end and return proper exit code if failing.
    [git-p4: depot-paths = "//depot/traktor/": change = 7183]

--- code/UnitTest/Case.cpp
+++ code/UnitTest/Case.cpp
@@ -4,13 +4,21 @@
 namespace traktor
 {
 
+void Case::execute(Context& context)
+{
+	m_context = &context;
+	run();
+}
+
 void Case::succeeded(const std::wstring& message)
 {
+	m_context->succeeded++;
 	log::info << message << Endl;
 }
 
 void Case::failed(const std::wstring& message)
 {
+	m_context->failed++;
 	log::error << message << Endl;
 }
 
