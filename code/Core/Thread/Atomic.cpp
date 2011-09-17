fab0e0d116e6b6df1be5c2b72ed1b23842c008d2
Anders Pistol <anders.pistol@doctorentertainment.com>
2011-09-17 22:51:40 +0100

    Traktor: Atomic class not inlined in debug; less compile time included symbols.
    [git-p4: depot-paths = "//depot/traktor/": change = 7184]

---
+++ code/Core/Thread/Atomic.cpp
@@ -0,0 +1,12 @@
+#include "Core/Thread/Atomic.h"
+#if defined(_DEBUG)
+#	if defined(_WIN32)
+#		include "Core/Thread/Win32/Atomic.inl"
+#	elif defined(__APPLE__)
+#		include "Core/Thread/OsX/Atomic.inl"
+#	elif defined(_PS3)
+#		include "Core/Thread/Ps3/Atomic.inl"
+#	else
+#		include "Core/Thread/Linux/Atomic.inl"
+#	endif
+#endif
