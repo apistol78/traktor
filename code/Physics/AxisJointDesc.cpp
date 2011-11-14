bbc007c31709370ccd6bbdd3a44d490d95c457a5
Anders Pistol <anders.pistol@doctorentertainment.com>
2011-11-14 22:13:33 +0100

    Traktor: Implemented Hinge2 joint in Bullet.
    [git-p4: depot-paths = "//depot/traktor/": change = 7327]

--- code/Physics/AxisJointDesc.cpp
+++ code/Physics/AxisJointDesc.cpp
@@ -1,5 +1,6 @@
 #include "Core/Math/Const.h"
 #include "Core/Serialization/AttributeDirection.h"
+#include "Core/Serialization/AttributePoint.h"
 #include "Core/Serialization/ISerializer.h"
 #include "Core/Serialization/Member.h"
 #include "Physics/AxisJointDesc.h"
@@ -12,8 +13,19 @@ namespace traktor
 T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.physics.AxisJointDesc", 0, AxisJointDesc, JointDesc)
 
 AxisJointDesc::AxisJointDesc()
-:	m_axis(1.0f, 0.0f, 0.0f, 0.0f)
+:	m_anchor(0.0f, 0.0f, 0.0f, 1.0f)
+,	m_axis(1.0f, 0.0f, 0.0f, 0.0f)
 {
+}
+
+void AxisJointDesc::setAnchor(const Vector4& anchor)
+{
+	m_anchor = anchor;
+}
+
+const Vector4& AxisJointDesc::getAnchor() const
+{
+	return m_anchor;
 }
 
 void AxisJointDesc::setAxis(const Vector4& axis)
@@ -28,6 +40,7 @@ const Vector4& AxisJointDesc::getAxis() const
 
 bool AxisJointDesc::serialize(ISerializer& s)
 {
+	s >> Member< Vector4 >(L"anchor", m_anchor, AttributePoint());
 	s >> Member< Vector4 >(L"axis", m_axis, AttributeDirection());
 	return true;
 }
