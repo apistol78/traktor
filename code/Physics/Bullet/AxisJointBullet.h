445d438e51c0d49773cae70649b0be222ade8b6a
Anders Pistol <anders.pistol@doctorentertainment.com>
2011-11-14 22:14:20 +0100

    Traktor: Implemented axis joint in Bullet physics.
    [git-p4: depot-paths = "//depot/traktor/": change = 7328]

---
+++ code/Physics/Bullet/AxisJointBullet.h
@@ -0,0 +1,36 @@
+#ifndef traktor_physics_AxisJointBullet_H
+#define traktor_physics_AxisJointBullet_H
+
+#include "Physics/AxisJoint.h"
+#include "Physics/Bullet/JointBullet.h"
+
+// import/export mechanism.
+#undef T_DLLCLASS
+#if defined(T_PHYSICS_BULLET_EXPORT)
+#	define T_DLLCLASS T_DLLEXPORT
+#else
+#	define T_DLLCLASS T_DLLIMPORT
+#endif
+
+namespace traktor
+{
+	namespace physics
+	{
+
+/*!
+ * \ingroup Bullet
+ */
+class T_DLLCLASS AxisJointBullet : public JointBullet< AxisJoint, btHingeConstraint >
+{
+	T_RTTI_CLASS;
+
+public:
+	AxisJointBullet(IWorldCallback* callback, btHingeConstraint* constraint, Body* body1, Body* body2);
+
+	virtual Vector4 getAxis() const;
+};
+
+	}
+}
+
+#endif	// traktor_physics_AxisJointBullet_H
