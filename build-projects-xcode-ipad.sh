3676f343ca0016d1e721d68ea6b54d00d9ec82a1
Anders Pistol <anders.pistol@doctorentertainment.com>
2011-12-18 21:26:14 +0100

    Traktor: No longer have to remember exporting environment variables before generating solution files in OSX.
    [git-p4: depot-paths = "//depot/traktor/": change = 7429]

--- build-projects-xcode-ipad.sh
+++ build-projects-xcode-ipad.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-export TRAKTOR_HOME=$PWD
+# Source environment configuration.
+. "`dirname \"$BASH_SOURCE\"`/config.sh"
 
+# Ensure correct path and build solution files.
+pushd $TRAKTOR_HOME
 $TRAKTOR_HOME/bin/osx/SolutionBuilder -f=xcode -xcode-root-suffix=-static -i=ipad TraktorIPad.xms -d=DebugStatic -r=ReleaseStatic
+popd
--- config.sh
+++ config.sh
@@ -1,1 +1,7 @@
 #!/bin/sh
+
+# Get normalized path to this script, excluding file name.
+TRAKTOR_HOME="`dirname \"$BASH_SOURCE\"`"
+TRAKTOR_HOME="`(cd \"$TRAKTOR_HOME\" && pwd)`"
+
+export TRAKTOR_HOME
