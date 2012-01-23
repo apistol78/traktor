540f764fdcb8b4570680a88c467f77ac243d58c5
Anders Pistol <anders.pistol@doctorentertainment.com>
2012-01-23 20:43:30 +0100

    Traktor: Implemented shared memory in OSX by using memory mapped files as "true" shared memory on OSX are globally capped to 4 MiB.
    [git-p4: depot-paths = "//depot/traktor/": change = 7529]

--- code/Core/System/OsX/SharedMemoryOsX.cpp
+++ code/Core/System/OsX/SharedMemoryOsX.cpp
@@ -1,5 +1,10 @@
-#include "Core/System/OsX/SharedMemoryOsX.h"
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "Core/Io/MemoryStream.h"
+#include "Core/Misc/AutoPtr.h"
+#include "Core/System/OsX/SharedMemoryOsX.h"
 
 namespace traktor
 {
@@ -7,19 +12,48 @@ namespace traktor
 T_IMPLEMENT_RTTI_CLASS(L"traktor.SharedMemoryOsX", SharedMemoryOsX, ISharedMemory)
 
 SharedMemoryOsX::SharedMemoryOsX(uint32_t size)
-:	m_data(new uint8_t [size])
-,	m_size(size)
+:	m_size(size)
 {
+	int32_t handle = open("/tmp/traktor.shm", O_RDWR);
+	if (handle == -1)
+	{
+		handle = open("/tmp/traktor.shm", O_CREAT | O_RDWR, 0666);
+		if (handle != -1)
+		{
+			AutoArrayPtr< uint8_t > dummy(new uint8_t [1024]);
+			for (uint32_t i = 0; i < (size + 1023) / 1024; ++i)
+				::write(handle, dummy.ptr(), 1024);
+
+			close(handle);
+
+			handle = open("/tmp/traktor.shm", O_RDWR);
+		}
+	}
+
+	if (handle != -1)
+	{
+		m_buffer = mmap(0, m_size, PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, handle, 0);
+		close(handle);
+	}
+}
+
+SharedMemoryOsX::~SharedMemoryOsX()
+{
+	if (m_buffer)
+	{
+		munmap(m_buffer, m_size);
+		m_buffer = 0;
+	}
 }
 	
 Ref< IStream > SharedMemoryOsX::read(bool exclusive)
 {
-	return new MemoryStream(m_data.ptr(), m_size, true, false);
+	return new MemoryStream(m_buffer, m_size, true, false);
 }
 	
 Ref< IStream > SharedMemoryOsX::write()
 {
-	return new MemoryStream(m_data.ptr(), m_size, false, true);
+	return new MemoryStream(m_buffer, m_size, false, true);
 }
 
 }
