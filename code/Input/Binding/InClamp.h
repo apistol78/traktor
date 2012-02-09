56a029443ad9eb2aa637cb9c99c774651cfed5ff
Anders Pistol <anders.pistol@doctorentertainment.com>
2012-02-09 13:42:17 +0100

    Traktor: Clamp input node added to input binding.
    [git-p4: depot-paths = "//depot/traktor/": change = 7602]

---
+++ code/Input/Binding/InClamp.h
@@ -0,0 +1,47 @@
+#ifndef traktor_input_InClamp_H
+#define traktor_input_InClamp_H
+
+#include "Input/Binding/IInputNode.h"
+
+// import/export mechanism.
+#undef T_DLLCLASS
+#if defined(T_INPUT_EXPORT)
+#	define T_DLLCLASS T_DLLEXPORT
+#else
+#	define T_DLLCLASS T_DLLIMPORT
+#endif
+
+namespace traktor
+{
+	namespace input
+	{
+
+class T_DLLCLASS InClamp : public IInputNode
+{
+	T_RTTI_CLASS;
+	
+public:
+	InClamp();
+
+	InClamp(IInputNode* source, float limitMin, float limitMax);
+	
+	virtual Ref< Instance > createInstance() const;
+
+	virtual float evaluate(
+		Instance* instance,
+		const InputValueSet& valueSet,
+		float T,
+		float dT
+	) const;
+
+	virtual bool serialize(ISerializer& s);
+	
+private:
+	Ref< IInputNode > m_source;
+	float m_limit[2];
+};
+
+	}
+}
+
+#endif	// traktor_input_InClamp_H
