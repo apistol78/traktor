092cbed7780e9f69161ae59845e9bd24b0edfe63
Anders Pistol <anders.pistol@doctorentertainment.com>
2012-02-15 12:39:54 +0100

    Traktor: Improved remote deploy tool. Fixed missing static reference to InClamp in Amalgam. Disable cube map lighting in OpenGL ES2 path.
    [git-p4: depot-paths = "//depot/traktor/": change = 7618]

--- bin/deploy/win32/deploy-ios.bat
+++ bin/deploy/win32/deploy-ios.bat
@@ -6,15 +6,24 @@ if "%1"=="build" (
 
 ) else if "%1"=="deploy" (
 
-	echo "Not available"
+	rem Deploy iPhone simulator launcher.
+	pushd %TRAKTOR_HOME%\bin\osx
+	%TRAKTOR_HOME%\bin\RemoteDeploy %DEPLOY_TARGET_HOST% waxsim > %DEPLOY_PROJECTROOT%\deploy.log
+	popd
+
+	rem Deploy iPhone binaries.
+	pushd %DEPLOY_PROJECTROOT:/=\%\bin\latest\iphone-simulator\releasestatic
+	%TRAKTOR_HOME%\bin\RemoteDeploy -recursive %DEPLOY_TARGET_HOST% *.* >> %DEPLOY_PROJECTROOT%\deploy.log
+	popd
 	
 ) else if "%1"=="launch" (
 
-	echo "Not available"
+	rem Launch application with simulator launcher on target.
+	%TRAKTOR_HOME%\bin\RemoteDeploy -target-base="%DEPLOY_EXECUTABLE%.app" %DEPLOY_TARGET_HOST% Application.config > %DEPLOY_PROJECTROOT%\launch.log
+	%TRAKTOR_HOME%\bin\RemoteLaunch %DEPLOY_TARGET_HOST% waxsim "%DEPLOY_EXECUTABLE%.app" >> %DEPLOY_PROJECTROOT%\launch.log
 
 ) else if "%1"=="migrate" (
 
 	%DEPLOY_PROJECTROOT%\bin\latest\win32\releaseshared\Traktor.Database.Migrate.App %DEPLOY_SOURCE_CS% %DEPLOY_TARGET_CS% %DEPLOY_MODULES% > %DEPLOY_PROJECTROOT%\migrate.log
 	
 )
-
