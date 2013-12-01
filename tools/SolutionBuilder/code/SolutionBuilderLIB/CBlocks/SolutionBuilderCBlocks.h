da9dea1b0007018bb25c10d281b9dc3c763ab07d
Jesper Rudberg <jesper.rudberg@doctorentertainment.com>
2013-12-01 19:00:22 +0100

    Gear Up: Integrated latest from dev.
    [git-p4: depot-paths = "//depot/traktor/": change = 14197]

--- tools/SolutionBuilder/code/SolutionBuilderLIB/CBlocks/SolutionBuilderCBlocks.h
+++ tools/SolutionBuilder/code/SolutionBuilderLIB/CBlocks/SolutionBuilderCBlocks.h
@@ -4,9 +4,6 @@
 #include <Core/Io/Path.h>
 #include "SolutionBuilderLIB/SolutionBuilder.h"
 
-//class Project;
-//class ProjectItem;
-//class Solution;
 class ScriptProcessor;
 
 /*! Generate solution files for Code::Blocks. */
@@ -17,6 +14,8 @@ class SolutionBuilderCBlocks : public SolutionBuilder
 public:
 	SolutionBuilderCBlocks();
 
+	virtual ~SolutionBuilderCBlocks();
+
 	virtual bool create(const traktor::CommandLine& cmdLine);
 
 	virtual bool generate(Solution* solution);
@@ -25,11 +24,6 @@ public:
 
 private:
 	traktor::Ref< ScriptProcessor > m_scriptProcessor;
-	//std::wstring m_compiler;
-
-	//void collectEmbedItems(const Solution* solution, const Project* project, const ProjectItem* item, std::set< traktor::Path >& outEmbedItems) const;
-
-	//void addItem(const Solution* solution, const Project* project, const ProjectItem* item, traktor::OutputStream& os) const;
 };
 
 #endif	// SolutionBuilderCBlocks_H
