8d805586906e73af9dd28dd42cf8e2aa095e8fd1
Anders Pistol <anders.pistol@doctorentertainment.com>
2014-04-10 10:19:34 +0100

    Gear Up: Dev->Stable.
    [git-p4: depot-paths = "//depot/traktor/": change = 15208]

--- code/Net/Replication/Pack.h
+++ code/Net/Replication/Pack.h
@@ -8,9 +8,28 @@ namespace traktor
 	namespace net
 	{
 
-void packUnit(const Vector4& u, uint8_t out[3]);
+//void packUnit(const Vector4& u, uint8_t out[3]);
+//
+//Vector4 unpackUnit(const uint8_t u[3]);
 
-Vector4 unpackUnit(const uint8_t u[3]);
+class PackedUnitVector
+{
+public:
+	PackedUnitVector();
+
+	PackedUnitVector(const Vector4& v);
+
+	PackedUnitVector(uint16_t v);
+
+	void pack(const Vector4& v);
+
+	Vector4 unpack() const;
+
+	uint16_t raw() const { return m_v; }
+
+private:
+	uint16_t m_v;
+};
 
 	}
 }
