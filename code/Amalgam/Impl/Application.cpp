c48f34a0d0fdc7ac67c5d5944826d2ac97d6ac38
Anders Pistol <anders.pistol@doctorentertainment.com>
2014-05-20 14:04:05 +0100

    Gear Up: Integrated latest stuff from dev.
    [git-p4: depot-paths = "//depot/traktor/": change = 15651]

--- code/Amalgam/Impl/Application.cpp
+++ code/Amalgam/Impl/Application.cpp
@@ -56,7 +56,7 @@ const int32_t c_databasePollInterval = 5;
 const uint32_t c_simulationFrequency = 60;
 const float c_simulationDeltaTime = 1.0f / c_simulationFrequency;
 const float c_maxDeltaTime = 1.0f / 15.0f;
-const int32_t c_maxDeltaTimeErrors = 50;
+const int32_t c_maxDeltaTimeErrors = 120;
 const float c_deltaTimeFilterCoeff = 0.99f;
 
 		}
@@ -583,11 +583,20 @@ bool Application::update()
 
 		// Measure delta time.
 		float deltaTime = float(m_timer.getDeltaTime());
-		if (!m_updateControl.m_pause && deltaTime > c_maxDeltaTime)
+		if (deltaTime > c_maxDeltaTime)
 		{
 			deltaTime = c_maxDeltaTime;
-			if (++m_deltaTimeError >= c_maxDeltaTimeErrors)
-				m_updateInfo.m_runningSlow = true;
+			if (renderViewActive && !m_updateControl.m_pause)
+			{
+				if (++m_deltaTimeError >= c_maxDeltaTimeErrors)
+					m_updateInfo.m_runningSlow = true;
+			}
+			else
+			{
+				// In case view isn't active this measurement cannot be fully trusted.
+				m_deltaTimeError = 0;
+				m_updateInfo.m_runningSlow = false;
+			}
 		}
 		else
 		{
