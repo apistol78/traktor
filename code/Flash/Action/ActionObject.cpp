956d1be4a5bceeda3607f48a4c54fe4dfd3f5889
Jesper Rudberg <jesper.rudberg@doctorentertainment.com>
2014-05-28 14:55:51 +0100

    Gear Up: Null pnt check.
    [git-p4: depot-paths = "//depot/traktor/": change = 15755]

--- code/Flash/Action/ActionObject.cpp
+++ code/Flash/Action/ActionObject.cpp
@@ -74,7 +74,7 @@ ActionObject* ActionObject::get__proto__()
 		}
 
 		// No prototype defined; assume plain object.
-		if (!m__proto__)
+		if (!m__proto__ && m_context)
 		{
 			m__proto__ = m_context->lookupClass("Object");
 			if (m__proto__)
