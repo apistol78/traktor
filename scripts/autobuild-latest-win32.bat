49e2f739609962ac2b62e9b55799682062cd2f60
Jesper Rudberg <jesper.rudberg@doctorentertainment.com>
2014-08-13 14:02:20 +0100

    Traktor: Updated to Visual studio 2012.
    [git-p4: depot-paths = "//depot/traktor/": change = 16448]

--- scripts/autobuild-latest-win32.bat
+++ scripts/autobuild-latest-win32.bat
@@ -1,7 +1,7 @@
 @echo off
 
 rem Setup VC environment variables.
-call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
 
 rem Setup our build environment.
 call %~dp0../config.bat
@@ -11,7 +11,7 @@ del /F /Q %~dp0autobuild-win32.log
 
 rem Rebuild entire solution.
 pushd %TRAKTOR_HOME%
-call build-projects-vs2008-win32.bat
+call build-projects-vs2012-win32.bat
 devenv "build\win32\Traktor Win32.sln" /Build DebugShared /Out %~dp0autobuild-win32.log
 devenv "build\win32\Traktor Win32.sln" /Build ReleaseShared /Out %~dp0autobuild-win32.log
 popd
