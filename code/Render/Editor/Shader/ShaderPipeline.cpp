aa979427f632299c996714aa21385b4ea5ee7484
Jesper Rudberg <jesper.rudberg@doctorentertainment.com>
2014-09-08 13:58:56 +0100

    Traktor: Debug compile fix.
    [git-p4: depot-paths = "//depot/traktor/": change = 16751]

--- code/Render/Editor/Shader/ShaderPipeline.cpp
+++ code/Render/Editor/Shader/ShaderPipeline.cpp
@@ -2,6 +2,7 @@
 #include "Core/Io/FileSystem.h"
 #include "Core/Io/IStream.h"
 #include "Core/Log/Log.h"
+#include "Core/Math/Const.h"
 #include "Core/Misc/String.h"
 #include "Core/Misc/WildCompare.h"
 #include "Core/Serialization/MemberComposite.h"
