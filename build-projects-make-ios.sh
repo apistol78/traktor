9c8040b9900acbf1ae15cddd0312265e4b2b89b4
Anders Pistol <anders.pistol@doctorentertainment.com>
2014-10-21 11:09:56 +0100

    Traktor: Enabled 64-bit builds of iOS.
    [git-p4: depot-paths = "//depot/traktor/": change = 17477]

--- build-projects-make-ios.sh
+++ build-projects-make-ios.sh
@@ -6,6 +6,6 @@
 # Build solution files.
 $TRAKTOR_HOME/bin/osx/SolutionBuilder -f=make -make-dialect=gnu -make-platform=ios -make-configuration=$TRAKTOR_HOME/bin/make-config-ios-armv7.inc -make-root-suffix=-armv7 TraktorIOS.xms
 $TRAKTOR_HOME/bin/osx/SolutionBuilder -f=make -make-dialect=gnu -make-platform=ios -make-configuration=$TRAKTOR_HOME/bin/make-config-ios-armv7s.inc -make-root-suffix=-armv7s TraktorIOS.xms
-#$TRAKTOR_HOME/bin/osx/SolutionBuilder -f=make -make-dialect=gnu -make-platform=ios -make-configuration=$TRAKTOR_HOME/bin/make-config-ios-arm64.inc -make-root-suffix=-arm64 TraktorIOS.xms
+$TRAKTOR_HOME/bin/osx/SolutionBuilder -f=make -make-dialect=gnu -make-platform=ios -make-configuration=$TRAKTOR_HOME/bin/make-config-ios-arm64.inc -make-root-suffix=-arm64 TraktorIOS.xms
 $TRAKTOR_HOME/bin/osx/SolutionBuilder -f=make -make-dialect=gnu -make-platform=ios -make-configuration=$TRAKTOR_HOME/bin/make-config-ios-i386.inc -make-root-suffix=-i386 TraktorIOS.xms
-#$TRAKTOR_HOME/bin/osx/SolutionBuilder -f=make -make-dialect=gnu -make-platform=ios -make-configuration=$TRAKTOR_HOME/bin/make-config-ios-x86_64.inc -make-root-suffix=-x86_64 TraktorIOS.xms
+$TRAKTOR_HOME/bin/osx/SolutionBuilder -f=make -make-dialect=gnu -make-platform=ios -make-configuration=$TRAKTOR_HOME/bin/make-config-ios-x86_64.inc -make-root-suffix=-x86_64 TraktorIOS.xms
