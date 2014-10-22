483f05e9369556ef8ce3b984c7b9b6a4518359f8
Anders Pistol <anders.pistol@doctorentertainment.com>
2014-10-22 20:20:39 +0100

    Traktor: Updated some scripts to ensure both win32 and win64 work properly.
    [git-p4: depot-paths = "//depot/traktor/": change = 17536]

--- config.bat
+++ config.bat
@@ -10,19 +10,3 @@ set ANGLE_SDK=%TRAKTOR_HOME%3rdp\angle-2014-05-09
 set GLSL_OPTIMIZER=%TRAKTOR_HOME%3rdp\glsl-optimizer-master-20141020
 set P4_X86_SDK=%TRAKTOR_HOME%3rdp\p4api-2013.3.707972.BETA-vs2008_dyn_x86
 set P4_X64_SDK=%TRAKTOR_HOME%3rdp\p4api-2013.3.707972.BETA-vs2008_dyn_x64
-
-:: set ANT_HOME=C:\apache-ant-1.9.1
-:: set ANDROID_HOME=C:\android-sdk
-:: set ANDROID_NDK_ROOT=C:\android-ndk-r10b
-
-:: set NACL_SDK_ROOT=%TRAKTOR_HOME%3rdp\nacl_sdk\pepper_37
-:: set CHROME_PATH="C:\Program Files (x86)\Google\Chrome"
-
-set PATH=%PATH%;%STEAMWORKS_SDK%\redistributable_bin
-set PATH=%PATH%;%STEAMWORKS_SDK%\redistributable_bin\win64
-set PATH=%PATH%;%TRAKTOR_HOME%3rdp\DirectX Redist (8.0)\x86
-set PATH=%PATH%;%SCE_PS3_ROOT%\host-win32\Cg\bin
-set PATH=%PATH%;%FBX_SDK%\lib\vs2008\x86
-set PATH=%PATH%;%PHYSX_SDK%\bin\win32
-set PATH=%PATH%;%TRAKTOR_HOME%3rdp\POWERVR SDK\OGLES2_WINDOWS_PCEMULATION_2.07.27.0484\Builds\OGLES2\WindowsPC\Lib
-set PATH=%PATH%;%TRAKTOR_HOME%3rdp\POWERVR SDK\OGLES2_WINDOWS_PCEMULATION_2.07.27.0484\Utilities\PVRTC\dll
--- scripts/copy-latest-win64.bat
+++ scripts/copy-latest-win64.bat
@@ -7,16 +7,20 @@ pushd %TRAKTOR_HOME%
 mkdir bin\latest\win64\debugshared
 mkdir bin\latest\win64\releaseshared
 
-rem DebugShared
-xcopy /C /I /R /Y %TRAKTOR_HOME%\build\win64\debugshared\*.dll bin\latest\win64\debugshared
-xcopy /C /I /R /Y %TRAKTOR_HOME%\build\win64\debugshared\*.exe bin\latest\win64\debugshared
-xcopy /C /I /R /Y %TRAKTOR_HOME%\build\win64\debugshared\*.pdb bin\latest\win64\debugshared
-xcopy /C /I /R /Y %STEAMWORKS_SDK%\redistributable_bin\win64\steam_api64.dll bin\latest\win64\debugshared
+:: DebugShared
+xcopy /C /I /R /Y "%TRAKTOR_HOME%\build\win64\debugshared\*.dll" bin\latest\win64\debugshared
+xcopy /C /I /R /Y "%TRAKTOR_HOME%\build\win64\debugshared\*.exe" bin\latest\win64\debugshared
+xcopy /C /I /R /Y "%TRAKTOR_HOME%\build\win64\debugshared\*.pdb" bin\latest\win64\debugshared
+xcopy /C /I /R /Y "%STEAMWORKS_SDK%\redistributable_bin\win64\steam_api64.dll" bin\latest\win64\debugshared
+xcopy /C /I /R /Y "%FBX_SDK%\lib\vs2008\x64\*.dll" bin\latest\win64\debugshared
+xcopy /C /I /R /Y "%TRAKTOR_HOME%3rdp\DirectX Redist (8.0)\x64\*.dll" bin\latest\win64\debugshared
 
-rem ReleaseShared
-xcopy /C /I /R /Y %TRAKTOR_HOME%\build\win64\releaseshared\*.dll bin\latest\win64\releaseshared
-xcopy /C /I /R /Y %TRAKTOR_HOME%\build\win64\releaseshared\*.exe bin\latest\win64\releaseshared
-xcopy /C /I /R /Y %TRAKTOR_HOME%\build\win64\releaseshared\*.pdb bin\latest\win64\releaseshared
-xcopy /C /I /R /Y %STEAMWORKS_SDK%\redistributable_bin\win64\steam_api64.dll bin\latest\win64\releaseshared
+:: ReleaseShared
+xcopy /C /I /R /Y "%TRAKTOR_HOME%\build\win64\releaseshared\*.dll" bin\latest\win64\releaseshared
+xcopy /C /I /R /Y "%TRAKTOR_HOME%\build\win64\releaseshared\*.exe" bin\latest\win64\releaseshared
+xcopy /C /I /R /Y "%TRAKTOR_HOME%\build\win64\releaseshared\*.pdb" bin\latest\win64\releaseshared
+xcopy /C /I /R /Y "%STEAMWORKS_SDK%\redistributable_bin\win64\steam_api64.dll" bin\latest\win64\releaseshared
+xcopy /C /I /R /Y "%FBX_SDK%\lib\vs2008\x64\*.dll" bin\latest\win64\releaseshared
+xcopy /C /I /R /Y "%TRAKTOR_HOME%3rdp\DirectX Redist (8.0)\x64\*.dll" bin\latest\win64\releaseshared
 
 popd
