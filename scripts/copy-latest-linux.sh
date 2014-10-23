08c7c35364cbeec0a07d31886eb41e296e96cd24
Anders Pistol <anders.pistol@doctorentertainment.com>
2014-10-23 17:56:01 +0100

    Traktor: Updated linux "copy-latest" script to copy dependent libraries and also debug binaries.
    [git-p4: depot-paths = "//depot/traktor/": change = 17547]

--- scripts/copy-latest-linux.sh
+++ scripts/copy-latest-linux.sh
@@ -3,13 +3,38 @@
 # Source environment configuration.
 . "`dirname \"$BASH_SOURCE\"`/../config.sh"
 
-# Copy products into bin directory.
+# Copy debug products into bin directory.
+pushd $TRAKTOR_HOME
+
+mkdir -p bin/latest/linux/debugshared
+cd bin/latest/linux/debugshared
+
+# Copy binaries.
+cp -f "$TRAKTOR_HOME/build/linux/debugshared/*" .
+if [ $(uname -m) == 'x86_64' ]; then
+	cp -f "$STEAMWORKS_SDK/redistributable_bin/linux64/*" .
+	cp -f "$FBX_SDK/lib/gcc4/x64/*.so" .
+else
+	cp -f "$STEAMWORKS_SDK/redistributable_bin/linux32/*" .
+	cp -f "$FBX_SDK/lib/gcc4/x86/*.so" .
+fi
+
+popd
+
+# Copy release products into bin directory.
 pushd $TRAKTOR_HOME
 
 mkdir -p bin/latest/linux/releaseshared
 cd bin/latest/linux/releaseshared
 
 # Copy binaries.
-cp -f $TRAKTOR_HOME/build/linux/releaseshared/* .
+cp -f "$TRAKTOR_HOME/build/linux/releaseshared/*" .
+if [ $(uname -m) == 'x86_64' ]; then
+	cp -f "$STEAMWORKS_SDK/redistributable_bin/linux64/*" .
+	cp -f "$FBX_SDK/lib/gcc4/x64/*.so" .
+else
+	cp -f "$STEAMWORKS_SDK/redistributable_bin/linux32/*" .
+	cp -f "$FBX_SDK/lib/gcc4/x86/*.so" .
+fi
 
 popd
