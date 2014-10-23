5cd9742c928939431c501919d5cfb20a76c072dd
Anders Pistol <anders.pistol@doctorentertainment.com>
2014-10-23 20:34:45 +0100

    Traktor: Android build scripts added.
    [git-p4: depot-paths = "//depot/traktor/": change = 17548]

--- scripts/copy-latest-android.bat
+++ scripts/copy-latest-android.bat
@@ -4,9 +4,11 @@ call %~dp0../config.bat
 
 pushd %TRAKTOR_HOME%
 
+mkdir bin\latest\android\debugstatic
 mkdir bin\latest\android\releasestatic
 
-rem ReleaseStatic
-xcopy /C /I /R /Y %TRAKTOR_HOME%\build\android\releasestatic\*.a bin\latest\android\releasestatic
+:: Copy binaries
+xcopy /C /I /R /Y "%TRAKTOR_HOME%\build\android\debugstatic\*.a" bin\latest\android\debugstatic
+xcopy /C /I /R /Y "%TRAKTOR_HOME%\build\android\releasestatic\*.a" bin\latest\android\releasestatic
 
 popd
