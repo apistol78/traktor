77f010b771f91eb3dfea597fb6f09dd6f328fa0e
Anders Pistol <anders.pistol@doctorentertainment.com>
2014-10-24 08:03:08 +0100

    Traktor: Added "debugstatic" build to scripts of pnacl.
    [git-p4: depot-paths = "//depot/traktor/": change = 17551]

--- scripts/copy-latest-pnacl.bat
+++ scripts/copy-latest-pnacl.bat
@@ -2,11 +2,14 @@
 
 call %~dp0../config.bat
 
+:: debugstatic
 pushd %TRAKTOR_HOME%
+mkdir bin\latest\pnacl\debugstatic
+xcopy /C /I /R /Y %TRAKTOR_HOME%\build\pnacl\debugstatic\*.a bin\latest\pnacl\debugstatic
+popd
 
+:: releasestatic
+pushd %TRAKTOR_HOME%
 mkdir bin\latest\pnacl\releasestatic
-
-rem ReleaseStatic
 xcopy /C /I /R /Y %TRAKTOR_HOME%\build\pnacl\releasestatic\*.a bin\latest\pnacl\releasestatic
-
 popd
