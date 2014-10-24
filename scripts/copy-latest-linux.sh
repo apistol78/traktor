4a7ea04b889f054a9420bc3496d601023b97f5ba
Anders Pistol <anders.pistol@doctorentertainment.com>
2014-10-24 15:43:56 +0100

    Traktor: Linux copy script fix.
    [git-p4: depot-paths = "//depot/traktor/": change = 17562]

--- scripts/copy-latest-linux.sh
+++ scripts/copy-latest-linux.sh
@@ -10,13 +10,13 @@ mkdir -p bin/latest/linux/debugshared
 cd bin/latest/linux/debugshared
 
 # Copy binaries.
-cp -f "$TRAKTOR_HOME/build/linux/debugshared/*" .
+cp -f $TRAKTOR_HOME/build/linux/debugshared/* .
 if [ $(uname -m) == 'x86_64' ]; then
-	cp -f "$STEAMWORKS_SDK/redistributable_bin/linux64/*" .
-	cp -f "$FBX_SDK/lib/gcc4/x64/*.so" .
+	cp -f $STEAMWORKS_SDK/redistributable_bin/linux64/* .
+	cp -f $FBX_SDK/lib/gcc4/x64/*.so .
 else
-	cp -f "$STEAMWORKS_SDK/redistributable_bin/linux32/*" .
-	cp -f "$FBX_SDK/lib/gcc4/x86/*.so" .
+	cp -f $STEAMWORKS_SDK/redistributable_bin/linux32/* .
+	cp -f $FBX_SDK/lib/gcc4/x86/*.so .
 fi
 
 popd
@@ -28,13 +28,13 @@ mkdir -p bin/latest/linux/releaseshared
 cd bin/latest/linux/releaseshared
 
 # Copy binaries.
-cp -f "$TRAKTOR_HOME/build/linux/releaseshared/*" .
+cp -f $TRAKTOR_HOME/build/linux/releaseshared/* .
 if [ $(uname -m) == 'x86_64' ]; then
-	cp -f "$STEAMWORKS_SDK/redistributable_bin/linux64/*" .
-	cp -f "$FBX_SDK/lib/gcc4/x64/*.so" .
+	cp -f $STEAMWORKS_SDK/redistributable_bin/linux64/* .
+	cp -f $FBX_SDK/lib/gcc4/x64/*.so .
 else
-	cp -f "$STEAMWORKS_SDK/redistributable_bin/linux32/*" .
-	cp -f "$FBX_SDK/lib/gcc4/x86/*.so" .
+	cp -f $STEAMWORKS_SDK/redistributable_bin/linux32/* .
+	cp -f $FBX_SDK/lib/gcc4/x86/*.so .
 fi
 
 popd
