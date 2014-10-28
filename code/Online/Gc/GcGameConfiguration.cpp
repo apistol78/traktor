1fad913cd12bc2c69de6377efff8cb355d99b508
Anders Pistol <anders.pistol@doctorentertainment.com>
2014-10-28 15:27:18 +0100

    Traktor: Added some keys to GC game configuration.
    [git-p4: depot-paths = "//depot/traktor/": change = 17593]

--- code/Online/Gc/GcGameConfiguration.cpp
+++ code/Online/Gc/GcGameConfiguration.cpp
@@ -7,13 +7,20 @@ namespace traktor
 	namespace online
 	{
 
-T_IMPLEMENT_RTTI_EDIT_CLASS(L"traktor.online.GcGameConfiguration", 0, GcGameConfiguration, IGameConfiguration)
+T_IMPLEMENT_RTTI_EDIT_CLASS(L"traktor.online.GcGameConfiguration", 1, GcGameConfiguration, IGameConfiguration)
 
 void GcGameConfiguration::serialize(ISerializer& s)
 {
 	s >> MemberStlList< std::wstring >(L"achievementIds", m_achievementIds);
 	s >> MemberStlList< std::wstring >(L"leaderboardIds", m_leaderboardIds);
 	s >> MemberStlList< std::wstring >(L"statsIds", m_statsIds);
+
+	if (s.getVersion() >= 1)
+	{
+		s >> Member< std::wstring >(L"kamcordDeveloperKey", m_kamcordDeveloperKey);
+		s >> Member< std::wstring >(L"kamcordDeveloperSecret", m_kamcordDeveloperSecret);
+		s >> Member< std::wstring >(L"kamcordAppName", m_kamcordAppName);
+	}
 }
 
 	}
