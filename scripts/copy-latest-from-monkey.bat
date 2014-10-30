032bccbcdb6e368eda6069cacec9525181170c84
Anders Pistol <anders.pistol@doctorentertainment.com>
2014-10-30 12:49:54 +0100

    Traktor: Updated copy-from-monkey script.
    [git-p4: depot-paths = "//depot/traktor/": change = 17644]

--- scripts/copy-latest-from-monkey.bat
+++ scripts/copy-latest-from-monkey.bat
@@ -8,25 +8,28 @@ set BUILD_SERVER=http://intranet.doctore:8111
 :: Get each platform binaries from build machines.
 
 echo Downloading Android...
-%TRAKTOR_HOME%\bin\win32\HttpGet %BUILD_SERVER%/guestAuth/repository/downloadAll/Traktor_Android/.lastFinished/artifacts.zip Traktor_Android.zip
+%TRAKTOR_HOME%\bin\win32\HttpGet %BUILD_SERVER%/guestAuth/repository/download/Traktor_Android/.lastFinished/traktor_android.zip traktor_android.zip
 
 echo Downloading iOS...
-%TRAKTOR_HOME%\bin\win32\HttpGet %BUILD_SERVER%/guestAuth/repository/downloadAll/Traktor_IOS/.lastFinished/artifacts.zip Traktor_IOS.zip
+%TRAKTOR_HOME%\bin\win32\HttpGet %BUILD_SERVER%/guestAuth/repository/download/Traktor_IOS/.lastFinished/traktor_ios.zip traktor_ios.zip
 
 echo Downloading Linux (x86)...
-%TRAKTOR_HOME%\bin\win32\HttpGet %BUILD_SERVER%/guestAuth/repository/downloadAll/Traktor_LinuxX86/.lastFinished/artifacts.zip Traktor_LinuxX86.zip
+%TRAKTOR_HOME%\bin\win32\HttpGet %BUILD_SERVER%/guestAuth/repository/download/Traktor_LinuxX86/.lastFinished/traktor_linux_x86.zip traktor_linux_x86.zip
 
 echo Downloading OSX...
-%TRAKTOR_HOME%\bin\win32\HttpGet %BUILD_SERVER%/guestAuth/repository/downloadAll/Traktor_OSX/.lastFinished/artifacts.zip Traktor_OSX.zip
+%TRAKTOR_HOME%\bin\win32\HttpGet %BUILD_SERVER%/guestAuth/repository/download/Traktor_OSX/.lastFinished/traktor_osx.zip traktor_osx.zip
 
 echo Downloading PNaCl...
-%TRAKTOR_HOME%\bin\win32\HttpGet %BUILD_SERVER%/guestAuth/repository/downloadAll/Traktor_PNaCl/.lastFinished/artifacts.zip Traktor_PNaCl.zip
+%TRAKTOR_HOME%\bin\win32\HttpGet %BUILD_SERVER%/guestAuth/repository/download/Traktor_PNaCl/.lastFinished/traktor_pnacl.zip traktor_pnacl.zip
+
+echo Downloading Emscripten...
+%TRAKTOR_HOME%\bin\win32\HttpGet %BUILD_SERVER%/guestAuth/repository/download/Traktor_PNaCl/.lastFinished/traktor_emscripten.zip traktor_emscripten.zip
 
 echo Downloading Win32...
-%TRAKTOR_HOME%\bin\win32\HttpGet %BUILD_SERVER%/guestAuth/repository/downloadAll/Traktor_Win32/.lastFinished/artifacts.zip Traktor_Win32.zip
+%TRAKTOR_HOME%\bin\win32\HttpGet %BUILD_SERVER%/guestAuth/repository/download/Traktor_Win32/.lastFinished/traktor_win32.zip traktor_win32.zip
 
 echo Downloading Win64...
-%TRAKTOR_HOME%\bin\win32\HttpGet %BUILD_SERVER%/guestAuth/repository/downloadAll/Traktor_Win64/.lastFinished/artifacts.zip Traktor_Win64.zip
+%TRAKTOR_HOME%\bin\win32\HttpGet %BUILD_SERVER%/guestAuth/repository/download/Traktor_Win32/.lastFinished/traktor_win64.zip traktor_win64.zip
 
 
 :: Ensure old binaries folder is cleaned.
@@ -40,26 +43,21 @@ rmdir /s /q "%TRAKTOR_HOME%\bin\latest"
 
 echo Decompressing...
 
-%TRAKTOR_HOME%\bin\win32\7za x -y -o"%TRAKTOR_HOME%\bin\latest\android" Traktor_Android.zip
-%TRAKTOR_HOME%\bin\win32\7za x -y -o"%TRAKTOR_HOME%\bin\latest\ios" Traktor_IOS.zip
-%TRAKTOR_HOME%\bin\win32\7za x -y -o"%TRAKTOR_HOME%\bin\latest\linux" Traktor_LinuxX86.zip
-%TRAKTOR_HOME%\bin\win32\7za x -y -o"%TRAKTOR_HOME%\bin\latest\osx" Traktor_OSX.zip
-%TRAKTOR_HOME%\bin\win32\7za x -y -o"%TRAKTOR_HOME%\bin\latest\pnacl" Traktor_PNaCl.zip
-%TRAKTOR_HOME%\bin\win32\7za x -y -o"%TRAKTOR_HOME%\bin\latest\win32" Traktor_Win32.zip
-%TRAKTOR_HOME%\bin\win32\7za x -y -o"%TRAKTOR_HOME%\bin\latest\win64" Traktor_Win64.zip
+%TRAKTOR_HOME%\bin\win32\7za x -y -o"%TRAKTOR_HOME%\bin\latest\android" traktor_android.zip
+%TRAKTOR_HOME%\bin\win32\7za x -y -o"%TRAKTOR_HOME%\bin\latest\ios" traktor_ios.zip
+%TRAKTOR_HOME%\bin\win32\7za x -y -o"%TRAKTOR_HOME%\bin\latest\linux" traktor_linux_x86.zip
+%TRAKTOR_HOME%\bin\win32\7za x -y -o"%TRAKTOR_HOME%\bin\latest\osx" traktor_osx.zip
+%TRAKTOR_HOME%\bin\win32\7za x -y -o"%TRAKTOR_HOME%\bin\latest\pnacl" traktor_pnacl.zip
+%TRAKTOR_HOME%\bin\win32\7za x -y -o"%TRAKTOR_HOME%\bin\latest\emscripten" traktor_emscripten.zip
+%TRAKTOR_HOME%\bin\win32\7za x -y -o"%TRAKTOR_HOME%\bin\latest\win32" traktor_win32.zip
+%TRAKTOR_HOME%\bin\win32\7za x -y -o"%TRAKTOR_HOME%\bin\latest\win64" traktor_win64.zip
 
 
 :: Cleanup
 
 echo Cleaning...
 
-del Traktor_Android.zip
-del Traktor_IOS.zip
-del Traktor_LinuxX86.zip
-del Traktor_OSX.zip
-del Traktor_PNaCl.zip
-del Traktor_Win32.zip
-del Traktor_Win64.zip
+del *.zip
 
 
 :: Done
