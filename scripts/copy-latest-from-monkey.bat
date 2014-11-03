4a0e47c8e023a53afe3097b97ae9f0251df8945d
Anders Pistol <anders.pistol@doctorentertainment.com>
2014-11-03 16:43:19 +0100

    Traktor: Updated copy from monkey script.
    [git-p4: depot-paths = "//depot/traktor/": change = 17700]

--- scripts/copy-latest-from-monkey.bat
+++ scripts/copy-latest-from-monkey.bat
@@ -31,6 +31,9 @@ echo Downloading Win32...
 echo Downloading Win64...
 %TRAKTOR_HOME%\bin\win32\HttpGet %BUILD_SERVER%/guestAuth/repository/download/Traktor_Win32/.lastFinished/traktor_win64.zip traktor_win64.zip
 
+echo Downloading Mobile6...
+%TRAKTOR_HOME%\bin\win32\HttpGet %BUILD_SERVER%/guestAuth/repository/download/Traktor_Win32/.lastFinished/traktor_mobile6.zip traktor_mobile6.zip
+
 
 :: Ensure old binaries folder is cleaned.
 
@@ -51,6 +54,7 @@ echo Decompressing...
 %TRAKTOR_HOME%\bin\win32\7za x -y -o"%TRAKTOR_HOME%\bin\latest\emscripten" traktor_emscripten.zip
 %TRAKTOR_HOME%\bin\win32\7za x -y -o"%TRAKTOR_HOME%\bin\latest\win32" traktor_win32.zip
 %TRAKTOR_HOME%\bin\win32\7za x -y -o"%TRAKTOR_HOME%\bin\latest\win64" traktor_win64.zip
+%TRAKTOR_HOME%\bin\win32\7za x -y -o"%TRAKTOR_HOME%\bin\latest\mobile6" traktor_mobile6.zip
 
 
 :: Cleanup
