c555bad7587daa371e2bc1f4f6937a8023d14f4e
Anders Pistol <anders.pistol@doctorentertainment.com>
2014-11-13 11:08:48 +0100

    Traktor: Add given meta data to Everyplay shares.
    [git-p4: depot-paths = "//depot/traktor/": change = 17872]

--- code/Online/Gc/GcVideoSharingEveryplay.mm
+++ code/Online/Gc/GcVideoSharingEveryplay.mm
@@ -1,96 +1,15 @@
 #import <Everyplay/Everyplay.h>
 #include "Core/Log/Log.h"
+#include "Core/Settings/PropertyGroup.h"
+#include "Core/Settings/PropertyInteger.h"
+#include "Core/Settings/PropertyString.h"
 #include "Online/Gc/GcGameConfiguration.h"
 #include "Online/Gc/GcVideoSharingEveryplay.h"
 
 using namespace traktor;
 
-@interface EveryplayCallback : NSObject< EveryplayDelegate >
-
-@end
-
-@implementation EveryplayCallback
-
-- (void)everyplayShown
+namespace
 {
-	log::info << L"EveryplayCallback -- everyplayShown" << Endl;
-}
-
-- (void)everyplayHidden
-{
-	log::info << L"EveryplayCallback -- everyplayHidden" << Endl;
-}
-
-- (void)everyplayReadyForRecording:(NSNumber *)enabled
-{
-	log::info << L"EveryplayCallback -- everyplayReadyForRecording" << Endl;
-}
-
-- (void)everyplayRecordingStarted
-{
-	log::info << L"EveryplayCallback -- everyplayRecordingStarted" << Endl;
-}
-
-- (void)everyplayRecordingStopped
-{
-	log::info << L"EveryplayCallback -- everyplayRecordingStopped" << Endl;
-	//[[Everyplay sharedInstance] mergeSessionDeveloperData:@{@"testString" : @"hello"}];
-	//[[Everyplay sharedInstance] mergeSessionDeveloperData:@{@"testInteger" : @42}];
-}
-
-- (void)everyplayFaceCamSessionStarted
-{
-	log::info << L"EveryplayCallback -- everyplayFaceCamSessionStarted" << Endl;
-}
-
-- (void)everyplayFaceCamRecordingPermission:(NSNumber *)granted
-{
-	log::info << L"EveryplayCallback -- everyplayFaceCamRecordingPermission" << Endl;
-}
-
-- (void)everyplayFaceCamSessionStopped
-{
-	log::info << L"EveryplayCallback -- everyplayFaceCamSessionStopped" << Endl;
-}
-
-- (void)everyplayUploadDidStart:(NSNumber *)videoId
-{
-	log::info << L"EveryplayCallback -- everyplayUploadDidStart" << Endl;
-}
-
-- (void)everyplayUploadDidProgress:(NSNumber *)videoId progress:(NSNumber *)progress
-{
-	log::info << L"EveryplayCallback -- everyplayUploadDidProgress" << Endl;
-}
-
-- (void)everyplayUploadDidComplete:(NSNumber *)videoId
-{
-	log::info << L"EveryplayCallback -- everyplayUploadDidComplete" << Endl;
-}
-
-- (void)everyplayThumbnailReadyAtFilePath:(NSString *)thumbnailFilePath
-{
-	log::info << L"EveryplayCallback -- everyplayThumbnailReadyAtFilePath" << Endl;
-}
-
-- (void)everyplayThumbnailReadyAtURL:(NSURL *)thumbnailUrl
-{
-	log::info << L"EveryplayCallback -- everyplayThumbnailReadyAtURL" << Endl;
-}
-
-- (void)everyplayThumbnailReadyAtTextureId:(NSNumber *)textureId portraitMode:(NSNumber *)portrait
-{
-	log::info << L"EveryplayCallback -- everyplayThumbnailReadyAtTextureId" << Endl;
-}
-
-@end
-
-namespace traktor
-{
-	namespace online
-	{
-		namespace
-		{
 
 NSString* makeNSString(const std::wstring& str)
 {
@@ -103,6 +22,103 @@ std::wstring fromNSString(const NSString* str)
 	[str getCString: (char*)buffer maxLength: sizeof_array(buffer) encoding: NSUTF32StringEncoding];
 	return std::wstring(buffer);
 }
+
+}
+
+@interface EveryplayCallback : NSObject< EveryplayDelegate >
+{
+@public
+	Ref< const PropertyGroup > m_metaData;
+}
+
+@end
+
+@implementation EveryplayCallback
+
+- (void)everyplayShown
+{
+}
+
+- (void)everyplayHidden
+{
+}
+
+- (void)everyplayReadyForRecording:(NSNumber *)enabled
+{
+}
+
+- (void)everyplayRecordingStarted
+{
+}
+
+- (void)everyplayRecordingStopped
+{
+	if (m_metaData)
+	{
+		const std::map< std::wstring, Ref< IPropertyValue > >& values = m_metaData->getValues();
+		for (std::map< std::wstring, Ref< IPropertyValue > >::const_iterator i = values.begin(); i != values.end(); ++i)
+		{
+			NSString* key = makeNSString(i->first);
+
+			if (const PropertyInteger* propInt = dynamic_type_cast< const PropertyInteger* >(i->second))
+			{
+				NSNumber* value = [[NSNumber numberWithInt: PropertyInteger::get(propInt)] autorelease];
+				[[Everyplay sharedInstance] mergeSessionDeveloperData:@{ key : value }];
+			}
+			else if (const PropertyString* propStr = dynamic_type_cast< const PropertyString* >(i->second))
+			{
+				NSString* value = makeNSString(PropertyString::get(propStr));
+				[[Everyplay sharedInstance] mergeSessionDeveloperData:@{ key : value }];
+			}
+		}
+		m_metaData = 0;
+	}
+}
+
+- (void)everyplayFaceCamSessionStarted
+{
+}
+
+- (void)everyplayFaceCamRecordingPermission:(NSNumber *)granted
+{
+}
+
+- (void)everyplayFaceCamSessionStopped
+{
+}
+
+- (void)everyplayUploadDidStart:(NSNumber *)videoId
+{
+}
+
+- (void)everyplayUploadDidProgress:(NSNumber *)videoId progress:(NSNumber *)progress
+{
+}
+
+- (void)everyplayUploadDidComplete:(NSNumber *)videoId
+{
+}
+
+- (void)everyplayThumbnailReadyAtFilePath:(NSString *)thumbnailFilePath
+{
+}
+
+- (void)everyplayThumbnailReadyAtURL:(NSURL *)thumbnailUrl
+{
+}
+
+- (void)everyplayThumbnailReadyAtTextureId:(NSNumber *)textureId portraitMode:(NSNumber *)portrait
+{
+}
+
+@end
+
+namespace traktor
+{
+	namespace online
+	{
+		namespace
+		{
 
 EveryplayCallback* s_callback = 0;
 
@@ -148,7 +164,10 @@ bool GcVideoSharingEveryplay::beginCapture(int32_t duration)
 void GcVideoSharingEveryplay::endCapture(const PropertyGroup* metaData)
 {
 	if (s_callback)
+	{
+		s_callback->m_metaData = metaData;
 		[[[Everyplay sharedInstance] capture] stopRecording];
+	}
 }
 
 bool GcVideoSharingEveryplay::showShareUI()
