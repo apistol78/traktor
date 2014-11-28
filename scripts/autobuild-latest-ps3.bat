2faa5ad822c6653c087ae454958b8b8ad4217c4f
Anders Pistol <anders.pistol@doctorentertainment.com>
2014-11-28 09:45:34 +0100

    Traktor: Forgot to submit PS3 autobuild script.
    [git-p4: depot-paths = "//depot/traktor/": change = 18114]

--- scripts/autobuild-latest-ps3.bat
+++ scripts/autobuild-latest-ps3.bat
@@ -1,17 +1,17 @@
 @echo off
 
-rem Setup VC environment variables.
-call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
+:: Setup VC environment variables.
+call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
 
-rem Setup our build environment.
+:: Setup our build environment.
 call %~dp0../config.bat
 
-rem Remove old log.
+:: Remove old log.
 del /F /Q %~dp0autobuild-ps3.log
 
-rem Rebuild entire solution.
+:: Rebuild entire solution.
 pushd %TRAKTOR_HOME%
-call build-projects-vs2008-ps3.bat
+call build-projects-vs2012-ps3.bat
 devenv "build\ps3\Traktor Ps3.sln" /Build DebugStatic /Out %~dp0autobuild-ps3.log
 devenv "build\ps3\Traktor Ps3.sln" /Build ReleaseStatic /Out %~dp0autobuild-ps3.log
 devenv "build\ps3-spu\Traktor Ps3 SPU.sln" /Build DebugStatic /Out %~dp0autobuild-ps3.log
