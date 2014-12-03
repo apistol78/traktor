c0ea18f5a163a9e7fe5736c1e579d4c896ff0554
Anders Pistol <anders.pistol@doctorentertainment.com>
2014-12-03 17:35:39 +0100

    Traktor: Merged a patch from Bullet forums to improve raycasting with heightfields. Implemented a simple min/max 2d grid in our heightfield shape which early reject many triangle tests. Also improves the resolution of those raycasts which fixes issues with high sloping terrain.
    [git-p4: depot-paths = "//depot/traktor/": change = 18173]

--- code/Physics/Bullet/HeightfieldShapeBullet.h
+++ code/Physics/Bullet/HeightfieldShapeBullet.h
@@ -2,6 +2,7 @@
 #define traktor_physics_HeightfieldShapeBullet_H
 
 #include <BulletCollision/CollisionShapes/btConcaveShape.h>
+#include "Core/Misc/AutoPtr.h"
 #include "Resource/Proxy.h"
 
 namespace traktor
@@ -34,12 +35,23 @@ public:
 
 	virtual void processAllTriangles(btTriangleCallback* callback, const btVector3& aabbMin, const btVector3& aabbMax) const;
 
+	virtual void processRaycastAllTriangles(btTriangleRaycastCallback *callback, const btVector3 &raySource, const btVector3 &rayTarget);
+
+	virtual void processConvexcastAllTriangles(btTriangleConvexcastCallback *callback, const btVector3 &boxSource, const btVector3 &boxTarget, const btVector3 &boxMin, const btVector3 &boxMax);
+
 	virtual void calculateLocalInertia(btScalar mass, btVector3& inertia) const;
 
 	virtual const char*	getName() const;
 
 private:
+	struct MinMax
+	{
+		float mn;
+		float mx;
+	};
+
 	resource::Proxy< hf::Heightfield > m_heightfield;
+	AutoArrayPtr< MinMax > m_gridMinMax;
 	btVector3 m_localScaling;
 };
 
