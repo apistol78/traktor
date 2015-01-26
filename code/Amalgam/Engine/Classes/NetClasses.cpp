3e0438fd0096c7c8e4c6d26fd32fa9a9397b03fe
Anders Pistol <anders.pistol@doctorentertainment.com>
2015-01-26 17:27:56 +0100

    Traktor: Replicator time synchronization improvements; after sync been lost time is re-sync;ed and when variance is once again below threshold time is updated, not continously as before which caused weird time jitter.
    [git-p4: depot-paths = "//depot/traktor/": change = 18647]

--- code/Amalgam/Engine/Classes/NetClasses.cpp
+++ code/Amalgam/Engine/Classes/NetClasses.cpp
@@ -239,7 +239,6 @@ void registerNetClasses(script::IScriptManager* scriptManager)
 	classReplicator->addMethod("setState", &net::Replicator::setState);
 	classReplicator->addMethod("getState", &net::Replicator::getState);
 	classReplicator->addMethod("setSendState", &net::Replicator::setSendState);
-	classReplicator->addMethod("setTimeSynchronization", &net::Replicator::setTimeSynchronization);
 	classReplicator->addMethod("getProxyCount", &net::Replicator::getProxyCount);
 	classReplicator->addMethod("getProxy", &net::Replicator::getProxy);
 	classReplicator->addMethod("getPrimaryProxy", &net::Replicator::getPrimaryProxy);
@@ -248,6 +247,7 @@ void registerNetClasses(script::IScriptManager* scriptManager)
 	classReplicator->addMethod("sendEventToPrimary", &net::Replicator::sendEventToPrimary);
 	classReplicator->addMethod("getTime", &net::Replicator::getTime);
 	classReplicator->addMethod("getTimeVariance", &net::Replicator::getTimeVariance);
+	classReplicator->addMethod("isTimeSynchronized", &net::Replicator::isTimeSynchronized);
 	scriptManager->registerClass(classReplicator);
 }
 
