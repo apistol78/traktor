f511018a259e440103291b05cbd3e4fe5670744c
Anders Pistol <anders.pistol@doctorentertainment.com>
2015-02-14 19:50:25 +0100

    Traktor: Using NVidia hack to get Optimus drivers to enable NVidia GPU instead of any integrated crap, verified working on ASUS "mini" computer.
    [git-p4: depot-paths = "//depot/traktor/": change = 18812]

--- code/Amalgam/App/Win32/Main.cpp
+++ code/Amalgam/App/Win32/Main.cpp
@@ -26,6 +26,12 @@
 
 using namespace traktor;
 
+// NVidia hack to get Optimus to enable NVidia GPU when possible.
+extern "C"
+{
+    _declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+}
+
 namespace
 {
 
