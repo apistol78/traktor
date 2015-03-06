959f1c5b1768c3f3bcdbd84f209ca3096055adf6
Anders Pistol <anders.pistol@doctorentertainment.com>
2015-03-06 10:57:29 +0100

    Traktor: Able to define "DictionaryAsset" instead of dictionary instances in source database; CSV is converted by a DictionaryPipeline. Stubbed XLSX importer.
    [git-p4: depot-paths = "//depot/traktor/": change = 18948]

---
+++ code/I18N/Editor/DictionaryFormatCSV.cpp
@@ -0,0 +1,82 @@
+#include "Core/Io/FileOutputStream.h"
+#include "Core/Io/StringReader.h"
+#include "Core/Io/Utf8Encoding.h"
+#include "Core/Misc/Split.h"
+#include "Core/Misc/String.h"
+#include "I18N/Dictionary.h"
+#include "I18N/Editor/DictionaryFormatCSV.h"
+
+namespace traktor
+{
+	namespace i18n
+	{
+
+T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.i18n.DictionaryFormatCSV", 0, DictionaryFormatCSV, IDictionaryFormat)
+
+bool DictionaryFormatCSV::supportExtension(const std::wstring& extension) const
+{
+	return compareIgnoreCase< std::wstring >(extension, L"csv") == 0;
+}
+
+Ref< Dictionary > DictionaryFormatCSV::read(IStream* stream, int32_t keyColumn, int32_t textColumn) const
+{
+	Ref< Dictionary > dictionary = new Dictionary();
+
+	StringReader sr(stream, new Utf8Encoding());
+	std::wstring line;
+
+	while (sr.readLine(line) >= 0)
+	{
+		std::vector< std::wstring > columns;
+
+		uint32_t i0 = 0;
+		bool quote = false;
+
+		for (uint32_t i = 0; i < line.length(); ++i)
+		{
+			if (line[i] == L',' && quote == false)
+			{
+				std::wstring tmp = trim(line.substr(i0, i - i0));
+				if (tmp.length() >= 2 && tmp[0] == L'\"' && tmp[tmp.length() - 1] == L'\"')
+					tmp = tmp.substr(1, tmp.length() - 2);
+				columns.push_back(tmp);
+				i0 = i + 1;
+			}
+			if (line[i] == L'\"')
+			{
+				quote = !quote;
+			}
+		}
+
+		if (!quote)
+		{
+			std::wstring tmp = trim(line.substr(i0));
+			if (tmp.length() >= 2 && tmp[0] == L'\"' && tmp[tmp.length() - 1] == L'\"')
+				tmp = tmp.substr(1, tmp.length() - 2);
+			columns.push_back(tmp);
+		}
+
+		Split< std::wstring >::any(line, L",", columns, true);
+		if (columns.size() > std::max(keyColumn, textColumn) && !columns[keyColumn].empty())
+		{
+			dictionary->set(
+				columns[keyColumn],
+				columns[textColumn]
+			);
+		}
+	}
+
+	return dictionary;
+}
+
+bool DictionaryFormatCSV::write(IStream* stream, const Dictionary* dictionary) const
+{
+	FileOutputStream fos(stream, new Utf8Encoding());
+	const std::map< std::wstring, std::wstring >& map = dictionary->get();
+	for (std::map< std::wstring, std::wstring >::const_iterator i = map.begin(); i != map.end(); ++i)
+		fos << i->first << L"," << i->second << Endl;
+	return true;
+}
+
+	}
+}
---
+++ code/I18N/Editor/IDictionaryFormat.cpp
@@ -0,0 +1,74 @@
+#include "Core/Io/FileSystem.h"
+#include "I18N/Editor/IDictionaryFormat.h"
+
+namespace traktor
+{
+	namespace i18n
+	{
+
+T_IMPLEMENT_RTTI_CLASS(L"traktor.i18n.IDictionaryFormat", IDictionaryFormat, Object)
+
+Ref< Dictionary > IDictionaryFormat::readAny(const Path& filePath, int32_t keyColumn, int32_t textColumn)
+{
+	Ref< IStream > file = FileSystem::getInstance().open(filePath, File::FmRead);
+	if (file)
+		return readAny(file, filePath.getExtension(), keyColumn, textColumn);
+	else
+		return 0;
+}
+
+Ref< Dictionary > IDictionaryFormat::readAny(IStream* stream, const std::wstring& extension, int32_t keyColumn, int32_t textColumn)
+{
+	Ref< Dictionary > dictionary;
+
+	TypeInfoSet formatTypes;
+	type_of< IDictionaryFormat >().findAllOf(formatTypes, false);
+
+	for (TypeInfoSet::iterator i = formatTypes.begin(); i != formatTypes.end(); ++i)
+	{
+		Ref< IDictionaryFormat > dictionaryFormat = dynamic_type_cast< IDictionaryFormat* >((*i)->createInstance());
+		if (!dictionaryFormat)
+			continue;
+
+		if (!dictionaryFormat->supportExtension(extension))
+			continue;
+
+		dictionary = dictionaryFormat->read(stream, keyColumn, textColumn);
+		if (dictionary)
+			break;
+	}
+
+	return dictionary;
+}
+
+bool IDictionaryFormat::writeAny(const Path& filePath, const Dictionary* dictionary)
+{
+	Ref< IStream > file = FileSystem::getInstance().open(filePath, File::FmWrite);
+	if (file)
+		return writeAny(file, filePath.getExtension(), dictionary);
+	else
+		return false;
+}
+
+bool IDictionaryFormat::writeAny(IStream* stream, const std::wstring& extension, const Dictionary* dictionary)
+{
+	TypeInfoSet formatTypes;
+	type_of< IDictionaryFormat >().findAllOf(formatTypes);
+
+	for (TypeInfoSet::iterator i = formatTypes.begin(); i != formatTypes.end(); ++i)
+	{
+		Ref< IDictionaryFormat > dictionaryFormat = dynamic_type_cast< IDictionaryFormat* >((*i)->createInstance());
+		if (!dictionaryFormat)
+			continue;
+
+		if (!dictionaryFormat->supportExtension(extension))
+			continue;
+
+		return dictionaryFormat->write(stream, dictionary);
+	}
+
+	return false;
+}
+
+	}
+}
