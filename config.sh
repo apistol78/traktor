6cf50cf0a500669b53c3eb90cf629ccbde9dc2e9
Anders Pistol <anders.pistol@doctorentertainment.com>
2015-03-24 17:31:58 +0100

    Traktor: Updated lua to 5.2.4.
    [git-p4: depot-paths = "//depot/traktor/": change = 19007]

--- config.sh
+++ config.sh
@@ -11,6 +11,7 @@ export FBX_SDK=$TRAKTOR_HOME/3rdp/FbxSdk/2013.1
 export GLSL_OPTIMIZER=$TRAKTOR_HOME/3rdp/glsl-optimizer-master-20141020
 export SQLITE_SDK=$TRAKTOR_HOME/3rdp/sqlite-3.8.7.2
 export WXWIDGETS_SDK=$TRAKTOR_HOME/3rdp/wxWidgets-3.0.2
+export LUA_SDK=$TRAKTOR_HOME/3rdp/lua-5.2.4
 
 # PNaCl SDK; need to be manually installed in 3rdp folder.
 export NACL_SDK_ROOT=$TRAKTOR_HOME/3rdp/nacl_sdk/pepper_37
