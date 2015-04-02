d599b7b4c326641063c35dc7751676dbf3d7cdde
Anders Pistol <anders.pistol@doctorentertainment.com>
2015-04-02 21:27:16 +0100

    Traktor: Create package image script.
    [git-p4: depot-paths = "//depot/traktor/": change = 19034]

--- run-editor-win64.bat
+++ run-editor-win64.bat
@@ -2,4 +2,11 @@
 
 call %~dp0config.bat
 
+:: Launch remote server.
+set SCRATCH_PATH=%APPDATA%\TraktorRemoteServer
+if not exist %SCRATCH_PATH% ( mkdir %SCRATCH_PATH% )
+
+start %TRAKTOR_HOME%\bin\win32\RemoteServer %SCRATCH_PATH%
+
+:: Launch editor.
 start %TRAKTOR_HOME%\bin\latest\win64\releaseshared\Traktor.Editor.App
