04ccabbe10175fda65bb4e556d544a4a45e2d64f
Anders Pistol <anders.pistol@doctorentertainment.com>
2015-04-18 11:33:03 +0100

    Traktor: Fixed Online.Lan on linux. Properly implemented Guid::create on Linux, now able to generate a unique ID using libuuid.
    [git-p4: depot-paths = "//depot/traktor/": change = 19107]

--- code/Core/Guid.cpp
+++ code/Core/Guid.cpp
@@ -3,6 +3,8 @@
 #if defined(__APPLE__)
 #	include <mach/mach_time.h>
 #	include "Core/Math/Random.h"
+#elif defined(__LINUX__)
+#	include <uuid/uuid.h>
 #endif
 #include "Core/Platform.h"
 #include "Core/Guid.h"
@@ -85,23 +87,30 @@ Guid Guid::create()
 	Guid guid;
 
 #if defined(_WIN32) && !defined(_XBOX)
-	
+
 	GUID tmp;
 	CoCreateGuid(&tmp);
 	std::memcpy(guid.m_data, &tmp, 16);
 	guid.m_valid = true;
-	
+
 #elif defined(__APPLE__)
-	
+
 	uint64_t cputick = mach_absolute_time();
 	std::memcpy(guid.m_data, &cputick, sizeof(cputick));
-	
+
 	static Random s_rnd;
 	for (int i = 8; i < 16; ++i)
 		guid.m_data[i] = uint8_t(s_rnd.nextDouble() * 255);
-	
+
 	guid.m_valid = true;
-	
+
+#elif defined(__LINUX__)
+
+	uuid_t id;
+	uuid_generate(id);
+	std::memcpy(guid.m_data, id, 16);
+	guid.m_valid = true;
+
 #endif
 
 	return guid;
