465fd9162e0f146819bcb549963977b8595bf96d
Anders Pistol <anders.pistol@doctorentertainment.com>
2015-06-06 14:49:12 +0100

    Traktor: Visual Studio 2013 solution builder files added. Generate VS2013 project for x64 windows.
    [git-p4: depot-paths = "//depot/traktor/": change = 19284]

---
+++ build-projects-vs2013-win64.bat
@@ -0,0 +1,7 @@
+@echo off
+
+call %~dp0config.bat
+
+mkdir %TRAKTOR_HOME%\build
+
+%TRAKTOR_HOME%\bin\win32\solutionbuilder -f=msvc -p=$(TRAKTOR_HOME)\bin\msvc-2013-win64.xml %TRAKTOR_HOME%\TraktorWin64.xms
