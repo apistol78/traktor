e214aa0dc7296e432ff2ad8f09945c84fd3ae99b
Anders Pistol <anders.pistol@doctorentertainment.com>
2015-07-30 08:18:09 +0100

    Traktor: Minor documentation in rich edit control.
    [git-p4: depot-paths = "//depot/traktor/": change = 19347]

--- code/Ui/Custom/RichEdit/RichEdit.h
+++ code/Ui/Custom/RichEdit/RichEdit.h
@@ -35,52 +35,76 @@ public:
 
 	bool create(Widget* parent, const std::wstring& text = L"", int32_t style = WsClientBorder | WsDoubleBuffer);
 
+	/*! \brief Set text content of text editor. */
 	virtual void setText(const std::wstring& text);
 	
+	/*! \brief Get textual content of text editor. */
 	virtual std::wstring getText() const;
 
+	/*! \brief Define a new attribute. */
 	int32_t addAttribute(const Color4ub& textColor, const Color4ub& backColor, bool bold, bool italic, bool underline);
 
+	/*! \brief Use attribute on a selection of the text. */
 	void setAttribute(int32_t start, int32_t length, int32_t attribute);
 
+	/*! \brief Define new image. */
 	int32_t addImage(Bitmap* image, uint32_t imageCount);
 
+	/*! \brief Use image on a line of text. */
 	void setImage(int32_t line, int32_t image);
 
+	/*! \brief Clear attributes, images or content of text. */
 	void clear(bool attributes, bool images, bool content);
 
+	/*! \brief Insert text at caret's current position. */
 	void insert(const std::wstring& text);
 
+	/*! \brief Get caret's current position. */
 	int32_t getCaretOffset() const;
 
+	/*! \brief Get line index of position. */
 	int32_t getLineFromOffset(int32_t offset) const;
 
+	/*! \brief Get number of textual lines. */
 	int32_t getLineCount() const;
 
+	/*! \brief Get starting offset of line. */
 	int32_t getLineOffset(int32_t line) const;
 
+	/*! \brief Get length of line. */
 	int32_t getLineLength(int32_t line) const;
 
+	/*! \brief Replace text at a specified line. */
 	void setLine(int32_t line, const std::wstring& text);
 
+	/*! \brief Get a single line of text. */
 	std::wstring getLine(int32_t line) const;
 
+	/*! \brief Get selection start offset. */
 	int32_t getSelectionStartOffset() const;
 
+	/*! \brief Get selection end offset. */
 	int32_t getSelectionStopOffset() const;
 
+	/*! \brief Get selected text. */
 	std::wstring getSelectedText() const;
 
+	/*! \brief Scroll text view to a certain line. */
 	bool scrollToLine(int32_t line);
 
+	/*! \brief Get current scrolled offset. */
 	int32_t getScrollLine() const;
 
+	/*! \brief Ensure line is visible. */
 	bool showLine(int32_t line);
 
+	/*! \brief Place caret at a specified offset. */
 	void placeCaret(int32_t offset);
 
+	/*! \brief Copy selected text into clipboard. */
 	bool copy();
 
+	/*! \brief Paste text from clipboard at caret position. */
 	bool paste();
 
 private:
