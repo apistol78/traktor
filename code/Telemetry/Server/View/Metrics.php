4bb7eee7a40dbadb9358bfa343d6515b343abee2
Anders Pistol <anders.pistol@doctorentertainment.com>
2015-08-24 13:13:52 +0100

    Traktor: Added MAU and DAU/MAU metrics.
    [git-p4: depot-paths = "//depot/traktor/": change = 19411]

--- code/Telemetry/Server/View/Metrics.php
+++ code/Telemetry/Server/View/Metrics.php
@@ -20,6 +20,7 @@ body {
 
 	$timeStampTo = time();
 	$timeStampFrom = $timeStampTo - (60 * 60 * 24);
+	$timeStampFromMonth = $timeStampTo - 28 * (60 * 60 * 24);
 
 	// Calculate "Total User Count".
 	$result = $db->query(
@@ -35,6 +36,14 @@ body {
 	);
 	$row = $result->fetch_assoc();
 	$DAU = $row["dau"];
+
+	// Calculate "Monthly Active Users".
+	$result = $db->query(
+		"SELECT COUNT(DISTINCT clientId) AS mau FROM tbl_events " .
+		"WHERE tbl_events.symbolId=(SELECT id FROM tbl_symbols WHERE symbol='" . $SYMBOL_LAUNCH_EVENT . "') AND tbl_events.serverTimeStamp >= " . $timeStampFromMonth . " AND tbl_events.serverTimeStamp <= " . $timeStampTo
+	);
+	$row = $result->fetch_assoc();
+	$MAU = $row["mau"];
 
 	// Calculate "Daily New Users".
 	$result = $db->query(
@@ -67,10 +76,16 @@ body {
 					<td>Daily Active Users</td><td><?php echo($DAU); ?></td>
 				</tr>
 				<tr>
+					<td>Monthly Active Users</td><td><?php echo($MAU); ?></td>
+				</tr>
+				<tr>
 					<td>Daily Sessions</td><td><?php echo($DS); ?></td>
 				</tr>
 				<tr>
-					<td>DS/DAU</td><td><?php echo($DS/$DAU); ?></td>
+					<td>DS/DAU</td><td><?php echo(round($DS/$DAU, 2)); ?></td>
+				</tr>
+				<tr>
+					<td>DAU/MAU</td><td><?php echo(round(100 * $DAU/$MAU, 2) . "%"); ?></td>
 				</tr>
 			</table>
 		</center>
