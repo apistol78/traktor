2187292c2002040f37dfd56a05bee3027141d49a
Anders Pistol <anders.pistol@doctorentertainment.com>
2015-10-26 21:07:47 +0100

    Traktor: Implemented a wizard to create new image process shaders.
    [git-p4: depot-paths = "//depot/traktor/": change = 19647]

---
+++ code/Render/Editor/ImageProcess/ImageProcessShaderWizardTool.h
@@ -0,0 +1,34 @@
+#ifndef traktor_render_ImageProcessShaderWizardTool_H
+#define traktor_render_ImageProcessShaderWizardTool_H
+
+#include "Editor/IWizardTool.h"
+
+// import/export mechanism.
+#undef T_DLLCLASS
+#if defined(T_RENDER_EDITOR_EXPORT)
+#	define T_DLLCLASS T_DLLEXPORT
+#else
+#	define T_DLLCLASS T_DLLIMPORT
+#endif
+
+namespace traktor
+{
+	namespace render
+	{
+
+class T_DLLCLASS ImageProcessShaderWizardTool : public editor::IWizardTool
+{
+	T_RTTI_CLASS;
+
+public:
+	virtual std::wstring getDescription() const;
+
+	virtual uint32_t getFlags() const;
+
+	virtual bool launch(ui::Widget* parent, editor::IEditor* editor, db::Group* group, db::Instance* instance);
+};
+
+	}
+}
+
+#endif	// traktor_render_ImageProcessShaderWizardTool_H
