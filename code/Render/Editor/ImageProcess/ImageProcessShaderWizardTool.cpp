cf902e2800d6eb7a4cf420d5aa98dcf6789504e0
Anders Pistol <anders.pistol@doctorentertainment.com>
2015-10-26 21:07:47 +0100

    Traktor: Implemented a wizard to create new image process shaders.
    [git-p4: depot-paths = "//depot/traktor/": change = 19647]

---
+++ code/Render/Editor/ImageProcess/ImageProcessShaderWizardTool.cpp
@@ -0,0 +1,76 @@
+#include "Database/Database.h"
+#include "Database/Group.h"
+#include "Database/Instance.h"
+#include "Editor/IEditor.h"
+#include "I18N/Text.h"
+#include "Render/Editor/ImageProcess/ImageProcessShaderWizardTool.h"
+#include "Render/Shader/ShaderGraph.h"
+#include "Ui/Custom/InputDialog.h"
+
+namespace traktor
+{
+	namespace render
+	{
+
+T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.render.ImageProcessShaderWizardTool", 0, ImageProcessShaderWizardTool, editor::IWizardTool)
+
+std::wstring ImageProcessShaderWizardTool::getDescription() const
+{
+	return i18n::Text(L"RENDER_IMAGEPROCESS_SHADER_WIZARDTOOL_DESCRIPTION");
+}
+
+uint32_t ImageProcessShaderWizardTool::getFlags() const
+{
+	return WfGroup;
+}
+
+bool ImageProcessShaderWizardTool::launch(ui::Widget* parent, editor::IEditor* editor, db::Group* group, db::Instance* instance)
+{
+	ui::custom::InputDialog inputDialog;
+	ui::custom::InputDialog::Field fields[] =
+	{
+		{
+			i18n::Text(L"RENDER_IMAGEPROCESS_SHADER_WIZARDTOOL_NAME"),
+			L"",
+			0,
+			0
+		},
+	};
+
+	inputDialog.create(
+		parent,
+		i18n::Text(L"RENDER_IMAGEPROCESS_SHADER_WIZARDTOOL_DIALOG_TITLE"),
+		i18n::Text(L"RENDER_IMAGEPROCESS_SHADER_WIZARDTOOL_DIALOG_MESSAGE"),
+		fields,
+		sizeof_array(fields)
+	);
+
+	if (inputDialog.showModal() != ui::DrOk)
+	{
+		inputDialog.destroy();
+		return false;
+	}
+
+	inputDialog.destroy();
+
+	Ref< render::ShaderGraph > sourceGraph = editor->getSourceDatabase()->getObjectReadOnly< render::ShaderGraph >(Guid(L"{B5537271-73A6-F545-A651-99D6DF10481B}"));
+	if (!sourceGraph)
+		return false;
+
+	Ref< db::Instance > outputInstance = group->createInstance(
+		fields[0].value,
+		db::CifReplaceExisting | db::CifKeepExistingGuid
+	);
+	if (!outputInstance)
+		return false;
+
+	outputInstance->setObject(sourceGraph);
+
+	if (!outputInstance->commit())
+		return false;
+
+	return true;
+}
+
+	}
+}
