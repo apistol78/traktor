fff088b91c3659da8498dd92ddf8fbf248c559b3
Anders Pistol <anders.pistol@doctorentertainment.com>
2015-10-30 13:10:49 +0100

    Traktor: Deleted DX10 renderer, never been used and not up to date.
    [git-p4: depot-paths = "//depot/traktor/": change = 19666]

--- code/Render/Dx10/Platform.h
+++
@@ -1,9 +0,0 @@
-#ifndef traktor_render_dx10_Platform_H
-#define traktor_render_dx10_Platform_H
-
-#define _WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#include <d3dx10.h>
-#include <tchar.h>
-
-#endif	// traktor_render_dx10_Platform_H
