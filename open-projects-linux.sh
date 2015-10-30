e229f311930bf0bab8534d4bad7f39b9626ee603
Anders Pistol <anders.pistol@doctorentertainment.com>
2015-10-30 21:04:32 +0100

    Traktor: Open CBlocks script "detach" launched process from terminal process.
    [git-p4: depot-paths = "//depot/traktor/": change = 19674]

--- open-projects-linux.sh
+++ open-projects-linux.sh
@@ -3,4 +3,5 @@
 # Source environment configuration.
 . "`dirname \"$BASH_SOURCE\"`/config.sh"
 
-codeblocks 'build/linux/Traktor Linux.workspace'
+(codeblocks 'build/linux/Traktor Linux.workspace' > /dev/null &)
+
