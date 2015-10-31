db4075cfa7fc429bf1d64b922c8db6ec0fc72024
Anders Pistol <anders.pistol@doctorentertainment.com>
2015-10-31 16:51:11 +0100

    Traktor: Removed build scripts for old or unsupported configurations.
    [git-p4: depot-paths = "//depot/traktor/": change = 19681]

--- build-projects-eclipse-win32.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-call %~dp0config.bat
-
-mkdir %TRAKTOR_HOME%\build
-
-%TRAKTOR_HOME%\bin\win32\solutionbuilder -f=eclipse -p=$(TRAKTOR_HOME)/bin/eclipse-msvc-project.sb -c=$(TRAKTOR_HOME)/bin/eclipse-msvc-cproject.sb %TRAKTOR_HOME%\TraktorWin32.xms
