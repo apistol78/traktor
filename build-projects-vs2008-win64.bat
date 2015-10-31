d736f72ccbaf5c9916230692cf9a774503e53cc0
Anders Pistol <anders.pistol@doctorentertainment.com>
2015-10-31 16:51:11 +0100

    Traktor: Removed build scripts for old or unsupported configurations.
    [git-p4: depot-paths = "//depot/traktor/": change = 19681]

--- build-projects-vs2008-win64.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-call %~dp0config.bat
-
-mkdir %TRAKTOR_HOME%\build
-
-%TRAKTOR_HOME%\bin\win32\solutionbuilder -f=msvc -p=$(TRAKTOR_HOME)\bin\msvc-2008-win64.xml %TRAKTOR_HOME%\TraktorWin64.xms
