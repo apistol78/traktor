0dbbf04e552c573839467e8f7dc3911323b9f6f6
Anders Pistol <anders.pistol@doctorentertainment.com>
2015-11-28 16:03:00 +0100

    Traktor: Minor fix, forgot to add static symbol to json lib.
    [git-p4: depot-paths = "//depot/traktor/": change = 19867]

--- code/Json/Module.cpp
+++ code/Json/Module.cpp
@@ -1,6 +1,7 @@
 #include "Core/Rtti/TypeInfo.h"
 
 #if defined(T_STATIC)
+#	include "Json/JsonClassFactory.h"
 #	include "Json/JsonDocument.h"
 
 namespace traktor
@@ -10,6 +11,7 @@ namespace traktor
 
 extern "C" void __module__Traktor_Json()
 {
+	T_FORCE_LINK_REF(JsonClassFactory);
 	T_FORCE_LINK_REF(JsonDocument);
 }
 
