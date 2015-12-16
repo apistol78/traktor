cde585543c981a41ccffadc097388717a8a7035a
Anders Pistol <anders.pistol@doctorentertainment.com>
2015-12-16 21:30:26 +0100

    Traktor: Futher UI work.
    [git-p4: depot-paths = "//depot/traktor/": change = 19981]

--- code/Amalgam/Editor/Ui/ProgressCell.cpp
+++ code/Amalgam/Editor/Ui/ProgressCell.cpp
@@ -2,55 +2,14 @@
 #include "Ui/Application.h"
 #include "Ui/Canvas.h"
 
-// Resources
-#include "Resources/ProgressBar.h"
-
 namespace traktor
 {
 	namespace amalgam
 	{
-		namespace
-		{
-
-void drawSkin(ui::Canvas& canvas, const ui::Rect& rc, ui::Bitmap* bitmap, int pieceOffset)
-{
-	int w = rc.getWidth();
-	int h = rc.getHeight();
-
-	if (w < 8)
-		return;
-
-	canvas.drawBitmap(
-		ui::Point(rc.left, rc.top),
-		ui::Size(4, h),
-		ui::Point(pieceOffset, 0),
-		ui::Size(4, 16),
-		bitmap
-	);
-
-	canvas.drawBitmap(
-		ui::Point(rc.left + 4, rc.top),
-		ui::Size(w - 8, h),
-		ui::Point(pieceOffset + 4, 0),
-		ui::Size(8, 16),
-		bitmap
-	);
-
-	canvas.drawBitmap(
-		ui::Point(rc.right - 4, rc.top),
-		ui::Size(4, h),
-		ui::Point(pieceOffset + 12, 0),
-		ui::Size(4, 16),
-		bitmap
-	);
-}
-
-		}
 
 ProgressCell::ProgressCell()
 :	m_progress(-1)
 {
-	m_imageProgressBar = ui::Bitmap::load(c_ResourceProgressBar, sizeof(c_ResourceProgressBar), L"png");
 }
 
 void ProgressCell::setText(const std::wstring& text)
@@ -71,14 +30,17 @@ void ProgressCell::paint(ui::Canvas& canvas, const ui::Rect& rect)
 {
 	if (m_progress >= 0)
 	{
-		drawSkin(canvas, rect, m_imageProgressBar, 16);
+		canvas.setBackground(Color4ub(255, 255, 255, 255));
+		canvas.fillRect(rect);
 
 		int32_t x = (rect.getWidth() * m_progress) / 100;
 		if (x > 0)
 		{
 			ui::Rect rect2 = rect;
 			rect2.right = rect2.left + x;
-			drawSkin(canvas, rect2, m_imageProgressBar, 0);
+
+			canvas.setBackground(Color4ub(0, 153, 0, 255));
+			canvas.fillRect(rect2);
 		}
 	}
 	if (!m_text.empty())
