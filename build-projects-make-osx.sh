08c1f353feb15ba0927ab3edbf3cc922b0c438a5
Anders Pistol <anders.pistol@doctorentertainment.com>
2016-01-11 14:35:59 +0100

    Traktor: Fixed OSX makefile generation.
    [git-p4: depot-paths = "//depot/traktor/": change = 20024]

--- build-projects-make-osx.sh
+++ build-projects-make-osx.sh
@@ -3,5 +3,19 @@
 # Source environment configuration.
 . "`dirname \"$BASH_SOURCE\"`/config.sh"
 
+# Generator configuration.
+set MAKE_DIALECT=make
+set MAKE_TOOLCHAIN=clang
+set MAKE_OS=osx
+set MAKE_INCLUDE=$(TRAKTOR_HOME)/bin/make-config-osx.inc
+set MAKE_OBJECT_FILE=%%s.o
+set MAKE_STATIC_LIBRARY_FILE=lib%%s.a
+set MAKE_SHARED_LIBRARY_FILE=lib%%s.dylib
+set MAKE_EXECUTABLE_FILE=%%s
+
 # Build solution files.
-$TRAKTOR_HOME/bin/osx/SolutionBuilder -f=make -make-dialect=gnu -make-platform=osx -make-configuration=$TRAKTOR_HOME/bin/make-config-osx.inc TraktorOSX.xms
+$TRAKTOR_HOME/bin/osx/SolutionBuilder \
+	-f=make2 \
+	-make-solution-template=$TRAKTOR_HOME/bin/make-solution.sbm \
+	-make-project-template=$TRAKTOR_HOME/bin/make-project.sbm \
+	$TRAKTOR_HOME/TraktorOSX.xms
--- scripts/copy-latest-ios.sh
+++ scripts/copy-latest-ios.sh
@@ -9,26 +9,8 @@ fi
 
 if [ $CONFIG == "both" ] || [ $CONFIG == "release" ] ; then
 
-	# All build outputs
-	TRAKTOR_BUILD_i386=$TRAKTOR_HOME/build/ios-i386/releasestatic
-	TRAKTOR_BUILD_x86_64=$TRAKTOR_HOME/build/ios-x86_64/releasestatic
-	TRAKTOR_BUILD_armv7=$TRAKTOR_HOME/build/ios-armv7/releasestatic
-	TRAKTOR_BUILD_armv7s=$TRAKTOR_HOME/build/ios-armv7s/releasestatic
-	TRAKTOR_BUILD_arm64=$TRAKTOR_HOME/build/ios-arm64/releasestatic
-
-	# Generate LIPO archive of all architectures.
-	pushd $TRAKTOR_HOME
-	mkdir -p bin/latest/ios/releasestatic
-
-	shopt -s nullglob
-
-	FILES="$TRAKTOR_BUILD_i386/*.a"
-	for FILE in $FILES
-	do
-		FILE=$(basename $FILE)
-		echo "Creating universal archive $FILE..."
-		lipo -create -output "bin/latest/ios/releasestatic/$FILE" -arch i386 "$TRAKTOR_BUILD_i386/$FILE" -arch x86_64 "$TRAKTOR_BUILD_x86_64/$FILE" -arch armv7 "$TRAKTOR_BUILD_armv7/$FILE" -arch armv7s "$TRAKTOR_BUILD_armv7s/$FILE" -arch arm64 "$TRAKTOR_BUILD_arm64/$FILE"
-	done
+	# Copy archives.
+	cp -R -f $TRAKTOR_HOME/build/ios/releasestatic/* bin/latest/ios/releasestatic/
 
 	# Copy 3rd-party frameworks.
 	cp -R -f $TRAKTOR_HOME/3rdp/Everyplay/Everyplay.bundle bin/latest/ios/releasestatic/
@@ -40,26 +22,8 @@ fi
 
 if [ $CONFIG == "both" ] || [ $CONFIG == "debug" ] ; then
 
-	# All build outputs
-	TRAKTOR_BUILD_i386=$TRAKTOR_HOME/build/ios-i386/debugstatic
-	TRAKTOR_BUILD_x86_64=$TRAKTOR_HOME/build/ios-x86_64/debugstatic
-	TRAKTOR_BUILD_armv7=$TRAKTOR_HOME/build/ios-armv7/debugstatic
-	TRAKTOR_BUILD_armv7s=$TRAKTOR_HOME/build/ios-armv7s/debugstatic
-	TRAKTOR_BUILD_arm64=$TRAKTOR_HOME/build/ios-arm64/debugstatic
-
-	# Generate LIPO archive of all architectures.
-	pushd $TRAKTOR_HOME
-	mkdir -p bin/latest/ios/debugstatic
-
-	shopt -s nullglob
-
-	FILES="$TRAKTOR_BUILD_i386/*.a"
-	for FILE in $FILES
-	do
-		FILE=$(basename $FILE)
-		echo "Creating universal archive $FILE..."
-		lipo -create -output "bin/latest/ios/debugstatic/$FILE" -arch i386 "$TRAKTOR_BUILD_i386/$FILE" -arch x86_64 "$TRAKTOR_BUILD_x86_64/$FILE" -arch armv7 "$TRAKTOR_BUILD_armv7/$FILE" -arch armv7s "$TRAKTOR_BUILD_armv7s/$FILE" -arch arm64 "$TRAKTOR_BUILD_arm64/$FILE"
-	done
+	# Copy archives.
+	cp -R -f $TRAKTOR_HOME/build/ios/releasestatic/* bin/latest/ios/debugstatic/
 
 	# Copy 3rd-party frameworks.
 	cp -R -f $TRAKTOR_HOME/3rdp/Everyplay/Everyplay.bundle bin/latest/ios/debugstatic/
