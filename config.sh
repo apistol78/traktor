e1499f9d7ffc3bb06ff73080cbce6b035ca97f9b
Anders Pistol <anders.pistol@doctorentertainment.com>
2016-01-14 13:11:02 +0100

    Traktor: Updated SteamWorks SDK to 1.35
    [git-p4: depot-paths = "//depot/traktor/": change = 20057]

--- config.sh
+++ config.sh
@@ -5,7 +5,7 @@ TRAKTOR_HOME="`dirname \"$BASH_SOURCE\"`"
 TRAKTOR_HOME="`(cd \"$TRAKTOR_HOME\" && pwd)`"
 
 export TRAKTOR_HOME
-export STEAMWORKS_SDK=$TRAKTOR_HOME/3rdp/steamworks-sdk-134
+export STEAMWORKS_SDK=$TRAKTOR_HOME/3rdp/steamworks-sdk-135
 export BULLET_SDK=$TRAKTOR_HOME/3rdp/bullet3-master
 export FBX_SDK=$TRAKTOR_HOME/3rdp/FbxSdk/2013.1
 export GLSL_OPTIMIZER=$TRAKTOR_HOME/3rdp/glsl-optimizer-master-20141020
