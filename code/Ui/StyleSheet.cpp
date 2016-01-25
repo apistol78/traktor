d4785ff28b03925ba90c524b1490c94e86921498
Anders Pistol <anders.pistol@doctorentertainment.com>
2016-01-25 13:08:05 +0100

    Traktor: Fixed issue with solution builder not updating existing files. Also fixed some issues in SolutionBuilderUI.
    [git-p4: depot-paths = "//depot/traktor/": change = 20120]

--- code/Ui/StyleSheet.cpp
+++ code/Ui/StyleSheet.cpp
@@ -14,27 +14,58 @@ T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.ui.StyleSheet", 0, StyleSheet, ISeriali
 Ref< StyleSheet > StyleSheet::createDefault()
 {
 	Ref< StyleSheet > ss = new StyleSheet();
+
 	ss->setColor(L"traktor.ui.Widget", L"color", Color4ub(30, 30, 30));
 	ss->setColor(L"traktor.ui.Widget", L"color-disabled", Color4ub(60, 60, 60));
 	ss->setColor(L"traktor.ui.Widget", L"background-color", Color4ub(239, 239, 242));
+	ss->setColor(L"traktor.ui.Widget", L"background-color-disabled", Color4ub(230, 230, 235));
 	ss->setColor(L"traktor.ui.Widget", L"border-color", Color4ub(160, 160, 160));
+
+	ss->setColor(L"traktor.ui.ListBox", L"background-color", Color4ub(255, 255, 255));
+
+	ss->setColor(L"traktor.ui.Edit", L"background-color", Color4ub(255, 255, 255));
+	ss->setColor(L"traktor.ui.Edit", L"background-color-disabled", Color4ub(239, 239, 242));
+
 	ss->setColor(L"traktor.ui.Dock", L"caption-background-color", Color4ub(0, 122, 204));
 	ss->setColor(L"traktor.ui.Dock", L"caption-color-focus", Color4ub(255, 255, 255));
 	ss->setColor(L"traktor.ui.Dock", L"caption-color-no-focus", Color4ub(30, 30, 30));
 	ss->setColor(L"traktor.ui.Dock", L"splitter-color", Color4ub(204, 200, 219));
+
 	ss->setColor(L"traktor.ui.Tab", L"tab-background-color", Color4ub(0, 122, 204));
 	ss->setColor(L"traktor.ui.Tab", L"tab-background-color-hover", Color4ub(28, 151, 234));
 	ss->setColor(L"traktor.ui.Tab", L"tab-line-color", Color4ub(0, 122, 204));
 	ss->setColor(L"traktor.ui.Tab", L"tab-color-active", Color4ub(255, 255, 255));
 	ss->setColor(L"traktor.ui.Tab", L"tab-color-inactive", Color4ub(30, 30, 30));
+
+	ss->setColor(L"traktor.ui.custom.BackgroundWorkerDialog", L"border-color", Color4ub(0, 122, 204));
+
+	ss->setColor(L"traktor.ui.custom.ToolBar", L"item-background-color-pushed", Color4ub(0, 122, 204));
+	ss->setColor(L"traktor.ui.custom.ToolBar", L"item-background-color-hover", Color4ub(254, 254, 254));
+	ss->setColor(L"traktor.ui.custom.ToolBar", L"item-color-toggled", Color4ub(51, 153, 255));
+	ss->setColor(L"traktor.ui.custom.ToolBar", L"item-color-seperator", Color4ub(160, 160, 160));
+	ss->setColor(L"traktor.ui.custom.ToolBar", L"item-background-color-dropdown", Color4ub(239, 239, 242));
+	ss->setColor(L"traktor.ui.custom.ToolBar", L"item-background-color-dropdown-hover", Color4ub(254, 254, 254));
+	ss->setColor(L"traktor.ui.custom.ToolBar", L"item-color-dropdown-hover", Color4ub(255, 255, 255));
+	ss->setColor(L"traktor.ui.custom.ToolBar", L"item-background-color-dropdown-button", Color4ub(243, 243, 246));
+	ss->setColor(L"traktor.ui.custom.ToolBar", L"item-color-dropdown-arrow", Color4ub(20, 20, 20));
+
 	ss->setColor(L"traktor.ui.custom.TreeView", L"background-color", Color4ub(246, 246, 246));
 	ss->setColor(L"traktor.ui.custom.TreeView", L"item-color-selected", Color4ub(255, 255, 255));
 	ss->setColor(L"traktor.ui.custom.TreeView", L"item-background-color-selected", Color4ub(51, 153, 255));
+
+	ss->setColor(L"traktor.ui.custom.GradientStatic", L"background-color-left", Color4ub(239, 239, 242));
+	ss->setColor(L"traktor.ui.custom.GradientStatic", L"background-color-right", Color4ub(239, 239, 242));
+
 	ss->setColor(L"traktor.ui.custom.GridView", L"background-color", Color4ub(255, 255, 255));
 	ss->setColor(L"traktor.ui.custom.GridView", L"header-background-color", Color4ub(239, 239, 242));
 	ss->setColor(L"traktor.ui.custom.GridView", L"item-color-selected", Color4ub(255, 255, 255));
 	ss->setColor(L"traktor.ui.custom.GridView", L"item-background-color-selected", Color4ub(51, 153, 255));
 	ss->setColor(L"traktor.ui.custom.GridView", L"line-color", Color4ub(239, 239, 242));
+
+	ss->setColor(L"traktor.ui.custom.ScrollBar", L"background-color", Color4ub(219, 219, 222));
+	ss->setColor(L"traktor.ui.custom.ScrollBar", L"color-arrow", Color4ub(120, 120, 120));
+	ss->setColor(L"traktor.ui.custom.ScrollBar", L"color-slider", Color4ub(189, 189, 192));
+
 	return ss;
 }
 
