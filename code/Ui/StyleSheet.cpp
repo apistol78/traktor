d2dd644141d6d349190f46c6df91bcb1d6abe3ad
Anders Pistol <anders.pistol@doctorentertainment.com>
2016-01-28 13:47:36 +0100

    Traktor: Added default style settings of LogList.
    [git-p4: depot-paths = "//depot/traktor/": change = 20167]

--- code/Ui/StyleSheet.cpp
+++ code/Ui/StyleSheet.cpp
@@ -66,6 +66,14 @@ Ref< StyleSheet > StyleSheet::createDefault()
 	ss->setColor(L"traktor.ui.custom.ScrollBar", L"color-arrow", Color4ub(120, 120, 120));
 	ss->setColor(L"traktor.ui.custom.ScrollBar", L"color-slider", Color4ub(189, 189, 192));
 
+	ss->setColor(L"traktor.ui.custom.LogList", L"background-color", Color4ub(255, 255, 255));
+	ss->setColor(L"traktor.ui.custom.LogList", L"background-color-info", Color4ub(255, 255, 255, 0));
+	ss->setColor(L"traktor.ui.custom.LogList", L"background-color-warning", Color4ub(255, 210, 87));
+	ss->setColor(L"traktor.ui.custom.LogList", L"background-color-error", Color4ub(255, 45, 45));
+	ss->setColor(L"traktor.ui.custom.LogList", L"color-info", Color4ub(0, 0, 0));
+	ss->setColor(L"traktor.ui.custom.LogList", L"color-warning", Color4ub(0, 0, 0));
+	ss->setColor(L"traktor.ui.custom.LogList", L"color-error", Color4ub(0, 0, 0));
+
 	return ss;
 }
 
