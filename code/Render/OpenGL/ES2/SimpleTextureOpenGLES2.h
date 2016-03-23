1e0a0cb444c9b554e37e91fab5be5ce37944985d
Anders Pistol <anders.pistol@doctorentertainment.com>
2016-03-23 17:43:04 +0100

    Traktor: Using glTexSubImage2D to update dynamic ES2 textures.
    [git-p4: depot-paths = "//depot/traktor/": change = 20500]

--- code/Render/OpenGL/ES2/SimpleTextureOpenGLES2.h
+++ code/Render/OpenGL/ES2/SimpleTextureOpenGLES2.h
@@ -1,6 +1,7 @@
 #ifndef traktor_render_SimpleTextureOpenGLES2_H
 #define traktor_render_SimpleTextureOpenGLES2_H
 
+#include "Core/Containers/AlignedVector.h"
 #include "Core/Math/Vector4.h"
 #include "Render/ISimpleTexture.h"
 #include "Render/Types.h"
@@ -29,21 +30,21 @@ public:
 	
 	bool create(const SimpleTextureCreateDesc& desc);
 
-	virtual void destroy();
+	virtual void destroy() T_OVERRIDE T_FINAL;
 
-	virtual ITexture* resolve();
+	virtual ITexture* resolve() T_OVERRIDE T_FINAL;
 
-	virtual int getWidth() const;
+	virtual int getWidth() const T_OVERRIDE T_FINAL;
 	
-	virtual int getHeight() const;
+	virtual int getHeight() const T_OVERRIDE T_FINAL;
 
-	virtual bool lock(int level, Lock& lock);
+	virtual bool lock(int level, Lock& lock) T_OVERRIDE T_FINAL;
 
-	virtual void unlock(int level);
+	virtual void unlock(int level) T_OVERRIDE T_FINAL;
 
-	virtual void bindSampler(GLuint unit, const SamplerStateOpenGL& samplerState, GLint locationTexture);
+	virtual void bindSampler(GLuint unit, const SamplerStateOpenGL& samplerState, GLint locationTexture) T_OVERRIDE T_FINAL;
 
-	virtual void bindSize(GLint locationSize);
+	virtual void bindSize(GLint locationSize) T_OVERRIDE T_FINAL;
 
 private:
 	Ref< ContextOpenGLES2 > m_context;
@@ -56,7 +57,7 @@ private:
 	GLenum m_format;
 	GLenum m_type;
 	uint32_t m_mipCount;
-	std::vector< uint8_t > m_data;
+	AlignedVector< uint8_t > m_data;
 	SamplerStateOpenGL m_shadowState;
 };
 		
