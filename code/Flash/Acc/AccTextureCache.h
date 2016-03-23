c2348ce377d2893ca538fd0736dfc75d0ae343d8
Anders Pistol <anders.pistol@doctorentertainment.com>
2016-03-23 17:32:54 +0100

    Traktor: Reusing dynamic textures in AccTextureCache instead of destroy/create.
    [git-p4: depot-paths = "//depot/traktor/": change = 20499]

--- code/Flash/Acc/AccTextureCache.h
+++ code/Flash/Acc/AccTextureCache.h
@@ -2,6 +2,7 @@
 #define traktor_flash_AccTextureCache_H
 
 #include "Core/Object.h"
+#include "Core/RefArray.h"
 
 namespace traktor
 {
@@ -41,11 +42,16 @@ public:
 
 	void destroy();
 
+	void clear();
+
 	Ref< AccBitmapRect > getBitmapTexture(const FlashBitmap& bitmap);
+
+	void freeTexture(render::ISimpleTexture* texture);
 
 private:
 	Ref< resource::IResourceManager > m_resourceManager;
 	Ref< render::IRenderSystem > m_renderSystem;
+	RefArray< render::ISimpleTexture > m_freeTextures;
 };
 
 	}
