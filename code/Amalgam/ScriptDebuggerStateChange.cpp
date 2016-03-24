2b72b81b4fc1aae2981730c304a390dff44c7f5d
Anders Pistol <anders.pistol@doctorentertainment.com>
2016-03-24 08:21:45 +0100

    Traktor: A bit more reliable debugee state change handling of remote targets.
    [git-p4: depot-paths = "//depot/traktor/": change = 20504]

--- code/Amalgam/ScriptDebuggerStateChange.cpp
+++ code/Amalgam/ScriptDebuggerStateChange.cpp
@@ -1,4 +1,6 @@
 #include "Amalgam/ScriptDebuggerStateChange.h"
+#include "Core/Serialization/ISerializer.h"
+#include "Core/Serialization/Member.h"
 
 namespace traktor
 {
@@ -7,8 +9,19 @@ namespace traktor
 
 T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.amalgam.ScriptDebuggerStateChange", 0, ScriptDebuggerStateChange, ISerializable)
 
+ScriptDebuggerStateChange::ScriptDebuggerStateChange()
+:	m_running(false)
+{
+}
+
+ScriptDebuggerStateChange::ScriptDebuggerStateChange(bool running)
+:	m_running(running)
+{
+}
+
 void ScriptDebuggerStateChange::serialize(ISerializer& s)
 {
+	s >> Member< bool >(L"running", m_running);
 }
 
 	}
