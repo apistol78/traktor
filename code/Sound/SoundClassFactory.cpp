bbc9953ac4a793da4e838355a16d503938bf30f2
Anders Pistol <anders.pistol@doctorentertainment.com>
2016-03-29 19:52:32 +0100

    Traktor: More sound classes exposed to script.
    [git-p4: depot-paths = "//depot/traktor/": change = 20518]

--- code/Sound/SoundClassFactory.cpp
+++ code/Sound/SoundClassFactory.cpp
@@ -2,6 +2,8 @@
 #include "Core/Class/Boxes.h"
 #include "Core/Class/IRuntimeClassRegistrar.h"
 #include "Sound/IFilter.h"
+#include "Sound/ISoundDriver.h"
+#include "Sound/ISoundMixer.h"
 #include "Sound/Sound.h"
 #include "Sound/SoundChannel.h"
 #include "Sound/SoundClassFactory.h"
@@ -18,6 +20,27 @@
 #include "Sound/Filters/SurroundFilter.h"
 #include "Sound/Player/ISoundHandle.h"
 #include "Sound/Player/ISoundPlayer.h"
+#include "Sound/Resound/BankBuffer.h"
+#include "Sound/Resound/BlendGrain.h"
+#include "Sound/Resound/BlendGrainData.h"
+#include "Sound/Resound/EnvelopeGrain.h"
+#include "Sound/Resound/EnvelopeGrainData.h"
+#include "Sound/Resound/InLoopOutGrain.h"
+#include "Sound/Resound/InLoopOutGrainData.h"
+#include "Sound/Resound/MuteGrain.h"
+#include "Sound/Resound/MuteGrainData.h"
+#include "Sound/Resound/PlayGrain.h"
+#include "Sound/Resound/PlayGrainData.h"
+#include "Sound/Resound/RandomGrain.h"
+#include "Sound/Resound/RandomGrainData.h"
+#include "Sound/Resound/RepeatGrain.h"
+#include "Sound/Resound/RepeatGrainData.h"
+#include "Sound/Resound/SequenceGrain.h"
+#include "Sound/Resound/SequenceGrainData.h"
+#include "Sound/Resound/SimultaneousGrain.h"
+#include "Sound/Resound/SimultaneousGrainData.h"
+#include "Sound/Resound/TriggerGrain.h"
+#include "Sound/Resound/TriggerGrainData.h"
 
 namespace traktor
 {
@@ -28,6 +51,16 @@ T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.sound.SoundClassFactory", 0, SoundClass
 
 void SoundClassFactory::createClasses(IRuntimeClassRegistrar* registrar) const
 {
+	Ref< AutoRuntimeClass< Sound > > classSound = new AutoRuntimeClass< Sound >();
+	classSound->addConstructor< ISoundBuffer*, handle_t, float, float, float, float >();
+	classSound->addMethod("getBuffer", &Sound::getBuffer);
+	classSound->addMethod("getCategory", &Sound::getCategory);
+	classSound->addMethod("getVolume", &Sound::getVolume);
+	classSound->addMethod("getPresence", &Sound::getPresence);
+	classSound->addMethod("getPresenceRate", &Sound::getPresenceRate);
+	classSound->addMethod("getRange", &Sound::getRange);
+	registrar->registerClass(classSound);
+
 	Ref< AutoRuntimeClass< SoundSystem > > classSoundSystem = new AutoRuntimeClass< SoundSystem >();
 	classSoundSystem->addMethod("getChannel", &SoundSystem::getChannel);
 	registrar->registerClass(classSoundSystem);
@@ -38,6 +71,15 @@ void SoundClassFactory::createClasses(IRuntimeClassRegistrar* registrar) const
 	classSoundChannel->addMethod("isPlaying", &SoundChannel::isPlaying);
 	classSoundChannel->addMethod("stop", &SoundChannel::stop);
 	registrar->registerClass(classSoundChannel);
+
+	Ref< AutoRuntimeClass< ISoundBuffer > > classISoundBuffer = new AutoRuntimeClass< ISoundBuffer >();
+	registrar->registerClass(classISoundBuffer);
+
+	Ref< AutoRuntimeClass< ISoundDriver > > classISoundDriver = new AutoRuntimeClass< ISoundDriver >();
+	registrar->registerClass(classISoundDriver);
+
+	Ref< AutoRuntimeClass< ISoundMixer > > classISoundMixer = new AutoRuntimeClass< ISoundMixer >();
+	registrar->registerClass(classISoundMixer);
 
 	Ref< AutoRuntimeClass< IFilter > > classIFilter = new AutoRuntimeClass< IFilter >();
 	registrar->registerClass(classIFilter);
@@ -126,6 +168,86 @@ void SoundClassFactory::createClasses(IRuntimeClassRegistrar* registrar) const
 	classISoundPlayer->addMethod("setListenerTransform", &ISoundPlayer::setListenerTransform);
 	classISoundPlayer->addMethod("getListenerTransform", &ISoundPlayer::getListenerTransform);
 	registrar->registerClass(classISoundPlayer);
+
+	Ref< AutoRuntimeClass< BankBuffer > > classBankBuffer = new AutoRuntimeClass< BankBuffer >();
+	classBankBuffer->addConstructor< const RefArray< IGrain >& >();
+	registrar->registerClass(classBankBuffer);
+
+	Ref< AutoRuntimeClass< IGrain > > classIGrain = new AutoRuntimeClass< IGrain >();
+	registrar->registerClass(classIGrain);
+
+	Ref< AutoRuntimeClass< IGrainData > > classIGrainData = new AutoRuntimeClass< IGrainData >();
+	registrar->registerClass(classIGrainData);
+
+	Ref< AutoRuntimeClass< BlendGrain > > classBlendGrain = new AutoRuntimeClass< BlendGrain >();
+	registrar->registerClass(classBlendGrain);
+
+	Ref< AutoRuntimeClass< BlendGrainData > > classBlendGrainData = new AutoRuntimeClass< BlendGrainData >();
+	classBlendGrainData->addConstructor();
+	registrar->registerClass(classBlendGrainData);
+
+	Ref< AutoRuntimeClass< EnvelopeGrain > > classEnvelopeGrain = new AutoRuntimeClass< EnvelopeGrain >();
+	registrar->registerClass(classEnvelopeGrain);
+
+	Ref< AutoRuntimeClass< EnvelopeGrainData > > classEnvelopeGrainData = new AutoRuntimeClass< EnvelopeGrainData >();
+	classEnvelopeGrainData->addConstructor();
+	registrar->registerClass(classEnvelopeGrainData);
+
+	Ref< AutoRuntimeClass< InLoopOutGrain > > classInLoopOutGrain = new AutoRuntimeClass< InLoopOutGrain >();
+	registrar->registerClass(classInLoopOutGrain);
+
+	Ref< AutoRuntimeClass< InLoopOutGrainData > > classInLoopOutGrainData = new AutoRuntimeClass< InLoopOutGrainData >();
+	classInLoopOutGrainData->addConstructor();
+	registrar->registerClass(classInLoopOutGrainData);
+
+	Ref< AutoRuntimeClass< MuteGrain > > classMuteGrain = new AutoRuntimeClass< MuteGrain >();
+	registrar->registerClass(classMuteGrain);
+
+	Ref< AutoRuntimeClass< MuteGrainData > > classMuteGrainData = new AutoRuntimeClass< MuteGrainData >();
+	classMuteGrainData->addConstructor();
+	registrar->registerClass(classMuteGrainData);
+
+	Ref< AutoRuntimeClass< PlayGrain > > classPlayGrain = new AutoRuntimeClass< PlayGrain >();
+	registrar->registerClass(classPlayGrain);
+
+	Ref< AutoRuntimeClass< PlayGrainData > > classPlayGrainData = new AutoRuntimeClass< PlayGrainData >();
+	classPlayGrainData->addConstructor();
+	registrar->registerClass(classPlayGrainData);
+
+	Ref< AutoRuntimeClass< RandomGrain > > classRandomGrain = new AutoRuntimeClass< RandomGrain >();
+	registrar->registerClass(classRandomGrain);
+
+	Ref< AutoRuntimeClass< RandomGrainData > > classRandomGrainData = new AutoRuntimeClass< RandomGrainData >();
+	classRandomGrainData->addConstructor();
+	registrar->registerClass(classRandomGrainData);
+
+	Ref< AutoRuntimeClass< RepeatGrain > > classRepeatGrain = new AutoRuntimeClass< RepeatGrain >();
+	registrar->registerClass(classRepeatGrain);
+
+	Ref< AutoRuntimeClass< RepeatGrainData > > classRepeatGrainData = new AutoRuntimeClass< RepeatGrainData >();
+	classRepeatGrainData->addConstructor();
+	registrar->registerClass(classRepeatGrainData);
+
+	Ref< AutoRuntimeClass< SequenceGrain > > classSequenceGrain = new AutoRuntimeClass< SequenceGrain >();
+	registrar->registerClass(classSequenceGrain);
+
+	Ref< AutoRuntimeClass< SequenceGrainData > > classSequenceGrainData = new AutoRuntimeClass< SequenceGrainData >();
+	classSequenceGrainData->addConstructor();
+	registrar->registerClass(classSequenceGrainData);
+
+	Ref< AutoRuntimeClass< SimultaneousGrain > > classSimultaneousGrain = new AutoRuntimeClass< SimultaneousGrain >();
+	registrar->registerClass(classSimultaneousGrain);
+
+	Ref< AutoRuntimeClass< SimultaneousGrainData > > classSimultaneousGrainData = new AutoRuntimeClass< SimultaneousGrainData >();
+	classSimultaneousGrainData->addConstructor();
+	registrar->registerClass(classSimultaneousGrainData);
+
+	Ref< AutoRuntimeClass< TriggerGrain > > classTriggerGrain = new AutoRuntimeClass< TriggerGrain >();
+	registrar->registerClass(classTriggerGrain);
+
+	Ref< AutoRuntimeClass< TriggerGrainData > > classTriggerGrainData = new AutoRuntimeClass< TriggerGrainData >();
+	classTriggerGrainData->addConstructor();
+	registrar->registerClass(classTriggerGrainData);
 }
 
 	}
