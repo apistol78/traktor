dec035b6305be8ffb7e6cb5374c5ccfd33ccf8ba
Anders Pistol <anders.pistol@doctorentertainment.com>
2016-03-31 11:47:39 +0100

    Traktor: Exposed more resource classes to script.
    [git-p4: depot-paths = "//depot/traktor/": change = 20528]

--- code/Resource/ResourceClassFactory.cpp
+++ code/Resource/ResourceClassFactory.cpp
@@ -1,18 +1,40 @@
 #include "Core/Class/AutoRuntimeClass.h"
+#include "Core/Class/Boxes.h"
 #include "Core/Class/IRuntimeClassRegistrar.h"
 #include "Resource/IResourceFactory.h"
 #include "Resource/IResourceManager.h"
+#include "Resource/ResourceBundle.h"
 #include "Resource/ResourceClassFactory.h"
 
 namespace traktor
 {
 	namespace resource
 	{
+		namespace
+		{
+
+Ref< ResourceHandle > IResourceManager_bind(IResourceManager* self, const TypeInfo& type, const Guid& guid)
+{
+	return self->bind(type, guid);
+}
+
+void IResourceManager_reload(IResourceManager* self, const Any& guidOrType, bool flushedOnly)
+{
+	if (CastAny< Guid >::accept(guidOrType))
+		self->reload(CastAny< Guid >::get(guidOrType), flushedOnly);
+	else if (CastAny< TypeInfo >::accept(guidOrType))
+		self->reload(CastAny< TypeInfo >::get(guidOrType), flushedOnly);
+}
+
+		}
 
 T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.resource.ResourceClassFactory", 0, ResourceClassFactory, IRuntimeClassFactory)
 
 void ResourceClassFactory::createClasses(IRuntimeClassRegistrar* registrar) const
 {
+	Ref< AutoRuntimeClass< ResourceBundle > > classResourceBundle = new AutoRuntimeClass< ResourceBundle >();
+	registrar->registerClass(classResourceBundle);
+
 	Ref< AutoRuntimeClass< IResourceFactory > > classIResourceFactory = new AutoRuntimeClass< IResourceFactory >();
 	classIResourceFactory->addMethod("isCacheable", &IResourceFactory::isCacheable);
 	registrar->registerClass(classIResourceFactory);
@@ -27,6 +49,11 @@ void ResourceClassFactory::createClasses(IRuntimeClassRegistrar* registrar) cons
 	classIResourceManager->addMethod("addFactory", &IResourceManager::addFactory);
 	classIResourceManager->addMethod("removeFactory", &IResourceManager::removeFactory);
 	classIResourceManager->addMethod("removeAllFactories", &IResourceManager::removeAllFactories);
+	classIResourceManager->addMethod("load", &IResourceManager::load);
+	classIResourceManager->addMethod("bind", &IResourceManager_bind);
+	classIResourceManager->addMethod("reload", &IResourceManager_reload);
+	classIResourceManager->addMethod("unload", &IResourceManager::unload);
+	classIResourceManager->addMethod("unloadUnusedResident", &IResourceManager::unloadUnusedResident);
 	registrar->registerClass(classIResourceManager);
 }
 
