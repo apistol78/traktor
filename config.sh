d1c1584c941af092196e2ee96f7db56170530675
Jesper Rudberg <jesper.rudberg@doctorentertainment.com>
2016-04-08 20:08:05 +0100

    Traktor: Latest FBX libs for linux.
    [git-p4: depot-paths = "//depot/traktor/": change = 20605]

--- config.sh
+++ config.sh
@@ -7,7 +7,7 @@ TRAKTOR_HOME="`(cd \"$TRAKTOR_HOME\" && pwd)`"
 export TRAKTOR_HOME
 export STEAMWORKS_SDK=$TRAKTOR_HOME/3rdp/steamworks-sdk-135
 export BULLET_SDK=$TRAKTOR_HOME/3rdp/bullet3-master
-export FBX_SDK=$TRAKTOR_HOME/3rdp/FbxSdk/2013.1
+export FBX_SDK=$TRAKTOR_HOME/3rdp/FbxSdk/2016.1.2
 export GLSL_OPTIMIZER=$TRAKTOR_HOME/3rdp/glsl-optimizer-master-20141020
 export SQLITE_SDK=$TRAKTOR_HOME/3rdp/sqlite-3.8.7.2
 export WXWIDGETS_SDK=$TRAKTOR_HOME/3rdp/wxWidgets-3.0.2
