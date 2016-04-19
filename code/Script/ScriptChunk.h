2ebaa3866f7172a326511e8e803c2261fe00f57d
Anders Pistol <anders.pistol@doctorentertainment.com>
2016-04-19 15:16:25 +0100

    Traktor: Fixed script hot reloading, each script is "wrapped" into a ScriptChunk resource which is tracked by the resource manager and thus are able to reload only actual modified script.
    [git-p4: depot-paths = "//depot/traktor/": change = 20645]

---
+++ code/Script/ScriptChunk.h
@@ -0,0 +1,22 @@
+#ifndef traktor_script_ScriptChunk_H
+#define traktor_script_ScriptChunk_H
+
+#include "Core/Object.h"
+
+namespace traktor
+{
+	namespace script
+	{
+
+/*! \brief
+ * \ingroup Script
+ */
+class ScriptChunk : public Object
+{
+	T_RTTI_CLASS;
+};
+
+	}
+}
+
+#endif	// traktor_script_ScriptChunk_H
