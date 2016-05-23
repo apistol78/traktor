8d41765be5735efe36f3978d187cfc1601f37a55
Anders Pistol <anders.pistol@doctorentertainment.com>
2016-05-23 13:30:53 +0100

    Traktor: Implemented option to automatically follow and open debugged script.
    [git-p4: depot-paths = "//depot/traktor/": change = 20741]

---
+++ code/Script/Editor/ScriptSettingsPage.cpp
@@ -0,0 +1,42 @@
+#include "Core/Misc/String.h"
+#include "Core/Settings/PropertyBoolean.h"
+#include "Core/Settings/PropertyGroup.h"
+#include "I18N/Text.h"
+#include "Script/Editor/ScriptSettingsPage.h"
+#include "Ui/CheckBox.h"
+#include "Ui/Container.h"
+#include "Ui/TableLayout.h"
+
+namespace traktor
+{
+	namespace script
+	{
+
+T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.script.ScriptSettingsPage", 0, ScriptSettingsPage, editor::ISettingsPage)
+
+bool ScriptSettingsPage::create(ui::Container* parent, PropertyGroup* settings, const std::list< ui::Command >& shortcutCommands)
+{
+	Ref< ui::Container > container = new ui::Container();
+	if (!container->create(parent, ui::WsNone, new ui::TableLayout(L"100%", L"*", 0, 4)))
+		return false;
+
+	m_checkAutoOpenScript = new ui::CheckBox();
+	m_checkAutoOpenScript->create(container, i18n::Text(L"EDITOR_SETTINGS_SCRIPT_AUTO_OPEN"), false);
+	m_checkAutoOpenScript->setChecked(settings->getProperty< PropertyBoolean >(L"Editor.AutoOpenDebuggedScript", true));
+
+	parent->setText(i18n::Text(L"EDITOR_SETTINGS_SCRIPT"));
+	return true;
+}
+
+void ScriptSettingsPage::destroy()
+{
+}
+
+bool ScriptSettingsPage::apply(PropertyGroup* settings)
+{
+	settings->setProperty< PropertyBoolean >(L"Editor.AutoOpenDebuggedScript", m_checkAutoOpenScript->isChecked());
+	return true;
+}
+
+	}
+}
---
+++ code/Script/Editor/ScriptSettingsPage.h
@@ -0,0 +1,47 @@
+#ifndef traktor_script_ScriptSettingsPage_H
+#define traktor_script_ScriptSettingsPage_H
+
+#include "Editor/ISettingsPage.h"
+
+// import/export mechanism.
+#undef T_DLLCLASS
+#if defined(T_SCRIPT_EDITOR_EXPORT)
+#	define T_DLLCLASS T_DLLEXPORT
+#else
+#	define T_DLLCLASS T_DLLIMPORT
+#endif
+
+namespace traktor
+{
+	namespace ui
+	{
+
+class CheckBox;
+
+	}
+
+	namespace script
+	{
+
+/*! \brief Editor script settings page.
+ * \ingroup Script
+ */
+class T_DLLCLASS ScriptSettingsPage : public editor::ISettingsPage
+{
+	T_RTTI_CLASS;
+
+public:
+	virtual bool create(ui::Container* parent, PropertyGroup* settings, const std::list< ui::Command >& shortcutCommands) T_OVERRIDE T_FINAL;
+
+	virtual void destroy() T_OVERRIDE T_FINAL;
+
+	virtual bool apply(PropertyGroup* settings) T_OVERRIDE T_FINAL;
+
+private:
+	Ref< ui::CheckBox > m_checkAutoOpenScript;
+};
+
+	}
+}
+
+#endif	// traktor_script_ScriptSettingsPage_H
