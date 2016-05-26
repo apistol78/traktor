e1cd61b9f7ceaec087a598d41049f94a27fef9b6
Anders Pistol <anders.pistol@doctorentertainment.com>
2016-05-26 13:11:47 +0100

    Traktor: Fixed thread issue with Semaphore on OSX and Linux (same implementation) which was causing crash when entering match in Gear Up.
    [git-p4: depot-paths = "//depot/traktor/": change = 20756]

--- code/Core/Thread/OsX/Semaphore.cpp
+++ code/Core/Thread/OsX/Semaphore.cpp
@@ -1,8 +1,4 @@
 #include <pthread.h>
-#include <sys/time.h>
-#include "Core/Misc/TString.h"
-#include "Core/Thread/Atomic.h"
-#include "Core/Thread/Event.h"
 #include "Core/Thread/Semaphore.h"
 
 namespace traktor
@@ -12,77 +8,46 @@ namespace traktor
 
 struct InternalData
 {
-	int32_t count;
-	int32_t value;
-	pthread_t owner;
-	Event event;
+	pthread_mutex_t outer;
 };
 
 	}
-
+	
 Semaphore::Semaphore()
 :	m_handle(0)
 {
-	InternalData* in = new InternalData();
+	InternalData* data = new InternalData();
+	std::memset(data, 0, sizeof(InternalData));
 
-	in->count = 0;
-	in->value = 0;
-	in->owner = 0;
+	pthread_mutexattr_t ma;
+	pthread_mutexattr_init(&ma);
+	pthread_mutexattr_settype(&ma, PTHREAD_MUTEX_RECURSIVE);
 
-	m_handle = in;
+	int rc = pthread_mutex_init(&data->outer, &ma);
+	T_ASSERT (rc == 0);
+
+	m_handle = data;
 }
 
 Semaphore::~Semaphore()
 {
-	InternalData* in = reinterpret_cast< InternalData* >(m_handle);
-	delete in;
+	delete reinterpret_cast< InternalData* >(m_handle);
 }
 
 bool Semaphore::wait(int32_t timeout)
 {
-	InternalData* in = reinterpret_cast< InternalData* >(m_handle);
-
-	pthread_t current = pthread_self();
-	T_ASSERT (current != 0);
-	
-	if (Atomic::increment(in->value) > 1)
-	{
-		if (pthread_equal(in->owner, current) != 0)
-		{
-			T_ASSERT (in->count > 0);
-			in->count++;
-			return true;
-		}
-		
-		if (!in->event.wait(timeout))
-		{
-			Atomic::decrement(in->value);
-			return false;
-		}
-	}
-	
-	T_ASSERT (in->owner == 0);
-	in->owner = current;
-	in->count = 1;
-
+	InternalData* data = reinterpret_cast< InternalData* >(m_handle);
+	while (pthread_mutex_lock(&data->outer) != 0)
+		;
 	return true;
 }
 
 void Semaphore::release()
 {
-	InternalData* in = reinterpret_cast< InternalData* >(m_handle);
-	T_ASSERT (pthread_equal(in->owner, pthread_self()) != 0);
-	
-	if (--in->count > 0)
-	{
-		Atomic::decrement(in->value);
-	}
-	else
-	{
-		in->owner = 0;
-		if (Atomic::decrement(in->value) > 0)
-			in->event.pulse();
-	}
+	InternalData* data = reinterpret_cast< InternalData* >(m_handle);
+
+	int rc = pthread_mutex_unlock(&data->outer);
+	T_ASSERT(rc == 0);
 }
 
 }
