f8604c95f87d34161c138b1d0e524dcb8ef5ab19
Anders Pistol <anders.pistol@doctorentertainment.com>
2016-05-26 12:47:24 +0100

    Traktor: OSX fixes, mainly rewritten command line parser but also enabled support for ioctl on sockets.
    [git-p4: depot-paths = "//depot/traktor/": change = 20754]

--- code/Core/Misc/CommandLine.h
+++ code/Core/Misc/CommandLine.h
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <string>
 #include <vector>
+#include "Core/Io/StringOutputStream.h"
 #include "Core/Misc/Split.h"
 #include "Core/Misc/String.h"
 #include "Core/Misc/TString.h"
@@ -25,34 +26,34 @@ public:
 		friend class CommandLine;
 
 	public:
-		inline Option(const std::wstring& name = L"", const std::wstring& value = L"")
+		Option(const std::wstring& name = L"", const std::wstring& value = L"")
 		:	m_name(name)
 		,	m_value(value)
 		{
 		}
 
-		inline Option(const Option& src)
+		Option(const Option& src)
 		:	m_name(src.m_name)
 		,	m_value(src.m_value)
 		{
 		}
 
-		inline wchar_t getShortName() const
+		wchar_t getShortName() const
 		{
 			return m_name.length() == 1 ? m_name[0] : 0;
 		}
 
-		inline const std::wstring& getLongName() const
+		const std::wstring& getLongName() const
 		{
 			return m_name;
 		}
 
-		inline int32_t getInteger() const
+		int32_t getInteger() const
 		{
 			return parseString< int32_t >(m_value);
 		}
 
-		inline const std::wstring& getString() const
+		const std::wstring& getString() const
 		{
 			return m_value;
 		}
@@ -62,85 +63,170 @@ public:
 		std::wstring m_value;
 	};
 
-	inline CommandLine(int argc, const char** argv)
+	explicit CommandLine(int argc, const char** argv)
 	{
 		m_file = mbstows(argv[0]);
+
+		StringOutputStream ss;
 		for (int i = 1; i < argc; ++i)
 		{
-			if (argv[i][0] == L'-')
-			{
-				const char* value = std::strchr(argv[i], '=');
-				if (value)
-					m_opts.push_back(Option(
-						mbstows(std::string(&argv[i][1], value)),
-						mbstows(value + 1)
-					));
-				else
-					m_opts.push_back(Option(
-						mbstows(std::string(&argv[i][1])),
-						L""
-					));
-			}
-			else
-				m_args.push_back(mbstows(argv[i]));
+			if (i > 1)
+				ss << L" ";
+			ss << mbstows(argv[i]);
 		}
+		parse(ss.str());
 	}
 
-	inline CommandLine(const std::vector< std::wstring >& argv)
+	explicit CommandLine(const std::vector< std::wstring >& argv)
 	{
 		m_file = argv[0];
+
+		StringOutputStream ss;
 		for (size_t i = 1; i < argv.size(); ++i)
 		{
-			if (argv[i][0] == L'-')
-			{
-				const wchar_t* cs = argv[i].c_str() + 1;
-				const wchar_t* value = wcschr(cs, L'=');
-				if (value)
-					m_opts.push_back(Option(
-						std::wstring(cs, value),
-						value + 1
-					));
-				else
-					m_opts.push_back(Option(
-						std::wstring(cs),
-						L""
-					));
-			}
-			else
-				m_args.push_back(argv[i]);
+			if (i > 1)
+				ss << L" ";
+			ss << argv[i];
 		}
+		parse(ss.str());
 	}
 
-	inline CommandLine(const std::wstring& file, const std::wstring& args)
+	explicit CommandLine(const std::wstring& file, const std::wstring& args)
 	:	m_file(file)
 	{
-		std::vector< std::wstring > argv;
+		parse(args);
+	}
 
-		size_t pos = 0;
-		for (size_t i = 0; i <= args.length(); )
+	const std::wstring& getFile() const
+	{
+		return m_file;
+	}
+
+	bool hasOption(wchar_t shortName) const
+	{
+		for (size_t i = 0; i < m_opts.size(); ++i)
 		{
-			bool eol = i >= args.length();
-			wchar_t ch = !eol ? args[i] : L' ';
+			if (m_opts[i].getShortName() == shortName)
+				return true;
+		}
+		return false;
+	}
 
+	const Option& getOption(wchar_t shortName) const
+	{
+		for (size_t i = 0; i < m_opts.size(); ++i)
+		{
+			if (m_opts[i].getShortName() == shortName)
+				return m_opts[i];
+		}
+		return m_invOpt;
+	}
+
+	bool hasOption(const std::wstring& longName) const
+	{
+		for (size_t i = 0; i < m_opts.size(); ++i)
+		{
+			if (m_opts[i].getLongName() == longName)
+				return true;
+		}
+		return false;
+	}
+
+	const Option& getOption(const std::wstring& longName) const
+	{
+		for (size_t i = 0; i < m_opts.size(); ++i)
+		{
+			if (m_opts[i].getLongName() == longName)
+				return m_opts[i];
+		}
+		return m_invOpt;
+	}
+
+	bool hasOption(wchar_t shortName, const std::wstring& longName) const
+	{
+		for (size_t i = 0; i < m_opts.size(); ++i)
+		{
+			if (m_opts[i].getShortName() == shortName || m_opts[i].getLongName() == longName)
+				return true;
+		}
+		return false;
+	}
+
+	const Option& getOption(wchar_t shortName, const std::wstring& longName) const
+	{
+		for (size_t i = 0; i < m_opts.size(); ++i)
+		{
+			if (m_opts[i].getShortName() == shortName || m_opts[i].getLongName() == longName)
+				return m_opts[i];
+		}
+		return m_invOpt;
+	}
+
+	size_t getCount() const
+	{
+		return m_args.size();
+	}
+
+	int32_t getInteger(size_t index) const
+	{
+		return parseString< int32_t >(m_args[index]);
+	}
+
+	const std::wstring& getString(size_t index) const
+	{
+		return m_args[index];
+	}
+
+private:
+	std::wstring m_file;
+	std::vector< Option > m_opts;
+	std::vector< std::wstring > m_args;
+	Option m_invOpt;
+
+	std::wstring trim(const std::wstring& s) const
+	{
+		if (s.size() >= 2 && s.front() == L'\"' && s.back() == L'\"')
+			return s.substr(1, s.length() - 2);
+		else
+			return s;
+	}
+
+	void parse(const std::wstring& args)
+	{
+		std::vector< std::wstring > argv;
+		wchar_t ch;
+
+		for (size_t i = 0; i < args.length(); )
+		{
+			ch = args[i];
 			if (ch == L' ' || ch == L'\t')
 			{
-				size_t len = i - pos;
-				if (len > 0)
-					argv.push_back(args.substr(pos, len));
-				pos = ++i;
-			}
-			else if (ch == L'\"')
-			{
-				pos = ++i;
-				while (i < args.length() && args[i] != L'\"')
-					++i;
-				size_t len = i - pos;
-				if (len > 0)
-					argv.push_back(args.substr(pos, len));
-				pos = ++i;
-			}
-			else
 				++i;
+				continue;
+			}
+
+			size_t s = i++;
+			while (i < args.length())
+			{
+				ch = args[i];
+				if (ch == L' ' || ch == '\t')
+					break;
+
+				++i;
+
+				if (ch == L'\"')
+				{
+					while (i < args.length())
+					{
+						ch = args[i++];
+						if (ch == L'\"')
+							break;
+					}
+				}
+			}
+
+			if (i > s)
+				argv.push_back(args.substr(s, i - s));
 		}
 
 		for (size_t i = 0; i < argv.size(); ++i)
@@ -152,7 +238,7 @@ public:
 				if (value)
 					m_opts.push_back(Option(
 						std::wstring(cs, value),
-						value + 1
+						trim(value + 1)
 					));
 				else
 					m_opts.push_back(Option(
@@ -161,95 +247,9 @@ public:
 					));
 			}
 			else
-				m_args.push_back(argv[i]);
-		}
-	}
-
-	inline const std::wstring& getFile() const
-	{
-		return m_file;
-	}
-
-	inline bool hasOption(wchar_t shortName) const
-	{
-		for (size_t i = 0; i < m_opts.size(); ++i)
-		{
-			if (m_opts[i].getShortName() == shortName)
-				return true;
-		}
-		return false;
-	}
-
-	inline const Option& getOption(wchar_t shortName) const
-	{
-		for (size_t i = 0; i < m_opts.size(); ++i)
-		{
-			if (m_opts[i].getShortName() == shortName)
-				return m_opts[i];
-		}
-		return m_invOpt;
-	}
-
-	inline bool hasOption(const std::wstring& longName) const
-	{
-		for (size_t i = 0; i < m_opts.size(); ++i)
-		{
-			if (m_opts[i].getLongName() == longName)
-				return true;
-		}
-		return false;
-	}
-
-	inline const Option& getOption(const std::wstring& longName) const
-	{
-		for (size_t i = 0; i < m_opts.size(); ++i)
-		{
-			if (m_opts[i].getLongName() == longName)
-				return m_opts[i];
-		}
-		return m_invOpt;
-	}
-
-	inline bool hasOption(wchar_t shortName, const std::wstring& longName) const
-	{
-		for (size_t i = 0; i < m_opts.size(); ++i)
-		{
-			if (m_opts[i].getShortName() == shortName || m_opts[i].getLongName() == longName)
-				return true;
-		}
-		return false;
-	}
-
-	inline const Option& getOption(wchar_t shortName, const std::wstring& longName) const
-	{
-		for (size_t i = 0; i < m_opts.size(); ++i)
-		{
-			if (m_opts[i].getShortName() == shortName || m_opts[i].getLongName() == longName)
-				return m_opts[i];
-		}
-		return m_invOpt;
-	}
-
-	inline size_t getCount() const
-	{
-		return m_args.size();
-	}
-
-	inline int32_t getInteger(size_t index) const
-	{
-		return parseString< int32_t >(m_args[index]);
-	}
-
-	inline const std::wstring& getString(size_t index) const
-	{
-		return m_args[index];
-	}
-
-private:
-	std::wstring m_file;
-	std::vector< Option > m_opts;
-	std::vector< std::wstring > m_args;
-	Option m_invOpt;
+				m_args.push_back(trim(argv[i]));
+		}		
+	}	
 };
 
 }
