d0e4e7c38f0de6f4c540d724578633c9eb6435e5
Anders Pistol <anders.pistol@doctorentertainment.com>
2016-08-28 19:30:41 +0100

    Traktor: Updated run editor script on OSX.
    [git-p4: depot-paths = "//depot/traktor/": change = 20956]

--- run-editor-osx.sh
+++ run-editor-osx.sh
@@ -3,4 +3,4 @@
 # Source environment configuration.
 . "`dirname \"$BASH_SOURCE\"`/config.sh"
 
-open $TRAKTOR_HOME/bin/latest/osx/releaseshared/TraktorEditor.app
+open $TRAKTOR_HOME/bin/latest/osx/releaseshared/Traktor.Editor.App
