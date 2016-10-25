08ea909561cd466382eb135ce06818678ef6775d
Anders Pistol <anders.pistol@doctorentertainment.com>
2016-10-25 12:08:19 +0100

    Traktor: More improvement to rain, able to configure more and also some fake wind rotation.
    [git-p4: depot-paths = "//depot/traktor/": change = 21136]

--- code/Weather/Precipitation/PrecipitationComponent.cpp
+++ code/Weather/Precipitation/PrecipitationComponent.cpp
@@ -1,3 +1,5 @@
+#include "Core/Math/Float.h"
+#include "Core/Math/Random.h"
 #include "Mesh/IMeshParameterCallback.h"
 #include "Mesh/Static/StaticMesh.h"
 #include "Render/Context/ProgramParameters.h"
@@ -17,12 +19,20 @@ class PrecipitationMeshCallback : public RefCountImpl< mesh::IMeshParameterCallb
 {
 public:
 	Vector4 m_frustumEdges[4];
+	float m_parallaxDistance;
+	float m_depthDistance;
+	float m_opacity;
+	float* m_layerAngle;
 
 	virtual void setCombination(render::Shader* shader) const T_FINAL {}
 
 	virtual void setParameters(render::ProgramParameters* programParameters) const T_FINAL
 	{
 		programParameters->setVectorArrayParameter(L"Precipitation_FrustumEdges", m_frustumEdges, sizeof_array(m_frustumEdges));
+		programParameters->setFloatParameter(L"Precipitation_ParallaxDistance", m_parallaxDistance);
+		programParameters->setFloatParameter(L"Precipitation_DepthDistance", m_depthDistance);
+		programParameters->setFloatParameter(L"Precipitation_Opacity", m_opacity);
+		programParameters->setFloatArrayParameter(L"Precipitation_LayerAngle", m_layerAngle, 4);
 	}
 };
 
@@ -30,11 +40,25 @@ public:
 
 T_IMPLEMENT_RTTI_CLASS(L"traktor.weather.PrecipitationComponent", PrecipitationComponent, world::IEntityComponent)
 
-PrecipitationComponent::PrecipitationComponent(const resource::Proxy< mesh::StaticMesh >& mesh)
+PrecipitationComponent::PrecipitationComponent(
+	const resource::Proxy< mesh::StaticMesh >& mesh,
+	float tiltRate,
+	float parallaxDistance,
+	float depthDistance,
+	float opacity
+)
 :	m_mesh(mesh)
+,	m_tiltRate(tiltRate)
+,	m_parallaxDistance(parallaxDistance)
+,	m_depthDistance(depthDistance)
+,	m_opacity(opacity)
 ,	m_lastEyePosition(Vector4::origo())
 ,	m_rotation(Quaternion::identity())
 {
+	m_layerAngle[0] =
+	m_layerAngle[1] =
+	m_layerAngle[2] =
+	m_layerAngle[3] = 0.0f;
 }
 
 void PrecipitationComponent::destroy()
@@ -57,6 +81,28 @@ Aabb3 PrecipitationComponent::getBoundingBox() const
 
 void PrecipitationComponent::update(const world::UpdateParams& update)
 {
+	const struct
+	{
+		float k1, k2, k3;
+	}
+	c_layerFactors[] =
+	{
+		{  0.7f,  0.4f, -0.02f },
+		{ -0.5f, -0.2f,  0.02f },
+		{  0.8f,  0.3f,  0.01f },
+		{ -0.3f,  0.1f,  0.02f }
+	};
+
+	float x = update.totalTime;
+
+	for (int i = 0; i < 4; ++i)
+	{
+		float s1 = std::sin(x);
+		float s2 = std::sin(2.0f * x);
+		float s3 = std::sin(3.0f * x);
+
+		m_layerAngle[i] = (s1 * c_layerFactors[i].k1 + s2 * c_layerFactors[i].k2 + s3 * c_layerFactors[i].k3) * 0.25f;
+	}
 }
 
 void PrecipitationComponent::render(world::WorldContext& worldContext, world::WorldRenderView& worldRenderView, world::IWorldRenderPass& worldRenderPass)
@@ -75,13 +121,16 @@ void PrecipitationComponent::render(world::WorldContext& worldContext, world::Wo
 	if (movement.length2() > Scalar(FUZZY_EPSILON))
 	{
 		pivot = cross(movement, Vector4(0.0f, 1.0f, 0.0f)).normalized();
-		angle = clamp(movement.length() * Scalar(0.8f), Scalar(-HALF_PI), Scalar(HALF_PI));
+		angle = clamp(movement.length() * Scalar(0.8f), Scalar(-HALF_PI / 2.0f), Scalar(HALF_PI / 2.0f));
 	}
 
 	m_lastEyePosition = eyePosition;
 
-	Quaternion Q = Quaternion::fromAxisAngle(pivot, angle);
-	m_rotation = lerp(m_rotation, Q, 0.1f);
+	m_rotation = lerp(
+		m_rotation,
+		Quaternion::fromAxisAngle(pivot, angle),
+		m_tiltRate / 60.0f
+	);
 
 	const Frustum& viewFrustum = worldRenderView.getViewFrustum();
 
@@ -90,6 +139,10 @@ void PrecipitationComponent::render(world::WorldContext& worldContext, world::Wo
 	mc.m_frustumEdges[1] = viewFrustum.corners[5] - viewFrustum.corners[1];
 	mc.m_frustumEdges[2] = viewFrustum.corners[6] - viewFrustum.corners[2];
 	mc.m_frustumEdges[3] = viewFrustum.corners[7] - viewFrustum.corners[3];
+	mc.m_parallaxDistance = m_parallaxDistance;
+	mc.m_depthDistance = m_depthDistance;
+	mc.m_opacity = m_opacity;
+	mc.m_layerAngle = m_layerAngle;
 
 	m_mesh->render(
 		worldContext.getRenderContext(),
