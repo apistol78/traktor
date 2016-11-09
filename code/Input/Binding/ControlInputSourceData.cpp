eda750bb36f2f1cb82c5ce7e70c7c638321d29f3
Anders Pistol <anders.pistol@doctorentertainment.com>
2016-11-09 19:15:01 +0100

    Traktor: Able to query "device available" and "device connected" from input bindings.
    [git-p4: depot-paths = "//depot/traktor/": change = 21180]

--- code/Input/Binding/ControlInputSourceData.cpp
+++ code/Input/Binding/ControlInputSourceData.cpp
@@ -10,11 +10,12 @@ namespace traktor
 	namespace input
 	{
 
-T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.input.ControlInputSourceData", 0, ControlInputSourceData, IInputSourceData)
+T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.input.ControlInputSourceData", 1, ControlInputSourceData, IInputSourceData)
 
 ControlInputSourceData::ControlInputSourceData()
 :	m_category(CtUnknown)
 ,	m_controlType(DtInvalid)
+,	m_controlQuery(CqConnectedDevice)
 ,	m_analogue(false)
 ,	m_index(-1)
 {
@@ -23,11 +24,13 @@ ControlInputSourceData::ControlInputSourceData()
 ControlInputSourceData::ControlInputSourceData(
 	InputCategory category,
 	InputDefaultControlType controlType,
+	ControlQuery controlQuery,
 	bool analogue,
 	bool normalize
 )
 :	m_category(category)
 ,	m_controlType(controlType)
+,	m_controlQuery(controlQuery)
 ,	m_analogue(analogue)
 ,	m_index(-1)
 {
@@ -35,13 +38,15 @@ ControlInputSourceData::ControlInputSourceData(
 
 ControlInputSourceData::ControlInputSourceData(
 	InputCategory category,
-	int32_t index,
 	InputDefaultControlType controlType,
+	ControlQuery controlQuery,
 	bool analogue,
-	bool normalize
+	bool normalize,
+	int32_t index
 )
 :	m_category(category)
 ,	m_controlType(controlType)
+,	m_controlQuery(controlQuery)
 ,	m_analogue(analogue)
 ,	m_index(index)
 {
@@ -65,6 +70,16 @@ void ControlInputSourceData::setControlType(InputDefaultControlType controlType)
 InputDefaultControlType ControlInputSourceData::getControlType() const
 {
 	return m_controlType;
+}
+
+void ControlInputSourceData::setControlQuery(ControlQuery controlQuery)
+{
+	m_controlQuery = controlQuery;
+}
+
+ControlInputSourceData::ControlQuery ControlInputSourceData::getControlQuery() const
+{
+	return m_controlQuery;
 }
 
 void ControlInputSourceData::setAnalogue(bool analogue)
@@ -94,8 +109,19 @@ Ref< IInputSource > ControlInputSourceData::createInstance(DeviceControlManager*
 
 void ControlInputSourceData::serialize(ISerializer& s)
 {
+	const MemberEnum< ControlQuery >::Key c_ControlQuery_Keys[] =
+	{
+		{ L"CqMatchingDevice", CqMatchingDevice },
+		{ L"CqConnectedDevice", CqConnectedDevice },
+		{ 0 }
+	};
+
 	s >> MemberEnum< InputCategory >(L"category", m_category, g_InputCategory_Keys);
 	s >> MemberEnum< InputDefaultControlType >(L"controlType", m_controlType, g_InputDefaultControlType_Keys);
+
+	if (s.getVersion() >= 1)
+		s >> MemberEnum< ControlQuery >(L"controlQuery", m_controlQuery, c_ControlQuery_Keys);
+
 	s >> Member< bool >(L"analogue", m_analogue);
 	s >> Member< int32_t >(L"index", m_index);
 }
