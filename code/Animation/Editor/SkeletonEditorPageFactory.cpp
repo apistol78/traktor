73b2fc47b99f4a1fe802534d490238929af71146
Anders Pistol <anders.pistol@doctorentertainment.com>
2016-11-21 14:04:34 +0100

    Traktor: Missing dependency fixed in various editors.
    [git-p4: depot-paths = "//depot/traktor/": change = 21216]

--- code/Animation/Editor/SkeletonEditorPageFactory.cpp
+++ code/Animation/Editor/SkeletonEditorPageFactory.cpp
@@ -20,6 +20,7 @@ const TypeInfoSet SkeletonEditorPageFactory::getEditableTypes() const
 bool SkeletonEditorPageFactory::needOutputResources(const TypeInfo& typeInfo, std::set< Guid >& outDependencies) const
 {
 	outDependencies.insert(Guid(L"{5B786C6B-8818-A24A-BD1C-EE113B79BCE2}"));	// System/Primitive/Shaders/Primitive
+	outDependencies.insert(Guid(L"{123602E4-BC6F-874D-92E8-A20852D140A3}"));	// System/Primitive/Textures/SmallFont
 	return false;
 }
 
