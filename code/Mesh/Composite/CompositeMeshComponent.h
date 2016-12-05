acc03411196f46efd68a085d6888c08eb5b99824
Anders Pistol <anders.pistol@doctorentertainment.com>
2016-12-05 23:08:46 +0100

    Traktor: Added "composite mesh component" to be able to dynamically composite a mesh from script. Also some minor fixes.
    [git-p4: depot-paths = "//depot/traktor/": change = 21241]

---
+++ code/Mesh/Composite/CompositeMeshComponent.h
@@ -0,0 +1,53 @@
+#ifndef traktor_mesh_CompositeMeshComponent_H
+#define traktor_mesh_CompositeMeshComponent_H
+
+#include "Core/RefArray.h"
+#include "Mesh/MeshComponent.h"
+
+// import/export mechanism.
+#undef T_DLLCLASS
+#if defined(T_MESH_EXPORT)
+#	define T_DLLCLASS T_DLLEXPORT
+#else
+#	define T_DLLCLASS T_DLLIMPORT
+#endif
+
+namespace traktor
+{
+	namespace mesh
+	{
+
+/*! \brief
+ * \ingroup Mesh
+ */
+class T_DLLCLASS CompositeMeshComponent : public MeshComponent
+{
+	T_RTTI_CLASS;
+
+public:
+	CompositeMeshComponent();
+
+	virtual void destroy() T_OVERRIDE T_FINAL;
+
+	virtual Aabb3 getBoundingBox() const T_OVERRIDE T_FINAL;
+
+	virtual void setTransform(const Transform& transform) T_OVERRIDE T_FINAL;
+
+	virtual void update(const world::UpdateParams& update) T_OVERRIDE T_FINAL;
+
+	virtual void render(world::WorldContext& worldContext, world::WorldRenderView& worldRenderView, world::IWorldRenderPass& worldRenderPass) T_OVERRIDE T_FINAL;
+
+	void removeAll();
+
+	void remove(MeshComponent* meshComponent);
+
+	void add(MeshComponent* meshComponent);
+
+private:
+	RefArray< MeshComponent > m_meshComponents;
+};
+
+	}
+}
+
+#endif	// traktor_mesh_CompositeMeshComponent_H
