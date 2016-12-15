57f435f0d898f3bb695130003828226e31ccea29
Anders Pistol <anders.pistol@doctorentertainment.com>
2016-12-15 13:05:42 +0100

    Traktor: Some more tweakable parameters of boids.
    [git-p4: depot-paths = "//depot/traktor/": change = 21273]

--- code/Animation/Boids/BoidsEntityData.cpp
+++ code/Animation/Boids/BoidsEntityData.cpp
@@ -10,12 +10,13 @@ namespace traktor
 	namespace animation
 	{
 
-T_IMPLEMENT_RTTI_EDIT_CLASS(L"traktor.animation.BoidsEntityData", 0, BoidsEntityData, world::EntityData)
+T_IMPLEMENT_RTTI_EDIT_CLASS(L"traktor.animation.BoidsEntityData", 1, BoidsEntityData, world::EntityData)
 
 BoidsEntityData::BoidsEntityData()
 :	m_boidCount(0)
 ,	m_spawnPositionDiagonal(1.0f, 1.0f, 1.0f, 0.0f)
 ,	m_spawnVelocityDiagonal(0.0f, 0.0f, 0.0f, 0.0f)
+,	m_constrain(1.0f, 1.0f, 1.0f, 1.0f)
 ,	m_followForce(0.0f)
 ,	m_repelDistance(0.0f)
 ,	m_repelForce(0.0f)
@@ -35,6 +36,7 @@ Ref< BoidsEntity > BoidsEntityData::createEntity(const world::IEntityBuilder* bu
 		getTransform(),
 		m_spawnPositionDiagonal,
 		m_spawnVelocityDiagonal,
+		m_constrain,
 		m_followForce,
 		m_repelDistance,
 		m_repelForce,
@@ -52,6 +54,10 @@ void BoidsEntityData::serialize(ISerializer& s)
 	s >> Member< uint32_t >(L"boidCount", m_boidCount);
 	s >> Member< Vector4 >(L"spawnPositionDiagonal", m_spawnPositionDiagonal);
 	s >> Member< Vector4 >(L"spawnVelocityDiagonal", m_spawnVelocityDiagonal);
+
+	if (s.getVersion() >= 1)
+		s >> Member< Vector4 >(L"constrain", m_constrain);
+
 	s >> Member< float >(L"followForce", m_followForce);
 	s >> Member< float >(L"repelDistance", m_repelDistance);
 	s >> Member< float >(L"repelForce", m_repelForce);
