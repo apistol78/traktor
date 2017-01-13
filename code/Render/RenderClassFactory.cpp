2e681d61b0f57f1838e622b1ddc4de1dd2b7531f
Anders Pistol <anders.pistol@doctorentertainment.com>
2017-01-13 16:16:42 +0100

    Traktor: Able to query render system about display modes from script.
    [git-p4: depot-paths = "//depot/traktor/": change = 21344]

--- code/Render/RenderClassFactory.cpp
+++ code/Render/RenderClassFactory.cpp
@@ -1,5 +1,6 @@
 #include "Core/Class/AutoRuntimeClass.h"
 #include "Core/Class/IRuntimeClassRegistrar.h"
+#include "Render/IRenderSystem.h"
 #include "Render/IRenderView.h"
 #include "Render/ITexture.h"
 #include "Render/ICubeTexture.h"
@@ -14,6 +15,40 @@ namespace traktor
 	{
 		namespace
 		{
+
+class BoxedDisplayMode : public Object
+{
+	T_RTTI_CLASS;
+
+public:
+	BoxedDisplayMode(const DisplayMode& displayMode)
+	:	m_displayMode(displayMode)
+	{
+	}
+
+	uint32_t getWidth() const { return m_displayMode.width; }
+
+	uint32_t getHeight() const { return m_displayMode.height; }
+
+	uint16_t getRefreshRate() const { return m_displayMode.refreshRate; }
+
+	uint16_t getColorBits() const { return m_displayMode.colorBits; }
+
+private:
+	DisplayMode m_displayMode;
+};
+
+T_IMPLEMENT_RTTI_CLASS(L"traktor.render.DisplayMode", BoxedDisplayMode, Object)
+
+Ref< BoxedDisplayMode > IRenderSystem_getDisplayMode(IRenderSystem* this_, uint32_t index)
+{
+	return new BoxedDisplayMode(this_->getDisplayMode(index));
+}
+
+Ref< BoxedDisplayMode > IRenderSystem_getCurrentDisplayMode(IRenderSystem* this_)
+{
+	return new BoxedDisplayMode(this_->getCurrentDisplayMode());
+}
 
 void ImageProcess_setCombination(ImageProcess* this_, const std::wstring& name, bool value)
 {
@@ -31,6 +66,13 @@ T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.render.RenderClassFactory", 0, RenderCl
 
 void RenderClassFactory::createClasses(IRuntimeClassRegistrar* registrar) const
 {
+	Ref< AutoRuntimeClass< BoxedDisplayMode > > classBoxedDisplayMode = new AutoRuntimeClass< BoxedDisplayMode >();
+	classBoxedDisplayMode->addMethod("getWidth", &BoxedDisplayMode::getWidth);
+	classBoxedDisplayMode->addMethod("getHeight", &BoxedDisplayMode::getHeight);
+	classBoxedDisplayMode->addMethod("getRefreshRate", &BoxedDisplayMode::getRefreshRate);
+	classBoxedDisplayMode->addMethod("getColorBits", &BoxedDisplayMode::getColorBits);
+	registrar->registerClass(classBoxedDisplayMode);
+
 	Ref< AutoRuntimeClass< ITexture > > classITexture = new AutoRuntimeClass< ITexture >();
 	registrar->registerClass(classITexture);
 
@@ -50,6 +92,13 @@ void RenderClassFactory::createClasses(IRuntimeClassRegistrar* registrar) const
 	classIVolumeTexture->addMethod("getHeight", &IVolumeTexture::getHeight);
 	classIVolumeTexture->addMethod("getDepth", &IVolumeTexture::getDepth);
 	registrar->registerClass(classIVolumeTexture);
+
+	Ref< AutoRuntimeClass< IRenderSystem > > classIRenderSystem = new AutoRuntimeClass< IRenderSystem >();
+	classIRenderSystem->addMethod("getDisplayModeCount", &IRenderSystem::getDisplayModeCount);
+	classIRenderSystem->addMethod("getDisplayMode", &IRenderSystem_getDisplayMode);
+	classIRenderSystem->addMethod("getCurrentDisplayMode", &IRenderSystem_getCurrentDisplayMode);
+	classIRenderSystem->addMethod("getDisplayAspectRatio", &IRenderSystem::getDisplayAspectRatio);
+	registrar->registerClass(classIRenderSystem);
 
 	Ref< AutoRuntimeClass< IRenderView > > classIRenderView = new AutoRuntimeClass< IRenderView >();
 	classIRenderView->addMethod("close", &IRenderView::close);
