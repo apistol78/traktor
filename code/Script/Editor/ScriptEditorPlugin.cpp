cf3d5b6ac6ba59d8d4346fbaf3da3168501699e8
Anders Pistol <anders.pistol@doctorentertainment.com>
2017-01-31 11:47:50 +0100

    Traktor: Auto open debugger when new session has started (optional).
    [git-p4: depot-paths = "//depot/traktor/": change = 21406]

---
+++ code/Script/Editor/ScriptEditorPlugin.cpp
@@ -0,0 +1,70 @@
+#include "Core/Settings/PropertyBoolean.h"
+#include "Core/Settings/PropertyGroup.h"
+#include "Editor/IEditor.h"
+#include "Script/Editor/ScriptEditorPlugin.h"
+
+namespace traktor
+{
+	namespace script
+	{
+	
+T_IMPLEMENT_RTTI_CLASS(L"traktor.script.ScriptEditorPlugin", ScriptEditorPlugin, editor::IEditorPlugin)
+	
+ScriptEditorPlugin::ScriptEditorPlugin(editor::IEditor* editor)
+:	m_editor(editor)
+{
+}
+
+bool ScriptEditorPlugin::create(ui::Widget* parent, editor::IEditorPageSite* site)
+{
+	return true;
+}
+
+void ScriptEditorPlugin::destroy()
+{
+}
+
+bool ScriptEditorPlugin::handleCommand(const ui::Command& command, bool result)
+{
+	return false;
+}
+
+void ScriptEditorPlugin::handleDatabaseEvent(db::Database* database, const Guid& eventId)
+{
+}
+
+void ScriptEditorPlugin::handleWorkspaceOpened()
+{
+	IScriptDebuggerSessions* scriptDebuggerSessions = m_editor->getStoreObject< IScriptDebuggerSessions >(L"ScriptDebuggerSessions");
+	if (scriptDebuggerSessions)
+		scriptDebuggerSessions->addListener(this);
+}
+
+void ScriptEditorPlugin::handleWorkspaceClosed()
+{
+	IScriptDebuggerSessions* scriptDebuggerSessions = m_editor->getStoreObject< IScriptDebuggerSessions >(L"ScriptDebuggerSessions");
+	if (scriptDebuggerSessions)
+		scriptDebuggerSessions->removeListener(this);
+}
+
+void ScriptEditorPlugin::notifyBeginSession(IScriptDebugger* scriptDebugger, IScriptProfiler* scriptProfiler)
+{
+	bool autoOpenDebuggedScript = m_editor->getSettings()->getProperty< PropertyBoolean >(L"Editor.AutoOpenDebuggedScript", true);
+	if (autoOpenDebuggedScript)
+		m_editor->openTool(L"traktor.script.ScriptDebuggerTool", L"");
+}
+
+void ScriptEditorPlugin::notifyEndSession(IScriptDebugger* scriptDebugger, IScriptProfiler* scriptProfiler)
+{
+}
+
+void ScriptEditorPlugin::notifySetBreakpoint(const Guid& scriptId, int32_t lineNumber)
+{
+}
+
+void ScriptEditorPlugin::notifyRemoveBreakpoint(const Guid& scriptId, int32_t lineNumber)
+{
+}
+
+	}
+}
---
+++ code/Script/Editor/ScriptEditorPluginFactory.cpp
@@ -0,0 +1,21 @@
+#include "Script/Editor/ScriptEditorPlugin.h"
+#include "Script/Editor/ScriptEditorPluginFactory.h"
+
+namespace traktor
+{
+	namespace script
+	{
+	
+T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.script.ScriptEditorPluginFactory", 0, ScriptEditorPluginFactory, editor::IEditorPluginFactory)	
+
+void ScriptEditorPluginFactory::getCommands(std::list< ui::Command >& outCommands) const
+{
+}
+
+Ref< editor::IEditorPlugin > ScriptEditorPluginFactory::createEditorPlugin(editor::IEditor* editor) const
+{
+	return new ScriptEditorPlugin(editor);
+}
+
+	}
+}
