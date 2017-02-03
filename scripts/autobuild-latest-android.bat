37d46e1016f326f3a996c58fb917683be4328241
Anders Pistol <anders.pistol@doctorentertainment.com>
2017-02-03 13:16:54 +0100

    Traktor: Fixed android autobuild script.
    [git-p4: depot-paths = "//depot/traktor/": change = 21432]

--- scripts/autobuild-latest-android.bat
+++ scripts/autobuild-latest-android.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :: Setup VC environment variables.
-call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
+call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"
 
 :: Setup our build environment.
 call %~dp0../config.bat
@@ -9,9 +9,14 @@ call %~dp0../config.bat
 :: Remove old log.
 del /F /Q %~dp0autobuild-android.log
 
+:: Set aggregate output path if not already defined.
+if "%AGGREGATE_OUTPUT_PATH%" == "" (
+	set AGGREGATE_OUTPUT_PATH=%TRAKTOR_HOME%\bin\latest\android
+)
+
 :: Rebuild entire solution.
 pushd %TRAKTOR_HOME%
-call build-projects-vs2012-android.bat
+call build-projects-vs2015-android.bat
 devenv "build\android\Traktor Android.sln" /Build DebugStatic /Out %~dp0autobuild-android.log
 devenv "build\android\Traktor Android.sln" /Build ReleaseStatic /Out %~dp0autobuild-android.log
 popd
