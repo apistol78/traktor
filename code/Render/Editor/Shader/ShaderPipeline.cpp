95e25b80108c95906f2a0c858ae612dcfb3cb5d9
Anders Pistol <anders.pistol@doctorentertainment.com>
2017-04-06 12:58:53 +0100

    Traktor: Bumped version on ShaderPipeline.
    [git-p4: depot-paths = "//depot/traktor/": change = 21618]

--- code/Render/Editor/Shader/ShaderPipeline.cpp
+++ code/Render/Editor/Shader/ShaderPipeline.cpp
@@ -283,7 +283,7 @@ struct BuildCombinationTask : public Object
 
 		}
 
-T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.render.ShaderPipeline", 76, ShaderPipeline, editor::IPipeline)
+T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.render.ShaderPipeline", 77, ShaderPipeline, editor::IPipeline)
 
 ShaderPipeline::ShaderPipeline()
 :	m_frequentUniformsAsLinear(false)
