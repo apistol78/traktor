2ad9ac03ebaf8ff4f3149f903489541d88f4039b
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-03-06 10:21:44 +0100

    Traktor: Fixed P4 SDK path.
    [git-p4: depot-paths = "//depot/traktor/": change = 21930]

--- scripts/config-vsenv.bat
+++ scripts/config-vsenv.bat
@@ -10,7 +10,7 @@ if /i not "%VERSION%" == "vs2012_x64" ( goto :skip_vs2012_x64 )
 echo Set VS2012 (x64) environment
 
 @set FBX_SDK_LIBPATH=%FBX_SDK%\lib\vs2012\x64\release
-@set P4_SDK=%TRAKTOR_HOME%3rdp\p4api-2016.1.1350954.BETA-vs2012_dyn_x64
+@set P4_SDK=%TRAKTOR_HOME%\3rdp\p4api-2016.1.1350954.BETA-vs2012_dyn_x64
 
 :skip_vs2012_x64
 
@@ -20,7 +20,7 @@ if /i not "%VERSION%" == "vs2012_x86" ( goto :skip_vs2012_x86 )
 echo Set VS2012 (x86) environment
 
 @set FBX_SDK_LIBPATH=%FBX_SDK%\lib\vs2012\x86\release
-@set P4_SDK=%TRAKTOR_HOME%3rdp\p4api-2016.1.1350954.BETA-vs2012_dyn_x86
+@set P4_SDK=%TRAKTOR_HOME%\3rdp\p4api-2016.1.1350954.BETA-vs2012_dyn_x86
 
 :skip_vs2012_x86
 
@@ -30,7 +30,7 @@ if /i not "%VERSION%" == "vs2015_x64" ( goto :skip_vs2015_x64 )
 echo Set VS2015 (x64) environment
 
 @set FBX_SDK_LIBPATH=%FBX_SDK%\lib\vs2015\x64\release
-@set P4_SDK=%TRAKTOR_HOME%3rdp\p4api-2016.1.1350954.BETA-vs2015_dyn_x64
+@set P4_SDK=%TRAKTOR_HOME%\3rdp\p4api-2016.1.1350954.BETA-vs2015_dyn_x64
 
 :skip_vs2015_x64
 
@@ -40,26 +40,26 @@ if /i not "%VERSION%" == "vs2015_x86" ( goto :skip_vs2015_x86 )
 echo Set VS2015 (x86) environment
 
 @set FBX_SDK_LIBPATH=%FBX_SDK%\lib\vs2015\x86\release
-@set P4_SDK=%TRAKTOR_HOME%3rdp\p4api-2016.1.1350954.BETA-vs2015_dyn_x86
+@set P4_SDK=%TRAKTOR_HOME%\3rdp\p4api-2016.1.1350954.BETA-vs2015_dyn_x86
 
 :skip_vs2015_x86
 
 :: -----------------------------------------------------------------------------
 
-if /i not "%VERSION%" == "vs2017_x64" ( goto :vs2017_x64 )
+if /i not "%VERSION%" == "vs2017_x64" ( goto :skip_vs2017_x64 )
 echo Set VS2017 (x64) environment
 
 @set FBX_SDK_LIBPATH=%FBX_SDK%\lib\vs2015\x64\release
-@set P4_SDK=%TRAKTOR_HOME%3rdp\p4api-2016.1.1350954.BETA-vs2015_dyn_x64
+@set P4_SDK=%TRAKTOR_HOME%\3rdp\p4api-2016.1.1350954.BETA-vs2015_dyn_x64
 
-:vs2017_x64
+:skip_vs2017_x64
 
 :: -----------------------------------------------------------------------------
 
-if /i not "%VERSION%" == "vs2017_x86" ( goto :vs2017_x86 )
+if /i not "%VERSION%" == "vs2017_x86" ( goto :skip_vs2017_x86 )
 echo Set VS2017 (x86) environment
 
 @set FBX_SDK_LIBPATH=%FBX_SDK%\lib\vs2015\x86\release
-@set P4_SDK=%TRAKTOR_HOME%3rdp\p4api-2016.1.1350954.BETA-vs2015_dyn_x86
+@set P4_SDK=%TRAKTOR_HOME%\3rdp\p4api-2016.1.1350954.BETA-vs2015_dyn_x86
 
-:vs2017_x86
+:skip_vs2017_x86
