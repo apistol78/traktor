4858ce5dc79c002c1dd409f2e898daa262033558
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-03-14 18:43:18 +0100

    Traktor: Script fixes.
    [git-p4: depot-paths = "//depot/traktor/": change = 21970]

--- scripts/autobuild/autobuild-latest-win32.bat
+++ scripts/autobuild/autobuild-latest-win32.bat
@@ -4,11 +4,15 @@
 call "%~dp0..\config.bat"
 call "%~dp0..\vsenv-x86.bat"
 
+:: \hack Setup paths to ABI specific binaries.
+@set FBX_SDK_LIBPATH=%FBX_SDK%\lib\vs2015\x86\release
+@set P4_SDK=%TRAKTOR_HOME%\3rdp\p4api-2016.1.1350954.BETA-vs2015_dyn_x86
+
 :: Rebuild entire solution.
 pushd "%TRAKTOR_HOME%"
 call "scripts\build-projects-make-win32.bat"
 cd "build\win32"
-%TRAKTOR_HOME%\3rdp\jom\jom -j4 -f "Extern Win32.mak"
-%TRAKTOR_HOME%\3rdp\jom\jom -j4 -f "Traktor Win32.mak"
+%TRAKTOR_HOME%\3rdp\jom\jom -f "Extern Win32.mak" ReleaseShared
+%TRAKTOR_HOME%\3rdp\jom\jom -f "Traktor Win32.mak" ReleaseShared
 popd
 
