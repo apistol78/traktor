5ff5b3b1b1177cb0def24c5aa010754acecc6415
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-03-16 11:35:01 +0100

    Traktor: Fixed issues with building win32/win64 binaries using makefiles.
    [git-p4: depot-paths = "//depot/traktor/": change = 21972]

--- scripts/autobuild/autobuild-latest-win64.bat
+++ scripts/autobuild/autobuild-latest-win64.bat
@@ -4,10 +4,14 @@
 call "%~dp0..\config.bat"
 call "%~dp0..\vsenv-x64.bat"
 
+:: \hack Setup paths to ABI specific binaries.
+@set FBX_SDK_LIBPATH=%FBX_SDK%\lib\vs2015\x64\release
+@set P4_SDK=%TRAKTOR_HOME%\3rdp\p4api-2016.1.1350954.BETA-vs2015_dyn_x64
+
 :: Rebuild entire solution.
 pushd "%TRAKTOR_HOME%"
 call "scripts\build-projects-make-win64.bat"
 cd "build\win64"
-%TRAKTOR_HOME%\3rdp\jom\jom -j4 -f "Extern Win64.mak"
-%TRAKTOR_HOME%\3rdp\jom\jom -j4 -f "Traktor Win64.mak"
+%TRAKTOR_HOME%\3rdp\jom\jom -f "Extern Win64.mak"
+%TRAKTOR_HOME%\3rdp\jom\jom -f "Traktor Win64.mak"
 popd
