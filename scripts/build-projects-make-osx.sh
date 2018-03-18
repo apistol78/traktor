edd83be2f3555008296c6ff27c16877c594da054
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-03-18 10:01:45 +0100

    Traktor: OSX makefile generation fix.
    [git-p4: depot-paths = "//depot/traktor/": change = 21980]

--- scripts/build-projects-make-osx.sh
+++ scripts/build-projects-make-osx.sh
@@ -4,10 +4,13 @@
 . "`dirname \"$BASH_SOURCE\"`/config.sh"
 
 # Generator configuration.
-export MAKE_DIALECT=make
-export MAKE_TOOLCHAIN=clang
 export MAKE_OS=osx
+export MAKE_DIALECT=make
+export MAKE_FORMAT=$TRAKTOR_HOME/resources/build/configurations/make-format.sbm
 export MAKE_INCLUDE="\$(TRAKTOR_HOME)/resources/build/configurations/make-config-osx.inc"
+
+# Optional parameters.
+export MAKE_TOOLCHAIN=clang
 export MAKE_OBJECT_FILE="%s.o"
 export MAKE_STATIC_LIBRARY_FILE="lib%s.a"
 export MAKE_SHARED_LIBRARY_FILE="lib%s.dylib"
