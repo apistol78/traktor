25fe95872b89c6ba229a90fc5525d23f4419f3a9
Build User <builduser@doctorentertainment.com>
2018-03-19 10:37:41 +0100

    Traktor: Some autobuild script fixes.
    [git-p4: depot-paths = "//depot/traktor/": change = 21982]

--- scripts/autobuild/autobuild-latest-ps3.bat
+++ scripts/autobuild/autobuild-latest-ps3.bat
@@ -2,14 +2,14 @@
 
 :: Setup our build environment.
 call "%~dp0..\config.bat"
-call "%~dp0..\vsenv.bat"
+call "%~dp0..\vsenv-x86.bat"
 
 :: Remove old log.
 del /F /Q %~dp0autobuild-ps3.log
 
 :: Rebuild entire solution.
 pushd %TRAKTOR_HOME%
-call build-projects-vs2012-ps3.bat
+call scripts\build-projects-vs2012-ps3.bat
 devenv "build\ps3\Traktor Ps3.sln" /Build DebugStatic /Out %~dp0autobuild-ps3.log
 devenv "build\ps3\Traktor Ps3.sln" /Build ReleaseStatic /Out %~dp0autobuild-ps3.log
 devenv "build\ps3-spu\Traktor Ps3 SPU.sln" /Build DebugStatic /Out %~dp0autobuild-ps3.log
