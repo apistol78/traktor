fdff47e1b1a19a04794b1e8138e99380861d4d24
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-03-19 14:14:15 +0100

    Traktor: Using JOM in android autobuild script.
    [git-p4: depot-paths = "//depot/traktor/": change = 21987]

--- scripts/autobuild/autobuild-latest-android.bat
+++ scripts/autobuild/autobuild-latest-android.bat
@@ -2,12 +2,11 @@
 
 :: Setup our build environment.
 call "%~dp0..\config.bat"
-call "%~dp0..\vsenv.bat"
 
 :: Rebuild entire solution.
 pushd "%TRAKTOR_HOME%"
 call "scripts\build-projects-make-android.bat"
 cd "build\android"
-nmake -f "Extern Android.mak"
-nmake -f "Traktor Android.mak"
+%TRAKTOR_HOME%\3rdp\jom\jom -f "Extern Android.mak"
+%TRAKTOR_HOME%\3rdp\jom\jom -f "Traktor Android.mak"
 popd
