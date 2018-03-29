64761a68a9aa3d1e67d47bac56bc3896c899a82c
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-03-29 08:09:16 +0100

    Traktor: Building android on Linux host fixes.
    [git-p4: depot-paths = "//depot/traktor/": change = 22032]

--- scripts/build-projects-make-android.sh
+++ scripts/build-projects-make-android.sh
@@ -3,19 +3,28 @@
 # Source environment configuration.
 . "`dirname \"$BASH_SOURCE\"`/config.sh"
 
-# Generator configuration.
-export MAKE_DIALECT=make
-export MAKE_TOOLCHAIN=gcc
+# Mandatory parameters.
 export MAKE_OS=linux
+export MAKE_DIALECT=make
+export MAKE_FORMAT=$TRAKTOR_HOME/resources/build/configurations/make-format.sbm
 export MAKE_INCLUDE="\$(TRAKTOR_HOME)/resources/build/configurations/make-config-android.inc"
-export MAKE_OBJECT_FILE="%s.o"
-export MAKE_STATIC_LIBRARY_FILE="lib%s.a"
-export MAKE_SHARED_LIBRARY_FILE="lib%s.so"
-export MAKE_EXECUTABLE_FILE="%s"
 
-# Build solution files.
-$TRAKTOR_HOME/bin/linux/SolutionBuilder \
+# Used by default format.
+export MAKE_TOOLCHAIN=gcc
+export MAKE_OBJECT_FILE=%s.o
+export MAKE_STATIC_LIBRARY_FILE=lib%s.a
+export MAKE_SHARED_LIBRARY_FILE=lib%s.so
+export MAKE_EXECUTABLE_FILE=%s
+
+$SOLUTIONBUILDER \
+	-f=make2 \
+	-make-solution-template=$TRAKTOR_HOME/resources/build/configurations/make-solution.sbm \
+	-make-project-template=$TRAKTOR_HOME/resources/build/configurations/make-project.sbm \
+	$TRAKTOR_HOME/resources/build/ExternAndroid.xms
+
+$SOLUTIONBUILDER \
 	-f=make2 \
 	-make-solution-template=$TRAKTOR_HOME/resources/build/configurations/make-solution.sbm \
 	-make-project-template=$TRAKTOR_HOME/resources/build/configurations/make-project.sbm \
 	$TRAKTOR_HOME/resources/build/TraktorAndroid.xms
+
