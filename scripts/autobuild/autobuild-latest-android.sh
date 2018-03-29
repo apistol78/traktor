d94ac9fd63f8c2f95ca98dd28fd1f3f657812e5c
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-03-29 20:12:58 +0100

    Traktor: Android autobuild script on Linux.
    [git-p4: depot-paths = "//depot/traktor/": change = 22033]

--- scripts/autobuild/autobuild-latest-android.sh
+++ scripts/autobuild/autobuild-latest-android.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 CONFIG=$1
 if [ "$CONFIG" == "" ]; then CONFIG=all; fi
 
 # Source environment configuration.
-source ../config.sh
+. "`dirname \"$BASH_SOURCE\"`/../config.sh"
 
 # Build Traktor
 pushd $TRAKTOR_HOME/build/android
-make -j 8 -f "Extern Android.mak" $CONFIG
-make -j 8 -f "Traktor Android.mak" $CONFIG
+make -j 4 -f "Extern Android.mak" $CONFIG
+make -j 4 -f "Traktor Android.mak" $CONFIG
 popd
