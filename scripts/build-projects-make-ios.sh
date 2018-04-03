77c3511d71f97e10e0731f24ef6c1c2cc8e42486
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-04-03 14:16:33 +0100

    Traktor: Build iOS make script fix.
    [git-p4: depot-paths = "//depot/traktor/": change = 22045]

--- scripts/build-projects-make-ios.sh
+++ scripts/build-projects-make-ios.sh
@@ -3,18 +3,26 @@
 # Source environment configuration.
 . "`dirname \"$BASH_SOURCE\"`/config.sh"
 
-# Generator configuration.
-export MAKE_DIALECT=make
-export MAKE_TOOLCHAIN=clang
+# Mandatory parameters.
 export MAKE_OS=osx
+export MAKE_DIALECT=make
+export MAKE_FORMAT=$TRAKTOR_HOME/resources/build/configurations/make-format.sbm
 export MAKE_INCLUDE="\$(TRAKTOR_HOME)/resources/build/configurations/make-config-ios.inc"
-export MAKE_OBJECT_FILE="%s.o"
-export MAKE_STATIC_LIBRARY_FILE="lib%s.a"
-export MAKE_SHARED_LIBRARY_FILE="lib%s.dylib"
-export MAKE_EXECUTABLE_FILE="%s"
 
-# Build solution files.
-$TRAKTOR_HOME/bin/osx/SolutionBuilder \
+# Used by default format.
+export MAKE_TOOLCHAIN=clang
+export MAKE_OBJECT_FILE=%s.o
+export MAKE_STATIC_LIBRARY_FILE=lib%s.a
+export MAKE_SHARED_LIBRARY_FILE=lib%s.dylib
+export MAKE_EXECUTABLE_FILE=%s
+
+$SOLUTIONBUILDER \
+	-f=make2 \
+	-make-solution-template=$TRAKTOR_HOME/resources/build/configurations/make-solution.sbm \
+	-make-project-template=$TRAKTOR_HOME/resources/build/configurations/make-project.sbm \
+	$TRAKTOR_HOME/resources/build/ExternIOS.xms
+
+$SOLUTIONBUILDER \
 	-f=make2 \
 	-make-solution-template=$TRAKTOR_HOME/resources/build/configurations/make-solution.sbm \
 	-make-project-template=$TRAKTOR_HOME/resources/build/configurations/make-project-ios.sbm \
