b0cc277edf82bfedff5e979e19feb0cc6d84eb1b
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-04-03 13:47:10 +0100

    Traktor: Simplified iOS autobuild script.
    [git-p4: depot-paths = "//depot/traktor/": change = 22044]

--- scripts/autobuild/autobuild-latest-ios.sh
+++ scripts/autobuild/autobuild-latest-ios.sh
@@ -6,22 +6,8 @@ if [ "$CONFIG" == "" ]; then CONFIG=all; fi
 # Source environment configuration.
 source ../config.sh
 
-# Get normalized path to this script, excluding file name.
-BUILD_LOG_DIR="`dirname \"$BASH_SOURCE\"`"
-BUILD_LOG_DIR="`(cd \"$BUILD_LOG_DIR\" && pwd)`"
-
-# Reset log file and add time stamp.
-echo "========== Build Begun ($CONFIG) ==========" >$BUILD_LOG_DIR/build-ios-stderr.log
-echo $(date +"%D %T") >>$BUILD_LOG_DIR/build-ios-stderr.log
-
 # Build Traktor
 pushd $TRAKTOR_HOME/build/ios
-if [ $CONFIG == "both" ] || [ $CONFIG == "release" ] ; then
-	echo "========== ReleaseStatic ==========" >>$BUILD_LOG_DIR/build-ios-stderr.log
-	make -j 4 ReleaseStatic 2>>$BUILD_LOG_DIR/build-ios-stderr.log
-fi
-if [ $CONFIG == "both" ] || [ $CONFIG == "debug" ] ; then
-	echo "========== DebugStatic ==========" >>$BUILD_LOG_DIR/build-ios-stderr.log
-	make -j 4 DebugStatic 2>>$BUILD_LOG_DIR/build-ios-stderr.log
-fi
+make -j 8 -f "Extern iOS.mak" $CONFIG
+make -j 8 -f "Traktor iOS.mak" $CONFIG
 popd
