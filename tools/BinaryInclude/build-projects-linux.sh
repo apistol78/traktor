32430e60b7df4bc1ed2020d75b43616ddc869b9d
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-04-04 13:35:07 +0100

    Traktor: BinaryInclude tool rewritten using Traktor.Run.App
    [git-p4: depot-paths = "//depot/traktor/": change = 22052]

--- tools/BinaryInclude/build-projects-linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Source environment configuration.
-. "`dirname \"$BASH_SOURCE\"`/../../config.sh"
-
-wine $TRAKTOR_HOME/bin/win32/SolutionBuilder -f=cblocks BinaryIncludeLinux.xms
-
