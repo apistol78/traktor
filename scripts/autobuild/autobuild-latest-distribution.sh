0789a53a907edea7d473403fd0020ff5dc87abd6
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-04-05 17:12:11 +0100

    Traktor: Added "distribution" autobuild script.
    [git-p4: depot-paths = "//depot/traktor/": change = 22061]

---
+++ scripts/autobuild/autobuild-latest-distribution.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Source environment configuration.
+. "`dirname \"$BASH_SOURCE\"`/../config.sh"
+
+OUTPUT=$TRAKTOR_HOME/build/distribution
+
+rm -rf $OUTPUT
+
+pushd $TRAKTOR_HOME
+
+mkdir -p $OUTPUT/bin/latest
+mkdir -p $OUTPUT/code
+mkdir -p $OUTPUT/resources
+mkdir -p $OUTPUT/scripts
+
+cp -r bin/linux $OUTPUT/bin/
+cp -r bin/osx $OUTPUT/bin/
+cp -r bin/win32 $OUTPUT/bin/
+cp -r bin/win64 $OUTPUT/bin/
+cp -r bin/latest $OUTPUT/bin/
+
+rsync -av --include "*/" --include "*.h" --include "*.inl" --exclude "*" code/. $OUTPUT/code/.
+
+cp -r resources/build $OUTPUT/resources/
+cp -r resources/icons $OUTPUT/resources/
+cp -r resources/runtime $OUTPUT/resources/
+
+cp -r scripts/binaryinclude $OUTPUT/scripts/
+cp -r scripts/runtime $OUTPUT/scripts/
+
+cp scripts/config.* $OUTPUT/scripts/
+cp scripts/vsenv* $OUTPUT/scripts/
+cp scripts/run* $OUTPUT/scripts/
+
+cp *.workspace $OUTPUT/
+
+popd
