9616aee25e9bcffe41ab626064c24ec84341f371
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-04-09 07:36:53 +0100

    Traktor: System data in distribution autobuild script.
    [git-p4: depot-paths = "//depot/traktor/": change = 22070]

--- scripts/autobuild/autobuild-latest-distribution.sh
+++ scripts/autobuild/autobuild-latest-distribution.sh
@@ -13,19 +13,24 @@ mkdir -p $OUTPUT/bin/latest
 mkdir -p $OUTPUT/code
 mkdir -p $OUTPUT/resources
 mkdir -p $OUTPUT/scripts
+mkdir -p $OUTPUT/data
 
+# Binaries
 cp -r bin/linux $OUTPUT/bin/
 cp -r bin/osx $OUTPUT/bin/
 cp -r bin/win32 $OUTPUT/bin/
 cp -r bin/win64 $OUTPUT/bin/
 cp -r bin/latest $OUTPUT/bin/
 
+# Headers
 rsync -av --include "*/" --include "*.h" --include "*.inl" --exclude "*" code/. $OUTPUT/code/.
 
+# Resources
 cp -r resources/build $OUTPUT/resources/
 cp -r resources/icons $OUTPUT/resources/
 cp -r resources/runtime $OUTPUT/resources/
 
+# Scripts
 cp -r scripts/binaryinclude $OUTPUT/scripts/
 cp -r scripts/runtime $OUTPUT/scripts/
 
@@ -33,6 +38,11 @@ cp scripts/config.* $OUTPUT/scripts/
 cp scripts/vsenv* $OUTPUT/scripts/
 cp scripts/run* $OUTPUT/scripts/
 
+# Data
+cp -r data/Assets $OUTPUT/data/
+cp -r data/Source $OUTPUT/data/
+
+# Workspace
 cp *.workspace $OUTPUT/
 
 popd
