1985f1a6d4c3ac8b4a82e1d3acb370d56081c2c6
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-04-10 13:33:34 +0100

    Traktor: Error checking in autobuild clean scripts added, only bat-files for now.
    [git-p4: depot-paths = "//depot/traktor/": change = 22077]

--- scripts/autobuild/autobuild-clean-android.bat
+++ scripts/autobuild/autobuild-clean-android.bat
@@ -3,6 +3,15 @@
 call "%~dp0..\config.bat"
 
 pushd %TRAKTOR_HOME%
-rmdir /Q /S "build\android" 2> nul
-rmdir /Q /S "bin\latest\android" 2> nul
+
+rd /Q /S "build\android" 2> nul || rem
+if %ERRORLEVEL% equ 2 ( cmd /C exit 0 )
+if %ERRORLEVEL% neq 0 ( exit /B 1 )
+
+rd /Q /S "bin\latest\android" 2> nul || rem
+if %ERRORLEVEL% equ 2 ( cmd /C exit 0 )
+if %ERRORLEVEL% neq 0 ( exit /B 1 )
+
 popd
+
+echo Clean successful
