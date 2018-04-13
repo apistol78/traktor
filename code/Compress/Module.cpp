d7503d9c7308fe897ee8b7fa5991b7513e2f78b3
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-04-13 18:18:08 +0100

    Traktor: Fixed missing statically linked items from Compress library. Emscripten deploy now works.
    [git-p4: depot-paths = "//depot/traktor/": change = 22088]

--- code/Compress/Module.cpp
+++ code/Compress/Module.cpp
@@ -7,6 +7,12 @@ Copyright 2017 Doctor Entertainment AB. All Rights Reserved.
 #include "Core/Rtti/TypeInfo.h"
 
 #if defined(T_STATIC)
+#	include "Compress/Lzf/DeflateStreamLzf.h"
+#	include "Compress/Lzf/InflateStreamLzf.h"
+#	include "Compress/Lzo/DeflateStreamLzo.h"
+#	include "Compress/Lzo/InflateStreamLzo.h"
+#	include "Compress/Zip/DeflateStreamZip.h"
+#	include "Compress/Zip/InflateStreamZip.h"
 
 namespace traktor
 {
@@ -15,6 +21,12 @@ namespace traktor
 
 extern "C" void __module__Traktor_Compress()
 {
+	T_FORCE_LINK_REF(DeflateStreamLzf);
+	T_FORCE_LINK_REF(InflateStreamLzf);
+	T_FORCE_LINK_REF(DeflateStreamLzo);
+	T_FORCE_LINK_REF(InflateStreamLzo);
+	T_FORCE_LINK_REF(DeflateStreamZip);
+	T_FORCE_LINK_REF(InflateStreamZip);
 }
 
 	}
