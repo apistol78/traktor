a239d4ce69af4185ee1c6a000222a177d4d0fdb7
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-04-22 10:37:28 +0100

    Traktor: Setting /p:BuildInParallel=true to msbuild in autobuild scripts.
    [git-p4: depot-paths = "//depot/traktor/": change = 22127]

--- scripts/autobuild/autobuild-latest-win64.bat
+++ scripts/autobuild/autobuild-latest-win64.bat
@@ -9,6 +9,6 @@ call "%~dp0..\vsenv-x64.bat"
 
 :: Rebuild entire solution.
 pushd "%TRAKTOR_HOME%\build\win64"
-msbuild "Extern Win64.sln" /nologo /maxcpucount /p:Configuration=%CONFIG% /p:Platform="x64"
-msbuild "Traktor Win64.sln" /nologo /maxcpucount /p:Configuration=%CONFIG% /p:Platform="x64"
+msbuild "Extern Win64.sln" /nologo /maxcpucount /p:BuildInParallel=true /p:Configuration=%CONFIG% /p:Platform="x64"
+msbuild "Traktor Win64.sln" /nologo /maxcpucount /p:BuildInParallel=true /p:Configuration=%CONFIG% /p:Platform="x64"
 popd
