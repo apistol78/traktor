d9794447c655e775d1d924824a3c9813d0442df7
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-04-22 10:30:41 +0100

    Traktor: Updated P4 api to latest version. But disabled Traktor.Database.Local.P4 for now until all link issues properly fixed.
    [git-p4: depot-paths = "//depot/traktor/": change = 22126]

--- scripts/build-projects-vs2017-win32.bat
+++ scripts/build-projects-vs2017-win32.bat
@@ -4,7 +4,7 @@ call %~dp0config.bat
 
 :: \hack Setup paths to ABI specific binaries.
 set FBX_SDK_LIBPATH=%FBX_SDK%\lib\vs2015\x86\release
-set P4_SDK=%TRAKTOR_HOME%\3rdp\p4api-2016.1.1350954.BETA-vs2015_dyn_x86
+set P4_SDK=%TRAKTOR_HOME%\3rdp\p4api-2018.1.1637071-vs2017_static_x86
 
 %SOLUTIONBUILDER% ^
 	-f=msvc ^
