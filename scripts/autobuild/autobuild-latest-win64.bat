d7f7d1e20570cc89408b60db1cfa3461e2916d81
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-04-22 08:34:22 +0100

    Traktor: Using msbuild when autobuilding win32/win64
    [git-p4: depot-paths = "//depot/traktor/": change = 22123]

--- scripts/autobuild/autobuild-latest-win64.bat
+++ scripts/autobuild/autobuild-latest-win64.bat
@@ -9,6 +9,10 @@ call "%~dp0..\vsenv-x64.bat"
 
 :: Rebuild entire solution.
 pushd "%TRAKTOR_HOME%\build\win64"
-%TRAKTOR_HOME%\3rdp\jom\jom /J 1 /L /F "Extern Win64.mak" %CONFIG%
-%TRAKTOR_HOME%\3rdp\jom\jom /J 1 /L /F "Traktor Win64.mak" %CONFIG%
+::%TRAKTOR_HOME%\3rdp\jom\jom /J 1 /L /F "Extern Win64.mak" %CONFIG%
+::%TRAKTOR_HOME%\3rdp\jom\jom /J 1 /L /F "Traktor Win64.mak" %CONFIG%
+
+msbuild "Extern Win64.sln" /nologo /maxcpucount /p:Configuration=%CONFIG%
+msbuild "Traktor Win64.sln" /nologo /maxcpucount /p:Configuration=%CONFIG%
+
 popd
