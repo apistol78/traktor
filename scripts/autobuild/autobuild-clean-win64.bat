bdcea544439f55cd913b8169f0e0812f13e9f4eb
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-04-23 08:22:16 +0100

    Traktor: Autobuild clean scripts also handle errorcode 3 (path not found).
    [git-p4: depot-paths = "//depot/traktor/": change = 22128]

--- scripts/autobuild/autobuild-clean-win64.bat
+++ scripts/autobuild/autobuild-clean-win64.bat
@@ -6,10 +6,12 @@ pushd %TRAKTOR_HOME%
 
 rd /Q /S "build\win64" 2> nul || rem
 if %ERRORLEVEL% equ 2 ( cmd /C exit 0 )
+if %ERRORLEVEL% equ 3 ( cmd /C exit 0 )
 if %ERRORLEVEL% neq 0 ( exit /B 1 )
 
 rd /Q /S "bin\latest\win64" 2> nul || rem
 if %ERRORLEVEL% equ 2 ( cmd /C exit 0 )
+if %ERRORLEVEL% equ 3 ( cmd /C exit 0 )
 if %ERRORLEVEL% neq 0 ( exit /B 1 )
 
 popd
