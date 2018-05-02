63ae473a66b661ecdf550ca580e2a5f569bc6ab0
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-05-02 13:31:15 +0100

    Traktor: Build issues fixed, properly set $(OutDir) in vcproj generation. Expat parser use utf8 internal, no longer attempt to use utf16 as it seems to be broken in expat.
    [git-p4: depot-paths = "//depot/traktor/": change = 22137]

--- code/SolutionBuilder/Version.h
+++ code/SolutionBuilder/Version.h
@@ -1,6 +1,6 @@
 #ifndef SB_VERSION
-#	define SB_VERSION L"3.6.2"
+#	define SB_VERSION L"3.6.3"
 #endif
 #ifndef SB_TITLE
-#	define SB_TITLE L"SolutionBuilder 3.6.2"
+#	define SB_TITLE L"SolutionBuilder 3.6.3"
 #endif
