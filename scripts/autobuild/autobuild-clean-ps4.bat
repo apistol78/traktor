f7241202bb841b025fba971c22c204f08640d193
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-05-03 11:22:08 +0100

    Traktor: PS4 autobuild scripts.
    [git-p4: depot-paths = "//depot/traktor/": change = 22147]

---
+++ scripts/autobuild/autobuild-clean-ps4.bat
@@ -0,0 +1,17 @@
+@echo off
+
+call "%~dp0..\config.bat"
+
+pushd %TRAKTOR_HOME%
+
+rd /Q /S "build\ps4" 2> nul || rem
+if %ERRORLEVEL% equ 2 ( cmd /C exit 0 )
+if %ERRORLEVEL% neq 0 ( exit /B 1 )
+
+rd /Q /S "bin\latest\ps4" 2> nul || rem
+if %ERRORLEVEL% equ 2 ( cmd /C exit 0 )
+if %ERRORLEVEL% neq 0 ( exit /B 1 )
+
+popd
+
+echo Clean successful
---
+++ scripts/autobuild/autobuild-latest-ps4.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set CONFIG=%*
+if "%CONFIG%" == "" ( set CONFIG=all )
+
+:: Setup our build environment.
+call "%~dp0..\config.bat"
+call "%~dp0..\vsenv-x86.bat"
+
+:: Rebuild entire solution.
+pushd "%TRAKTOR_HOME%\build\ps4"
+msbuild "Extern Ps4.sln" /nologo /maxcpucount /p:BuildInParallel=true /p:Configuration=%CONFIG% /p:Platform="ORBIS"
+msbuild "Traktor Ps4.sln" /nologo /maxcpucount /p:BuildInParallel=true /p:Configuration=%CONFIG% /p:Platform="ORBIS"
+popd
+
