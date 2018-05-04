ed46319cca4f1c1cde3b13bc42a0d50ce0e91bb3
Build User <builduser@doctorentertainment.com>
2018-05-04 13:25:18 +0100

    Traktor - Fixed iOS compile issue in Online GC
    [git-p4: depot-paths = "//depot/traktor/": change = 22167]

--- code/Online/Gc/GcStatistics.h
+++ code/Online/Gc/GcStatistics.h
@@ -19,9 +19,9 @@ class GcStatistics : public IStatisticsProvider
 	T_RTTI_CLASS;
 
 public:
-	virtual bool enumerate(std::map< std::wstring, float >& outStats) T_OVERRIDE T_FINAL;
+	virtual bool enumerate(std::map< std::wstring, int32_t >& outStats) T_OVERRIDE T_FINAL;
 
-	virtual bool set(const std::wstring& statId, float value) T_OVERRIDE T_FINAL;
+	virtual bool set(const std::wstring& statId, int32_t value) T_OVERRIDE T_FINAL;
 };
 
 	}
--- code/Online/Gc/GcStatistics.mm
+++ code/Online/Gc/GcStatistics.mm
@@ -7,12 +7,12 @@ namespace traktor
 
 T_IMPLEMENT_RTTI_CLASS(L"traktor.online.GcStatistics", GcStatistics, IStatisticsProvider)
 
-bool GcStatistics::enumerate(std::map< std::wstring, float >& outStats)
+bool GcStatistics::enumerate(std::map< std::wstring, int32_t >& outStats)
 {
 	return false;
 }
 
-bool GcStatistics::set(const std::wstring& statId, float value)
+bool GcStatistics::set(const std::wstring& statId, int32_t value)
 {
 	return false;
 }
