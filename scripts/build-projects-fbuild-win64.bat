a591ed2cf80b0d2e76954c19f091a073ffd95953
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-05-25 14:02:20 +0100

    Traktor: Win64 built using FASTBuild.
    [git-p4: depot-paths = "//depot/traktor/": change = 22224]

--- scripts/build-projects-fbuild-win64.bat
+++ scripts/build-projects-fbuild-win64.bat
@@ -3,20 +3,20 @@
 call %~dp0config.bat
 
 :: \hack Setup paths to ABI specific binaries.
-set FBX_SDK_LIBPATH=%FBX_SDK%\lib\vs2015\x86\release
-set P4_SDK=%TRAKTOR_HOME%\3rdp\p4api-2016.1.1350954.BETA-vs2015_dyn_x86
+set FBX_SDK_LIBPATH=%FBX_SDK%\lib\vs2015\x64\release
+set P4_SDK=%TRAKTOR_HOME%\3rdp\p4api-2016.1.1350954.BETA-vs2015_dyn_x64
 
 :: Mandatory parameters.
-set FBUILD_INCLUDE=$(TRAKTOR_HOME)\resources\build\configurations\fbuild-config-win32.bff
+set FBUILD_INCLUDE=$(TRAKTOR_HOME)\resources\build\configurations\fbuild-config-win64.bff
 
 %SOLUTIONBUILDER% ^
 	-f=fbuild ^
 	-fastbuild-solution-template=%TRAKTOR_HOME%\resources\build\configurations\fbuild-solution.sb ^
 	-fastbuild-project-template=%TRAKTOR_HOME%\resources\build\configurations\fbuild-project.sb ^
-	%TRAKTOR_HOME%\resources\build\ExternWin32.xms
+	%TRAKTOR_HOME%\resources\build\ExternWin64.xms
 
 %SOLUTIONBUILDER% ^
 	-f=fbuild ^
 	-fastbuild-solution-template=%TRAKTOR_HOME%\resources\build\configurations\fbuild-solution.sb ^
 	-fastbuild-project-template=%TRAKTOR_HOME%\resources\build\configurations\fbuild-project.sb ^
-	%TRAKTOR_HOME%\resources\build\TraktorWin32.xms
+	%TRAKTOR_HOME%\resources\build\TraktorWin64.xms
