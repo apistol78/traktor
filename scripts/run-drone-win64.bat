68d988287e50531f1e4f59b208beca708474a744
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-08-10 08:39:06 +0100

    Traktor: Using ShadowLaunch to launch drone and editor.
    [git-p4: depot-paths = "//depot/traktor/": change = 22389]

--- scripts/run-drone-win64.bat
+++ scripts/run-drone-win64.bat
@@ -1,2 +1,7 @@
-@call %~dp0config.bat
-@start %TRAKTOR_HOME%\bin\win64\releasestatic\Traktor.Drone.App.exe $(TRAKTOR_HOME)/resources/runtime/configurations/Traktor.Drone.config
+@echo off
+
+call %~dp0config.bat
+
+set SH=%TRAKTOR_HOME%\bin\win64\ShadowLaunch
+
+%SH% TraktorDrone %TRAKTOR_HOME%\bin\latest\win64\releaseshared\Traktor.Drone.App.exe $(TRAKTOR_HOME)/resources/runtime/configurations/Traktor.Drone.config
