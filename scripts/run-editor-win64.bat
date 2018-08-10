7400ef0e04966d0822001aeb8a1b0e0d600e9aa3
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-08-10 08:39:06 +0100

    Traktor: Using ShadowLaunch to launch drone and editor.
    [git-p4: depot-paths = "//depot/traktor/": change = 22389]

--- scripts/run-editor-win64.bat
+++ scripts/run-editor-win64.bat
@@ -2,8 +2,10 @@
 
 call %~dp0config.bat
 
+set SH=%TRAKTOR_HOME%\bin\win64\ShadowLaunch
+
 :: Launch remote server.
-start %TRAKTOR_HOME%\bin\latest\win64\releaseshared\Traktor.Remote.Server.App %APPDATA%\TraktorRemoteServer
+%SH% TraktorRemoteServer %TRAKTOR_HOME%\bin\latest\win64\releaseshared\Traktor.Remote.Server.App.exe %APPDATA%\TraktorRemoteServer
 
 :: Launch editor.
-start %TRAKTOR_HOME%\bin\latest\win64\releaseshared\Traktor.Editor.App
+%SH% TraktorEditor %TRAKTOR_HOME%\bin\latest\win64\releaseshared\Traktor.Editor.App.exe
