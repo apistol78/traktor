c5a99144978c2b65ac788abbbee9d38a4a242282
Anders Pistol <anders.pistol@doctorentertainment.com>
2018-12-11 19:32:35 +0100

    Traktor: Launching remote server when running editor script.
    [git-p4: depot-paths = "//depot/traktor/": change = 22727]

--- scripts/run-editor-linux.sh
+++ scripts/run-editor-linux.sh
@@ -3,4 +3,8 @@
 # Source environment configuration.
 . "`dirname \"$BASH_SOURCE\"`/config.sh"
 
+# Launch remote server.
+$TRAKTOR_HOME/bin/latest/linux/releaseshared/Traktor.Remote.Server.App "$HOME/Library/Doctor Entertainment AB/TraktorRemoteServer" &
+
+# Launch editor.
 ($TRAKTOR_HOME/bin/latest/linux/releaseshared/Traktor.Editor.App > /dev/null &)
