7e7ac6bd9c8dc15438f23ca0849876507485c3b6
Anders Pistol <anders.pistol@doctorentertainment.com>
2019-02-06 11:21:58 +0100

    Traktor: Shortened length of debug texture names from deferred world renderer.
    [git-p4: depot-paths = "//depot/traktor/": change = 22843]

--- code/Core/Math/Winding3.h
+++ code/Core/Math/Winding3.h
@@ -1,11 +1,4 @@
-/*
-================================================================================================
-CONFIDENTIAL AND PROPRIETARY INFORMATION/NOT FOR DISCLOSURE WITHOUT WRITTEN PERMISSION
-Copyright 2017 Doctor Entertainment AB. All Rights Reserved.
-================================================================================================
-*/
-#ifndef traktor_Winding3_H
-#define traktor_Winding3_H
+#pragma once
 
 #include "Core/Containers/AlignedVector.h"
 #include "Core/Math/Plane.h"
@@ -87,5 +80,3 @@ private:
 };
 
 }
-
-#endif	// traktor_Winding3_H
