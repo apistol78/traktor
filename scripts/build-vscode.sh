2dba2ed4d84255d4c0fb75a12a8ee07a945077fa
Anders Pistol <anders.pistol@doctorentertainment.com>
2019-02-17 10:34:02 +0100

    Traktor: Silenced vscode build.
    [git-p4: depot-paths = "//depot/traktor/": change = 22852]

--- scripts/build-vscode.sh
+++ scripts/build-vscode.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Ensure terminal output is cleared.
+clear
+
 # Source environment configuration.
 . "`dirname \"$BASH_SOURCE\"`/config.sh"
 
 # Build
 pushd $TRAKTOR_HOME/build/linux
-make -j6 -f 'Extern Linux.mak' $1
-make -j6 -f 'Traktor Linux.mak' $1
+#make -s -j6 -f 'Extern Linux.mak' $1
+make -s -j6 -f 'Traktor Linux.mak' $1
 popd
