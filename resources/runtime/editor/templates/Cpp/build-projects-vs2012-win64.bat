ea7e66cc310d1c48242ce68eadb709b85e270a55
Anders Pistol <anders.pistol@doctorentertainment.com>
2019-03-05 15:41:39 +0100

    Traktor: Removed C++ project template, will maybe revisit someday if it becomes necessary.
    [git-p4: depot-paths = "//depot/traktor/": change = 22890]

--- resources/runtime/editor/templates/Cpp/build-projects-vs2012-win64.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-call %~dp0config.bat
-
-%TRAKTOR_HOME%\bin\win32\solutionbuilder -f=msvc "-p=$(TRAKTOR_HOME)\bin\msvc-2012-win64.xml" <!-- output:print(os:getEnvironment("WIZARD_NAME")) --!>Win64.xms
