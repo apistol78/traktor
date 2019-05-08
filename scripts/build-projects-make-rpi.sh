13d9f069c007f316f92e9335dcf6af429758e858
Anders Pistol <anders.pistol@doctorentertainment.com>
2019-05-08 19:55:58 +0100

    Traktor: Linux setup for Raspberry cross compile.
    [git-p4: depot-paths = "//depot/traktor/": change = 23036]

---
+++ scripts/build-projects-make-rpi.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Source environment configuration.
+. "`dirname \"$BASH_SOURCE\"`/config.sh"
+
+# Mandatory parameters.
+export MAKE_OS=linux
+export MAKE_DIALECT=make
+export MAKE_FORMAT=$TRAKTOR_HOME/resources/build/configurations/make-format.sbm
+export MAKE_INCLUDE="\$(TRAKTOR_HOME)/resources/build/configurations/make-config-rpi.inc"
+
+# Used by default format.
+export MAKE_TOOLCHAIN=gcc
+export MAKE_OBJECT_FILE=%s.o
+export MAKE_STATIC_LIBRARY_FILE=lib%s.a
+export MAKE_SHARED_LIBRARY_FILE=lib%s.so
+export MAKE_EXECUTABLE_FILE=%s
+#export MAKE_VERBOSE=false
+
+$SOLUTIONBUILDER \
+	-f=make2 \
+	-make-solution-template=$TRAKTOR_HOME/resources/build/configurations/make-solution.sbm \
+	-make-project-template=$TRAKTOR_HOME/resources/build/configurations/make-project.sbm \
+	$TRAKTOR_HOME/resources/build/ExternRPi.xms
+
+$SOLUTIONBUILDER \
+	-f=make2 \
+	-make-solution-template=$TRAKTOR_HOME/resources/build/configurations/make-solution.sbm \
+	-make-project-template=$TRAKTOR_HOME/resources/build/configurations/make-project.sbm \
+	$TRAKTOR_HOME/resources/build/TraktorRPi.xms
+
