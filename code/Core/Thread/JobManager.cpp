ca76526678e595ef9ccf4643d4a9b02e49400c22
Anders Pistol <anders.pistol@doctorentertainment.com>
2019-05-23 20:25:20 +0100

    Traktor: Create one less than # cpu cores in job manager.
    [git-p4: depot-paths = "//depot/traktor/": change = 23066]

--- code/Core/Thread/JobManager.cpp
+++ code/Core/Thread/JobManager.cpp
@@ -9,14 +9,21 @@ namespace traktor
 
 JobManager& JobManager::getInstance()
 {
-	static JobManager* s_instance = 0;
+	static JobManager* s_instance = nullptr;
 	if (!s_instance)
 	{
 		s_instance = new JobManager();
 		SingletonManager::getInstance().addBefore(s_instance, &ThreadManager::getInstance());
 
+		int32_t coreCount = OS::getInstance().getCPUCoreCount();
+		if (coreCount >= 2)
+		{
+			// Reserve one core for main thread for good measure.
+			coreCount -= 1;
+		}
+
 		s_instance->m_queue.create(
-			OS::getInstance().getCPUCoreCount(),
+			coreCount,
 			Thread::Normal
 		);
 	}
