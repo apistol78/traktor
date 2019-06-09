948a582ed2e1e89bfa742ace4199d0ac5dc4d9ee
Anders Pistol <anders.pistol@doctorentertainment.com>
2019-06-09 20:33:01 +0100

    Traktor: Forgot files.
    [git-p4: depot-paths = "//depot/traktor/": change = 23107]

---
+++ code/SolutionBuilder/HeaderScanner.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+#include "Core/Object.h"
+#include "Core/Containers/SmallMap.h"
+#include "Core/Containers/SmallSet.h"
+
+namespace traktor
+{
+    namespace sb
+    {
+
+class HeaderScanner : public Object
+{
+    T_RTTI_CLASS;
+
+public:
+    virtual ~HeaderScanner();
+
+    void removeAllIncludePaths();
+
+    void addIncludePath(const std::wstring& includePath);
+
+    bool get(const std::wstring& fileName, const std::wstring& projectPath, SmallSet< std::wstring >& outHeaderFiles);
+
+private:
+    struct Includes
+    {
+        SmallSet< std::wstring > files;
+    };
+
+    SmallSet< std::wstring > m_includePaths;
+    SmallMap< std::wstring, Includes* > m_cache;
+
+    /*! \brief Scan header dependencies of source file. */
+    const Includes* scan(const std::wstring& fileName);
+};
+
+    }
+}
