5a5d6c4bba240ade38cd6372bdd40564e41745a3
Anders Pistol <anders.pistol@doctorentertainment.com>
2019-07-19 10:38:07 +0100

    Traktor: Some fixes when generating Android makefiles.
    [git-p4: depot-paths = "//depot/traktor/": change = 23197]

--- scripts/build-projects-make-android.sh
+++ scripts/build-projects-make-android.sh
@@ -5,17 +5,18 @@
 . "`dirname \"$BASH_SOURCE\"`/config-android.sh"
 
 # Mandatory parameters.
-export MAKE_OS=linux
 export MAKE_DIALECT=make
 export MAKE_FORMAT=$TRAKTOR_HOME/resources/build/configurations/make-format.sbm
 if [[ `uname -s` == Linux* ]]; then
+	export MAKE_OS=linux
 	export MAKE_INCLUDE="\$(TRAKTOR_HOME)/resources/build/configurations/make-config-android-linux.inc"
 elif [[ `uname -s` == Darwin* ]]; then
+	export MAKE_OS=osx
 	export MAKE_INCLUDE="\$(TRAKTOR_HOME)/resources/build/configurations/make-config-android-darwin.inc"
 fi
 
 # Used by default format.
-export MAKE_TOOLCHAIN=gcc
+export MAKE_TOOLCHAIN=clang
 export MAKE_OBJECT_FILE=%s.o
 export MAKE_STATIC_LIBRARY_FILE=lib%s.a
 export MAKE_SHARED_LIBRARY_FILE=lib%s.so
