20d38e58538ccd19daeed91c3388a22a8dfa54ab
Anders Pistol <anders.pistol@doctorentertainment.com>
2019-07-29 17:17:31 +0100

    Traktor: Able to deploy Android from Linux host.
    [git-p4: depot-paths = "//depot/traktor/": change = 23213]

--- scripts/config-android.sh
+++ scripts/config-android.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# Android SDK
+if [[ `uname -s` == Linux* ]]; then
+	export ANDROID_HOME=$TRAKTOR_HOME/3rdp/android-sdk/linux
+elif [[ `uname -s` == Darwin* ]]; then
+	export ANDROID_HOME=$TRAKTOR_HOME/3rdp/android-sdk/darwin
+fi
+
+# Android NDK
 export ANDROID_NDK_ROOT=$TRAKTOR_HOME/3rdp/android-ndk-r20
