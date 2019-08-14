b158967106b1e86c918e55f9de2eee0025febd31
Anders Pistol <anders.pistol@doctorentertainment.com>
2019-08-14 21:42:47 +0100

    Traktor: Using ASTC texture encoder in texture pipeline. WIP
    [git-p4: depot-paths = "//depot/traktor/": change = 23278]

---
+++ code/Render/Editor/Texture/AstcCompressor.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Render/Editor/Texture/ICompressor.h"
+
+namespace traktor
+{
+	namespace render
+	{
+
+class AstcCompressor : public ICompressor
+{
+	T_RTTI_CLASS;
+
+public:
+	virtual bool compress(Writer& writer, const RefArray< drawing::Image >& mipImages, TextureFormat textureFormat, bool needAlpha, int32_t compressionQuality) const override final;
+};
+
+	}
+}
+
