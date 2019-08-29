dcc5aef645e2ae9c0b8f11a1bb5021b3ef652e1a
Anders Pistol <anders.pistol@doctorentertainment.com>
2019-08-29 20:05:53 +0100

    Traktor: Using solid model generator in pipeline.
    [git-p4: depot-paths = "//depot/traktor/": change = 23329]

--- code/Shape/Editor/Solid/SolidEntityPipeline.cpp
+++ code/Shape/Editor/Solid/SolidEntityPipeline.cpp
@@ -9,11 +9,9 @@
 #include "Physics/StaticBodyDesc.h"
 #include "Physics/Editor/MeshAsset.h"
 #include "Physics/World/RigidBodyComponentData.h"
-#include "Shape/Editor/Solid/IShape.h"
-#include "Shape/Editor/Solid/PrimitiveEntityData.h"
 #include "Shape/Editor/Solid/SolidEntityData.h"
 #include "Shape/Editor/Solid/SolidEntityPipeline.h"
-#include "Shape/Editor/Solid/Utilities.h"
+#include "Shape/Editor/Solid/SolidModelGenerator.h"
 #include "World/Entity/ComponentEntityData.h"
 
 namespace traktor
@@ -21,7 +19,7 @@ namespace traktor
 	namespace shape
 	{
 
-T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.shape.SolidEntityPipeline", 0, SolidEntityPipeline, world::EntityPipeline)
+T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.shape.SolidEntityPipeline", 1, SolidEntityPipeline, world::EntityPipeline)
 
 bool SolidEntityPipeline::create(const editor::IPipelineSettings* settings)
 {
@@ -62,78 +60,9 @@ Ref< ISerializable > SolidEntityPipeline::buildOutput(
 {
 	const SolidEntityData* solidEntityData = mandatory_non_null_type_cast< const SolidEntityData* >(sourceAsset);
     
-    // Get all primitive entities with shape.
-    RefArray< const PrimitiveEntityData > primitiveEntityDatas;
-    for (auto entityData : solidEntityData->getEntityData())
-    {
-        if (const auto primitiveEntityData = dynamic_type_cast< const PrimitiveEntityData* >(entityData))
-        {
-            if (primitiveEntityData->getShape() != nullptr)
-                primitiveEntityDatas.push_back(primitiveEntityData);
-        }
-    }
-
-    // Merge all windings into a single group of windings.
-    AlignedVector< Winding3 > outputWindings;
-
-    auto it = primitiveEntityDatas.begin();
-    if (it != primitiveEntityDatas.end())
-    {
-        (*it)->getShape()->createWindings(outputWindings);
-        outputWindings = transform(outputWindings, (*it)->getTransform());
-
-        for (++it; it != primitiveEntityDatas.end(); ++it)
-        {
-            AlignedVector< Winding3 > windings;
-            (*it)->getShape()->createWindings(windings);
-            if (windings.empty())
-                continue;
-
-            windings = transform(windings, (*it)->getTransform());
-
-            switch ((*it)->getOperation())
-            {
-            case BooleanOperation::BoUnion:
-                {
-                    auto result = unioon(outputWindings, windings);
-                    outputWindings.swap(result);
-                }
-                break;
-
-            case BooleanOperation::BoIntersection:
-                {
-                    auto result = intersection(outputWindings, windings);
-                    outputWindings.swap(result);
-                }
-                break;
-
-            case BooleanOperation::BoDifference:
-                {
-                    auto result = difference(outputWindings, windings);
-                    outputWindings.swap(result);
-                }
-                break;
-            }
-        }
-    }
-
-    // Create model from windings.
-    Ref< model::Model > outputModel = new model::Model();
-    outputModel->addMaterial(model::Material(
-        L"Default"
-    ));
-    for (const auto& winding : outputWindings)
-    {
-        model::Polygon polygon;
-        polygon.setMaterial(0);
-        for (const auto& vx : winding.get())
-        {
-            model::Vertex vertex;
-            vertex.setPosition(outputModel->addUniquePosition(vx));
-            polygon.addVertex(outputModel->addUniqueVertex(vertex));
-        }
-        outputModel->addUniquePolygon(polygon);
-    }
+    Ref< model::Model > outputModel = SolidModelGenerator().createModel(solidEntityData);
+    if (!outputModel)
+        return nullptr;
 
     // model::ModelFormat::writeAny(L"data/Temp/Solid/" + solidEntityData->getName() + L".tmd", outputModel);
 
