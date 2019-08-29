8161c961f2d71a72c0cd2bd98fb714391472691d
Anders Pistol <anders.pistol@doctorentertainment.com>
2019-08-29 14:55:33 +0100

    Traktor: Fixed condition issue with GLSL Iterate2 emitter (Vulkan). Fixed filtering of shadows in Vulkan. Merged some render passes in deferred renderer.
    [git-p4: depot-paths = "//depot/traktor/": change = 23325]

--- code/Render/Vulkan/Editor/Glsl/GlslEmitter.cpp
+++ code/Render/Vulkan/Editor/Glsl/GlslEmitter.cpp
@@ -732,7 +732,7 @@ bool emitIterate2(GlslContext& cx, Iterate2* node)
 		Ref< GlslVariable > condition = cx.emitInput(node, L"Condition");
 		if (condition)
 		{
-			fs << L"if (!(bool)" << condition->cast(GtFloat) << L")" << Endl;
+			fs << L"if (int(" << condition->cast(GtFloat) << L") == 0)" << Endl;
 			fs << L"\tbreak;" << Endl;
 		}
 
