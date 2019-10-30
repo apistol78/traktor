9ffc0277de5feb7bf04f402fbd2d42b8c4c4f751
Anders Pistol <anders.pistol@doctorentertainment.com>
2019-10-30 19:52:18 +0100

    Traktor - Missing files.
    [git-p4: depot-paths = "//depot/traktor/": change = 23520]

---
+++ code/Core/Thread/OsX/Utilities.cpp
@@ -0,0 +1,35 @@
+#include <sys/time.h>
+#include "Core/Thread/OsX/Utilities.h"
+
+namespace traktor
+{
+    namespace
+    {
+
+const int64_t c_nanosPerSecond = 1000000000;
+
+    }
+
+void addNanoSecToTimeSpec(struct timespec* r, int64_t b)
+{
+	r->tv_sec = r->tv_sec + (b / c_nanosPerSecond);
+	r->tv_nsec = r->tv_nsec + (b % c_nanosPerSecond);
+
+	if (r->tv_nsec >= c_nanosPerSecond)
+    {
+		r->tv_sec++;
+		r->tv_nsec -= c_nanosPerSecond;
+	}
+    else if (r->tv_nsec < 0)
+    {
+		r->tv_sec--;
+		r->tv_nsec += c_nanosPerSecond;
+	}
+}
+
+void addMilliSecToTimeSpec(struct timespec* r, int64_t b)
+{
+	addNanoSecToTimeSpec(r, b * 1000000);
+}
+
+}
