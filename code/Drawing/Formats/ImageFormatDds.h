39917c05bf09b3652f9b7f2f75344f03e57028c1
Anders Pistol <anders.pistol@doctorentertainment.com>
2019-11-11 20:54:20 +0100

    Traktor: Implemented a DDS image reader, supports atm DXT1 and DXT5 decoding (also uncompressed).
    [git-p4: depot-paths = "//depot/traktor/": change = 23557]

---
+++ code/Drawing/Formats/ImageFormatDds.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Drawing/IImageFormat.h"
+
+// import/export mechanism.
+#undef T_DLLCLASS
+#if defined(T_DRAWING_EXPORT)
+#	define T_DLLCLASS T_DLLEXPORT
+#else
+#	define T_DLLCLASS T_DLLIMPORT
+#endif
+
+namespace traktor
+{
+	namespace drawing
+	{
+
+/*! DDS format.
+ * \ingroup Drawing
+ */
+class T_DLLCLASS ImageFormatDds : public IImageFormat
+{
+	T_RTTI_CLASS;
+
+public:
+	virtual Ref< Image > read(IStream* stream) override final;
+
+	virtual bool write(IStream* stream, Image* image) override final;
+};
+
+	}
+}
+
