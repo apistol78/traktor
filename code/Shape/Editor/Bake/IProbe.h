7abd7d073521b6c0c64e45bdfda68b7b613104c0
Anders Pistol <anders.pistol@doctorentertainment.com>
2019-11-24 17:31:54 +0100

    Traktor: Clamp excessive values when generating IBL probe in tracer.
    [git-p4: depot-paths = "//depot/traktor/": change = 23598]

--- code/Shape/Editor/Bake/IProbe.h
+++ code/Shape/Editor/Bake/IProbe.h
@@ -13,10 +13,6 @@ class IProbe : public Object
 	T_RTTI_CLASS;
 
 public:
-	virtual float getDensity(const Vector4& direction) const = 0;
-
-	virtual float getProbability(const Vector4& direction) const = 0;
-
 	virtual Color4f sample(const Vector4& direction) const = 0;
 };
 	
