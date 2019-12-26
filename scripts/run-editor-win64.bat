ab9806d441ceed27a674e81d9f62409f88961bf2
Anders Pistol <anders.pistol@doctorentertainment.com>
2019-12-26 21:54:02 +0100

    Traktor: Not launching server process in bat file since it's embedded into editor process.
    [git-p4: depot-paths = "//depot/traktor/": change = 23693]

--- scripts/run-editor-win64.bat
+++ scripts/run-editor-win64.bat
@@ -4,8 +4,5 @@
 call %~dp0config.bat
 call %~dp0config-android.bat
 
-:: Launch remote server.
-start %TRAKTOR_HOME%\bin\latest\win64\releaseshared\Traktor.Remote.Server.App.exe %APPDATA%\TraktorRemoteServer
-
 :: Launch editor.
 start %TRAKTOR_HOME%\bin\latest\win64\releaseshared\Traktor.Editor.App.exe
