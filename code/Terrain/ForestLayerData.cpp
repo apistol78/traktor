9fe512270dfe22926f84e5569fd4d39859501510
Anders Pistol <anders.pistol@doctorentertainment.com>
2020-03-03 15:06:04 +0100

    Traktor: Exposed tweakable parameters of forest layer.
    [git-p4: depot-paths = "//depot/traktor/": change = 23821]

--- code/Terrain/ForestLayerData.cpp
+++ code/Terrain/ForestLayerData.cpp
@@ -1,4 +1,8 @@
+#include "Core/Math/Const.h"
+#include "Core/Serialization/AttributeRange.h"
+#include "Core/Serialization/AttributeUnit.h"
 #include "Core/Serialization/ISerializer.h"
+#include "Core/Serialization/Member.h"
 #include "Mesh/Instance/InstanceMesh.h"
 #include "Resource/Member.h"
 #include "Terrain/ForestLayer.h"
@@ -12,6 +16,13 @@ namespace traktor
 T_IMPLEMENT_RTTI_EDIT_CLASS(L"traktor.terrain.ForestLayerData", 0, ForestLayerData, ITerrainLayerData)
 
 ForestLayerData::ForestLayerData()
+:	m_material(0)
+,	m_density(0.1f)
+,	m_lod0distance(50.0f)
+,	m_lod1distance(200.0f)
+,	m_upness(0.8f)
+,	m_randomScale(0.2f)
+,	m_randomTilt(deg2rad(4.0f))
 {
 }
 
@@ -32,6 +43,13 @@ void ForestLayerData::serialize(ISerializer& s)
 {
 	s >> resource::Member< mesh::InstanceMesh >(L"lod0mesh", m_lod0mesh);
 	s >> resource::Member< mesh::InstanceMesh >(L"lod1mesh", m_lod1mesh);
+	s >> Member< uint8_t >(L"material", m_material);
+	s >> Member< float >(L"density", m_density, AttributeRange(0.0f));
+	s >> Member< float >(L"lod0distance", m_lod0distance, AttributeRange(0.0f) | AttributeUnit(AuMetres));
+	s >> Member< float >(L"lod1distance", m_lod1distance, AttributeRange(0.0f) | AttributeUnit(AuMetres));
+	s >> Member< float >(L"upness", m_upness, AttributeRange(0.0f, 1.0f) | AttributeUnit(AuPercent));
+	s >> Member< float >(L"randomScale", m_randomScale, AttributeRange(0.0f, 1.0f) | AttributeUnit(AuPercent));
+	s >> Member< float >(L"randomTilt", m_randomTilt, AttributeRange(0.0f, HALF_PI) | AttributeUnit(AuDegrees));
 }
 
 	}
