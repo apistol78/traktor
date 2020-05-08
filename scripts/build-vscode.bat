2edac532612bfc00824d18b066e3524711c50de1
Anders Pistol <anders.pistol@doctorentertainment.com>
2020-05-08 11:32:26 +0100

    Traktor: Able to build android from VS code.
    [git-p4: depot-paths = "//depot/traktor/": change = 23974]

--- scripts/build-vscode.bat
+++ scripts/build-vscode.bat
@@ -1,10 +1,21 @@
 @echo off
 
-set CONFIG=%*
+set PLATFORM=%1
+set CONFIG=%2
 
 call %~dp0config.bat
-call %~dp0config-vs-x64.bat
 
-pushd "%TRAKTOR_HOME%\build\win64"
-devenv "Traktor Win64.sln" /Build %CONFIG%
-popd
+if "%PLATFORM%" == "Windows" (
+    call %~dp0config-vs-x64.bat
+    pushd "%TRAKTOR_HOME%\build\win64"
+    devenv "Traktor Win64.sln" /Build %CONFIG%
+    popd
+)
+
+if "%PLATFORM%" == "Android" (
+    call %~dp0config-android.bat
+    pushd "%TRAKTOR_HOME%\build\android"
+    %TRAKTOR_HOME%\3rdp\jom\jom /L /F "Extern Android.mak" %CONFIG%
+    %TRAKTOR_HOME%\3rdp\jom\jom /L /F "Traktor Android.mak" %CONFIG%
+    popd
+)
