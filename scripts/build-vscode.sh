d908b5144fd0c5c3887ff9cc1c06ceb57e01f580
Anders Pistol <anders.pistol@doctorentertainment.com>
2020-05-12 11:41:29 +0100

    Traktor - Able to build iOS and macOS from VSCode.
    [git-p4: depot-paths = "//depot/traktor/": change = 23986]

--- scripts/build-vscode.sh
+++ scripts/build-vscode.sh
@@ -6,15 +6,34 @@ clear
 # Source environment configuration.
 . "`dirname \"$BASH_SOURCE\"`/config.sh"
 
-# Build
-if [[ `uname -s` == Linux* ]]; then
+if [[ "$1" == "Linux" ]]; then
+
 	pushd $TRAKTOR_HOME/build/linux
-	make -s -j6 -f 'Extern Linux.mak' $1
-	make -s -j6 -f 'Traktor Linux.mak' $1
+	make -s -j6 -f 'Extern Linux.mak' $2
+	make -s -j6 -f 'Traktor Linux.mak' $2
 	popd
-elif [[ `uname -s` == Darwin* ]]; then
+
+elif [[ "$1" == "macOS" ]]; then
+
 	pushd $TRAKTOR_HOME/build/osx
-	make -s -j6 -f 'Extern OSX.mak' $1
-	make -s -j6 -f 'Traktor OSX.mak' $1
+	make -s -j6 -f 'Extern OSX.mak' $2
+	make -s -j6 -f 'Traktor OSX.mak' $2
 	popd
+
+elif [[ "$1" == "Android" ]]; then
+
+	. "`dirname \"$BASH_SOURCE\"`/config-android.sh"
+
+	pushd $TRAKTOR_HOME/build/android
+	make -s -j6 -f 'Extern Android.mak' $2
+	make -s -j6 -f 'Traktor Android.mak' $2
+	popd
+
+elif [[ "$1" == "iOS" ]]; then
+
+	pushd $TRAKTOR_HOME/build/ios
+	make -s -j6 -f 'Extern iOS.mak' $2
+	make -s -j6 -f 'Traktor iOS.mak' $2
+	popd
+
 fi
