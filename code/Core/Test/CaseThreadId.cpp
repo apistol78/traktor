94df23f34140d82a8a47b493d6872a52391668a2
Anders Pistol <anders.pistol@doctorentertainment.com>
2020-05-17 20:39:46 +0100

    Traktor: Added "thread ID" unit test case.
    [git-p4: depot-paths = "//depot/traktor/": change = 24000]

---
+++ code/Core/Test/CaseThreadId.cpp
@@ -0,0 +1,51 @@
+#include <set>
+#include "Core/Log/Log.h"
+#include "Core/Test/CaseThreadId.h"
+#include "Core/Thread/Thread.h"
+#include "Core/Thread/ThreadManager.h"
+
+namespace traktor
+{
+	namespace test
+	{
+		namespace
+		{
+
+int32_t g_ids[16];
+
+void threadTest(int32_t number)
+{
+	int32_t id = ThreadManager::getInstance().getCurrentThread()->id();
+	g_ids[number] = id;
+	//log::info << number << L", id " << id << Endl;
+}
+
+		}
+
+T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.test.CaseThreadId", 0, CaseThreadId, Case)
+
+void CaseThreadId::run()
+{
+	Thread* threads[16] = {};
+
+	for (int32_t i = 0; i < 16; ++i)
+		g_ids[i] = -1;
+	for (int32_t i = 0; i < 16; ++i)
+		threads[i] = ThreadManager::getInstance().create(makeStaticFunctor(threadTest, i), L"Thread Id test");
+	for (int32_t i = 0; i < 16; ++i)
+		threads[i]->start();
+	for (int32_t i = 0; i < 16; ++i)
+		threads[i]->wait();
+	for (int32_t i = 0; i < 16; ++i)
+		ThreadManager::getInstance().destroy(threads[i]);
+
+	std::set< int32_t > unique;
+	for (int32_t i = 0; i < 16; ++i)
+		unique.insert(g_ids[i]);
+
+	CASE_ASSERT_EQUAL(unique.size(), 16);
+	CASE_ASSERT(unique.find(-1) == unique.end());
+}
+
+	}
+}
