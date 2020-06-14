4fd6289e00a73ee93191e71c07806a09707f67c8
Anders Pistol <anders.pistol@doctorentertainment.com>
2020-06-14 20:06:11 +0100

    Traktor: Able to create textures, map/unmap, from script.
    [git-p4: depot-paths = "//depot/traktor/": change = 24036]

--- code/Render/RenderClassFactory.cpp
+++ code/Render/RenderClassFactory.cpp
@@ -1,5 +1,6 @@
 #include "Core/Class/AutoRuntimeClass.h"
 #include "Core/Class/IRuntimeClassRegistrar.h"
+#include "Core/Class/Boxes/BoxedPointer.h"
 #include "Render/IRenderSystem.h"
 #include "Render/IRenderView.h"
 #include "Render/ITexture.h"
@@ -39,9 +40,62 @@ private:
 
 T_IMPLEMENT_RTTI_CLASS(L"traktor.render.DisplayMode", BoxedDisplayMode, Object)
 
-Ref< BoxedDisplayMode > IRenderSystem_getDisplayMode(IRenderSystem* this_, uint32_t index)
+class BoxedSimpleTextureCreateDesc : public Object
 {
-	return new BoxedDisplayMode(this_->getDisplayMode(index));
+	T_RTTI_CLASS;
+
+public:
+	BoxedSimpleTextureCreateDesc()
+	{
+		// \fixme Expose texture formats to script.
+		m_value.format = TfR8G8B8A8;
+	}
+
+	BoxedSimpleTextureCreateDesc(const SimpleTextureCreateDesc& stcd)
+	:	m_value(stcd)
+	{
+	}
+
+	void setWidth(int32_t width) { m_value.width = width; }
+
+	int32_t getWidth() const { return m_value.width; }
+
+	void setHeight(int32_t height) { m_value.height = height; }
+
+	int32_t getHeight() const { return m_value.height; }
+
+	void setMipCount(int32_t mipCount) { m_value.mipCount = mipCount; }
+
+	int32_t getMipCount() const { return m_value.mipCount; }
+
+	void set_sRGB(bool sRGB) { m_value.sRGB = sRGB; }
+
+	bool get_sRGB() const { return m_value.sRGB; }
+
+	void setImmutable(bool immutable) { m_value.immutable = immutable; }
+
+	bool getImmutable() const { return m_value.immutable; }
+
+	const SimpleTextureCreateDesc& unbox() const { return m_value; }
+
+private:
+	SimpleTextureCreateDesc m_value;
+};
+
+T_IMPLEMENT_RTTI_CLASS(L"traktor.render.SimpleTextureCreateDesc", BoxedSimpleTextureCreateDesc, Object)
+
+Ref< BoxedPointer > ISimpleTexture_lock(ISimpleTexture* self, int32_t level)
+{
+	ITexture::Lock lock;
+	if (self->lock(level, lock))
+		return new BoxedPointer(lock.bits);
+	else
+		return nullptr;
+}
+
+handle_t IRenderSystem_getHandle(const std::wstring& id)
+{
+	return getParameterHandle(id);
 }
 
 Ref< BoxedDisplayMode > IRenderSystem_getCurrentDisplayMode(IRenderSystem* this_)
@@ -49,9 +103,14 @@ Ref< BoxedDisplayMode > IRenderSystem_getCurrentDisplayMode(IRenderSystem* this_
 	return new BoxedDisplayMode(this_->getCurrentDisplayMode());
 }
 
-handle_t IRenderSystem_getHandle(const std::wstring& id)
+Ref< BoxedDisplayMode > IRenderSystem_getDisplayMode(IRenderSystem* this_, uint32_t index)
 {
-	return getParameterHandle(id);
+	return new BoxedDisplayMode(this_->getDisplayMode(index));
+}
+
+Ref< ISimpleTexture > IRenderSystem_createSimpleTexture(IRenderSystem* self, const BoxedSimpleTextureCreateDesc* stcd)
+{
+	return self->createSimpleTexture(stcd->unbox(), T_FILE_LINE_W);
 }
 
 		}
@@ -67,6 +126,15 @@ void RenderClassFactory::createClasses(IRuntimeClassRegistrar* registrar) const
 	classBoxedDisplayMode->addProperty("colorBits", &BoxedDisplayMode::getColorBits);
 	registrar->registerClass(classBoxedDisplayMode);
 
+	auto classBoxedSimpleTextureCreateDesc = new AutoRuntimeClass< BoxedSimpleTextureCreateDesc >();
+	classBoxedSimpleTextureCreateDesc->addConstructor();
+	classBoxedSimpleTextureCreateDesc->addProperty("width", &BoxedSimpleTextureCreateDesc::setWidth, &BoxedSimpleTextureCreateDesc::getWidth);
+	classBoxedSimpleTextureCreateDesc->addProperty("height", &BoxedSimpleTextureCreateDesc::setHeight, &BoxedSimpleTextureCreateDesc::getHeight);
+	classBoxedSimpleTextureCreateDesc->addProperty("mipCount", &BoxedSimpleTextureCreateDesc::setMipCount, &BoxedSimpleTextureCreateDesc::getMipCount);
+	classBoxedSimpleTextureCreateDesc->addProperty("sRGB", &BoxedSimpleTextureCreateDesc::set_sRGB, &BoxedSimpleTextureCreateDesc::get_sRGB);
+	classBoxedSimpleTextureCreateDesc->addProperty("immutable", &BoxedSimpleTextureCreateDesc::setImmutable, &BoxedSimpleTextureCreateDesc::getImmutable);
+	registrar->registerClass(classBoxedSimpleTextureCreateDesc);
+
 	auto classITexture = new AutoRuntimeClass< ITexture >();
 	classITexture->addProperty("mips", &ITexture::getMips);
 	registrar->registerClass(classITexture);
@@ -78,6 +146,8 @@ void RenderClassFactory::createClasses(IRuntimeClassRegistrar* registrar) const
 	auto classISimpleTexture = new AutoRuntimeClass< ISimpleTexture >();
 	classISimpleTexture->addProperty("width", &ISimpleTexture::getWidth);
 	classISimpleTexture->addProperty("height", &ISimpleTexture::getHeight);
+	classISimpleTexture->addMethod("lock", &ISimpleTexture_lock);
+	classISimpleTexture->addMethod("unlock", &ISimpleTexture::unlock);
 	registrar->registerClass(classISimpleTexture);
 
 	auto classIVolumeTexture = new AutoRuntimeClass< IVolumeTexture >();
@@ -92,6 +162,12 @@ void RenderClassFactory::createClasses(IRuntimeClassRegistrar* registrar) const
 	classIRenderSystem->addProperty("currentDisplayMode", &IRenderSystem_getCurrentDisplayMode);
 	classIRenderSystem->addProperty("displayAspectRatio", &IRenderSystem::getDisplayAspectRatio);
 	classIRenderSystem->addMethod("getDisplayMode", &IRenderSystem_getDisplayMode);
+	// classIRenderSystem->addMethod("createVertexBuffer", &IRenderSystem_createVertexBuffer);
+	// classIRenderSystem->addMethod("createIndexBuffer", &IRenderSystem_createIndexBuffer);
+	// classIRenderSystem->addMethod("createStructBuffer", &IRenderSystem_createStructBuffer);
+	classIRenderSystem->addMethod("createSimpleTexture", &IRenderSystem_createSimpleTexture);
+	// classIRenderSystem->addMethod("createCubeTexture", &IRenderSystem_createCubeTexture);
+	// classIRenderSystem->addMethod("createVolumeTexture", &IRenderSystem_createVolumeTexture);
 	registrar->registerClass(classIRenderSystem);
 
 	auto classIRenderView = new AutoRuntimeClass< IRenderView >();
