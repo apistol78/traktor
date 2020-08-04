54217b2f884a20618288ffcd9fca42e1a22a8bff
Anders Pistol <anders.pistol@doctorentertainment.com>
2020-08-04 13:40:56 +0100

    Traktor: Replaced thekla atlas with xatlas.
    [git-p4: depot-paths = "//depot/traktor/": change = 24088]

--- code/Model/Operations/UnwrapUV.cpp
+++ code/Model/Operations/UnwrapUV.cpp
@@ -1,13 +1,11 @@
 #include <cmath>
-#include <thekla/thekla_atlas.h>
+#include <xatlas.h>
 #include "Core/Log/Log.h"
 #include "Core/Math/Aabb2.h"
 #include "Core/Serialization/ISerializer.h"
 #include "Core/Serialization/Member.h"
 #include "Model/Model.h"
 #include "Model/Operations/UnwrapUV.h"
-
-using namespace Thekla;
 
 namespace traktor
 {
@@ -24,138 +22,124 @@ UnwrapUV::UnwrapUV(int32_t channel, uint32_t textureSize)
 
 bool UnwrapUV::apply(Model& model) const
 {
-	Atlas_Options options;
-	atlas_set_default_options(&options);
-	options.packer_options.witness.packing_quality = 1;
-	options.packer_options.witness.texel_area = 128.0f / (float)m_textureSize;
-	//options.packer_options.witness.conservative = true;
+	xatlas::Atlas* atlas = xatlas::Create();
 
-	// Build input vertices to atlas generator.
-	AlignedVector< Atlas_Input_Vertex > inputVertices;
-	for (int32_t i = 0; i < (int32_t)model.getVertexCount(); ++i)
+	AlignedVector< float > vertices;
+	AlignedVector< float > normals;
+	AlignedVector< float > texCoords;
+	AlignedVector< uint32_t > indices;
+	SmallMap< uint32_t, uint32_t > vertexToPolygon;
+
+	for (uint32_t i = 0; i < model.getVertexCount(); ++i)
 	{
-		auto& aiv = inputVertices.push_back();
-
 		const Vertex& vertex = model.getVertex(i);
+
 		const Vector4& p = model.getPosition(vertex.getPosition());
-		aiv.position[0] = p.x();
-		aiv.position[1] = p.y();
-		aiv.position[2] = p.z();
+		vertices.push_back(p.x());
+		vertices.push_back(p.y());
+		vertices.push_back(p.z());
 
 		if (vertex.getNormal() != c_InvalidIndex)
 		{
 			const Vector4& n = model.getNormal(vertex.getNormal());
-			aiv.normal[0] = n.x();
-			aiv.normal[1] = n.y();
-			aiv.normal[2] = n.z();
-		}
-		else
-		{
-			aiv.normal[0] = 0.0f;
-			aiv.normal[1] = 0.0f;
-			aiv.normal[2] = 0.0f;
+			normals.push_back(n.x());
+			normals.push_back(n.y());
+			normals.push_back(n.z());
 		}
 
 		if (vertex.getTexCoord(m_channel) != c_InvalidIndex)
 		{
 			const Vector2& tc = model.getTexCoord(vertex.getTexCoord(m_channel));
-			aiv.uv[0] = tc.x * m_textureSize;
-			aiv.uv[1] = tc.y * m_textureSize;
-		}
-		else
-		{
-			aiv.uv[0] = 0.0f;
-			aiv.uv[1] = 0.0f;
-		}
-
-		aiv.first_colocal = i;
-		for (int32_t j = 0; j < i; ++j)
-		{
-			if (model.getVertex(j).getPosition() == vertex.getPosition())
-			{
-				aiv.first_colocal = j;
-				break;
-			}
+			texCoords.push_back(tc.x);
+			texCoords.push_back(tc.y);
 		}
 	}
-
-	// Build input faces to atlas generator.
-	AlignedVector< Atlas_Input_Face > inputFaces;
-	SmallMap< uint32_t, uint32_t > vertexToPolygon;
 
 	for (uint32_t i = 0; i < model.getPolygonCount(); ++i)
 	{
 		const Polygon& polygon = model.getPolygon(i);
+		if (polygon.getVertexCount() != 3)
+			continue;
 
-		auto& aif = inputFaces.push_back();
-		aif.vertex_index[0] = polygon.getVertex(0);
-		aif.vertex_index[1] = polygon.getVertex(1);
-		aif.vertex_index[2] = polygon.getVertex(2);
-		aif.material_index = 0;
+		indices.push_back(polygon.getVertex(0));
+		indices.push_back(polygon.getVertex(1));
+		indices.push_back(polygon.getVertex(2));
 
-		vertexToPolygon[aif.vertex_index[0]] = i;
-		vertexToPolygon[aif.vertex_index[1]] = i;
-		vertexToPolygon[aif.vertex_index[2]] = i;
+		vertexToPolygon[polygon.getVertex(0)] = i;
+		vertexToPolygon[polygon.getVertex(1)] = i;
+		vertexToPolygon[polygon.getVertex(2)] = i;
 	}
 
-	// Generate UV atlas.
-	Atlas_Input_Mesh input;
-    input.vertex_count = (int)inputVertices.size();
-    input.vertex_array = inputVertices.ptr();
-    input.face_count = (int)inputFaces.size();
-    input.face_array = inputFaces.ptr();
+	xatlas::MeshDecl meshDecl;
+	meshDecl.vertexCount = (uint32_t)vertices.size() / 3;
+	meshDecl.vertexPositionData = vertices.c_ptr();
+	meshDecl.vertexPositionStride = sizeof(float) * 3;
+	if (!normals.empty())
+	{
+		meshDecl.vertexNormalData = normals.c_ptr();
+		meshDecl.vertexNormalStride = sizeof(float) * 3;
+	}
+	if (!texCoords.empty())
+	{
+		meshDecl.vertexUvData = texCoords.c_ptr();
+		meshDecl.vertexUvStride = sizeof(float) * 2;
+	}
+	meshDecl.indexCount = (uint32_t)indices.size();
+	meshDecl.indexData = indices.c_ptr();
+	meshDecl.indexFormat = xatlas::IndexFormat::UInt32;
 
-	Atlas_Error error = Atlas_Error_Success;
-	Atlas_Output_Mesh* output = atlas_generate(&input, &options, &error);
-	if (!output || error != Atlas_Error_Success)
+	xatlas::AddMeshError::Enum error = xatlas::AddMesh(atlas, meshDecl);
+	if (error != xatlas::AddMeshError::Success) 
+	{
+		xatlas::Destroy(atlas);
 		return false;
-
-	// Calculate bounding box of generated texcoords.
-	Aabb2 uvbb;
-	for (int32_t i = 0; i < output->vertex_count; ++i)
-	{
-		const auto& aov = output->vertex_array[i];
-		uvbb.contain(Vector2(aov.uv[0], aov.uv[1]));
 	}
-	uvbb.mn -= Vector2(1.0f, 1.0f);
-	uvbb.mx += Vector2(1.0f, 1.0f);
 
-	// Insert normalized texcoords into model.
+	xatlas::Generate(atlas);
+
+	if (atlas->width == 0 || atlas->height == 0)
 	{
-		AlignedVector< Polygon > originalPolygons = model.getPolygons();
+		xatlas::Destroy(atlas);
+		return false;
+	}
 
-		model.clear(Model::CfPolygons);
+	AlignedVector< Vertex > originalVertices = model.getVertices();
+	AlignedVector< Polygon > originalPolygons = model.getPolygons();
 
-		for (int32_t i = 0; i < output->index_count; i += 3)
+	model.clear(Model::CfVertices | Model::CfPolygons);
+
+	for (uint32_t i = 0; i < atlas->meshCount; ++i)
+	{
+		const xatlas::Mesh& mesh = atlas->meshes[i];
+		for (uint32_t j = 0; j < mesh.indexCount; j += 3)
 		{
-			Polygon polygon = originalPolygons[vertexToPolygon[
-				output->vertex_array[output->index_array[i]].xref
-			]];
+			const xatlas::Vertex& vfirst = mesh.vertexArray[mesh.indexArray[j]];
 
-			for (int32_t j = 0; j < 3; ++j)
+			Polygon polygon = originalPolygons[vertexToPolygon[vfirst.xref]];
+			if (polygon.getVertexCount() != 3)
+				continue;
+
+			for (int32_t k = 0; k < 3; ++k)
 			{
-				int32_t index = output->index_array[i + j];
-				const auto& aov = output->vertex_array[index];
+				int32_t index = mesh.indexArray[j + k];
+				const xatlas::Vertex& v = mesh.vertexArray[index];
 
-				Vertex vx = model.getVertex(aov.xref);
+				Vertex vx = originalVertices[v.xref];
 
-				Vector2 uv(aov.uv[0], aov.uv[1]);
-				uv -= uvbb.mn;
-				uv /= uvbb.mx - uvbb.mn;
-
-				uv.x = (std::floor(uv.x * m_textureSize)) / m_textureSize;
-				uv.y = (std::floor(uv.y * m_textureSize)) / m_textureSize;
+				Vector2 uv(v.uv[0], v.uv[1]);
+				uv.x /= (float)atlas->width;
+				uv.y /= (float)atlas->height;
 
 				vx.setTexCoord(m_channel, model.addTexCoord(uv));
 
-				polygon.setVertex(j, model.addUniqueVertex(vx));
+				polygon.setVertex(k, model.addUniqueVertex(vx));
 			}
 
 			model.addPolygon(polygon);
 		}
 	}
 
-	atlas_free(output);
+	xatlas::Destroy(atlas);
 	return true;
 }
 
