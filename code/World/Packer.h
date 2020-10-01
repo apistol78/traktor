16cf299cf2da777c4c9b95c0b594ac9711b82ae1
Anders Pistol <anders.pistol@doctorentertainment.com>
2020-10-01 18:13:26 +0100

    Traktor: Shadows in Forward renderer fixed, using a single shadow map for everything and a 2d online allocator to allocate part of map for each light.
    [git-p4: depot-paths = "//depot/traktor/": change = 24231]

---
+++ code/World/Packer.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Core/Object.h"
+
+namespace traktor
+{
+	namespace world
+	{
+
+class Packer : public Object
+{
+	T_RTTI_CLASS;
+
+public:
+	struct Rectangle
+	{
+		int32_t x;
+		int32_t y;
+		int32_t width;
+		int32_t height;
+	};
+
+	Packer(int32_t width, int32_t height);
+
+	virtual ~Packer();
+
+	bool insert(int32_t width, int32_t height, Rectangle& outRectangle);
+
+	void reset();
+
+private:
+	class PackerImpl* m_impl;
+};
+
+	}
+}
