f3d55ae986a2d7ffb29e08251cd308e0926c7806
Anders Pistol <anders.pistol@doctorentertainment.com>
2020-11-17 20:44:49 +0100

    Traktor: Process RGBM lightmaps before ASTC compression, need to prepare them because of blocked compression.
    [git-p4: depot-paths = "//depot/traktor/": change = 24424]

--- code/Shape/Editor/Bake/TracerProcessor.cpp
+++ code/Shape/Editor/Bake/TracerProcessor.cpp
@@ -112,7 +112,7 @@ Ref< drawing::Image > denoise(const GBuffer& gbuffer, drawing::Image* lightmap)
 
 void encodeRGBM(drawing::Image* image)
 {
-	const Scalar c_rangeDenom = 1.0_simd / 8.0_simd;
+	const Scalar c_rangeDenom = 1.0_simd / 5.0_simd;
 	const Scalar c_epsilon= 1e-6_simd;
 
 	Color4f cl;
@@ -136,6 +136,108 @@ void encodeRGBM(drawing::Image* image)
 			image->setPixelUnsafe(x, y, cl);
 		}
 	}
+}
+
+// Force val[N+1] to be contrained to not be less than val[N]*(R/256).
+void imposeRatio(uint8_t* data, int32_t length, int32_t stride, int32_t ratio)
+{
+    uint8_t val = *data;
+    for (int32_t i = 1; i < length; i++)
+	{
+        data += stride;
+
+        int32_t min_val = (val * ratio) >> 8;
+        uint8_t vl2 = *data;
+        if (vl2 < min_val)
+            *data = vl2 = min_val;
+
+        val = vl2;
+    }
+}
+
+// https://github.com/ARM-software/astc-encoder/blob/master/Utils/astc_rgbm_blockify.cpp
+void prepareForBlockCompression(drawing::Image* image, int32_t blockDimX, int32_t blockDimY, float ratio)
+{
+	const int32_t ratioI = (int32_t)std::floor(ratio * 256.0f + 0.5f);
+
+	int32_t blockCountX = (image->getWidth() + blockDimX - 1) / blockDimX;
+	int32_t blockCountY = (image->getHeight() + blockDimY - 1) / blockDimY;
+	int32_t leftOverY = blockDimY * blockCountY - image->getHeight();
+
+	// For each block, obtain the maximum M-value.
+	AlignedVector< uint8_t > mbuf;
+	mbuf.resize(blockCountX * blockCountY, 0);
+
+	uint8_t* imageData = (uint8_t*)image->getData();
+	for (int32_t y = 0; y < image->getHeight(); y++)
+	{
+		uint8_t* s = imageData + 4 * y * image->getWidth() + 3;
+		uint8_t* d = mbuf.ptr() + ((y + leftOverY) / blockDimY) * blockCountX;
+
+		int32_t ctr = 0;
+
+		int32_t accum = *d;
+		for (int x = 0; x < image->getWidth(); x++)
+		{
+			uint8_t p = s[4 * x];
+			if (p > accum)
+				accum = p;
+
+			if (++ctr == blockDimX)
+			{
+				*d++ = accum;
+				accum = *d;
+				ctr = 0;
+			}
+		}
+
+		if (ctr != 0) {
+			*d = accum;
+		}
+	}
+
+    // Impose ratio restriction on M-values in adjacent blocks.
+	for (int32_t y = 0;  y < blockCountY; y++)
+	{
+		imposeRatio(mbuf.ptr() + y * blockCountX, blockCountX, 1, ratioI);
+		imposeRatio(mbuf.ptr() + ((y + 1) * blockCountX) - 1, blockCountX, - 1, ratioI);
+	}
+
+	for (int32_t x = 0; x < blockCountX; x++)
+	{
+		imposeRatio(mbuf.ptr() + x, blockCountY, blockCountX, ratioI);
+		imposeRatio(mbuf.ptr() + x + (blockCountX * (blockCountY - 1)), blockCountY, -blockCountX, ratioI);
+	}
+
+    // For each pixel, scale the pixel RGB values based on chosen M.
+    for (int32_t y = 0; y < image->getHeight(); y++)
+	{
+		uint8_t* s = imageData + 4 * y * image->getWidth();
+		uint8_t* d = mbuf.ptr() + ((y + leftOverY) / blockDimY) * blockCountX;
+
+		int32_t ctr = 0;
+		int32_t mm = *d++;
+		for (int32_t x = 0; x < image->getWidth(); x++)
+		{
+			uint8_t m = s[4 * x + 3];
+			if (m != mm)
+			{
+				uint8_t r = s[4 * x];
+				uint8_t g = s[4 * x + 1];
+				uint8_t b = s[4 * x + 2];
+				s[4 * x]     = (r * m + (mm >> 1)) / mm;
+				s[4 * x + 1] = (g * m + (mm >> 1)) / mm;
+				s[4 * x + 2] = (b * m + (mm >> 1)) / mm;
+				s[4 * x + 3] = mm;
+			}
+
+			if (++ctr == blockDimX)
+			{
+				ctr = 0;
+				mm = *d++;
+			}
+		}
+    }
 }
 
 bool writeTexture(
@@ -166,6 +268,7 @@ bool writeTexture(
 	{
 		encodeRGBM(lightmapFormat);
 		lightmapFormat->convert(drawing::PixelFormat::getR8G8B8A8().endianSwapped());
+		prepareForBlockCompression(lightmapFormat, 4, 4, 0.8f);
 		textureFormat = render::TfASTC4x4;
 		compressor = new render::AstcCompressor();
 		needAlpha = true;
@@ -395,7 +498,7 @@ bool TracerProcessor::process(const TracerTask* task)
 		const uint32_t channel = renderModel->getTexCoordChannel(L"Lightmap");
 
 		// Update status.
-		m_status.description = tracerOutput->getName() + L"...";
+		m_status.description = str(L"%s (%d)...", tracerOutput->getName().c_str(), width);
 
 		// Create GBuffer of mesh's geometry.
 		GBuffer gbuffer;
