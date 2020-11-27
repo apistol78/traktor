9f25e13a05c2a35909769f9c05a6553ebe2aba8c
Anders Pistol <anders.pistol@doctorentertainment.com>
2020-11-27 14:26:45 +0100

    Traktor: Removed BugSplat reference. Also fixed missing symbols when statically linking target.
    [git-p4: depot-paths = "//depot/traktor/": change = 24461]

--- code/Render/Module.cpp
+++ code/Render/Module.cpp
@@ -4,6 +4,7 @@
 #	include "Render/RenderClassFactory.h"
 #	include "Render/Image2/AmbientOcclusionData.h"
 #	include "Render/Image2/DirectionalBlurData.h"
+#	include "Render/Image2/ImagePassData.h"
 #	include "Render/Image2/ShadowProjectData.h"
 #	include "Render/Image2/SimpleData.h"
 
@@ -17,6 +18,7 @@ extern "C" void __module__Traktor_Render()
 	T_FORCE_LINK_REF(RenderClassFactory);
 	T_FORCE_LINK_REF(AmbientOcclusionData);
 	T_FORCE_LINK_REF(DirectionalBlurData);
+	T_FORCE_LINK_REF(ImagePassData);
 	T_FORCE_LINK_REF(ShadowProjectData);
 	T_FORCE_LINK_REF(SimpleData);
 }
