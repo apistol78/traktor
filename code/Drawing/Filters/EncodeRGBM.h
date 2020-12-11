f87197434ae68ae821702bf71eedfc86835e2f2b
Anders Pistol <anders.pistol@doctorentertainment.com>
2020-12-11 15:44:40 +0100

    Traktor: Moved RGBM encoder to an Image filter class.
    [git-p4: depot-paths = "//depot/traktor/": change = 24502]

---
+++ code/Drawing/Filters/EncodeRGBM.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Drawing/IImageFilter.h"
+
+// import/export mechanism.
+#undef T_DLLCLASS
+#if defined(T_DRAWING_EXPORT)
+#	define T_DLLCLASS T_DLLEXPORT
+#else
+#	define T_DLLCLASS T_DLLIMPORT
+#endif
+
+namespace traktor
+{
+	namespace drawing
+	{
+
+/*! RGBM encoding filter.
+ * \ingroup Drawing
+ */
+class T_DLLCLASS EncodeRGBM : public IImageFilter
+{
+	T_RTTI_CLASS;
+
+public:
+	EncodeRGBM(float range, int32_t blockSizeX = 0, int32_t blockSizeY = 0, float blockFilterRatio = 0.0f);
+
+protected:
+	virtual void apply(Image* image) const override final;
+
+private:
+	float m_range;
+	int32_t m_blockSizeX;
+	int32_t m_blockSizeY;
+	float m_blockFilterRatio;
+};
+
+	}
+}
+
