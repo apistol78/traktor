67eb3621e325ce85ce5a8f2f626b19b2b2a75048
Anders Pistol <anders.pistol@doctorentertainment.com>
2021-03-11 17:49:49 +0100

    Traktor: Only standalone pipeline built on Linux.
    [git-p4: depot-paths = "//depot/traktor/": change = 24809]

--- scripts/run-editor-linux.sh
+++ scripts/run-editor-linux.sh
@@ -4,8 +4,5 @@
 . "`dirname \"$BASH_SOURCE\"`/config.sh"
 . "`dirname \"$BASH_SOURCE\"`/config-android.sh"
 
-# Launch remote server.
-$TRAKTOR_HOME/bin/latest/linux/releaseshared/Traktor.Remote.Server.App "$HOME/Library/Doctor Entertainment AB/TraktorRemoteServer" &
-
 # Launch editor.
 ($TRAKTOR_HOME/bin/latest/linux/releaseshared/Traktor.Editor.App > /dev/null &)
