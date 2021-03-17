cbd88cb0c34eb2d28c5b4798e0d2ff569fe8352c
Anders Pistol <anders.pistol@doctorentertainment.com>
2021-03-17 14:19:33 +0100

    Traktor: Updated Bullet physics library.
    [git-p4: depot-paths = "//depot/traktor/": change = 24830]

--- scripts/config.bat
+++ scripts/config.bat
@@ -8,7 +8,7 @@ if %TRAKTOR_HOME:~-1%==\ set TRAKTOR_HOME=%TRAKTOR_HOME:~0,-1%
 set STEAMWORKS_SDK=%TRAKTOR_HOME%\3rdp\steamworks-sdk-142
 set DETOURS_SDK=%TRAKTOR_HOME%\3rdp\Detours-master-180809
 set FBX_SDK=%TRAKTOR_HOME%\3rdp\FbxSdk\2016.1.2
-set BULLET_SDK=%TRAKTOR_HOME%\3rdp\bullet3-master-170526
+set BULLET_SDK=%TRAKTOR_HOME%\3rdp\bullet3-master-210317
 set PHYSX_SDK=%TRAKTOR_HOME%\3rdp\PhysX-3.3.1
 set ANGLE_SDK=%TRAKTOR_HOME%\3rdp\angle-master-160921
 set GLSL_OPTIMIZER=%TRAKTOR_HOME%\3rdp\glsl-optimizer-master-20160321
