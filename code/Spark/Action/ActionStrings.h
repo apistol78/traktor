4af58a3efe27f6a647aecab98ddcb4c0747f953d
Anders Pistol <anders.pistol@doctorentertainment.com>
2021-03-24 19:31:07 +0100

    Traktor: "Tiny spark", disabled a lot of Flash classes since we don't use them any more.
    [git-p4: depot-paths = "//depot/traktor/": change = 24848]

--- code/Spark/Action/ActionStrings.h
+++ code/Spark/Action/ActionStrings.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <map>
 #include <string>
 #include "Core/Object.h"
+#include "Core/Containers/SmallMap.h"
 
 namespace traktor
 {
@@ -14,15 +14,13 @@ class ActionStrings : public Object
 	T_RTTI_CLASS;
 
 public:
-	ActionStrings();
-
 	uint32_t operator [] (const std::string& str);
 
 	const std::string& operator [] (uint32_t id) const;
 
 private:
-	std::map< std::string, uint32_t > m_strings;
-	uint32_t m_next;
+	SmallMap< std::string, uint32_t > m_strings;
+	uint32_t m_next = 0;
 };
 
 	}
