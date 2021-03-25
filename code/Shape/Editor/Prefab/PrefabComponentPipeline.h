7caad84758bc82e47e940b8462171ba5f7b815fc
Anders Pistol <anders.pistol@doctorentertainment.com>
2021-03-25 22:37:38 +0100

    Traktor: Minor cleanup in prefab pipeline.
    [git-p4: depot-paths = "//depot/traktor/": change = 24850]

--- code/Shape/Editor/Prefab/PrefabComponentPipeline.h
+++ code/Shape/Editor/Prefab/PrefabComponentPipeline.h
@@ -20,19 +20,9 @@ class T_DLLCLASS PrefabComponentPipeline : public world::EntityPipeline
 	T_RTTI_CLASS;
 
 public:
-	PrefabComponentPipeline();
-
 	virtual bool create(const editor::IPipelineSettings* settings) override final;
 
 	virtual TypeInfoSet getAssetTypes() const override final;
-
-	virtual bool buildDependencies(
-		editor::IPipelineDepends* pipelineDepends,
-		const db::Instance* sourceInstance,
-		const ISerializable* sourceAsset,
-		const std::wstring& outputPath,
-		const Guid& outputGuid
-	) const override;
 
 	virtual Ref< ISerializable > buildOutput(
 		editor::IPipelineBuilder* pipelineBuilder,
@@ -42,7 +32,7 @@ public:
 	) const override final;
 
 private:
-	bool m_editor;
+	bool m_editor = false;
 };
 
 	}
