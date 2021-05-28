867ab13ca21697a4dc03e998ac946f2984324c1d
Anders Pistol <anders.pistol@doctorentertainment.com>
2021-05-28 13:22:18 +0100

    Traktor: Irradiance grid now uses 32-bit vec4 instead of half4 since support is weird on desktop.
    [git-p4: depot-paths = "//depot/traktor/": change = 24963]

--- code/World/IrradianceGrid.h
+++ code/World/IrradianceGrid.h
@@ -32,16 +32,6 @@ class StructBuffer;
  */
 struct IrradianceGridData
 {
-#if !defined(__ANDROID__)
-	half_t shR0_3[4];   // 0-8
-	half_t shR4_7[4];   // 8-16
-	half_t shG0_3[4];   // 16-24
-	half_t shG4_7[4];   // 24-32
-	half_t shB0_3[4];   // 32-40
-	half_t shB4_7[4];   // 40-48
-	half_t shRGB_8[4];  // 48-56
-    uint8_t pad[8];     // 56-64
-#else
 	float shR0_3[4];
 	float shR4_7[4];
 	float shG0_3[4];
@@ -49,7 +39,6 @@ struct IrradianceGridData
 	float shB0_3[4];
 	float shB4_7[4];
 	float shRGB_8[4];
-#endif
 };
 
 #pragma pack()
