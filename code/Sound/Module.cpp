0bbb1feff05b16d1f0a96a333efe30f1fc34086a
Anders Pistol <anders.pistol@doctorentertainment.com>
2021-06-13 09:17:30 +0100

    Traktor: Fixed static link of sound processor.
    [git-p4: depot-paths = "//depot/traktor/": change = 24994]

--- code/Sound/Module.cpp
+++ code/Sound/Module.cpp
@@ -8,6 +8,24 @@
 #	include "Sound/Decoders/Mp3StreamDecoder.h"
 #	include "Sound/Decoders/OggStreamDecoder.h"
 #	include "Sound/Decoders/WavStreamDecoder.h"
+
+// Processor
+#	include "Sound/Processor/GraphResource.h"
+#	include "Sound/Processor/Nodes/Add.h"
+#	include "Sound/Processor/Nodes/Blend.h"
+#	include "Sound/Processor/Nodes/Divide.h"
+#	include "Sound/Processor/Nodes/Modulate.h"
+#	include "Sound/Processor/Nodes/Multiply.h"
+#	include "Sound/Processor/Nodes/Output.h"
+#	include "Sound/Processor/Nodes/Parameter.h"
+#	include "Sound/Processor/Nodes/Pitch.h"
+#	include "Sound/Processor/Nodes/Scalar.h"
+#	include "Sound/Processor/Nodes/Sine.h"
+#	include "Sound/Processor/Nodes/Source.h"
+#	include "Sound/Processor/Nodes/Subtract.h"
+#	include "Sound/Processor/Nodes/Time.h"
+
+// Resound
 #	include "Sound/Resound/BankResource.h"
 #	include "Sound/Resound/BlendGrainData.h"
 #	include "Sound/Resound/EnvelopeGrainData.h"
@@ -34,6 +52,24 @@ extern "C" void __module__Traktor_Sound()
 	T_FORCE_LINK_REF(Mp3StreamDecoder);
 	T_FORCE_LINK_REF(OggStreamDecoder);
 	T_FORCE_LINK_REF(WavStreamDecoder);
+
+	// Processor
+	T_FORCE_LINK_REF(GraphResource);
+	T_FORCE_LINK_REF(Add);
+	T_FORCE_LINK_REF(Blend);
+	T_FORCE_LINK_REF(Divide);
+	T_FORCE_LINK_REF(Modulate);
+	T_FORCE_LINK_REF(Multiply);
+	T_FORCE_LINK_REF(Output);
+	T_FORCE_LINK_REF(Parameter);
+	T_FORCE_LINK_REF(Pitch);
+	T_FORCE_LINK_REF(Scalar);
+	T_FORCE_LINK_REF(Sine);
+	T_FORCE_LINK_REF(Source);
+	T_FORCE_LINK_REF(Subtract);
+	T_FORCE_LINK_REF(Time);
+
+	// Resound
 	T_FORCE_LINK_REF(BankResource);
 	T_FORCE_LINK_REF(BlendGrainData);
 	T_FORCE_LINK_REF(EnvelopeGrainData);
