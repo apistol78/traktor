1f0da8421f0d4d147129129bc5268acb2e082972
apistol <anders.pistol@doctorentertainment.com>
2021-06-30 20:09:05 +0200

    Traktor: Fixed autobuild clean doc script.

--- scripts/autobuild/autobuild-clean-doc.bat
+++ scripts/autobuild/autobuild-clean-doc.bat
@@ -4,9 +4,26 @@ call "%~dp0..\config.bat"
 
 pushd %TRAKTOR_HOME%
 
-rd /Q /S "bin\latest\html" 2> nul || rem
-if %ERRORLEVEL% equ 2 ( cmd /C exit 0 )
-if %ERRORLEVEL% neq 0 ( exit /B 1 )
+rd /Q /S "resources\documentation\latest" 2> nul || rem
+
+if %ERRORLEVEL% equ 1 (
+	popd
+	cmd /C exit 0
+)
+if %ERRORLEVEL% equ 2 (
+	popd
+	cmd /C exit 0
+)
+if %ERRORLEVEL% equ 3 (
+	popd
+	cmd /C exit 0
+)
+if %ERRORLEVEL% neq 0 ( 
+	echo Unable to remove old documentation output.
+	echo %ERRORLEVEL%
+	popd
+	exit /B 1
+)
 
 popd
 
