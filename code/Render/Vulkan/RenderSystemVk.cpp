9d16b790fadfe9969c547e0dc2dc504a8a097f5b
Anders Pistol <anders.pistol@doctorentertainment.com>
2021-07-20 20:44:20 +0200

    Traktor: Enable Vulkan 1.2 in VMA. Also let library resolve it's api functions it self.

--- code/Render/Vulkan/RenderSystemVk.cpp
+++ code/Render/Vulkan/RenderSystemVk.cpp
@@ -360,31 +360,32 @@ bool RenderSystemVk::create(const RenderSystemDesc& desc)
 	}
 
 	// Create memory allocator.
-	VmaVulkanFunctions vf = {};
-	vf.vkGetPhysicalDeviceProperties = vkGetPhysicalDeviceProperties;
-	vf.vkGetPhysicalDeviceMemoryProperties = vkGetPhysicalDeviceMemoryProperties;
-	vf.vkAllocateMemory = vkAllocateMemory;
-	vf.vkFreeMemory = vkFreeMemory;
-	vf.vkMapMemory = vkMapMemory;
-	vf.vkUnmapMemory = vkUnmapMemory;
-	vf.vkFlushMappedMemoryRanges = vkFlushMappedMemoryRanges;
-	vf.vkInvalidateMappedMemoryRanges = vkInvalidateMappedMemoryRanges;
-	vf.vkBindBufferMemory = vkBindBufferMemory;
-	vf.vkBindImageMemory = vkBindImageMemory;
-	vf.vkGetBufferMemoryRequirements = vkGetBufferMemoryRequirements;
-	vf.vkGetImageMemoryRequirements = vkGetImageMemoryRequirements;
-	vf.vkCreateBuffer = vkCreateBuffer;
-	vf.vkDestroyBuffer = vkDestroyBuffer;
-	vf.vkCreateImage = vkCreateImage;
-	vf.vkDestroyImage = vkDestroyImage;
-	vf.vkCmdCopyBuffer = vkCmdCopyBuffer;
-	vf.vkGetBufferMemoryRequirements2KHR = vkGetBufferMemoryRequirements2KHR;
-	vf.vkGetImageMemoryRequirements2KHR = vkGetImageMemoryRequirements2KHR;
+	// VmaVulkanFunctions vf = {};
+	// vf.vkGetPhysicalDeviceProperties = vkGetPhysicalDeviceProperties;
+	// vf.vkGetPhysicalDeviceMemoryProperties = vkGetPhysicalDeviceMemoryProperties;
+	// vf.vkAllocateMemory = vkAllocateMemory;
+	// vf.vkFreeMemory = vkFreeMemory;
+	// vf.vkMapMemory = vkMapMemory;
+	// vf.vkUnmapMemory = vkUnmapMemory;
+	// vf.vkFlushMappedMemoryRanges = vkFlushMappedMemoryRanges;
+	// vf.vkInvalidateMappedMemoryRanges = vkInvalidateMappedMemoryRanges;
+	// vf.vkBindBufferMemory = vkBindBufferMemory;
+	// vf.vkBindImageMemory = vkBindImageMemory;
+	// vf.vkGetBufferMemoryRequirements = vkGetBufferMemoryRequirements;
+	// vf.vkGetImageMemoryRequirements = vkGetImageMemoryRequirements;
+	// vf.vkCreateBuffer = vkCreateBuffer;
+	// vf.vkDestroyBuffer = vkDestroyBuffer;
+	// vf.vkCreateImage = vkCreateImage;
+	// vf.vkDestroyImage = vkDestroyImage;
+	// vf.vkCmdCopyBuffer = vkCmdCopyBuffer;
+	// vf.vkGetBufferMemoryRequirements2KHR = vkGetBufferMemoryRequirements2KHR;
+	// vf.vkGetImageMemoryRequirements2KHR = vkGetImageMemoryRequirements2KHR;
 
 	VmaAllocatorCreateInfo aci = {};
+	aci.vulkanApiVersion = VK_API_VERSION_1_2;
 	aci.physicalDevice = m_physicalDevice;
 	aci.device = m_logicalDevice;
-	aci.pVulkanFunctions = &vf;
+	//aci.pVulkanFunctions = &vf;
 	aci.instance = m_instance;
 #if defined(__IOS__)
 	aci.preferredLargeHeapBlockSize = 32 * 1024 * 1024;
