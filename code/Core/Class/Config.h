80284fcb77f3da446f8c9aa6401f9bb46ff1e194
Anders Pistol <anders.pistol@doctorentertainment.com>
2021-08-21 21:20:25 +0200

    Traktor: Incorrect, __OSX__, symbol used.

--- code/Core/Class/Config.h
+++ code/Core/Class/Config.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#if defined(_WIN32) || defined(__LINUX__) || defined(__RPI__) || defined(__OSX__)
+#if defined(_WIN32) || defined(__LINUX__) || defined(__RPI__) || (defined(__APPLE__) && !defined(__IOS__))
 #	define T_NEED_RUNTIME_SIGNATURE
 #endif
