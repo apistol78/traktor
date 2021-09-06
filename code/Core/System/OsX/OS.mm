ead272f2bbae58e0ef7f9ca39f1407b56c835a3f
apistol <anders.pistol@doctorentertainment.com>
2021-09-06 17:00:01 +0200

    Traktor: Implemented OS::setEnvironment on macOS.

--- code/Core/System/OsX/OS.mm
+++ code/Core/System/OsX/OS.mm
@@ -175,7 +175,11 @@ bool OS::exploreFile(const std::wstring& file) const
 
 bool OS::setEnvironment(const std::wstring& name, const std::wstring& value) const
 {
-	return false;
+	return bool(setenv(
+		wstombs(name).c_str(),
+		wstombs(value).c_str(),
+		1
+	) == 0);
 }
 
 Ref< Environment > OS::getEnvironment() const
