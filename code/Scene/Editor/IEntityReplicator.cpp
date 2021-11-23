2eeefeab3777c8b80e5ea8cf1d5fa402a7dede28
Anders Pistol <anders.pistol@doctorentertainment.com>
2021-11-23 21:03:55 +0100

    Traktor: Yet another attempt to fix string constants in IEntityReplicator.

--- code/Scene/Editor/IEntityReplicator.cpp
+++ code/Scene/Editor/IEntityReplicator.cpp
@@ -7,5 +7,10 @@ namespace traktor
 
 T_IMPLEMENT_RTTI_CLASS(L"traktor.scene.IEntityReplicator", IEntityReplicator, Object)
 
+const wchar_t* IEntityReplicator::VisualMesh = L"Visual.Mesh";
+const wchar_t* IEntityReplicator::CollisionMesh = L"Collision.Mesh";
+const wchar_t* IEntityReplicator::CollisionShape = L"Collision.Shape";
+const wchar_t* IEntityReplicator::CollisionBody = L"Collision.Body";
+
     }
 }
