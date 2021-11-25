0b4a0c0895aa88718d996ac1e4b84177ddfeff81
Anders Pistol <anders.pistol@doctorentertainment.com>
2021-11-25 18:40:28 +0100

    Traktor: Compile issues fixed in DX11 renderer.

--- code/Render/Dx11/VertexLayoutDx11.cpp
+++ code/Render/Dx11/VertexLayoutDx11.cpp
@@ -16,11 +16,11 @@ Ref< VertexLayoutDx11 > VertexLayoutDx11::create(const AlignedVector< VertexElem
 	layout->m_d3dInputElements.resize(vertexElements.size());
 	for (uint32_t i = 0; i < vertexElements.size(); ++i)
 	{
-		T_FATAL_ASSERT (vertexElements[i].getDataUsage() < sizeof_array(c_dxgiInputSemantic));
-		T_FATAL_ASSERT (vertexElements[i].getDataType() < sizeof_array(c_dxgiInputType));
+		T_ASSERT((int)vertexElements[i].getDataUsage() < sizeof_array(c_dxgiInputSemantic));
+		T_ASSERT((int)vertexElements[i].getDataType() < sizeof_array(c_dxgiInputType));
 
 		std::memset(&layout->m_d3dInputElements[i], 0, sizeof(D3D11_INPUT_ELEMENT_DESC));
-		layout->m_d3dInputElements[i].SemanticName = c_dxgiInputSemantic[vertexElements[i].getDataUsage()];
+		layout->m_d3dInputElements[i].SemanticName = c_dxgiInputSemantic[(int)vertexElements[i].getDataUsage()];
 		layout->m_d3dInputElements[i].SemanticIndex = vertexElements[i].getIndex();
 		layout->m_d3dInputElements[i].Format = c_dxgiInputType[vertexElements[i].getDataType()];
 		layout->m_d3dInputElements[i].InputSlot = 0;
