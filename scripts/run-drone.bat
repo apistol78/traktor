8a02d864c421bb3b6ae5a9d15b54c423ff896cb1
Anders Pistol <anders.pistol@doctorentertainment.com>
2022-05-19 15:23:17 +0200

    Traktor: Removed Drone.

--- scripts/run-drone.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-:: Source environment configuration.
-call %~dp0config.bat
-
-:: Launch drone.
-%TRAKTOR_HOME%\bin\latest\win64\releaseshared\Traktor.Drone.App.exe $(TRAKTOR_HOME)/resources/runtime/configurations/Traktor.Drone.config
