e1fe68dadfb59f484b2123738ff1c85a4cbdd71c
Anders Pistol <anders.pistol@doctorentertainment.com>
2022-06-23 09:57:26 +0200

    Traktor: Able to specify which projects to include when generating solution on the command line.

--- code/SolutionBuilder/Version.h
+++ code/SolutionBuilder/Version.h
@@ -1,6 +1,6 @@
 #ifndef SB_VERSION
-#	define SB_VERSION L"3.9.2"
+#	define SB_VERSION L"3.9.3"
 #endif
 #ifndef SB_TITLE
-#	define SB_TITLE L"SolutionBuilder 3.9.2"
+#	define SB_TITLE L"SolutionBuilder 3.9.3"
 #endif
