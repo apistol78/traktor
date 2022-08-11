8e0ba57c3a0f2a90536bb3f8aa34110a87e26724
Anders Pistol <anders.pistol@doctorentertainment.com>
2022-08-11 13:52:15 +0200

    Traktor: Cleanup some 3rdp dependencies.

--- scripts/config.sh
+++ scripts/config.sh
@@ -10,25 +10,26 @@ TRAKTOR_HOME="`dirname \"$SCRIPT\"`/.."
 TRAKTOR_HOME="`(cd \"$TRAKTOR_HOME\" && pwd)`"
 export TRAKTOR_HOME
 
+# Path to 3rd party packages.
+export TRAKTOR_3RDP=$TRAKTOR_HOME/3rdp
+
 # Paths to 3rd party dependencies.
-export STEAMWORKS_SDK=$TRAKTOR_HOME/3rdp/steamworks-sdk-142
-export BULLET_SDK=$TRAKTOR_HOME/3rdp/bullet3-master-210317
-export FBX_SDK=$TRAKTOR_HOME/3rdp/FbxSdk/2016.1.2
-export GLSL_OPTIMIZER=$TRAKTOR_HOME/3rdp/glsl-optimizer-master-20141020
-export GLSLANG=$TRAKTOR_HOME/3rdp/glslang-master-200605
-export SQLITE_SDK=$TRAKTOR_HOME/3rdp/sqlite-3.8.7.2
-export LUA_SDK=$TRAKTOR_HOME/3rdp/lua-5.4.3
-export GLEW_SDK=$TRAKTOR_HOME/3rdp/glew-1.13.0
-export EXPAT_SDK=$TRAKTOR_HOME/3rdp/libexpat-R_2_2_5/expat
-export ZLIB_SDK=$TRAKTOR_HOME/3rdp/zlib-1.2.11
-export P4_SDK=$TRAKTOR_HOME/3rdp/p4api-2018.1.1660568
-export RPI_SYSROOT=$TRAKTOR_HOME/3rdp/raspberry-sysroot
-export VULKAN_SDK=$TRAKTOR_HOME/3rdp/vulkan/Vulkan-Headers-master-201012
-export MOLTEN_SDK=$TRAKTOR_HOME/3rdp/vulkan/vulkansdk-macos-1.2.154.0/MoltenVK 
-export VMA_SDK=$TRAKTOR_HOME/3rdp/VulkanMemoryAllocator-master-210504
-export SPIRVTOOLS_SDK=$TRAKTOR_HOME/3rdp/SPIRV-Tools-master-201029
-export SPIRVCROSS_SDK=$TRAKTOR_HOME/3rdp/SPIRV-Cross-master-201009
-export ASTCENC_SDK=$TRAKTOR_HOME/3rdp/astc-encoder-master-201104
+export STEAMWORKS_SDK=$TRAKTOR_3RDP/steamworks-sdk-142
+export BULLET_SDK=$TRAKTOR_3RDP/bullet3-master-210317
+export FBX_SDK=$TRAKTOR_3RDP/FbxSdk/2016.1.2
+export GLSLANG=$TRAKTOR_3RDP/glslang-master-200605
+export SQLITE_SDK=$TRAKTOR_3RDP/sqlite-3.8.7.2
+export LUA_SDK=$TRAKTOR_3RDP/lua-5.4.3
+export EXPAT_SDK=$TRAKTOR_3RDP/libexpat-R_2_2_5/expat
+export ZLIB_SDK=$TRAKTOR_3RDP/zlib-1.2.11
+export P4_SDK=$TRAKTOR_3RDP/p4api-2018.1.1660568
+export RPI_SYSROOT=$TRAKTOR_3RDP/raspberry-sysroot
+export VULKAN_SDK=$TRAKTOR_3RDP/vulkan/Vulkan-Headers-master-201012
+export MOLTEN_SDK=$TRAKTOR_3RDP/vulkan/vulkansdk-macos-1.2.154.0/MoltenVK 
+export VMA_SDK=$TRAKTOR_3RDP/VulkanMemoryAllocator-master-210504
+export SPIRVTOOLS_SDK=$TRAKTOR_3RDP/SPIRV-Tools-master-201029
+export SPIRVCROSS_SDK=$TRAKTOR_3RDP/SPIRV-Cross-master-201009
+export ASTCENC_SDK=$TRAKTOR_3RDP/astc-encoder-master-201104
 
 # Determine platform from some simple heuristic.
 if [[ `uname -s` == Linux* ]]; then
@@ -43,11 +44,11 @@ fi
 
 # Platform specific 3rd party dependencies.
 if [[ $TRAKTOR_PLATFORM == linux ]]; then
-	export EMBREE_SDK=$TRAKTOR_HOME/3rdp/embree-3.5.2.x86_64.linux
-	export OIDN_SDK=$TRAKTOR_HOME/3rdp/oidn-1.3.0.x86_64.linux
+	export EMBREE_SDK=$TRAKTOR_3RDP/embree-3.5.2.x86_64.linux
+	export OIDN_SDK=$TRAKTOR_3RDP/oidn-1.3.0.x86_64.linux
 elif [[ $TRAKTOR_PLATFORM == macos ]]; then
-	export EMBREE_SDK=$TRAKTOR_HOME/3rdp/embree-3.6.1.x86_64.macosx
-	export OIDN_SDK=$TRAKTOR_HOME/3rdp/oidn-1.3.0.x86_64.macos
+	export EMBREE_SDK=$TRAKTOR_3RDP/embree-3.6.1.x86_64.macosx
+	export OIDN_SDK=$TRAKTOR_3RDP/oidn-1.3.0.x86_64.macos
 fi
 
 # Export name of solution builder binary.
