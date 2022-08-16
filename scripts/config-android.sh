34c111a58eb4e99e6886d1027d9dcb4ba785b487
Anders Pistol <anders.pistol@doctorentertainment.com>
2022-08-16 15:38:58 +0200

    Traktor: Most of Android builds. Installing Android SDK automatically.

--- scripts/config-android.sh
+++ scripts/config-android.sh
@@ -2,10 +2,10 @@
 
 # Android SDK
 if [[ `uname -s` == Linux* ]]; then
-	export ANDROID_HOME=$TRAKTOR_HOME/3rdp/android-sdk/linux
+	export ANDROID_HOME=$TRAKTOR_HOME/3rdp_/android-sdk-linux
 elif [[ `uname -s` == Darwin* ]]; then
-	export ANDROID_HOME=$TRAKTOR_HOME/3rdp/android-sdk/darwin
+	export ANDROID_HOME=$TRAKTOR_HOME/3rdp_/android-sdk-macOS
 fi
 
 # Android NDK
-export ANDROID_NDK_ROOT=$TRAKTOR_HOME/3rdp/android-ndk-r21
+export ANDROID_NDK_ROOT=$ANDROID_HOME/ndk-bundle
