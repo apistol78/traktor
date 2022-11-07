32ae3b325a79744508cb9ec9d44c095b0ac3beef
Anders Pistol <anders.pistol@doctorentertainment.com>
2022-11-07 13:41:46 +0100

    Traktor: Added jom as a non-restrict 3rdp.

--- scripts/autobuild/autobuild-latest-rpi.bat
+++ scripts/autobuild/autobuild-latest-rpi.bat
@@ -9,6 +9,6 @@ call "%~dp0..\config-rpi.bat"
 
 :: Rebuild entire solution.
 pushd "%TRAKTOR_HOME%\build\rpi"
-%TRAKTOR_HOME%\3rdp\jom\jom /L /F "Extern Raspberry Pi.mak" %CONFIG%
-%TRAKTOR_HOME%\3rdp\jom\jom /L /F "Traktor Raspberry Pi.mak" %CONFIG%
+%TRAKTOR_HOME%\3rdp\jom-windows\jom /L /F "Extern Raspberry Pi.mak" %CONFIG%
+%TRAKTOR_HOME%\3rdp\jom-windows\jom /L /F "Traktor Raspberry Pi.mak" %CONFIG%
 popd
