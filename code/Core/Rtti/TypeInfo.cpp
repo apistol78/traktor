ca3811ec7f65ced3b1f11c82ec4c642f82e1989f
Anders Pistol <anders.pistol@doctorentertainment.com>
2022-11-10 16:14:44 +0100

    Traktor: Added MPL license and header to all source files.

--- code/Core/Rtti/TypeInfo.cpp
+++ code/Core/Rtti/TypeInfo.cpp
@@ -1,3 +1,11 @@
+/*
+ * TRAKTOR
+ * Copyright (c) 2022 Anders Pistol.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 // If non zero then registry size is fixed.
 #define T_REGISTRY_SIZE 16384
 
