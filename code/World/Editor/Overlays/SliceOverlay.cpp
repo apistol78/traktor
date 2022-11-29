7eae93a3b274729b836252ba6de196d9d05bbfca
Anders Pistol <anders.pistol@doctorentertainment.com>
2022-11-29 16:42:49 +0100

    Traktor: Slice debug overlay.

---
+++ code/World/Editor/Overlays/SliceOverlay.cpp
@@ -0,0 +1,89 @@
+/*
+ * TRAKTOR
+ * Copyright (c) 2022 Anders Pistol.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+#include "Render/IRenderTargetSet.h"
+#include "Render/ScreenRenderer.h"
+#include "Render/Shader.h"
+#include "Render/Context/RenderContext.h"
+#include "Render/Frame/RenderGraph.h"
+#include "Resource/IResourceManager.h"
+#include "World/Editor/Overlays/SliceOverlay.h"
+#include "World/Forward/WorldRendererForward.h"
+
+namespace traktor::world
+{
+	namespace
+	{
+
+const resource::Id< render::Shader > c_debugShader(Guid(L"{949B3C96-0196-F24E-B36E-98DD504BCE9D}"));
+const render::Handle c_handleDebugTechnique(L"Slice");
+const render::Handle c_handleDebugAlpha(L"Scene_DebugAlpha");
+const render::Handle c_handleDebugSlicePositions(L"Scene_DebugSlicePositions");
+const render::Handle c_handleDebugTexture(L"Scene_DebugTexture");
+
+render::handle_t findTargetByName(const render::RenderGraph& renderGraph, const wchar_t* name)
+{
+	for (const auto& tm : renderGraph.getTargets())
+	{
+		if (wcscmp(tm.second.name, name) == 0)
+			return tm.first;
+	}
+	return 0;
+}
+
+	}
+
+T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.world.SliceOverlay", 0, SliceOverlay, IDebugOverlay)
+
+bool SliceOverlay::create(resource::IResourceManager* resourceManager)
+{
+	if (!resourceManager->bind(c_debugShader, m_shader))
+		return false;
+
+	return true;
+}
+
+void SliceOverlay::setup(render::RenderGraph& renderGraph, render::ScreenRenderer* screenRenderer, IWorldRenderer* worldRenderer, const WorldRenderView& worldRenderView, float alpha) const
+{
+	auto wrf = dynamic_type_cast< WorldRendererForward* >(worldRenderer);
+	if (!wrf)
+		return;
+
+	render::handle_t gbufferId = findTargetByName(renderGraph, L"GBuffer");
+	if (!gbufferId)
+		return;
+
+	Ref< render::RenderPass > rp = new render::RenderPass(L"Slice overlay");
+	rp->setOutput(0, render::TfColor, render::TfColor);
+	rp->addInput(gbufferId);
+	rp->addBuild([=](const render::RenderGraph& renderGraph, render::RenderContext* renderContext) {
+		auto gbufferTargetSet = renderGraph.getTargetSet(gbufferId);
+		if (!gbufferTargetSet)
+			return;
+
+		const Vector4 slicePositions(
+			wrf->m_slicePositions[0],
+			wrf->m_slicePositions[1],
+			wrf->m_slicePositions[2],
+			wrf->m_slicePositions[3]
+		);
+
+		auto pp = renderContext->alloc< render::ProgramParameters >();
+		pp->beginParameters(renderContext);
+		pp->setFloatParameter(c_handleDebugAlpha, alpha);
+		pp->setVectorParameter(c_handleDebugSlicePositions, slicePositions);
+		pp->setTextureParameter(c_handleDebugTexture, gbufferTargetSet->getColorTexture(0));
+		pp->endParameters(renderContext);
+
+		const render::Shader::Permutation perm(c_handleDebugTechnique);
+		screenRenderer->draw(renderContext, m_shader, perm, pp);
+	});
+	renderGraph.addPass(rp);
+}
+
+}
