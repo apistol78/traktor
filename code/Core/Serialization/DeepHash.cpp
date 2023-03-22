d4e246094ba1174bc6971d193d5ef6399b984df6
apistol78 <anders@pistol.one>
2023-03-22 13:59:49 +0100

    Traktor: Implemented a serialization attribute to prevent member to be included in hash. Used to ignore "preview angle" from issue a mesh build.

--- code/Core/Serialization/DeepHash.cpp
+++ code/Core/Serialization/DeepHash.cpp
@@ -17,6 +17,7 @@
 #include "Core/Math/Quaternion.h"
 #include "Core/Math/Vector2.h"
 #include "Core/Misc/Murmur3.h"
+#include "Core/Serialization/AttributeNoHash.h"
 #include "Core/Serialization/DeepHash.h"
 #include "Core/Serialization/Serializer.h"
 #include "Core/Serialization/MemberArray.h"
@@ -27,6 +28,13 @@ namespace traktor
 {
 	namespace
 	{
+
+template < typename AttributeType, typename MemberType >
+const AttributeType* findAttribute(const MemberType& m)
+{
+	const Attribute* attributes = m.getAttributes();
+	return attributes ? attributes->find< AttributeType >() : nullptr;
+}
 
 class HashSerializer : public Serializer
 {
@@ -44,169 +52,219 @@ public:
 
 	virtual void operator >> (const Member< bool >& m) override final
 	{
-		m_hasher.feed< uint8_t >(m ? 0xff : 0x00);
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+			m_hasher.feed< uint8_t >(m ? 0xff : 0x00);
 	}
 
 	virtual void operator >> (const Member< int8_t >& m) override final
 	{
-		m_hasher.feed< int8_t >(m);
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+			m_hasher.feed< int8_t >(m);
 	}
 
 	virtual void operator >> (const Member< uint8_t >& m) override final
 	{
-		m_hasher.feed< uint8_t >(m);
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+			m_hasher.feed< uint8_t >(m);
 	}
 
 	virtual void operator >> (const Member< int16_t >& m) override final
 	{
-		m_hasher.feed< int64_t >(m);
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+			m_hasher.feed< int64_t >(m);
 	}
 
 	virtual void operator >> (const Member< uint16_t >& m) override final
 	{
-		m_hasher.feed< uint16_t >(m);
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+			m_hasher.feed< uint16_t >(m);
 	}
 
 	virtual void operator >> (const Member< int32_t >& m) override final
 	{
-		m_hasher.feed< int32_t >(m);
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+			m_hasher.feed< int32_t >(m);
 	}
 
 	virtual void operator >> (const Member< uint32_t >& m) override final
 	{
-		m_hasher.feed< uint32_t >(m);
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+			m_hasher.feed< uint32_t >(m);
 	}
 
 	virtual void operator >> (const Member< int64_t >& m) override final
 	{
-		m_hasher.feed< int64_t >(m);
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+			m_hasher.feed< int64_t >(m);
 	}
 
 	virtual void operator >> (const Member< uint64_t >& m) override final
 	{
-		m_hasher.feed< uint64_t >(m);
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+			m_hasher.feed< uint64_t >(m);
 	}
 
 	virtual void operator >> (const Member< float >& m) override final
 	{
-		m_hasher.feed< float >(m);
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+			m_hasher.feed< float >(m);
 	}
 
 	virtual void operator >> (const Member< double >& m) override final
 	{
-		m_hasher.feed< double >(m);
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+			m_hasher.feed< double >(m);
 	}
 
 	virtual void operator >> (const Member< std::string >& m) override final
 	{
-		const std::string& str = m;
-		m_hasher.feed(str.data(), str.size());
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+		{
+			const std::string& str = m;
+			m_hasher.feed(str.data(), str.size());
+		}
 	}
 
 	virtual void operator >> (const Member< std::wstring >& m) override final
 	{
-		const std::wstring& str = m;
-		for (size_t i = 0; i < str.length(); ++i)
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
 		{
-			uint32_t ch = (uint32_t)str[i];
-			m_hasher.feed(ch);
+			const std::wstring& str = m;
+			for (size_t i = 0; i < str.length(); ++i)
+			{
+				uint32_t ch = (uint32_t)str[i];
+				m_hasher.feed(ch);
+			}
 		}
 	}
 
 	virtual void operator >> (const Member< Guid >& m) override final
 	{
-		const Guid& guid = m;
-		m_hasher.feed((const uint8_t*)guid, 16);
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+		{
+			const Guid& guid = m;
+			m_hasher.feed((const uint8_t*)guid, 16);
+		}
 	}
 
 	virtual void operator >> (const Member< Path >& m) override final
 	{
-		std::wstring path = m->getOriginal();
-		for (size_t i = 0; i < path.length(); ++i)
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
 		{
-			uint32_t ch = (uint32_t)path[i];
-			m_hasher.feed(ch);
+			const std::wstring path = m->getOriginal();
+			for (size_t i = 0; i < path.length(); ++i)
+			{
+				const uint32_t ch = (uint32_t)path[i];
+				m_hasher.feed(ch);
+			}
 		}
 	}
 
 	virtual void operator >> (const Member< Color4ub >& m) override final
 	{
-		m_hasher.feed< uint8_t >(m->r);
-		m_hasher.feed< uint8_t >(m->g);
-		m_hasher.feed< uint8_t >(m->b);
-		m_hasher.feed< uint8_t >(m->a);
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+		{
+			m_hasher.feed< uint8_t >(m->r);
+			m_hasher.feed< uint8_t >(m->g);
+			m_hasher.feed< uint8_t >(m->b);
+			m_hasher.feed< uint8_t >(m->a);
+		}
 	}
 
 	virtual void operator >> (const Member< Color4f >& m) override final
 	{
-		float T_MATH_ALIGN16 e[4];
-		m->storeAligned(e);
-		m_hasher.feed(e, sizeof(e));
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+		{
+			float T_MATH_ALIGN16 e[4];
+			m->storeAligned(e);
+			m_hasher.feed(e, sizeof(e));
+		}
 	}
 
 	virtual void operator >> (const Member< Scalar >& m) override final
 	{
-		float v = (float)(const Scalar&)m;
-		m_hasher.feed< float >(v);
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+		{
+			const float v = (float)(const Scalar&)m;
+			m_hasher.feed< float >(v);
+		}
 	}
 
 	virtual void operator >> (const Member< Vector2 >& m) override final
 	{
-		m_hasher.feed< float >(m->x);
-		m_hasher.feed< float >(m->y);
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+		{
+			m_hasher.feed< float >(m->x);
+			m_hasher.feed< float >(m->y);
+		}
 	}
 
 	virtual void operator >> (const Member< Vector4 >& m) override final
 	{
-		float T_MATH_ALIGN16 e[4];
-		m->storeAligned(e);
-		m_hasher.feed(e, sizeof(e));
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+		{
+			float T_MATH_ALIGN16 e[4];
+			m->storeAligned(e);
+			m_hasher.feed(e, sizeof(e));
+		}
 	}
 
 	virtual void operator >> (const Member< Matrix33 >& m) override final
 	{
-		for (int i = 0; i < 3 * 3; ++i)
-			m_hasher.feed< float >(m->m[i]);
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+		{
+			for (int i = 0; i < 3 * 3; ++i)
+				m_hasher.feed< float >(m->m[i]);
+		}
 	}
 
 	virtual void operator >> (const Member< Matrix44 >& m) override final
 	{
-		float T_MATH_ALIGN16 e[16];
-		m->storeAligned(e);
-		m_hasher.feed(e, sizeof(e));
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+		{
+			float T_MATH_ALIGN16 e[16];
+			m->storeAligned(e);
+			m_hasher.feed(e, sizeof(e));
+		}
 	}
 
 	virtual void operator >> (const Member< Quaternion >& m) override final
 	{
-		float T_MATH_ALIGN16 e[4];
-		m->e.storeAligned(e);
-		m_hasher.feed(e, sizeof(e));
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+		{
+			float T_MATH_ALIGN16 e[4];
+			m->e.storeAligned(e);
+			m_hasher.feed(e, sizeof(e));
+		}
 	}
 
 	virtual void operator >> (const Member< ISerializable* >& m) override final
 	{
-		ISerializable* object = *m;
-		if (object)
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
 		{
-			auto it = m_written.find(object);
-			if (it == m_written.end())
+			ISerializable* object = *m;
+			if (object)
 			{
-				m_written.insert(std::make_pair(object, ++m_writtenCount));
-
-				const wchar_t* const typeName = type_of(object).getName();
-				for (size_t i = 0; typeName[i]; ++i)
+				auto it = m_written.find(object);
+				if (it == m_written.end())
 				{
-					uint32_t ch = (uint32_t)typeName[i];
-					m_hasher.feed(ch);
-				}
+					m_written.insert(std::make_pair(object, ++m_writtenCount));
 
-				serialize(object);
+					const wchar_t* const typeName = type_of(object).getName();
+					for (size_t i = 0; typeName[i]; ++i)
+					{
+						uint32_t ch = (uint32_t)typeName[i];
+						m_hasher.feed(ch);
+					}
+
+					serialize(object);
+				}
+				else
+					m_hasher.feed< uint32_t >(it->second);
 			}
 			else
-				m_hasher.feed< uint32_t >(it->second);
+				m_hasher.feed< uint32_t >(0);
 		}
-		else
-			m_hasher.feed< uint32_t >(0);
 	}
 
 	virtual void operator >> (const Member< void* >& m) override final
@@ -216,19 +274,24 @@ public:
 
 	virtual void operator >> (const MemberArray& m) override final
 	{
-		size_t size = m.size();
-		for (size_t i = 0; i < size; ++i)
-			m.write(*this);
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+		{
+			const size_t size = m.size();
+			for (size_t i = 0; i < size; ++i)
+				m.write(*this);
+		}
 	}
 
 	virtual void operator >> (const MemberComplex& m) override final
 	{
-		m.serialize(*this);
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+			m.serialize(*this);
 	}
 
 	virtual void operator >> (const MemberEnumBase& m) override final
 	{
-		m.serialize(*this);
+		if (findAttribute< AttributeNoHash >(m) == nullptr)
+			m.serialize(*this);
 	}
 
 private:
