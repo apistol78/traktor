3424bc84bd7bbca16719a3347ff4bc98dea72040
apistol78 <anders@pistol.one>
2023-05-08 14:16:33 +0200

    Traktor: Only include visible objects in Blend files.

--- code/Model/Formats/Blend/ModelFormatBlend.cpp
+++ code/Model/Formats/Blend/ModelFormatBlend.cpp
@@ -66,6 +66,7 @@ Ref< Model > ModelFormatBlend::read(const Path& filePath, const std::wstring& fi
 	os << L"	axis_forward=\"Z\"," << Endl;
 	os << L"	axis_up=\"Y\"," << Endl;
 	os << L"	use_selection=False," << Endl;
+	os << L"	use_visible=True," << Endl;
 	os << L"	global_scale=0.01," << Endl;
 	os << L"	use_custom_props=True" << Endl;
 	os << L")" << Endl;
