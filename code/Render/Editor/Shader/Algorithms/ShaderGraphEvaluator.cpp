3f4a6f5170a2e8629226e27b7883cc3c9c867de5
apistol78 <anders@pistol.one>
2023-05-25 16:40:43 +0200

    Traktor: Cleaned up ShaderGraphEvaluator a bit, using lambda to keep code clean.

--- code/Render/Editor/Shader/Algorithms/ShaderGraphEvaluator.cpp
+++ code/Render/Editor/Shader/Algorithms/ShaderGraphEvaluator.cpp
@@ -17,30 +17,33 @@ namespace traktor
 {
 	namespace render
 	{
-		namespace
-		{
 
-struct EvaluateVisitor
+T_IMPLEMENT_RTTI_CLASS(L"traktor.render.ShaderGraphEvaluator", ShaderGraphEvaluator, Object)
+
+ShaderGraphEvaluator::ShaderGraphEvaluator(const ShaderGraph* shaderGraph)
+:	m_shaderGraph(shaderGraph)
 {
-	const ShaderGraph* shaderGraph;
+}
+
+Constant ShaderGraphEvaluator::evaluate(const OutputPin* outputPin) const
+{
 	SmallMap< const OutputPin*, Constant > evaluatedConstants;
 	AlignedVector< PinType > inputPinTypes;
 	AlignedVector< Constant > inputConstants;
 
-	bool operator () (Node* node)
-	{
+	GraphTraverse(m_shaderGraph, outputPin->getNode()).postorder([&, this](const Node* node) {
 		const INodeTraits* nodeTraits = INodeTraits::find(node);
 		T_ASSERT(nodeTraits);
 
 		// Get node's input types and constants.
-		int32_t inputPinCount = node->getInputPinCount();
+		const int32_t inputPinCount = node->getInputPinCount();
 		inputPinTypes.resize(inputPinCount);
 		inputConstants.resize(inputPinCount);
 
 		for (int32_t i = 0; i < inputPinCount; ++i)
 		{
 			const InputPin* inputPin = node->getInputPin(i);
-			const OutputPin* outputPin = shaderGraph->findSourcePin(inputPin);
+			const OutputPin* outputPin = m_shaderGraph->findSourcePin(inputPin);
 			if (outputPin)
 			{
 				inputPinTypes[i] = evaluatedConstants[outputPin].getType();
@@ -54,13 +57,13 @@ struct EvaluateVisitor
 		}
 
 		// Evaluate output constants from input set.
-		int32_t outputPinCount = node->getOutputPinCount();
+		const int32_t outputPinCount = node->getOutputPinCount();
 		for (int32_t i = 0; i < outputPinCount; ++i)
 		{
 			const OutputPin* outputPin = node->getOutputPin(i);
 
-			PinType outputPinType = nodeTraits->getOutputPinType(
-				shaderGraph,
+			const PinType outputPinType = nodeTraits->getOutputPinType(
+				m_shaderGraph,
 				node,
 				outputPin,
 				inputPinTypes.c_ptr()
@@ -69,39 +72,17 @@ struct EvaluateVisitor
 			evaluatedConstants[outputPin] = Constant(outputPinType);
 
 			nodeTraits->evaluatePartial(
-				shaderGraph,
+				m_shaderGraph,
 				node,
 				outputPin,
 				inputConstants.c_ptr(),
 				evaluatedConstants[outputPin]
 			);
 		}
-
 		return true;
-	}
+	});
 
-	bool operator () (Edge* edge)
-	{
-		return true;
-	}
-
-};
-
-		}
-
-T_IMPLEMENT_RTTI_CLASS(L"traktor.render.ShaderGraphEvaluator", ShaderGraphEvaluator, Object)
-
-ShaderGraphEvaluator::ShaderGraphEvaluator(const ShaderGraph* shaderGraph)
-:	m_shaderGraph(shaderGraph)
-{
-}
-
-Constant ShaderGraphEvaluator::evaluate(const OutputPin* outputPin) const
-{
-	EvaluateVisitor visitor;
-	visitor.shaderGraph = m_shaderGraph;
-	GraphTraverse(m_shaderGraph, outputPin->getNode()).postorder(visitor);
-	return visitor.evaluatedConstants[outputPin];
+	return evaluatedConstants[outputPin];
 }
 
 	}
