b2873422a05b08045a84d7ce1a13b1a574d04337
apistol78 <anders@pistol.one>
2023-06-21 09:18:52 +0200

    Traktor: Merged inl files into Point, Size and Rect headers.

--- code/Ui/Point.h
+++ code/Ui/Point.h
@@ -10,41 +10,82 @@
 
 #include "Ui/Size.h"
 
+ // import/export mechanism.
+#undef T_DLLCLASS
+#if defined(T_UI_EXPORT)
+#	define T_DLLCLASS T_DLLEXPORT
+#else
+#	define T_DLLCLASS T_DLLIMPORT
+#endif
+
 namespace traktor::ui
 {
 
 /*! 2D point.
  * \ingroup UI
  */
-class Point
+class T_DLLCLASS Point
 {
 public:
-	int32_t x;
-	int32_t y;
+	int32_t x = 0;
+	int32_t y = 0;
 
-	inline Point();
+	Point() = default;
 
-	inline Point(const Point& pnt);
+	Point(const Point& pnt)
+	:	x(pnt.x)
+	,	y(pnt.y)
+	{
+	}
 
-	inline Point(const std::pair< int32_t, int32_t >& pr);
+	Point(const std::pair< int32_t, int32_t >& pr)
+	:	x(pr.first)
+	,	y(pr.second)
+	{
+	}
 
-	inline Point(int32_t x_, int32_t y_);
+	Point(int32_t x_, int32_t y_)
+	:	x(x_)
+	,	y(y_)
+	{
+	}
 
-	inline Point operator + (const Size& s) const;
+	Point operator + (const Size& s) const
+	{
+		return Point(x + s.cx, y + s.cy);
+	}
 
-	inline Point operator - (const Size& s) const;
+	Point operator - (const Size& s) const
+	{
+		return Point(x - s.cx, y - s.cy);
+	}
 
-	inline const Point& operator += (const Size& s);
+	const Point& operator += (const Size& s)
+	{
+		x += s.cx; y += s.cy;
+		return *this;
+	}
 
-	inline const Point& operator -= (const Size& s);
+	const Point& operator -= (const Size& s)
+	{
+		x -= s.cx; y -= s.cy;
+		return *this;
+	}
 
-	inline Size operator - (const Point& p) const;
+	Size operator - (const Point& p) const
+	{
+		return Size(x - p.x, y - p.y);
+	}
 
-	inline bool operator == (const Point& p) const;
+	bool operator == (const Point& p) const
+	{
+		return bool(x == p.x && y == p.y);
+	}
 
-	inline bool operator != (const Point& p) const;
+	bool operator != (const Point& p) const
+	{
+		return bool(x != p.x || y != p.y);
+	}
 };
 
 }
-
-#include "Ui/Point.inl"
