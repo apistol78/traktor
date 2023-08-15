3e1490d6b63a0c543b4d9c2b293dac5e0705af33
Anders Pistol <anders.pistol@doctorentertainment.com>
2023-08-15 20:58:19 +0200

    Traktor: Mapped file Linux implementation.

---
+++ code/Core/Io/Linux/NativeMappedFile.h
@@ -0,0 +1,46 @@
+/*
+ * TRAKTOR
+ * Copyright (c) 2023 Anders Pistol.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+#pragma once
+
+#include "Core/Io/IMappedFile.h"
+
+// import/export mechanism.
+#undef T_DLLCLASS
+#if defined(T_CORE_EXPORT)
+#	define T_DLLCLASS T_DLLEXPORT
+#else
+#	define T_DLLCLASS T_DLLIMPORT
+#endif
+
+namespace traktor
+{
+
+/*!
+ * \ingroup Core
+ */
+class T_DLLCLASS NativeMappedFile : public IMappedFile
+{
+	T_RTTI_CLASS;
+
+public:
+	explicit NativeMappedFile(int fd, void* base, int64_t size);
+
+	virtual ~NativeMappedFile();
+
+	virtual void* getBase() const override final;
+
+	virtual int64_t getSize() const override final;
+
+private:
+	int m_fd;
+	void* m_base;
+	int64_t m_size;
+};
+
+}
