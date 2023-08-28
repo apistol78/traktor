8eb78b9d261691be28d30508ceb1c2dad7f75531
Anders Pistol <anders.pistol@doctorentertainment.com>
2023-08-28 21:42:33 +0200

    Traktor: Added a browse preview generator for animation assets.

---
+++ code/Animation/Editor/AnimationBrowsePreview.h
@@ -0,0 +1,37 @@
+/*
+ * TRAKTOR
+ * Copyright (c) 2023 Anders Pistol.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+#pragma once
+
+#include "Editor/IBrowsePreview.h"
+
+// import/export mechanism.
+#undef T_DLLCLASS
+#if defined(T_ANIMATION_EDITOR_EXPORT)
+#	define T_DLLCLASS T_DLLEXPORT
+#else
+#	define T_DLLCLASS T_DLLIMPORT
+#endif
+
+namespace traktor::animation
+{
+
+/*!
+ * \ingroup Animation
+ */
+class T_DLLCLASS AnimationBrowsePreview : public editor::IBrowsePreview
+{
+	T_RTTI_CLASS;
+
+public:
+	virtual TypeInfoSet getPreviewTypes() const override final;
+
+	virtual Ref< ui::Bitmap > generate(const editor::IEditor* editor, db::Instance* instance) const override final;
+};
+
+}
