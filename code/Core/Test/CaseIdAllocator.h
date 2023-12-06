5b5e38189d0010fa8240a1586ab48f508e6de28e
Anders Pistol <anders.pistol@doctorentertainment.com>
2023-12-06 22:19:58 +0100

    Traktor: Begun rewriting IdAllocator, added unit test but still needs fixing.

---
+++ code/Core/Test/CaseIdAllocator.h
@@ -0,0 +1,42 @@
+/*
+ * TRAKTOR
+ * Copyright (c) 2023 Anders Pistol.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+#pragma once
+
+#include "Core/Test/Case.h"
+
+// import/export mechanism.
+#undef T_DLLCLASS
+#if defined(T_CORE_EXPORT)
+#	define T_DLLCLASS T_DLLEXPORT
+#else
+#	define T_DLLCLASS T_DLLIMPORT
+#endif
+
+namespace traktor
+{
+
+class IdAllocator;
+
+}
+
+namespace traktor::test
+{
+
+class T_DLLCLASS CaseIdAllocator : public Case
+{
+	T_RTTI_CLASS;
+
+public:
+	virtual void run() override final;
+
+private:
+    void dump(const IdAllocator& id) const;
+};
+
+}
