d23efd8c96c449b8c8bb0844e0e7f9da7189df74
apistol78 <anders@pistol.one>
2024-01-10 12:55:29 +0100

    Traktor: Nested namespace in TreeView class.

--- code/Ui/TreeView/TreeViewItemActivateEvent.cpp
+++ code/Ui/TreeView/TreeViewItemActivateEvent.cpp
@@ -1,6 +1,6 @@
 /*
  * TRAKTOR
- * Copyright (c) 2022 Anders Pistol.
+ * Copyright (c) 2022-2024 Anders Pistol.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -9,10 +9,8 @@
 #include "Ui/TreeView/TreeViewItem.h"
 #include "Ui/TreeView/TreeViewItemActivateEvent.h"
 
-namespace traktor
+namespace traktor::ui
 {
-	namespace ui
-	{
 
 T_IMPLEMENT_RTTI_CLASS(L"traktor.ui.TreeViewItemActivateEvent", TreeViewItemActivateEvent, Event)
 
@@ -27,5 +25,4 @@ TreeViewItem* TreeViewItemActivateEvent::getItem() const
 	return m_item;
 }
 
-	}
 }
