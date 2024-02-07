37c5ceccdc2bbb8b8d7fb0d4389ed6e5297772f8
apistol78 <anders@pistol.one>
2024-02-07 16:53:33 +0100

    Traktor: Fixed JointBindingComponent updating bound entities.

--- code/Animation/JointBindingComponent.cpp
+++ code/Animation/JointBindingComponent.cpp
@@ -1,6 +1,6 @@
 /*
  * TRAKTOR
- * Copyright (c) 2022 Anders Pistol.
+ * Copyright (c) 2022-2024 Anders Pistol.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -97,9 +97,6 @@ void JointBindingComponent::update(const world::UpdateParams& update)
 			}
 		}
 	}
-
-	for (const auto& binding : m_bindings)
-		binding.entity->update(update);
 }
 
 world::Entity* JointBindingComponent::getEntity(const std::wstring& name, int32_t index) const
