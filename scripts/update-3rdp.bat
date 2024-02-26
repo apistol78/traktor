4f8c6b1097ad9638c6c659ccb580e38094724f79
apistol78 <anders@pistol.one>
2024-02-26 10:41:20 +0100

    Traktor: No longer automatically install GIT client; checks if client exist and fail with error message if not.

--- scripts/update-3rdp.bat
+++ scripts/update-3rdp.bat
@@ -3,7 +3,12 @@
 call %~dp0config.bat
 
 :: Ensure GIT is installed first.
-winget install --id Git.Git -e --source winget
+where /q git
+if errorlevel 1 (
+	echo GIT client is missing. Ensure it is installed and placed in your PATH.
+	echo Tip! "winget install --id Git.Git -e --source winget"
+	exit /b
+)
 
 pushd %TRAKTOR_HOME%
 %TRAKTOR_HOME%/bin/win64/releasestatic/Traktor.Run.App ./scripts/misc/update-3rdp.run
