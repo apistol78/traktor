510b958dfabf3ec2c73f5a849fffdc9916d7976d
Anders Pistol <anders.pistol@doctorentertainment.com>
2024-02-29 21:35:44 +0100

    Traktor: Bumped Spark pipeline version.

--- code/Spark/Editor/Pipeline.cpp
+++ code/Spark/Editor/Pipeline.cpp
@@ -1,6 +1,6 @@
 /*
  * TRAKTOR
- * Copyright (c) 2022 Anders Pistol.
+ * Copyright (c) 2022-2024 Anders Pistol.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -62,7 +62,7 @@ struct AtlasBucket
 
 	}
 
-T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.spark.Pipeline", 10, Pipeline, editor::IPipeline)
+T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.spark.Pipeline", 11, Pipeline, editor::IPipeline)
 
 Pipeline::Pipeline()
 :	m_generateMips(false)
