fc1128ac4d13dbe3e8a7b2962d24b1f9e5f7aad0
Anders Pistol <anders.pistol@doctorentertainment.com>
2024-04-09 22:18:16 +0200

    Traktor: Linux compile fix.

--- code/Core/RefArray.h
+++ code/Core/RefArray.h
@@ -1,6 +1,6 @@
 /*
  * TRAKTOR
- * Copyright (c) 2022 Anders Pistol.
+ * Copyright (c) 2022-2024 Anders Pistol.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -10,6 +10,7 @@
 
 #include <algorithm>
 #include "Core/InplaceRef.h"
+#include "Core/Misc/Align.h"
 
 // import/export mechanism.
 #undef T_DLLCLASS
