c3c62e354105f2fe1a57007ffdd8c18bdd5db47f
Anders Pistol <anders.pistol@doctorentertainment.com>
2024-05-13 20:48:35 +0200

    Traktor: Compile issue fixed with RenderDoc tool.

--- code/Runtime/Editor/LaunchRenderDocTool.h
+++ code/Runtime/Editor/LaunchRenderDocTool.h
@@ -1,6 +1,6 @@
 /*
  * TRAKTOR
- * Copyright (c) 2022 Anders Pistol.
+ * Copyright (c) 2022-2024 Anders Pistol.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -12,8 +12,6 @@
 
 namespace traktor::runtime
 {
-
-#if defined(_WIN32)
 
 class LaunchRenderDocTool : public editor::IEditorTool
 {
@@ -28,7 +26,5 @@ public:
 
 	virtual bool launch(ui::Widget* parent, editor::IEditor* runtime, const PropertyGroup* param) override final;
 };
-
-#endif
 
 }
