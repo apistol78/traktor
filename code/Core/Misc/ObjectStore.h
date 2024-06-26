88a35e453786fc75aed3fd39d903e3c7f9c58051
Anders Pistol <anders.pistol@doctorentertainment.com>
2024-06-26 22:36:50 +0200

    Traktor: Added documentation to ObjectStore.

--- code/Core/Misc/ObjectStore.h
+++ code/Core/Misc/ObjectStore.h
@@ -31,18 +31,38 @@ class T_DLLCLASS ObjectStore : public Object
 	T_RTTI_CLASS;
 
 public:
+	/*! Add object to store.
+	 * Any object of the same type gets replaced.
+	 * \param object Object to add to store.
+	 */
 	void set(Object* object);
 
+	/*! Remove object from store.
+	 * \param object Object to remove from store.
+	 * \return True if object was removed from store.
+	 */
 	bool unset(Object* object);
 
+	/*! Get object of a specified type.
+	 * \param objectType Type of object.
+	 * \return Object which is of specified type.
+	 */
 	Object* get(const TypeInfo& objectType) const;
 
+	/*! Get object of a specified type.
+	 * \param ObjectType Type of object.
+	 * \return Object which is of specified type.
+	 */
 	template < typename ObjectType >
 	ObjectType* get() const
 	{
 		return dynamic_type_cast< ObjectType* >(get(type_of< ObjectType >()));
 	}
 
+	/*! Remove object of a specified type.
+	 * \param ObjectType Type of object.
+	 * \return True if an object was removed.
+	 */
 	template < typename ObjectType >
 	bool unset()
 	{
