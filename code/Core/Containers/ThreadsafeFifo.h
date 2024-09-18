e31271d0092ba7c4a447a06b57cd629b91cd7ae3
apistol78 <anders@pistol.one>
2024-09-18 15:54:28 +0200

    Traktor: Simplified ThreadsafeFifo, non-lock version wasn't working fully.

--- code/Core/Containers/ThreadsafeFifo.h
+++ code/Core/Containers/ThreadsafeFifo.h
@@ -1,6 +1,6 @@
 /*
  * TRAKTOR
- * Copyright (c) 2022-2023 Anders Pistol.
+ * Copyright (c) 2022-2024 Anders Pistol.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -8,7 +8,9 @@
  */
 #pragma once
 
-#include <atomic>
+#include "Core/Containers/AlignedVector.h"
+#include "Core/Thread/Acquire.h"
+#include "Core/Thread/SpinLock.h"
 
 namespace traktor
 {
@@ -19,96 +21,34 @@ template < typename T >
 class ThreadsafeFifo
 {
 public:
-	ThreadsafeFifo()
-	{
-		Node* dummy = new Node(T());
-		m_head.store(dummy);
-		m_tail.store(dummy);
-	}
-
-	~ThreadsafeFifo()
-	{
-		while (Node* oldHead = m_head.load())
-		{
-			m_head.store(oldHead->next);
-			delete oldHead;
-		}
-	}
-
 	void put(const T& item)
 	{
-		Node* newNode = new Node(item);
-		Node* oldTail = nullptr;
-
-		while (true)
-		{
-			oldTail = m_tail.load();
-			Node* oldNext = oldTail->next.load();
-
-			if (oldTail == m_tail.load())  // still consistent?
-			{
-				if (oldNext == nullptr)
-				{
-					if (oldTail->next.compare_exchange_strong(oldNext, newNode))
-						break;
-				}
-				else
-					m_tail.compare_exchange_strong(oldTail, oldNext);
-			}
-		}
-		m_tail.compare_exchange_strong(oldTail, newNode);
+		T_ANONYMOUS_VAR(Acquire< SpinLock >)(m_lock);
+		m_fifo.push_back(item);
 	}
 
 	bool get(T& item)
 	{
-		Node* oldHead = nullptr;
-		while (true)
+		T_ANONYMOUS_VAR(Acquire< SpinLock >)(m_lock);
+		if (!m_fifo.empty())
 		{
-			oldHead = m_head.load();
-			Node* oldTail = m_tail.load();
-			Node* oldNext = oldHead->next.load();
-
-			if (oldHead == m_head.load())	// still consistent?
-			{
-				if (oldHead == oldTail)
-				{
-					if (oldNext == nullptr)  // queue is empty
-						return false;
-					m_tail.compare_exchange_strong(oldTail, oldNext);
-				}
-				else
-				{
-					item = oldNext->data;
-					if (m_head.compare_exchange_strong(oldHead, oldNext))
-						break;
-				}
-			}
+			item = m_fifo.back();
+			m_fifo.pop_back();
+			return true;
 		}
-		delete oldHead;
-		return true;
+		else
+			return false;
 	}
 
 	void clear()
 	{
-		T dummy;
-		while (get(dummy));
+		T_ANONYMOUS_VAR(Acquire< SpinLock >)(m_lock);
+		m_fifo.clear();
 	}
 
 private:
-	struct Node
-	{
-		T data;
-		std::atomic< Node* > next;
-
-		Node(const T& data)
-		:	data(data)
-		,	next(nullptr)
-		{
-		}
-	};
-
-	std::atomic< Node* > m_head;
-	std::atomic< Node* > m_tail;
+	SpinLock m_lock;
+	AlignedVector< T > m_fifo;
 };
 
 }
