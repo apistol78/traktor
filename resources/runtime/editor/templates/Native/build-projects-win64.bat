455e85c37c26895a0eddd605a0d1e54d62b80549
Robert Campbell <waprave@gmail.com>
2024-09-20 16:55:29 -0400

    Update build-projects-win64.bat
    Reference msvc-2022. There is no msvc-2019 config file.

--- resources/runtime/editor/templates/Native/build-projects-win64.bat
+++ resources/runtime/editor/templates/Native/build-projects-win64.bat
@@ -4,5 +4,5 @@ call "<!-- output:print(os:getEnvironment("TRAKTOR_HOME")) --!>\scripts\config.b
 
 %SOLUTIONBUILDER% ^
 	-f=msvc ^
-	"-p=$(TRAKTOR_HOME)\resources\build\configurations\msvc-2019-win64.xml" ^
+	"-p=$(TRAKTOR_HOME)\resources\build\configurations\msvc-2022-win64.xml" ^
 	"<!-- output:print(os:getEnvironment("WIZARD_NAME")) --!>Win64.xms"
