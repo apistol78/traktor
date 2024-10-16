ab4964123c2b34a56af521d21924f184005c56a5
apistol78 <anders.pistol@doctorentertainment.com>
2024-10-16 09:26:20 +0200

    Traktor: Removed "open-project..." scripts; not be used anymore.

--- scripts/open-projects-android.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-call %~dp0config.bat
-
-start %TRAKTOR_HOME%\build\android\"Traktor Android.sln"
--- scripts/open-projects-win64.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-call %~dp0config.bat
-
-start %TRAKTOR_HOME%\build\win64\"Traktor Win64.sln"
