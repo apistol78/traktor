52dd9795a6d46e42f9ab43fb1eec262e07e3f0bf
apistol78 <anders@pistol.one>
2024-12-05 17:30:24 +0100

    Traktor: Bumped MeshPipeline version.

--- code/Mesh/Editor/MeshPipeline.cpp
+++ code/Mesh/Editor/MeshPipeline.cpp
@@ -147,7 +147,7 @@ bool buildEmbeddedTexture(editor::IPipelineBuilder* pipelineBuilder, model::Mate
 
 	}
 
-T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.mesh.MeshPipeline", 48, MeshPipeline, editor::IPipeline)
+T_IMPLEMENT_RTTI_FACTORY_CLASS(L"traktor.mesh.MeshPipeline", 49, MeshPipeline, editor::IPipeline)
 
 MeshPipeline::MeshPipeline()
 :	m_promoteHalf(false)
