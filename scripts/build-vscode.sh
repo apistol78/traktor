7102e5e97229cea65f91b1398d3d974566d9659a
Anders Pistol <anders.pistol@doctorentertainment.com>
2025-04-24 22:35:17 +0200

    Traktor: Using as many cores as build machines support when compiling from vscode.

--- scripts/build-vscode.sh
+++ scripts/build-vscode.sh
@@ -9,36 +9,36 @@ clear
 if [[ "$1" == "Linux" ]]; then
 
 	pushd $TRAKTOR_HOME/build/linux
-	make -s -j6 -f 'Extern Linux.mak' $2
-	make -s -j6 -f 'Traktor Linux.mak' $2
+	make -s -j -f 'Extern Linux.mak' $2
+	make -s -j -f 'Traktor Linux.mak' $2
 	popd
 
 elif [[ "$1" == "macOS" ]]; then
 
 	pushd $TRAKTOR_HOME/build/osx
-	make -s -j6 -f 'Extern OSX.mak' $2
-	make -s -j6 -f 'Traktor OSX.mak' $2
+	make -s -j -f 'Extern OSX.mak' $2
+	make -s -j -f 'Traktor OSX.mak' $2
 	popd
 
 elif [[ "$1" == "Android" ]]; then
 
 	pushd $TRAKTOR_HOME/build/android
-	make -s -j6 -f 'Extern Android.mak' $2
-	make -s -j6 -f 'Traktor Android.mak' $2
+	make -s -j -f 'Extern Android.mak' $2
+	make -s -j -f 'Traktor Android.mak' $2
 	popd
 
 elif [[ "$1" == "iOS" ]]; then
 
 	pushd $TRAKTOR_HOME/build/ios
-	make -s -j6 -f 'Extern iOS.mak' $2
-	make -s -j6 -f 'Traktor iOS.mak' $2
+	make -s -j -f 'Extern iOS.mak' $2
+	make -s -j -f 'Traktor iOS.mak' $2
 	popd
 
 elif [[ "$1" == "RPi" ]]; then
 
 	pushd $TRAKTOR_HOME/build/rpi
-	make -s -j6 -f 'Extern Raspberry Pi.mak' $2
-	make -s -j6 -f 'Traktor Raspberry Pi.mak' $2
+	make -s -j -f 'Extern Raspberry Pi.mak' $2
+	make -s -j -f 'Traktor Raspberry Pi.mak' $2
 	popd
 
 else
