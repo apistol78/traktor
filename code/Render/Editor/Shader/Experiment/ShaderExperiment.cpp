f6ef1de8f747460f4ba89129334c1a374de21574
Anders Pistol <anders@pistol.one>
2025-11-18 15:12:01 +0100

    Traktor: Moved "shader experiment" code to it's own folder.

--- code/Render/Editor/Shader/Experiment/ShaderExperiment.cpp
+++ code/Render/Editor/Shader/Experiment/ShaderExperiment.cpp
@@ -6,12 +6,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+#include "Render/Editor/Shader/Experiment/ShaderExperiment.h"
+
 #include "Core/Serialization/AttributeType.h"
 #include "Core/Serialization/ISerializer.h"
 #include "Core/Serialization/Member.h"
 #include "Core/Serialization/MemberAlignedVector.h"
 #include "Core/Serialization/MemberComposite.h"
-#include "Render/Editor/Shader/ShaderExperiment.h"
 #include "Render/Editor/Shader/ShaderGraph.h"
 #include "Render/Editor/Shader/StructDeclaration.h"
 
@@ -22,22 +23,22 @@ T_IMPLEMENT_RTTI_EDIT_CLASS(L"traktor.render.ShaderExperiment", 0, ShaderExperim
 
 void ShaderExperiment::serialize(ISerializer& s)
 {
-    s >> Member< Guid >(L"shader", m_shader, AttributeType(type_of< ShaderGraph >()));
-    s >> MemberAlignedVector< Data, MemberComposite< Data > >(L"data", m_data);
-    s >> MemberAlignedVector< Pass, MemberComposite< Pass > >(L"pass", m_passes);
+	s >> Member< Guid >(L"shader", m_shader, AttributeType(type_of< ShaderGraph >()));
+	s >> MemberAlignedVector< Data, MemberComposite< Data > >(L"data", m_data);
+	s >> MemberAlignedVector< Pass, MemberComposite< Pass > >(L"pass", m_passes);
 }
 
 void ShaderExperiment::Data::serialize(ISerializer& s)
 {
-    s >> Member< std::wstring >(L"name", name);
-    s >> Member< Guid >(L"structDeclaration", structDeclaration, AttributeType(type_of< StructDeclaration >()));
-    s >> Member< int32_t >(L"count", count);
+	s >> Member< std::wstring >(L"name", name);
+	s >> Member< Guid >(L"structDeclaration", structDeclaration, AttributeType(type_of< StructDeclaration >()));
+	s >> Member< int32_t >(L"count", count);
 }
 
 void ShaderExperiment::Pass::serialize(ISerializer& s)
 {
-    s >> Member< std::wstring >(L"technique", technique);
-    s >> Member< int32_t >(L"workSize", workSize);
+	s >> Member< std::wstring >(L"technique", technique);
+	s >> Member< int32_t >(L"workSize", workSize);
 }
 
 }
